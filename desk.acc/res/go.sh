69ff47b3cf0c25d6918a8e12c0f797b407f76321
Joshua Bell <inexorabletash@gmail.com>
2018-02-19 10:49:45 -0800

    Move calc fixes to branch

--- desk.acc/res/go.sh
+++ desk.acc/res/go.sh
@@ -2,16 +2,26 @@
 
 set -e
 
+function cecho {
+    case $1 in
+        red)    tput setaf 1 ;;
+        green)  tput setaf 2 ;;
+        yellow) tput setaf 3 ;;
+    esac
+    echo -e "$2"
+    tput sgr0
+}
+
 function do_make {
     make "$1" \
-        && (tput setaf 2 ; echo "make $1 good" ; tput sgr0 ) \
-        || (tput setaf 1 ; tput blink ; echo "MAKE $1 BAD" ; tput sgr0 ; return 1)
+        && (cecho green "make $1 good") \
+        || (tput blink ; cecho red "MAKE $1 BAD" ; return 1)
 }
 
 function verify {
     diff "orig/$1.bin" "$1.F1" \
-        && (tput setaf 2 ; echo "diff $1 good" ; tput sgr0 ) \
-        || (tput setaf 1 ; tput blink ; echo -e "DIFF $1 BAD" ; tput sgr0 ; return 1)
+        && (cecho green "diff $1 good" ) \
+        || (tput blink ; cecho red "DIFF $1 BAD" ; return 1)
 }
 
 function stats {
@@ -37,5 +47,5 @@ stats "puzzle.s"
 cat show_image_file.F1 > mount/SHOW.IMAGE.FILE.\$F1 \
     && echo "Updated mountable file (SIF)"
 
-cat calc_fixed.F1 > mount/TEST.\$F1 \
-    && echo "Updated mountable file (Test)"
+#cat calc_fixed.F1 > mount/TEST.\$F1 \
+#    && echo "Updated mountable file (Test)"
