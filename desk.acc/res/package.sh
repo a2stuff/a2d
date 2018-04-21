e04f5a1a8cc7fc56de4ce3704e074e72c8f35d38
Joshua Bell <inexorabletash@gmail.com>
2018-04-20 22:56:35 -0700

    Add packager script, hook up travis

---
+++ desk.acc/res/package.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Use Cadius to create a disk image for distribution
+# https://github.com/mach-kernel/cadius
+
+set -e
+
+CADIUS="${CADIUS:-$HOME/dev/cadius/bin/release/cadius}"
+
+DAS="calculator show.text.file date puzzle sort.directory \
+    show.image.file this.apple eyes"
+
+PACKDIR="out/package"
+FINFO="$PACKDIR/_FileInformation.txt"
+IMGFILE="out/DeskAccessories.2mg"
+VOLNAME="DESK.ACC"
+
+mkdir -p "$PACKDIR"
+echo "" > "$FINFO"
+
+# Prepare _FileInformation.txt file with extra ProDOS file entry data
+# and copy renamed files into package directory.
+
+for file in $DAS; do
+    ucfile=$(echo $file | tr a-z A-Z)
+    echo "$ucfile=Type(F1),AuxType(0640),VersionCreate(00),MinVersion(B9),Access(E3),FolderInfo1(000000000000000000000000000000000000),FolderInfo2(000000000000000000000000000000000000)" >> "$FINFO"
+    cp "out/$file.\$F1" "out/package/$ucfile"
+done
+
+# Create a new disk image.
+
+# TODO: Make images 143KB once issue is fixed:
+# https://github.com/mach-kernel/cadius/issues/22
+$CADIUS CREATEVOLUME $IMGFILE $VOLNAME 800KB
+
+# Add the files into the disk image.
+
+for file in $DAS; do
+    ucfile=$(echo $file | tr a-z A-Z)
+    $CADIUS ADDFILE $IMGFILE "/$VOLNAME" $PACKDIR/$ucfile
+done
