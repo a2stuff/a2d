513fe1b8f07290119ceb10f6e0c33457f6e38c49
Joshua Bell <inexorabletash@gmail.com>
2019-12-31 09:11:47 -0800

    Add res/install.sh script for updating existing images

---
+++ res/install.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Use Cadius to install A2D onto an existing file.
+# https://github.com/mach-kernel/cadius
+#
+# Usage:
+#
+#   INSTALL_IMG=/path/to/hd.2mg INSTALL_PATH=/hd/a2.desktop install.sh
+
+set -e
+
+if [ -z "$INSTALL_IMG" ]; then
+    tput setaf 1 && echo "Variable \$INSTALL_IMG not set, aborting." && tput sgr0
+    exit 1
+fi
+if [ -z "$INSTALL_PATH" ]; then
+    tput setaf 1 && echo "Variable \$INSTALL_PATH not set, aborting." && tput sgr0
+    exit 1
+fi
+if ! command -v "cadius" >/dev/null; then
+    tput setaf 1 && echo "Cadius not installed." && tput sgr0
+    exit 1
+fi
+
+tput setaf 2 && echo "Installing into image: $INSTALL_IMG at path: $INSTALL_PATH" && tput sgr0
+
+DA_DIRS="desk.acc preview"
+
+PACKDIR="out/install"
+
+mkdir -p $PACKDIR
+
+# Add the files into the disk images.
+
+add_file () {
+    img_file="$1"
+    src_file="$2"
+    folder="$3"
+    dst_file="$4"
+    suffix="$5"
+    tmp_file="$PACKDIR/$dst_file#$suffix"
+
+    cp "$src_file" "$tmp_file"
+    cadius DELETEFILE "$img_file" "$folder/$dst_file" > /dev/null # Ignore errors
+    cadius ADDFILE "$img_file" "$folder" "$tmp_file" --quiet --no-case-bits
+    rm "$tmp_file"
+}
+
+cadius CREATEFOLDER $INSTALL_IMG $INSTALL_PATH --quiet --no-case-bits
+
+add_file $INSTALL_IMG "desktop.system/out/desktop.system.SYS" "$INSTALL_PATH" "DeskTop.system" FF0000
+add_file $INSTALL_IMG "desktop/out/DESKTOP2.built" "$INSTALL_PATH" "DeskTop2" F10000
+
+for da_dir in $DA_DIRS; do
+    folder="$INSTALL_PATH/$da_dir"
+    cadius CREATEFOLDER $INSTALL_IMG $folder --quiet --no-case-bits
+    for file in $(cat $da_dir/TARGETS); do
+        add_file "$INSTALL_IMG" "$da_dir/out/$file.da" $folder $file F10640
+    done
+done
+
+rmdir "$PACKDIR"
--- res/package.sh
+++ res/package.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env/bash
 
 # Use Cadius to create disk images for distribution
 # https://github.com/mach-kernel/cadius
@@ -7,7 +7,10 @@ set -e
 
 tput setaf 2 && echo "Building disk images" && tput sgr0
 
-CADIUS="${CADIUS:-$HOME/dev/cadius/bin/release/cadius}"
+if ! command -v "cadius" >/dev/null; then
+    tput setaf 1 && echo "Cadius not installed." && tput sgr0
+    exit 1
+fi
 
 DA_DIRS="desk.acc preview"
 
@@ -35,11 +38,11 @@ rm -f $IMGFILE_COMPLETE
 rm -f $IMGFILE_PART1
 rm -f $IMGFILE_PART2
 
-$CADIUS CREATEVOLUME $IMGFILE_COMPLETE $VOLNAME_COMPLETE 800KB --quiet --no-case-bits
+cadius CREATEVOLUME $IMGFILE_COMPLETE $VOLNAME_COMPLETE 800KB --quiet --no-case-bits
 
 # BUG: Should be 140KB. https://github.com/mach-kernel/cadius/issues/28
-$CADIUS CREATEVOLUME $IMGFILE_PART1 $VOLNAME_PART1 143KB --quiet --no-case-bits
-$CADIUS CREATEVOLUME $IMGFILE_PART2 $VOLNAME_PART2 143KB --quiet --no-case-bits
+cadius CREATEVOLUME $IMGFILE_PART1 $VOLNAME_PART1 143KB --quiet --no-case-bits
+cadius CREATEVOLUME $IMGFILE_PART2 $VOLNAME_PART2 143KB --quiet --no-case-bits
 
 # Add the files into the disk images.
 
@@ -52,7 +55,7 @@ add_file () {
     tmp_file="$PACKDIR/$dst_file#$suffix"
 
     cp "$src_file" "$tmp_file"
-    $CADIUS ADDFILE "$img_file" "$folder" "$tmp_file" --quiet --no-case-bits
+    cadius ADDFILE "$img_file" "$folder" "$tmp_file" --quiet --no-case-bits
     rm "$tmp_file"
 }
 
@@ -70,8 +73,8 @@ add_file $IMGFILE_PART1 "desktop/out/DESKTOP2.built" "/$VOLNAME_PART1" "DeskTop2
 for da_dir in $DA_DIRS; do
     folder1="/$VOLNAME_COMPLETE/$(titlecase $da_dir)"
     folder2="/$VOLNAME_PART2/$(titlecase $da_dir)"
-    $CADIUS CREATEFOLDER $IMGFILE_COMPLETE $folder1 --quiet --no-case-bits
-    $CADIUS CREATEFOLDER $IMGFILE_PART2 $folder2 --quiet --no-case-bits
+    cadius CREATEFOLDER $IMGFILE_COMPLETE $folder1 --quiet --no-case-bits
+    cadius CREATEFOLDER $IMGFILE_PART2 $folder2 --quiet --no-case-bits
     for file in $(cat $da_dir/TARGETS); do
         add_file "$IMGFILE_COMPLETE" "$da_dir/out/$file.da" $folder1 $(titlecase $file) F10640
         add_file "$IMGFILE_PART2" "$da_dir/out/$file.da" $folder2 $(titlecase $file) F10640
@@ -87,12 +90,12 @@ fi
 # Verify and clean up
 
 tput setaf 2 && echo "Catalog of 800k disk:" && tput sgr0
-$CADIUS CATALOG $IMGFILE_COMPLETE --quiet
+cadius CATALOG $IMGFILE_COMPLETE --quiet
 
 tput setaf 2 && echo "Catalog of 143k disk 1:" && tput sgr0
-$CADIUS CATALOG $IMGFILE_PART1 --quiet
+cadius CATALOG $IMGFILE_PART1 --quiet
 
 tput setaf 2 && echo "Catalog of 143k disk 2:" && tput sgr0
-$CADIUS CATALOG $IMGFILE_PART2 --quiet
+cadius CATALOG $IMGFILE_PART2 --quiet
 
 rmdir "$PACKDIR"
