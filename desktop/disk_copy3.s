a2stuff/a2d

commit d59d4735a7a7b5c9f77f90523cff3dc4033b2955

Loc: Produce res files; make room for longer strings. #327

desktop/disk_copy3.s
@@ -2479,18 +2479,10 @@ str_confirm_erase1_buf:  .res    18, 0
 kLenConfirmErase1 = 21
 
 ;;; This string is seen when copying a ProDOS disk to DOS 3.3 or Pascal disk.
-;;; Unclear why a message with extra spaces used ???
 str_confirm_erase2:
         PASCAL_STRING res_string_prompt_erase_slot_drive_pattern
         kStrConfirmErase2SlotOffset = res_const_prompt_erase_slot_drive_pattern_slot_offset
         kStrConfirmErase2DriveOffset = res_const_prompt_erase_slot_drive_pattern_drive_offset
-
-;;; Unknown which path leads to this; there don't seem to be any calls
-;;; to show the dialog with this string ???
-str_confirm_erase3:
-        PASCAL_STRING res_string_prompt_erase_slot_drive_pattern_alt
-        kStrConfirmErase3SlotOffset = res_const_prompt_erase_slot_drive_pattern_alt_slot_offset
-        kStrConfirmErase3DriveOffset = res_const_prompt_erase_slot_drive_pattern_alt_drive_offset
 
 str_copy_success:
         PASCAL_STRING res_string_label_status_copy_success
@@ -2518,7 +2510,6 @@ message_table:
         .addr   str_dest_protected
         .addr   str_confirm_erase1
         .addr   str_confirm_erase2
-        .addr   str_confirm_erase3
         .addr   str_copy_success
         .addr   str_copy_fail
         .addr   str_insert_source_or_cancel
@@ -2615,9 +2606,9 @@ LEC3F:  cmp     #$06
         lda     #$07
         bne     LEC5E           ; always
 
-LEC55:  cmp     #$08
+LEC55:  cmp     #$08            ; TODO: Unused duplicate?
         bne     LEC5E
-        jsr     set_confirm_erase3_slot_drive
+        jsr     set_confirm_erase2_slot_drive
         lda     #$08
 
 LEC5E:  ldy     #$00
@@ -3113,26 +3104,6 @@ state:
         rol     a
         adc     #'1'            ; Drive 1 or 2
         sta     str_confirm_erase2 + kStrConfirmErase2DriveOffset
-        rts
-.endproc
-
-;;; ============================================================
-
-.proc set_confirm_erase3_slot_drive
-        txa
-        and     #$70            ; Mask off slot
-        lsr     a
-        lsr     a
-        lsr     a
-        lsr     a
-        ora     #'0'
-        sta     str_confirm_erase3 + kStrConfirmErase3SlotOffset
-        txa
-        and     #$80            ; Mask off drive
-        asl     a               ; Shift to low bit
-        rol     a
-        adc     #'1'            ; Drive 1 or 2
-        sta     str_confirm_erase3 + kStrConfirmErase3DriveOffset
         rts
 .endproc
 

bin/loc_makeres.pl
@@ -33,7 +33,7 @@ sub encode($$) {
         die "Unknown lang: $lang\n";
     }
 
-    die "Unencodable in line $.: $s\n" unless $s =~ /^[\x20-\x7e]*$/;
+    die "Unencodable ($lang) in line $.: $s\n" unless $s =~ /^[\x20-\x7e]*$/;
 
     return $s;
 }
@@ -56,7 +56,7 @@ sub check($$$) {
 
     # Apply same leading/trailing spaces
     $t = trim($t);
-    $t = $1 . $t . $2 if $en =~ m/^(\s*).*(\s*)$/;
+    $t = $1 . $t . $2 if $en =~ m/^(\s*).*?(\s*)$/;
 
     return $t;
 }
@@ -66,18 +66,27 @@ sub check($$$) {
 my $header = <STDIN>; # ignore header
 my $last_file = '';
 my %fhs = ();
+my @langs = ('en', 'fr', 'de', 'it');
 while (<STDIN>) {
     my ($file, $label, $comment, $en, $fr, $de, $it) = split(/\t/);
+    my %strings = (en => $en, fr => $fr, de => $de, it => $it);
+
     next unless $file and $label;
 
     if ($file ne $last_file) {
         $last_file = $file;
-        open $fhs{'en'}, '>'.($file =~ s/\.s$/.res.en/r) or die $!;
+        foreach my $lang (@langs) {
+           open $fhs{$lang}, '>'.($file =~ s/\.s$/.res.$lang/r) or die $!;
+        }
     }
 
-    # TODO: Check/encode/output all translations
-    check($label, $en, $it);
-    $it = encode('it', $it);
+    foreach my $lang (@langs) {
+        my $str = $strings{$lang};
 
-    print {$fhs{'en'}} ".define $label ", enquote($label, $en), "\n";
+        if ($lang ne 'en') {
+            $str = check($label, $en, $str);
+            $str = encode($lang, $str);
+        }
+        print {$fhs{$lang}} ".define $label ", enquote($label, $str), "\n";
+    }
 }

desk.acc/Makefile
@@ -1,4 +1,4 @@
-HEADERS = $(wildcard ../*.inc) $(wildcard ../*/*.inc) $(wildcard ../lib/*.s) $(wildcard *.inc) $(wildcard *.res)
+HEADERS = $(wildcard ../*.inc) $(wildcard ../*/*.inc) $(wildcard ../lib/*.s) $(wildcard *.inc) $(wildcard *.res.??)
 
 include ../res/Makefile.common
 

desktop.system/Makefile
@@ -1,4 +1,4 @@
-HEADERS = $(wildcard ../*.inc) $(wildcard ../inc/*.inc) $(wildcard ../lib/*.s) $(wildcard *.res)
+HEADERS = $(wildcard ../*.inc) $(wildcard ../inc/*.inc) $(wildcard ../lib/*.s) $(wildcard *.res.??)
 
 include ../res/Makefile.common
 

desktop/Makefile
@@ -1,4 +1,4 @@
-HEADERS = $(wildcard ../*.inc) $(wildcard ../inc/*.inc) $(wildcard ../lib/*.s) $(wildcard ../mgtk/*.inc) $(wildcard *.inc) $(wildcard *.res)
+HEADERS = $(wildcard ../*.inc) $(wildcard ../inc/*.inc) $(wildcard ../lib/*.s) $(wildcard ../mgtk/*.inc) $(wildcard *.inc) $(wildcard *.res.??)
 
 include ../res/Makefile.common
 

desktop/disk_copy3.res.en
@@ -49,12 +49,9 @@
 .define res_string_errmsg_format_error "Error during formating."
 .define res_string_errmsg_dest_protected "The destination volume is write protected !"
 .define res_string_prompt_erase_prefix_alt "Do you want to erase "
-.define res_string_prompt_erase_slot_drive_pattern "Do you want to  erase  the disk in slot # drive # ?"
-.define res_const_prompt_erase_slot_drive_pattern_slot_offset 41
-.define res_const_prompt_erase_slot_drive_pattern_drive_offset 49
-.define res_string_prompt_erase_slot_drive_pattern_alt "Do you want to erase the disk in slot # drive # ?"
-.define res_const_prompt_erase_slot_drive_pattern_alt_slot_offset 39
-.define res_const_prompt_erase_slot_drive_pattern_alt_drive_offset 47
+.define res_string_prompt_erase_slot_drive_pattern "Do you want to erase the disk in slot # drive # ?"
+.define res_const_prompt_erase_slot_drive_pattern_slot_offset 39
+.define res_const_prompt_erase_slot_drive_pattern_drive_offset 47
 .define res_string_label_status_copy_success "The copy was successful."
 .define res_string_label_status_copy_fail "The copy was not completed."
 .define res_string_prompt_insert_source_or_cancel "Insert source disk or press Escape to cancel."

desktop/res.s
@@ -757,13 +757,13 @@ str_4_spaces:
         PASCAL_STRING "    "    ; do not localize
 
 dow_strings:
-        STRING  res_string_weekday_abbrev_1
-        STRING  res_string_weekday_abbrev_2
-        STRING  res_string_weekday_abbrev_3
-        STRING  res_string_weekday_abbrev_4
-        STRING  res_string_weekday_abbrev_5
-        STRING  res_string_weekday_abbrev_6
-        STRING  res_string_weekday_abbrev_7
+        STRING  .sprintf("%4s", res_string_weekday_abbrev_1)
+        STRING  .sprintf("%4s", res_string_weekday_abbrev_2)
+        STRING  .sprintf("%4s", res_string_weekday_abbrev_3)
+        STRING  .sprintf("%4s", res_string_weekday_abbrev_4)
+        STRING  .sprintf("%4s", res_string_weekday_abbrev_5)
+        STRING  .sprintf("%4s", res_string_weekday_abbrev_6)
+        STRING  .sprintf("%4s", res_string_weekday_abbrev_7)
         ASSERT_RECORD_TABLE_SIZE dow_strings, 7, 4
 
 .params dow_str_params

selector/Makefile
@@ -1,4 +1,4 @@
-HEADERS = $(wildcard ../*.inc) $(wildcard ../inc/*.inc) $(wildcard ../lib/*.s) $(wildcard ../mgtk/*.inc) $(wildcard *.inc) $(wildcard *.res)
+HEADERS = $(wildcard ../*.inc) $(wildcard ../inc/*.inc) $(wildcard ../lib/*.s) $(wildcard ../mgtk/*.inc) $(wildcard *.inc) $(wildcard *.res.??)
 
 include ../res/Makefile.common
 

commit e20a1b0272ea5ac2f572e10fd7aafeffbeae3b52

Disk Copy: Fix completion message
As noted in b8a1943a6244425a54ee711237fcf0d6a47cd1ff, something was
amiss with disk copies; a copy reported failure even though it looked
okay.

Change d59d4735a7a7b5c9f77f90523cff3dc4033b2955 inadvertently altered
the message table so failure was being reported instead of success.
Fix this, and do a little disasm as well.

desktop/disk_copy3.s
@@ -577,7 +577,9 @@ LD6F9:  lda     current_drive_selection
         MGTK_RELAY_CALL2 MGTK::PaintRect, rect_D211
         MGTK_RELAY_CALL2 MGTK::CloseWindow, winfo_drive_select
         MGTK_RELAY_CALL2 MGTK::PaintRect, rect_D432
-LD734:  param_call show_alert_dialog, $0000 ; Insert Source
+LD734:  ldx     #0
+        lda     #0              ; Insert Source
+        jsr     show_alert_dialog
         beq     LD740
         jmp     LD61C
 
@@ -685,9 +687,9 @@ LD817:  lda     main__on_line_buffer2
 
 LD82C:  sta     main__on_line_buffer2
         param_call adjust_case, main__on_line_buffer2
-        ldx     #$00
-        ldy     #$13
 
+        ldx     #$00
+        ldy     #$13            ; ???
         lda     #2              ; Confirm Erase
 LD83C:  jsr     show_alert_dialog
         cmp     #$01
@@ -2336,9 +2338,9 @@ reserved:       .byte   0
         DEFINE_RECT maprect, 0, 0, 36, 23
 .endparams
 
-        DEFINE_RECT rect_E89F, 65, 45, 485, 100
-        DEFINE_RECT_INSET rect_E8A7, 4, 2, 420, 55
-        DEFINE_RECT_INSET rect_E8AF, 5, 3, 420, 55
+        DEFINE_RECT alert_rect, 65, 45, 485, 100
+        DEFINE_RECT_INSET outer_frame, 4, 2, 420, 55
+        DEFINE_RECT_INSET inner_frame, 5, 3, 420, 55
 
 .params portbits1
         DEFINE_POINT viewloc, 65, 45
@@ -2403,9 +2405,11 @@ str_insert_dest_or_cancel:
 
 char_space:
         .byte   ' '
+
 char_question_mark:
         .byte   '?'
 
+;;; TODO: Remove unused messages
 message_index_table:
         .byte   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
 
@@ -2416,12 +2420,13 @@ message_table:
         .addr   str_dest_format_fail
         .addr   str_format_error
         .addr   str_dest_protected
-        .addr   str_confirm_erase0
+        .addr   str_confirm_erase0 ; TODO: Unused?
         .addr   str_confirm_erase2
+        .addr   str_confirm_erase2 ; TODO: Unused! Remove duplicate, adjust messages
         .addr   str_copy_success
         .addr   str_copy_fail
-        .addr   str_insert_source_or_cancel
-        .addr   str_insert_dest_or_cancel
+        .addr   str_insert_source_or_cancel ; TODO: How is this used?
+        .addr   str_insert_dest_or_cancel   ; TODO: How is this used?
 
         ;; $C0 (%11xxxxxx) = Cancel + Ok
         ;; $81 (%10xxxxx1) = Cancel + Yes + No
@@ -2450,32 +2455,38 @@ message_flags_table:
         .byte   MessageFlags::Ok
         .byte   MessageFlags::Ok
 
-LEB81:  .addr   0
-LEB83:  .byte   0
+message_num:
+        .byte   0
+xarg:   .byte   0               ; ???
+yarg:   .byte   0               ; ???
 
 show_alert_dialog:
-        stax    LEB81
-        sty     LEB83
+        sta     message_num
+        stx     xarg
+        sty     yarg
+
+        ;; Draw the alert
         MGTK_RELAY_CALL2 MGTK::InitPort, grafport
         MGTK_RELAY_CALL2 MGTK::SetPort, grafport
         MGTK_RELAY_CALL2 MGTK::SetPenMode, pencopy
-        MGTK_RELAY_CALL2 MGTK::PaintRect, rect_E89F
+        MGTK_RELAY_CALL2 MGTK::PaintRect, alert_rect
         jsr     set_pen_xor
-        MGTK_RELAY_CALL2 MGTK::FrameRect, rect_E89F
+        MGTK_RELAY_CALL2 MGTK::FrameRect, alert_rect
         MGTK_RELAY_CALL2 MGTK::SetPortBits, portbits1
-        MGTK_RELAY_CALL2 MGTK::FrameRect, rect_E8A7
-        MGTK_RELAY_CALL2 MGTK::FrameRect, rect_E8AF
+        MGTK_RELAY_CALL2 MGTK::FrameRect, outer_frame
+        MGTK_RELAY_CALL2 MGTK::FrameRect, inner_frame
         MGTK_RELAY_CALL2 MGTK::SetPenMode, pencopy
         MGTK_RELAY_CALL2 MGTK::HideCursor
         MGTK_RELAY_CALL2 MGTK::PaintBits, alert_bitmap_mapinfo
         MGTK_RELAY_CALL2 MGTK::ShowCursor
-        lda     #$00
-        sta     LD41E
-        lda     LEB81
+
+        copy    #0, LD41E
+
+        lda     message_num
         jsr     maybe_bell
-        ldy     LEB83
-        ldx     LEB81+1
-        lda     LEB81
+        ldy     yarg
+        ldx     xarg
+        lda     message_num
         bne     LEC1F
         cpx     #$00
         beq     LEC5E
@@ -2520,11 +2531,11 @@ LEC55:  cmp     #$08            ; TODO: Unused duplicate?
         lda     #$08
 
 LEC5E:  ldy     #$00
-LEC60:  cmp     message_index_table,y
+:       cmp     message_index_table,y
         beq     LEC6C
         iny
         cpy     #$1E
-        bne     LEC60
+        bne     :-
         ldy     #$00
 LEC6C:  tya
         asl     a
@@ -2702,7 +2713,7 @@ clear_and_return_value:
         pha
         MGTK_RELAY_CALL2 MGTK::SetPortBits, portbits2
         MGTK_RELAY_CALL2 MGTK::SetPenMode, pencopy
-        MGTK_RELAY_CALL2 MGTK::PaintRect, rect_E89F
+        MGTK_RELAY_CALL2 MGTK::PaintRect, alert_rect
         pla
         rts
 