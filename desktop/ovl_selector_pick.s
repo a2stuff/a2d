a2stuff/a2d

commit 44a88baf05ab3387ad3b8578c881a48c50e3335f

DeskTop: Close Delete an Entry after deleting. Fixes #190
The original behavior for Selector > Delete an Entry was that on OK
the entry would be removed, but the dialog rould remain open. Cancel
would close the dialog, but not cancel any deletions. This was
convenient if you understood the behavior and wanted to delete
multiple entries, but confusing otherwise.

Now the dialog closes after deleting the item, as the UI would
indicate.

(An alternate fix would be to change the button labels to "Delete
Entry" and "Close", and change the menu item to "Delete Entries" or
something, but that's more intrusive.)

desktop/ovl_selector_pick.s
@@ -174,24 +174,18 @@ dialog_loop:
 
 ;;; ============================================================
 
-do_delete:
+.proc do_delete
         lda     selected_index
         jsr     maybe_toggle_entry_hilite
         jsr     main::set_cursor_watch
         lda     selected_index
         jsr     remove_entry
-        beq     :+
+
+        ;; Z set on success, but action is the same either way:
+        ;; close the dialog.
         jsr     main::set_cursor_pointer
         jmp     do_cancel
-
-:       jsr     main::set_cursor_pointer
-        copy    #$FF, selected_index
-        jsr     draw_items_rect
-        jsr     draw_all_entries
-        jsr     populate_entries_flag_table
-        inc     L938F
-
-        jmp     dialog_loop
+.endproc
 
 ;;; ============================================================
 

commit 17eecc409dac5185c5926f4a97c4eb063456715d

DeskTop: Fix prompting to save when deleting selector entry. Fixes #459
Probably broken in 44a88baf05ab3387ad3b8578c881a48c50e3335f - the
"clean/dirty" flag wasn't set when deleting an entry, so if running
from RAMDisk the user wasn't prompted about saving.

desktop/ovl_selector_pick.s
@@ -16,14 +16,14 @@ selector_list   := $0C00
 exec:
         sta     selector_action
         ldx     #$FF
-        stx     L938F
+        stx     clean_flag      ; set "clean"
         cmp     #SelectorAction::add
         beq     L903C
         jmp     init
 
 L900F:  pha
-        lda     L938F
-        bpl     L9017
+        lda     clean_flag
+        bpl     L9017           ; dirty, check about saving
 L9015:  pla
 L9016:  rts
 
@@ -71,7 +71,7 @@ L9052:  lda     #$00
         tax
         pla
         bne     L900F
-        inc     L938F
+        inc     clean_flag      ; mark as "dirty"
         stx     which_run_list
         sty     copy_when
         lda     #$00
@@ -123,7 +123,7 @@ L90F1:  jmp     L900F
 
 L90F4:  lda     #kWarningMsgSelectorListFull
 L90F6:  jsr     show_warning_dialog
-        dec     L938F
+        dec     clean_flag      ; reset to "clean"
         jmp     L9016
 
 L90FF:  lda     #$02
@@ -180,10 +180,11 @@ dialog_loop:
         jsr     main::set_cursor_watch
         lda     selected_index
         jsr     remove_entry
+        bne     :+              ; Z set on success
 
-        ;; Z set on success, but action is the same either way:
-        ;; close the dialog.
-        jsr     main::set_cursor_pointer
+        inc     clean_flag      ; mark as "dirty"
+
+:       jsr     main::set_cursor_pointer
         jmp     do_cancel
 .endproc
 
@@ -245,7 +246,7 @@ l3:     clc
         beq     l4
         rts
 
-l4:     inc     L938F
+l4:     inc     clean_flag      ; mark as "dirty"
         stx     which_run_list
         sty     copy_when
         lda     #$00
@@ -444,7 +445,8 @@ selected_index:
 selector_action:
         .byte   0
 
-L938F:  .byte   0               ; ???
+clean_flag:                     ; high bit set if "clean", cleared if "dirty"
+        .byte   0               ; and should save to original prefix
 
 ;;; ============================================================
 