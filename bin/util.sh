80a00cba3be40eaebe79a255697aabfe8f5414d4
Joshua Bell <inexorabletash@gmail.com>
2022-03-13 10:18:13 -0700

    bin/install: Fail on Error output from Cadius

--- bin/util.sh
+++ bin/util.sh
@@ -16,6 +16,7 @@ function cecho {
 
 # suppress - hide command output unless it failed; and if so show in red
 # ex: suppress command_that_might_fail args ...
+# Also looks for "Error" lines in stdout, those cause failure too.
 function suppress {
     set +e
     local result=$("$@")
@@ -23,5 +24,11 @@ function suppress {
         cecho red "$result" >&2
         exit 1
     fi
+    while read line; do
+        if [[ $line == *"Error"* ]]; then
+            cecho red "$line" >&2
+            exit 1
+        fi
+    done <<<"$result"
     set -e
 }
