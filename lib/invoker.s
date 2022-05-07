5206a302badaaa96f9e31c27884f84cb148552dd
Joshua Bell <inexorabletash@gmail.com>
2022-05-07 15:09:21 -0700

    Invoker: Shave 36 bytes
    This is common code living in page 2/3 in Selector and DeskTop, used
    to invoke BASIC, binary, system, s16, and (via BASIS.SYSTEM) other
    file types. While there's no specific need to add functionality here,
    it's prime real estate.

--- lib/invoker.s
+++ lib/invoker.s
@@ -45,7 +45,6 @@ str_basic_system:
         beq     :+
         pla
         pla
-        jmp     exit
 :       rts
 .endproc
 
@@ -68,27 +67,23 @@ begin:  bit     ROMIN2
 :       sta     BITMAP,x
         dex
         bne     :-
+        lda     #%00000001      ; ProDOS global page
+        sta     BITMAP+BITMAP_SIZE-1
+        lda     #%11001111      ; ZP, Stack, Text Page 1
+        sta     BITMAP
 
+        ;; Set prefix and check file type
         jsr     SetPrefix
         lda     INVOKER_PREFIX
         sta     prefix_length
         MLI_CALL GET_FILE_INFO, get_info_params
         beq     :+
-        jmp     exit
+        rts
 :       lda     get_info_params::file_type
-
-
-;;; ProDOS 16 System file (S16) - invoke via QUIT call
-        cmp     #FT_S16
-        bne     not_s16
-
-        jsr     update_bitmap
-        jmp     quit_call
-not_s16:
 
 ;;; Binary file (BIN) - load and invoke at A$=AuxType
         cmp     #FT_BINARY
-        bne     not_binary
+    IF_EQ
 
         lda     get_info_params::aux_type
         sta     jmp_addr
@@ -100,56 +95,28 @@ not_s16:
         cmp     #$0C            ; If loading at page < $0C
         bcs     :+
         lda     #$BB            ; ... use a high address buffer ($BB)
-        sta     open_params::io_buffer+1
-        bne     load_target     ; always
+        .byte   OPC_BIT_abs     ; skip next 2-byte instruction
 :       lda     #$08            ; ... otherwise a low address buffer ($08)
         sta     open_params::io_buffer+1
-        bne     load_target     ; always
-not_binary:
-
-;;; BASIC file (BAS) - invoke interpreter as path instead.
-;;; (If not found, ProDOS QUIT will be invoked.)
-        cmp     #FT_BASIC       ; BASIC?
-        bne     not_basic
-
-        copy16  #str_basic_system, open_params::pathname
-
-        ;; Try opening interpreter with current prefix.
-check_for_interpreter:
-        jsr     Open
-        beq     found_interpreter
-        ldy     INVOKER_PREFIX   ; Pop a path segment to try
-:       lda     INVOKER_PREFIX,y ; parent directory.
-        cmp     #'/'
-        beq     update_prefix
-        dey
-        cpy     #1
-        bne     :-
-        jmp     exit
-
-update_prefix:                  ; Update prefix and try again.
-        dey
-        sty     INVOKER_PREFIX
-        jsr     SetPrefix
-        jmp     check_for_interpreter
-
-found_interpreter:
-        copy    #$80, interpreter_flag
-        lda     prefix_length
-        sta     INVOKER_PREFIX
-        jmp     do_read
-
-not_basic:
+        jmp     load_target
+    END_IF
 
 ;;; ProDOS 8 System file (SYS) - load at default $2000
         cmp     #FT_SYSTEM
         beq     load_target
 
+;;; ProDOS 16 System file (S16) - invoke via QUIT call
+        cmp     #FT_S16
+        beq     quit_call
+
+;;; BASIC file (BAS) - invoke interpreter as path instead.
+;;; (If not found, ProDOS QUIT will be invoked.)
+        cmp     #FT_BASIC       ; BASIC?
+        beq     use_interpreter
+
 ;;; Use BASIS.SYSTEM as fallback if present.
 ;;; (If not found, ProDOS QUIT will be invoked.)
         copy    #'S', str_basic_system + kBSOffset ; "BASIC" -> "BASIS"
-        copy16  #str_basic_system, open_params::pathname
-        jmp     check_for_interpreter
 
 ;;; TODO: ProDOS 2.4's Bitsy Bye invokes BASIS.SYSTEM with:
 ;;; * [x] ProDOS prefix set to directory containing file.
@@ -158,6 +125,34 @@ not_basic:
 ;;; * [ ] $380 set to path of launched SYS (i.e. path to BASIS.SYSTEM)
 ;;; Not all should be necessary, but not doing so may lead to future
 ;;; compatibility issues. Those addresses conflict with this code.
+
+use_interpreter:
+        copy16  #str_basic_system, open_params::pathname
+
+        ;; Try opening interpreter with current prefix.
+test:   jsr     Open
+        beq     found_interpreter
+        ldy     INVOKER_PREFIX   ; Pop a path segment to try
+:       lda     INVOKER_PREFIX,y ; parent directory.
+        cmp     #'/'
+        beq     :+
+        dey
+        cpy     #1
+        bne     :-
+        rts
+
+        ;; Update prefix and try again.
+:       dey
+        sty     INVOKER_PREFIX
+        jsr     SetPrefix
+        jmp     test
+
+found_interpreter:
+        sec                     ; set flag
+        ror     interpreter_flag
+        lda     prefix_length
+        sta     INVOKER_PREFIX
+        jmp     do_read
 
 ;;; ============================================================
 ;;; Load target at given address
@@ -174,38 +169,27 @@ do_read:
         bne     exit
 
         ;; If interpreter, set prefix and copy filename to interpreter buffer.
-        lda     get_info_params::file_type
         bit     interpreter_flag
-        bpl     update_stack
-
+    IF_NS
         jsr     SetPrefix
         ldy     INVOKER_FILENAME
 :       lda     INVOKER_FILENAME,y
         sta     PRODOS_INTERPRETER_BUF,y         ; ProDOS interpreter protocol
         dey
         bpl     :-
+    END_IF
 
         ;; Set return address to the QUIT call below
-update_stack:
         lda     #>(quit_call-1)
         pha
         lda     #<(quit_call-1)
         pha
-        jsr     update_bitmap
 
         jmp_addr := *+1
         jmp     PRODOS_SYS_START
 
 quit_call:
         MLI_CALL QUIT, quit_params
-
-        ;; Update system bitmap
-update_bitmap:
-        lda     #%00000001      ; ProDOS global page
-        sta     BITMAP+BITMAP_SIZE-1
-        lda     #%11001111      ; ZP, Stack, Text Page 1
-        sta     BITMAP
-        FALL_THROUGH_TO exit
 
 exit:   rts
 
