a2stuff/a2d

commit 87e5322906b618baee2dfa009abcc72e3c4f6833

File Dialog: Set ports and avoid flickering dimmed buttons
Button re-drawing on click was not setting a GrafPort. This was not
typically a problem as the line edit would have set a port, but this
shouldn't be relied upon.

Also, when updating the dimmed button state, even if the state didn't
change the button labels would be redrawn resulting in a flicker. Add
flags to track the dimmed state for each button to avoid unnecessary
redraws.

lib/file_dialog.s
@@ -111,6 +111,11 @@ routine_table:
 
         copy    #$FF, file_dialog_res::selected_index
 
+        lda     #$40            ; not $00 or $80
+        sta     open_button_dimmed_flag
+        sta     close_button_dimmed_flag
+        sta     change_drive_button_dimmed_flag
+
 .if FD_EXTENDED
         pla
         asl     a
@@ -250,6 +255,7 @@ ret:    rts
     IF_EQ
         jsr     IsChangeDriveAllowed
         bcs     :+
+        jsr     SetPortForDialog
         param_call ButtonEventLoop, file_dialog_res::kFilePickerDlgWindowID, file_dialog_res::change_drive_button_rect
         bmi     :+
         jsr     DoChangeDrive
@@ -267,6 +273,7 @@ ret:    rts
      IF_EQ
         jsr     IsOpenAllowed
         bcs     :+
+        jsr     SetPortForDialog
         param_call ButtonEventLoop, file_dialog_res::kFilePickerDlgWindowID, file_dialog_res::open_button_rect
         bmi     :+
         jsr     DoOpen
@@ -281,6 +288,7 @@ ret:    rts
     IF_EQ
         jsr     IsCloseAllowed
         bcs     :+
+        jsr     SetPortForDialog
         param_call ButtonEventLoop, file_dialog_res::kFilePickerDlgWindowID, file_dialog_res::close_button_rect
         bmi     :+
         jsr     DoClose
@@ -293,6 +301,7 @@ ret:    rts
         MGTK_CALL MGTK::InRect, file_dialog_res::ok_button_rect
         cmp     #MGTK::inrect_inside
     IF_EQ
+        jsr     SetPortForDialog
         param_call ButtonEventLoop, file_dialog_res::kFilePickerDlgWindowID, file_dialog_res::ok_button_rect
         bmi     :+
         jsr     HandleOk
@@ -305,6 +314,7 @@ ret:    rts
         MGTK_CALL MGTK::InRect, file_dialog_res::cancel_button_rect
         cmp     #MGTK::inrect_inside
     IF_EQ
+        jsr     SetPortForDialog
         param_call ButtonEventLoop, file_dialog_res::kFilePickerDlgWindowID, file_dialog_res::cancel_button_rect
         bmi     :+
         jsr     HandleCancel
@@ -316,6 +326,7 @@ ret:    rts
 
         bit     extra_controls_flag
     IF_NS
+        jsr     SetPortForDialog
         jsr     click_handler_hook
         bpl     :+
         rts
@@ -1319,35 +1330,68 @@ l1:     ldx     num_file_names
 
 ;;; ============================================================
 
+;;; bit 7 set = dimmed, $00 = not dimmed, anything else = ???
+open_button_dimmed_flag:
+        .byte   0
+close_button_dimmed_flag:
+        .byte   0
+change_drive_button_dimmed_flag:
+        .byte   0
+
+;;; ============================================================
+
 .proc DrawOpenLabel
+        jsr     IsOpenAllowed
+        lda     #0
+        ror                     ; C into high bit
+        cmp     open_button_dimmed_flag
+        beq     ret             ; no change
+
+        sta     open_button_dimmed_flag
         MGTK_CALL MGTK::MoveTo, file_dialog_res::open_button_pos
         param_call DrawString, file_dialog_res::open_button_label
-        jsr     IsOpenAllowed
-        bcc     :+
+        bit     open_button_dimmed_flag
+        bpl     ret
         param_call DisableButton, file_dialog_res::open_button_rect
-:       rts
+
+ret:    rts
 .endproc
 
 ;;; ============================================================
 
 .proc DrawCloseLabel
+        jsr     IsCloseAllowed
+        lda     #0
+        ror                     ; C into high bit
+        cmp     close_button_dimmed_flag
+        beq     ret             ; no change
+
+        sta     close_button_dimmed_flag
         MGTK_CALL MGTK::MoveTo, file_dialog_res::close_button_pos
         param_call DrawString, file_dialog_res::close_button_label
-        jsr     IsCloseAllowed
-        bcc     :+
+        bit     close_button_dimmed_flag
+        bpl     ret
         param_call DisableButton, file_dialog_res::close_button_rect
-:       rts
+
+ret:    rts
 .endproc
 
 ;;; ============================================================
 
 .proc DrawChangeDriveLabel
+        jsr     IsChangeDriveAllowed
+        lda     #0
+        ror                     ; C into high bit
+        cmp     change_drive_button_dimmed_flag
+        beq     ret             ; no change
+
+        sta     change_drive_button_dimmed_flag
         MGTK_CALL MGTK::MoveTo, file_dialog_res::change_drive_button_pos
         param_call DrawString, file_dialog_res::change_drive_button_label
-        jsr     IsChangeDriveAllowed
-        bcc     :+
+        bpl     ret
         param_call DisableButton, file_dialog_res::change_drive_button_rect
-:       rts
+
+ret:    rts
 .endproc
 
 ;;; ============================================================

desktop/internal.inc
@@ -214,7 +214,7 @@ _segoffset .set 0
 ;;; Dynamically loaded overlays
         DEFSEG OverlayFormatErase,  $0800, $1100
         DEFSEG OverlayShortcutPick, $9000, $0C00
-        DEFSEG OverlayFileDialog,   $5000, $1700
+        DEFSEG OverlayFileDialog,   $5000, $1800
         DEFSEG OverlayFileCopy,     $7000, $0200
         DEFSEG OverlayFileDelete,   $7000, $0100
         DEFSEG OverlayShortcutEdit, $7000, $0500

selector/selector.s
@@ -53,7 +53,7 @@ _segoffset .set 0
         DEFSEG InvokerSegment, INVOKER, $160
         DEFSEG AppSegment, $4000, $6400
         DEFSEG AlertSegment, $D000, $800
-        DEFSEG OverlayFileDialog, OVERLAY_ADDR, $1700
+        DEFSEG OverlayFileDialog, OVERLAY_ADDR, $1800
         DEFSEG OverlayCopyDialog, OVERLAY_ADDR, $D00
 
 ;;; ============================================================

commit 3290b81d79684e2500bc2e9838f42d3a52354cc8

DeskTop: Fix radio button clicks in Add/Edit a Shortcut dialog
Regressed in 87e53229; setting the port invalidates the current
position, so subsequent MGTK::InRect tests were not valid. Move the
grafport selection closer to the drawing.

desktop/ovl_selector_edit.s
@@ -420,6 +420,7 @@ draw:
         dey
         bpl     :-
 
+        jsr     file_dialog::SetPortForDialog
         MGTK_CALL MGTK::SetPenMode, notpencopy
         MGTK_CALL MGTK::HideCursor
         MGTK_CALL MGTK::PaintBits, rb_params

lib/file_dialog.s
@@ -326,7 +326,6 @@ ret:    rts
 
         bit     extra_controls_flag
     IF_NS
-        jsr     SetPortForDialog
         jsr     click_handler_hook
         bpl     :+
         rts