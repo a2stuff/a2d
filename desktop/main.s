a2stuff/a2d

commit ca8a3da730e9240e0aab951632c1618d84c97ed5

Use correct 16-bit comparisons: the final chapter?
* Where the inputs are trivially known to be unsigned, use cmp16 but
  ensure branching uses the C flag.

* Where inputs may be signed, use scmp16 instead and ensure branching
  uses the N flag.

desktop/main.s
@@ -242,7 +242,7 @@ ClearUpdates := ClearUpdatesImpl::clear
         stax    winfo_ptr
         ldy     #MGTK::Winfo::port + MGTK::GrafPort::viewloc + MGTK::Point::ycoord
         sub16in (winfo_ptr),y, window_grafport::viewloc::ycoord, yoff
-        cmp16   yoff, #kWindowHeaderHeight+1
+        scmp16  yoff, #kWindowHeaderHeight+1
         bpl     skip_adjust_port
 
         ;; Adjust grafport to account for header
@@ -252,7 +252,7 @@ ClearUpdates := ClearUpdatesImpl::clear
         ;; nothing to draw, skip drawing!
         ;; https://github.com/a2stuff/a2d/issues/369
         ldx     #MGTK::GrafPort::viewloc + MGTK::Point::ycoord
-        cmp16   window_grafport,x, #kScreenHeight
+        scmp16  window_grafport,x, #kScreenHeight
         bpl     done
 
         ;; Apply the computed grafport to the Winfo
@@ -5263,7 +5263,7 @@ aa:     .byte   0               ; A
         sta     useful_height
 
         sub16_8 window_grafport::cliprect::y1, useful_height, delta
-        cmp16   delta, iconbb_rect+MGTK::Rect::y1
+        scmp16  delta, iconbb_rect+MGTK::Rect::y1
         bmi     clamp
         ldax    delta
         jmp     adjust
@@ -5289,7 +5289,7 @@ delta:  .word   0
         sta     useful_height
 
         add16_8 window_grafport::cliprect::y2, useful_height, delta
-        cmp16   delta, iconbb_rect+MGTK::Rect::y2
+        scmp16  delta, iconbb_rect+MGTK::Rect::y2
         bpl     clamp
         ldax    delta
         jmp     adjust
@@ -5324,7 +5324,7 @@ delta:  .word   0
         stax    width
 
         sub16   window_grafport::cliprect::x1, width, delta
-        cmp16   delta, iconbb_rect+MGTK::Rect::x1
+        scmp16  delta, iconbb_rect+MGTK::Rect::x1
         bmi     clamp
 
         ldax    delta
@@ -5347,7 +5347,7 @@ delta:  .word   0
         stax    width
 
         add16   window_grafport::cliprect::x2, width, delta
-        cmp16   delta, iconbb_rect+MGTK::Rect::x2
+        scmp16  delta, iconbb_rect+MGTK::Rect::x2
         bpl     clamp
         ldax    delta
         jmp     adjust
@@ -5466,7 +5466,7 @@ delta:  .word   0
         lda     #0              ; content near edge within window; clamp
         beq     calc            ; always
 
-:       cmp16   window_grafport::cliprect::x2, iconbb_rect+MGTK::Rect::x2
+:       scmp16  window_grafport::cliprect::x2, iconbb_rect+MGTK::Rect::x2
         bmi     :+              ; content far edge within window? no
         tya                     ; yes; skip calculation
         jmp     skip
@@ -5524,7 +5524,7 @@ size:   .word   0
         lda     #0              ; content near edge within window; clamp
         beq     calc            ; always
 
-:       cmp16   window_grafport::cliprect::y2, iconbb_rect+MGTK::Rect::y2
+:       scmp16  window_grafport::cliprect::y2, iconbb_rect+MGTK::Rect::y2
         bmi     neg             ; content far edge within window? no
         tya                     ; yes; skip calculation
         jmp     skip
@@ -6344,9 +6344,9 @@ config_port:
         jsr     ApplyActiveWinfoToWindowGrafport
 
         ;; check horizontal bounds
-        cmp16   iconbb_rect+MGTK::Rect::x1, window_grafport::cliprect::x1
+        scmp16  iconbb_rect+MGTK::Rect::x1, window_grafport::cliprect::x1
         bmi     activate_hscroll
-        cmp16   window_grafport::cliprect::x2, iconbb_rect+MGTK::Rect::x2
+        scmp16  window_grafport::cliprect::x2, iconbb_rect+MGTK::Rect::x2
         bmi     activate_hscroll
 
         ;; deactivate horizontal scrollbar
@@ -6369,9 +6369,9 @@ activate_hscroll:
 
 check_vscroll:
         ;; check vertical bounds
-        cmp16   iconbb_rect+MGTK::Rect::y1, window_grafport::cliprect::y1
+        scmp16  iconbb_rect+MGTK::Rect::y1, window_grafport::cliprect::y1
         bmi     activate_vscroll
-        cmp16   window_grafport::cliprect::y2, iconbb_rect+MGTK::Rect::y2
+        scmp16  window_grafport::cliprect::y2, iconbb_rect+MGTK::Rect::y2
         bmi     activate_vscroll
 
         ;; deactivate vertical scrollbar
@@ -7516,9 +7516,9 @@ common: sta     preserve_window_size_flag
 
         ;; Check if width is < min or > max
         cmp16   iconbb_rect+MGTK::Rect::x2, #kMinWindowWidth
-        bmi     use_minw
+        bcc     use_minw
         cmp16   iconbb_rect+MGTK::Rect::x2, #kMaxWindowWidth
-        bpl     use_maxw
+        bcs     use_maxw
         ldax    iconbb_rect+MGTK::Rect::x2
         jmp     assign_width
 
@@ -7544,9 +7544,9 @@ assign_width:
         ;; Check if height is < min or > max
 
         cmp16   iconbb_rect+MGTK::Rect::y2, #kMinWindowHeight
-        bmi     use_minh
+        bcc     use_minh
         cmp16   iconbb_rect+MGTK::Rect::y2, #kMaxWindowHeight
-        bpl     use_maxh
+        bcs     use_maxh
         ldax    iconbb_rect+MGTK::Rect::y2
         jmp     assign_height
 
@@ -8524,7 +8524,7 @@ found:  txa
         bit     LCBANK1
 
         ;; Below bottom?
-        cmp16   pos_col_name::ycoord, window_grafport::cliprect::y2
+        scmp16  pos_col_name::ycoord, window_grafport::cliprect::y2
         bpl     ret
 
         add16_8 pos_col_icon::ycoord, #kRowHeight
@@ -8534,7 +8534,7 @@ found:  txa
         add16_8 pos_col_date::ycoord, #kRowHeight
 
         ;; Above top?
-        cmp16   pos_col_name::ycoord, window_grafport::cliprect::y1
+        scmp16  pos_col_name::ycoord, window_grafport::cliprect::y1
         bpl     in_range
 ret:    rts
 

desktop/auxmem.s
@@ -1571,8 +1571,8 @@ L9D7C:  stx     rect3_y1
         ldy     #MGTK::Winfo::port + MGTK::GrafPort::viewloc + MGTK::Point::ycoord
         add16in (win_ptr),y, #kWindowHeaderHeight + 1, headery
         cmp16   findwindow_params::mousey, headery
-        bmi     done
-        bpl     find_icon       ; always
+        bcc     done
+        bcs     find_icon       ; always
 
         ;; --------------------------------------------------
         ;; On desktop - A=0, note that as window_id
@@ -1786,37 +1786,37 @@ start:  ldy     #IconInRectParams::icon
         ;; Bitmap's bbox: v0-v1-v2-v7
 
         ;; top of bitmap > bottom of rect --> outside
-        cmp16   rect::y2, poly::v0::ycoord
+        scmp16  rect::y2, poly::v0::ycoord
         bmi     :+
 
         ;; left of bitmap > right of rect --> outside
-        cmp16   rect::x2, poly::v0::xcoord
+        scmp16  rect::x2, poly::v0::xcoord
         bmi     :+
 
         ;; bottom of bitmap < top of rect --> outside
-        cmp16   poly::v2::ycoord, rect::y1
+        scmp16  poly::v2::ycoord, rect::y1
         bmi     :+
 
         ;; right of bitmap < left of rect --> outside
-        cmp16   poly::v1::xcoord, rect::x1
+        scmp16  poly::v1::xcoord, rect::x1
         bpl     inside
 
         ;; Text's bbox: v6-v3-v4-v5
 :
         ;; top of text > bottom of rect --> outside
-        cmp16   rect::y2, poly::v6::ycoord
+        scmp16  rect::y2, poly::v6::ycoord
         bmi     outside
 
         ;; left of text > right of rect --> outside
-        cmp16   rect::x2, poly::v6::xcoord
+        scmp16  rect::x2, poly::v6::xcoord
         bmi     outside
 
         ;; bottom of text < top of rect --> outside
-        cmp16   poly::v4::ycoord, rect::y1
+        scmp16  poly::v4::ycoord, rect::y1
         bmi     outside
 
         ;; right of text < left of rect --> outside
-        cmp16   poly::v3::xcoord, rect::x1
+        scmp16  poly::v3::xcoord, rect::x1
         bpl     inside
 
 outside:
@@ -2923,7 +2923,7 @@ do_pt:  lda     pt_num
         ;; Cases 1/2/3 (and continue below)
         ;; if (cr_r > win_r)
         ;; . cr_r = win_r + 1
-        cmp16   cr_r, win_r
+        scmp16  cr_r, win_r
         bmi     case789
 
         ldx     win_r
@@ -2939,7 +2939,7 @@ do_pt:  lda     pt_num
         ;; if (win_l > cr_l)
         ;; . cr_r = win_l
 case789:
-        cmp16   win_l, cr_l
+        scmp16  win_l, cr_l
         bmi     vert
 
         copy16  win_l, cr_r
@@ -2948,7 +2948,7 @@ case789:
         ;; Cases 3/6 (and done)
         ;; if (win_t > cr_t)
         ;; . cr_b = win_t
-vert:   cmp16   win_t, cr_t
+vert:   scmp16  win_t, cr_t
         bmi     :+
 
         copy16  win_t, cr_b
@@ -2959,7 +2959,7 @@ vert:   cmp16   win_t, cr_t
         ;; if (win_b < cr_b)
         ;; . cr_t = win_b + 2
         ;; . vy   = win_b + 2
-:       cmp16   win_b, cr_b
+:       scmp16  win_b, cr_b
         bpl     case2
 
         ldx     win_b
@@ -2980,7 +2980,7 @@ vert:   cmp16   win_t, cr_t
         ;; . vx   = win_r + 2
         ;; . cr_r = stash_r + 2 (workaround for https://github.com/a2stuff/a2d/issues/153)
 case2:
-        cmp16   win_r, stash_r
+        scmp16  win_r, stash_r
         bpl     :+
 
         lda     win_r

commit e5881f1b8726d4f6b957fce35422c6a160df7104

DeskTop: Fix initial window size for windows without icons.
Regressed in ca8a3da7 - that change was correct, but it revealed that
the icon bounding box calculation is bogus for windows without icons.
However, rather than tweaking that (because it's tricky) fix the code
that assumes it's valid to just use the min size if the window has no
icons.

desktop/main.s
@@ -7514,6 +7514,12 @@ common: sta     preserve_window_size_flag
         ldy     #MGTK::Winfo::port + MGTK::GrafPort::viewloc + MGTK::Point::ycoord
         sub16in iconbb_rect+MGTK::Rect::y2, (winfo_ptr),y, iconbb_rect+MGTK::Rect::y2
 
+        ;; --------------------------------------------------
+        ;; Width
+
+        lda     cached_window_entry_count
+        beq     use_minw        ; `iconbb_rect` is bogus if there are no icons
+
         ;; Check if width is < min or > max
         cmp16   iconbb_rect+MGTK::Rect::x2, #kMinWindowWidth
         bcc     use_minw
@@ -7541,8 +7547,13 @@ assign_width:
         iny
         sta     (winfo_ptr),y
 
-        ;; Check if height is < min or > max
+        ;; --------------------------------------------------
+        ;; Height
 
+        lda     cached_window_entry_count
+        beq     use_minh        ; `iconbb_rect` is bogus if there are no icons
+
+        ;; Check if height is < min or > max
         cmp16   iconbb_rect+MGTK::Rect::y2, #kMinWindowHeight
         bcc     use_minh
         cmp16   iconbb_rect+MGTK::Rect::y2, #kMaxWindowHeight
@@ -7568,6 +7579,8 @@ assign_height:
         txa
         iny
         sta     (winfo_ptr),y
+
+        ;; --------------------------------------------------
 
         ;; Update scrollbars
         lda     thumbmax