a2stuff/a2d

commit 2e04b4560817914becd166bb5d7a8f09bc79d4b2

List Boxes: Align implementations, continued
* Ensure all have HandleListScroll, centralize ignore-if-disabled there
* Fix recent stack corruption in Find Files if disabled sb is clicked
* Dispense with local max_top, use vthumbmax instead
* Simplify selected index calcs

desk.acc/sounds.s
@@ -453,11 +453,11 @@ grafport_win:       .tag    MGTK::GrafPort
         cmp     #CHAR_UP
     IF_EQ
         lda     #MGTK::Part::page_up
-        jmp     HandleScrollWithPart
+        jmp     HandleListScrollWithPart
     END_IF
         ;; CHAR_DOWN
         lda     #MGTK::Part::page_down
-        jmp     HandleScrollWithPart
+        jmp     HandleListScrollWithPart
 
 ret:    rts
 
@@ -494,7 +494,7 @@ ret:    rts
     IF_EQ
         lda     findwindow_params::which_area
         cmp     #MGTK::Area::content
-        beq     HandleClick
+        beq     HandleDialogClick
         jmp     InputLoop
     END_IF
 
@@ -513,7 +513,7 @@ ret:    rts
 
 ;;; ============================================================
 
-.proc HandleClick
+.proc HandleDialogClick
         MGTK_CALL MGTK::ScreenToWindow, screentowindow_params
         MGTK_CALL MGTK::MoveTo, screentowindow_params::window
 
@@ -543,34 +543,31 @@ ret:    rts
         MGTK_CALL MGTK::FindControl, findcontrol_params
         lda     findcontrol_params::which_ctl
         cmp     #MGTK::Ctl::vertical_scroll_bar
-        jeq     HandleScroll
+        jeq     HandleListScroll
 
         cmp     #MGTK::Ctl::not_a_control
-        bne     :+
+        beq     :+
         rts
 :
-        lda     winfo_listbox::window_id
-        sta     screentowindow_params::window_id
+        copy    #kListBoxWindowId, screentowindow_params::window_id
         MGTK_CALL MGTK::ScreenToWindow, screentowindow_params
-        MGTK_CALL MGTK::MoveTo, screentowindow_params::window
-
-        add16   screentowindow_params::windowy, winfo_listbox::maprect::y1, tmp
-        ldax    tmp
+        ldax    screentowindow_params::windowy
         ldy     #kListItemHeight
         jsr     Divide_16_8_16
-        jmp     SelectIndex
+        clc
+        adc     winfo_listbox::vthumbpos
 
-tmp:    .word   0
+        jmp     SelectIndex
 .endproc
 
 ;;; ============================================================
 
-.proc HandleScrollWithPart
+.proc HandleListScrollWithPart
         sta     findcontrol_params::which_part
-        FALL_THROUGH_TO HandleScroll
+        FALL_THROUGH_TO HandleListScroll
 .endproc
 
-.proc HandleScroll
+.proc HandleListScroll
         lda     findcontrol_params::which_part
 
         ;; scroll up by one line
@@ -589,7 +586,7 @@ tmp:    .word   0
         cmp     #MGTK::Part::down_arrow
     IF_EQ
         lda     winfo_listbox::vthumbpos
-        cmp     #kMaxTop
+        cmp     winfo_listbox::vthumbmax
         jcs     done
 
         clc
@@ -616,9 +613,9 @@ tmp:    .word   0
         lda     winfo_listbox::vthumbpos
         clc
         adc     #kListRows
-        cmp     #kMaxTop
+        cmp     winfo_listbox::vthumbmax
         bcc     update
-        lda     #kMaxTop
+        lda     winfo_listbox::vthumbmax
         jmp     update
     END_IF
 

disk_copy/auxlc.s
@@ -1030,11 +1030,11 @@ ret:    rts
         cmp     #CHAR_UP
     IF_EQ
         lda     #MGTK::Part::page_up
-        jmp     HandleScrollWithPart
+        jmp     HandleListScrollWithPart
     END_IF
         ;; CHAR_DOWN
         lda     #MGTK::Part::page_down
-        jmp     HandleScrollWithPart
+        jmp     HandleListScrollWithPart
 
 ret:    rts
 
@@ -1103,7 +1103,10 @@ LDA7D:  copy    #0, checkitem_params::check
         cmp     #winfo_dialog::kWindowId
         beq     HandleDialogClick
         cmp     winfo_drive_select
-        jeq     HandleListClick
+    IF_EQ
+        jsr     HandleListClick
+        return  #$FF
+    END_IF
         rts
 .endproc
 
@@ -1141,26 +1144,27 @@ LDA7D:  copy    #0, checkitem_params::check
         MGTK_CALL MGTK::FindControl, findcontrol_params
         lda     findcontrol_params::which_ctl
         cmp     #MGTK::Ctl::vertical_scroll_bar
-        jeq     HandleScroll
+        jeq     HandleListScroll
 
         cmp     #MGTK::Ctl::not_a_control
-        jne     LDBCA
-
+        beq     :+
+        rts
+:
         copy    #winfo_drive_select::kWindowId, screentowindow_params::window_id
         MGTK_CALL MGTK::ScreenToWindow, screentowindow_params
-        MGTK_CALL MGTK::MoveTo, screentowindow_params::window
         ldax    screentowindow_params::windowy
         ldy     #kListItemHeight
         jsr     Divide_16_8_16
         clc
         adc     winfo_drive_select::vthumbpos
+
         cmp     num_drives
         bcc     LDB98
         lda     current_drive_selection
         jsr     HighlightRow
         lda     #$FF
         sta     current_drive_selection           ; $FF if no selection?
-        jmp     LDBCA
+        jmp     ret
 
 LDB98:  cmp     current_drive_selection
         bne     LDBCD
@@ -1173,7 +1177,7 @@ LDBC0:  lda     #$FF
         sta     LD368
         lda     #$64
         sta     LD367
-LDBCA:  return  #$FF
+ret:    rts
 
 LDBCD:  pha
         lda     current_drive_selection
@@ -1610,23 +1614,18 @@ CheckAlpha:
 
 ;;; ============================================================
 
-.proc HandleScrollWithPart
+.proc HandleListScrollWithPart
         sta     findcontrol_params::which_part
-        FALL_THROUGH_TO HandleScroll
+        FALL_THROUGH_TO HandleListScroll
 .endproc
 
-.proc HandleScroll
+.proc HandleListScroll
         ;; Ignore unless vscroll is enabled
         lda     winfo_drive_select::vscroll
         and     #MGTK::Scroll::option_active
         bne     :+
         rts
 :
-        lda     num_drives
-        sec
-        sbc     #kListRows
-        sta     max_top
-
         lda     findcontrol_params::which_part
 
         cmp     #MGTK::Part::up_arrow
@@ -1643,7 +1642,7 @@ CheckAlpha:
         cmp     #MGTK::Part::down_arrow
     IF_EQ
         lda     winfo_drive_select::vthumbpos
-        cmp     max_top
+        cmp     winfo_drive_select::vthumbmax
         jcs     done
 
         clc
@@ -1668,9 +1667,9 @@ CheckAlpha:
         lda     winfo_drive_select::vthumbpos
         clc
         adc     #kListRows
-        cmp     max_top
+        cmp     winfo_drive_select::vthumbmax
         bcc     update
-        lda     max_top
+        lda     winfo_drive_select::vthumbmax
         jmp     update
     END_IF
 
@@ -1693,10 +1692,7 @@ update: sta     updatethumb_params::thumbpos
         bmi     :+
         jsr     HighlightRow
 :
-done:   return  #$FF
-
-max_top:
-        .byte   0
+done:   rts
 .endproc
 
 ;;; ============================================================

lib/file_dialog.s
@@ -235,6 +235,8 @@ ret:    rts
         lda     findwindow_params::window_id
         cmp     #file_dialog_res::kFilePickerDlgWindowID
         beq     :+
+        bit     listbox_disabled_flag
+        bmi     :+
         bit     is_apple_click_flag
         bmi     ret             ; ignore except for Change Drive
         jmp     HandleListClick
@@ -378,37 +380,27 @@ click_handler_hook:
 ;;; ============================================================
 
 .proc HandleListClick
-        bit     listbox_disabled_flag
-        bmi     rts1
         MGTK_CALL MGTK::FindControl, findcontrol_params
         lda     findcontrol_params::which_ctl
-        .assert MGTK::Ctl::not_a_control = 0, error, "enum mismatch"
-        beq     in_list
-
         cmp     #MGTK::Ctl::vertical_scroll_bar
-        bne     rts1
-        lda     file_dialog_res::winfo_listbox::vscroll
-        and     #MGTK::Scroll::option_active
-        beq     rts1
-        jmp     HandleVScrollClick
+        jeq     HandleListScroll
 
-rts1:   rts
-
-in_list:
-        lda     #file_dialog_res::kEntryListCtlWindowID
-        sta     screentowindow_params::window_id
+        cmp     #MGTK::Ctl::not_a_control
+        beq     :+
+        rts
+:
+        copy    #file_dialog_res::kEntryListCtlWindowID, screentowindow_params::window_id
         MGTK_CALL MGTK::ScreenToWindow, screentowindow_params
-        add16   screentowindow_params::windowy, file_dialog_res::winfo_listbox::maprect::y1, screentowindow_params::windowy
         ldax    screentowindow_params::windowy
         ldy     #kListItemHeight
         jsr     Divide_16_8_16
-        new_index := screentowindow_params::windowy
-        stax    screentowindow_params::windowy
+        clc
+        adc     file_dialog_res::winfo_listbox::vthumbpos
 
-        lda     file_dialog_res::selected_index
-        cmp     new_index
-        beq     same
-        jmp     different
+        sta     new_index
+        cmp     file_dialog_res::selected_index
+        bne     different
+        FALL_THROUGH_TO same
 
         ;; --------------------------------------------------
         ;; Click on the previous entry
@@ -423,8 +415,7 @@ open:   ldx     file_dialog_res::selected_index
 
         ;; File - select it.
         BTK_CALL BTK::Flash, file_dialog_res::ok_button_params
-        jsr     HandleOk
-        jmp     rts1
+        jmp     HandleOk
 
         ;; Folder - open it.
 folder: and     #$7F
@@ -440,7 +431,8 @@ folder: and     #$7F
         ;; Click on a different entry
 
 different:
-        lda     new_index
+        new_index := *+1
+        lda     #SELF_MODIFIED_BYTE
         cmp     num_file_names
         bcc     :+
         rts
@@ -478,7 +470,13 @@ different:
 
 ;;; ============================================================
 
-.proc HandleVScrollClick
+.proc HandleListScroll
+        ;; Ignore unless vscroll is enabled
+        lda     file_dialog_res::winfo_listbox::vscroll
+        and     #MGTK::Scroll::option_active
+        bne     :+
+        rts
+:
         lda     findcontrol_params::which_part
         cmp     #MGTK::Part::up_arrow
         jeq     HandleLineUp

desk.acc/find.files.s
@@ -1063,11 +1063,11 @@ ignore:
         cmp     #CHAR_UP
       IF_EQ
         lda     #MGTK::Part::up_arrow
-        jmp     HandleScrollWithPart
+        jmp     HandleListScrollWithPart
       END_IF
         ;; CHAR_DOWN
         lda     #MGTK::Part::down_arrow
-        jmp     HandleScrollWithPart
+        jmp     HandleListScrollWithPart
     END_IF
 
         ;; Double modifiers
@@ -1076,22 +1076,22 @@ ignore:
         cmp     #CHAR_UP
       IF_EQ
         lda     #kPartHome
-        jmp     HandleScrollWithPart
+        jmp     HandleListScrollWithPart
       END_IF
         ;; CHAR_DOWN
         lda     #kPartEnd
-        jmp     HandleScrollWithPart
+        jmp     HandleListScrollWithPart
     END_IF
 
         ;; Single modifier
         cmp     #CHAR_UP
     IF_EQ
         lda     #MGTK::Part::page_up
-        jmp     HandleScrollWithPart
+        jmp     HandleListScrollWithPart
     END_IF
         ;; CHAR_DOWN
         lda     #MGTK::Part::page_down
-        jmp     HandleScrollWithPart
+        jmp     HandleListScrollWithPart
 
 .endproc
 
@@ -1262,10 +1262,9 @@ done:   jmp     InputLoop
         MGTK_CALL MGTK::FindControl, findcontrol_params
         lda     findcontrol_params::which_ctl
         cmp     #MGTK::Ctl::vertical_scroll_bar
-        bne     ret
+        jeq     HandleListScroll
 
-        jsr     HandleScroll
-ret:    rts
+        rts
 .endproc
 
 ;;; ============================================================
@@ -1275,22 +1274,18 @@ ret:    rts
 kPartHome = $80
 kPartEnd  = $81
 
-.proc HandleScrollWithPart
+.proc HandleListScrollWithPart
         sta     findcontrol_params::which_part
-        FALL_THROUGH_TO HandleScroll
+        FALL_THROUGH_TO HandleListScroll
 .endproc
 
-.proc HandleScroll
+.proc HandleListScroll
         ;; Ignore unless vscroll is enabled
         lda     winfo_results::vscroll
         and     #MGTK::Scroll::option_active
-        jeq     InputLoop
-
-        lda     num_entries
-        sec
-        sbc     #kResultsRows
-        sta     max_top
-
+        bne     :+
+        rts
+:
         lda     findcontrol_params::which_part
 
         ;; scroll to start
@@ -1308,10 +1303,10 @@ kPartEnd  = $81
         cmp     #kPartEnd
     IF_EQ
         lda     winfo_results::vthumbpos
-        cmp     max_top
+        cmp     winfo_results::vthumbmax
         jcs     done
 
-        lda     max_top
+        lda     winfo_results::vthumbmax
         jmp     update
     END_IF
 
@@ -1331,7 +1326,7 @@ kPartEnd  = $81
         cmp     #MGTK::Part::down_arrow
     IF_EQ
         lda     winfo_results::vthumbpos
-        cmp     max_top
+        cmp     winfo_results::vthumbmax
         jcs     done
 
         clc
@@ -1358,9 +1353,9 @@ kPartEnd  = $81
         lda     winfo_results::vthumbpos
         clc
         adc     #kResultsRows
-        cmp     max_top
+        cmp     winfo_results::vthumbmax
         bcc     update
-        lda     max_top
+        lda     winfo_results::vthumbmax
         jmp     update
     END_IF
 
@@ -1380,8 +1375,6 @@ update: sta     updatethumb_params::thumbpos
         jsr     DrawResults
 
 done:   rts
-
-max_top:        .byte   0
 .endproc
 
 ;;; ============================================================

commit 92d8d1afb5f6877387c260f17f1db110e8a51efe

List Boxes: Fix regression in selecting in scrolled boxes
Broken by 2e04b456 which tried to be clever and use vthumbpos as
offset for top item; in some cases this wasn't set to 0 when the
scrollbar was deactivated. It really should be, but using the actual
scroll offset makes the code more robust, if a bit longer.

desk.acc/sounds.s
@@ -548,11 +548,10 @@ ret:    rts
 :
         copy    #kListBoxWindowId, screentowindow_params::window_id
         MGTK_CALL MGTK::ScreenToWindow, screentowindow_params
+        add16   screentowindow_params::windowy, winfo_listbox::maprect::y1, screentowindow_params::windowy
         ldax    screentowindow_params::windowy
         ldy     #kListItemHeight
         jsr     Divide_16_8_16
-        clc
-        adc     winfo_listbox::vthumbpos
 
         jmp     SelectIndex
 .endproc

disk_copy/auxlc.s
@@ -1149,11 +1149,10 @@ LDA7D:  copy    #0, checkitem_params::check
 :
         copy    #winfo_drive_select::kWindowId, screentowindow_params::window_id
         MGTK_CALL MGTK::ScreenToWindow, screentowindow_params
+        add16   screentowindow_params::windowy, winfo_drive_select::maprect::y1, screentowindow_params::windowy
         ldax    screentowindow_params::windowy
         ldy     #kListItemHeight
         jsr     Divide_16_8_16
-        clc
-        adc     winfo_drive_select::vthumbpos
 
         cmp     num_drives
         bcc     :+

lib/file_dialog.s
@@ -391,11 +391,10 @@ click_handler_hook:
 :
         copy    #file_dialog_res::kEntryListCtlWindowID, screentowindow_params::window_id
         MGTK_CALL MGTK::ScreenToWindow, screentowindow_params
+        add16   screentowindow_params::windowy, file_dialog_res::winfo_listbox::maprect::y1, screentowindow_params::windowy
         ldax    screentowindow_params::windowy
         ldy     #kListItemHeight
         jsr     Divide_16_8_16
-        clc
-        adc     file_dialog_res::winfo_listbox::vthumbpos
 
         sta     new_index
         cmp     file_dialog_res::selected_index