a2stuff/a2d

commit 3168b3956034a2ed54e0428a686c2ac60075db42

Align list box implementations
* Don't need to multiply when drawing all list entries
* Tweak heights to fit integral number of items, not show stray pixels at top
* Simplify adjusting viewport (removing dead code, etc)

No intentional behavior changes.

lib/file_dialog.s
@@ -1723,15 +1723,17 @@ d4:     .byte   0
         jsr     SetPortForList
         MGTK_CALL MGTK::PaintRect, file_dialog_res::winfo_listbox::maprect
         copy    #file_dialog_res::kListEntryNameX, file_dialog_res::picker_entry_pos::xcoord ; high byte always 0
-        copy16  #kListItemHeight-1, file_dialog_res::picker_entry_pos::ycoord
+        copy16  #kListItemTextOffsetY, file_dialog_res::picker_entry_pos::ycoord
         copy    #0, index
 
 loop:   lda     index
         cmp     num_file_names
         bne     :+
         rts
+:
+        MGTK_CALL MGTK::MoveTo, file_dialog_res::picker_entry_pos
+        add16_8 file_dialog_res::picker_entry_pos::ycoord, #kListItemHeight
 
-:       MGTK_CALL MGTK::MoveTo, file_dialog_res::picker_entry_pos
         ldx     index
         lda     file_list_index,x
         and     #$7F
@@ -1755,7 +1757,6 @@ loop:   lda     index
         jsr     InvertEntry
 l2:     inc     index
 
-        add16_8 file_dialog_res::picker_entry_pos::ycoord, #kListItemHeight, file_dialog_res::picker_entry_pos::ycoord
         jmp     loop
 
 index:  .byte   0

common.inc
@@ -64,6 +64,8 @@ kTextBoxTextVOffset = kSystemFontHeight+1 ; from top edge of rect to text pos
 
 ;;; Standard list box control metrics
 kListItemHeight = kSystemFontHeight+1
+kListItemTextOffsetX = kTextBoxTextHOffset
+kListItemTextOffsetY = kListItemHeight - 1
 
 ;;; Mouse threshold for double click detection.
 kDoubleClickDeltaX      = 5

desk.acc/find.files.s
@@ -838,7 +838,7 @@ entries_buffer := *
 
 kDAWindowID     = 63
 kDAWidth        = 465
-kDAHeight       = 151
+kDAHeight       = 150
 kDALeft         = (kScreenWidth - kDAWidth)/2
 kDATop          = (kScreenHeight - kMenuBarHeight - kDAHeight)/2 + kMenuBarHeight
 
@@ -1250,16 +1250,15 @@ kPartEnd  = $81
 
 .proc HandleScrollWithPart
         sta     findcontrol_params::which_part
+        FALL_THROUGH_TO HandleScroll
+.endproc
 
+.proc HandleScroll
         ;; Ignore unless vscroll is enabled
         lda     winfo_results::vscroll
         and     #MGTK::Scroll::option_active
         jeq     InputLoop
 
-        FALL_THROUGH_TO HandleScroll
-.endproc
-
-.proc HandleScroll
         lda     num_entries
         sec
         sbc     #kResultsRows
@@ -1358,18 +1357,15 @@ done:   jmp     InputLoop
 max_top:        .byte   0
 .endproc
 
+;;; ============================================================
+
 ;;; Assumes `top_row` is set.
 .proc UpdateViewport
-        ;; Update top of maprect: 1 + top_row * kListItemHeight
-        copy16  #0, winfo_results::maprect::y1
-        ldx     top_row
-        beq     bottom
-:       add16   #kListItemHeight, winfo_results::maprect::y1, winfo_results::maprect::y1
-        dex
-        bne     :-
-
-        ;; Update bottom of maprect
-bottom: add16   winfo_results::maprect::y1, #kResultsHeight, winfo_results::maprect::y2
+        ldax    #kListItemHeight
+        ldy     top_row
+        jsr     Multiply_16_8_16
+        stax    winfo_results::maprect::y1
+        addax   #kResultsHeight, winfo_results::maprect::y2
 
         rts
 .endproc
@@ -1478,13 +1474,13 @@ done:   MGTK_CALL MGTK::ShowCursor
         MGTK_CALL MGTK::ShowCursor
 
         copy    #0, cur_line
-        copy16  #0, cur_pos::ycoord
+        copy16  #kListItemTextOffsetY, cur_pos::ycoord
         rts
 .endproc
 
 .proc DrawNextResult
-        add16_8   cur_pos::ycoord, #kListItemHeight, cur_pos::ycoord
         MGTK_CALL MGTK::MoveTo, cur_pos
+        add16_8   cur_pos::ycoord, #kListItemHeight, cur_pos::ycoord
 
         lda     cur_line
         jsr     GetEntry
@@ -1514,6 +1510,7 @@ done:   MGTK_CALL MGTK::ShowCursor
 
         .include "../lib/drawstring.s"
         .include "../lib/measurestring.s"
+        .include "../lib/muldiv.s"
 
 ;;; ============================================================
 

desk.acc/sounds.s
@@ -289,8 +289,6 @@ fontptr:        .addr   DEFAULT_FONT
 nextwinfo:      .addr   0
 .endparams
 
-        kListItemTextOffsetX = kTextBoxTextHOffset
-        kListItemTextOffsetY = kListItemHeight - 1
         DEFINE_POINT itempos, kListItemTextOffsetX, 0
         DEFINE_RECT itemrect, 0, 0, winfo_listbox::kWidth, 0
 
@@ -683,11 +681,8 @@ ret:    rts
 
 ;;; ============================================================
 
-;;; Assumes `top_row` is set.
+;;; Assert: `top_row` is set.
 .proc UpdateViewport
-        ;; Compute height of line (font height + 1)
-        ;; Update top of maprect: 1 + top_row * line_height
-        copy16  #0, winfo_listbox::maprect::y1
         ldax    #kListItemHeight
         ldy     top_row
         jsr     Multiply_16_8_16
@@ -838,19 +833,17 @@ ret:    rts
 
         lda     #0
         sta     index
+        copy16  #kListItemTextOffsetY, itempos::ycoord
+
+loop:   MGTK_CALL MGTK::MoveTo, itempos
+        add16_8  itempos::ycoord, #kListItemHeight
 
         index := *+1
-loop:   lda     #SELF_MODIFIED_BYTE
-        ldx     #0
-        ldy     #kListItemHeight
-        jsr     Multiply_16_8_16
-        addax   #kListItemTextOffsetY, itempos::ycoord
-        MGTK_CALL MGTK::MoveTo, itempos
-
-        ptr := $06
-        lda     index
+        lda     #SELF_MODIFIED_BYTE
         asl
         tax
+
+        ptr := $06
         copy16  name_table,x, ptr
         param_call_indirect DrawString, ptr
 

disk_copy/auxlc.s
@@ -1599,6 +1599,7 @@ CheckAlpha:
 
 ;;; Assert: `top_row` is set.
 .proc HandleScroll
+        ;; Ignore unless vscroll is enabled
         lda     winfo_drive_select::vscroll
         and     #MGTK::Scroll::option_active
         bne     :+
@@ -1723,12 +1724,10 @@ ret:    rts
 
 ;;; ============================================================
 
-;;; Assert: `top_row` is set.
+;;; Assumes `top_row` is set.
 .proc UpdateViewport
-        copy16  #0, winfo_drive_select::maprect::y1
-        lda     top_row         ; lo
-        ldx     #0              ; hi
-        ldy     #kListItemHeight
+        ldax    #kListItemHeight
+        ldy     top_row
         jsr     Multiply_16_8_16
         stax    winfo_drive_select::maprect::y1
         addax   #kListBoxHeight, winfo_drive_select::maprect::y2
@@ -1904,13 +1903,12 @@ match:  lda     DEVLST,x
         MGTK_CALL MGTK::SetPenMode, pencopy
         MGTK_CALL MGTK::PaintRect, winfo_drive_select::maprect
 
-        lda     #0
-        sta     index
+        copy    #0, index
+        copy16  #kListItemTextOffsetY, list_entry_pos::ycoord
 
 loop:   lda     index
-        jsr     SetYCoord
-        lda     index
         jsr     DrawDeviceListEntry
+        add16_8 list_entry_pos::ycoord, #kListItemHeight
 
         inc     index
         lda     index
@@ -1987,14 +1985,13 @@ src_block_count:
         MGTK_CALL MGTK::SetPenMode, pencopy
         MGTK_CALL MGTK::PaintRect, winfo_drive_select::maprect
 
-        lda     #0
-        sta     index
+        copy    #0, index
+        copy16  #kListItemTextOffsetY, list_entry_pos::ycoord
 
-loop:   lda     index
-        jsr     SetYCoord
-        ldx     index
+loop:   ldx     index
         lda     destination_index_table,x
         jsr     DrawDeviceListEntry
+        add16_8 list_entry_pos::ycoord, #kListItemHeight
 
         inc     index
         lda     index
@@ -2064,16 +2061,6 @@ index:  .byte   0
 
 device_index:
         .byte   0
-.endproc
-
-;;; ============================================================
-
-.proc SetYCoord
-        ldx     #0              ; hi (A=lo)
-        ldy     #kListItemHeight
-        jsr     Multiply_16_8_16
-        addax   #kListItemHeight-1, list_entry_pos::ycoord
-        rts
 .endproc
 
 ;;; ============================================================

commit 124396d4c8f07aec41f4b697f0f352460eeeeb0a

File Dialog: fix entry drawing
Regression introduced in 3168b395 - when iterating the position was
simplified, it didn't account for when the folder glyph was drawn.
Move the y-pos increment later in the proc.

lib/file_dialog.s
@@ -1759,7 +1759,6 @@ loop:   lda     index
         rts
 :
         MGTK_CALL MGTK::MoveTo, file_dialog_res::picker_entry_pos
-        add16_8 file_dialog_res::picker_entry_pos::ycoord, #kListItemHeight
 
         ldx     index
         lda     file_list_index,x
@@ -1784,6 +1783,7 @@ loop:   lda     index
         jsr     InvertEntry
 l2:     inc     index
 
+        add16_8 file_dialog_res::picker_entry_pos::ycoord, #kListItemHeight
         jmp     loop
 
 index:  .byte   0