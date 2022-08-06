a2stuff/a2d

commit 2cc73d01f3159b8801b2cccb5094eb319dba19f5

DeskTop: Shuffle resources
This is with a goal of having Aux and LC free for toolkits, graphical
resources, and strings.

* Move some data out of LC bank into Main that's only used there
* Move some procs out of Aux into LC or Main, adjust relays

desktop/main.s
@@ -15847,6 +15847,7 @@ ret:    rts
         .include "../lib/doubleclick.s"
         .include "../lib/reconnect_ram.s"
         .include "../lib/muldiv.s"
+        .include "../lib/bell.s"
 
 ;;; ============================================================
 ;;; Resources (that are only used from Main, i.e. not MGTK)
@@ -16006,6 +16007,101 @@ datetime_for_conversion := list_view_filerecord + FileRecord::modification_date
 
 ;;; ============================================================
 
+
+path_buf4:
+        .res    ::kPathBufferSize, 0
+path_buf3:
+        .res    ::kPathBufferSize, 0
+filename_buf:
+        .res    16, 0
+
+        ;; Set to $80 for Copy, $FF for Run
+copy_run_flag:
+        .byte   0
+
+delete_skip_decrement_flag:     ; always set to 0 ???
+        .byte   0
+
+op_ref_num:
+        .byte   0
+
+process_depth:
+        .byte   0               ; tracks recursion depth
+
+;;; Number of file entries per directory block
+num_entries_per_block:
+        .byte   13
+
+entries_read:
+        .word   0
+entries_to_skip:
+        .word   0
+
+;;; During directory traversal, the number of file entries processed
+;;; at the current level is pushed here, so that following a descent
+;;; the previous entries can be skipped.
+entry_count_stack:
+        .res    ::kDirStackBufferSize, 0
+
+entry_count_stack_index:
+        .byte   0
+
+entries_read_this_block:
+        .byte   0
+
+;;; ============================================================
+
+        ;; index is device number (in DEVLST), value is icon number
+device_to_icon_map:
+        .res    ::kMaxVolumes, 0
+
+;;; Window to file record mapping list. Each entry is a window
+;;; id. Position in the list is the same as position in the
+;;; subsequent file record list.
+window_id_to_filerecord_list_count:
+        .byte   0
+window_id_to_filerecord_list_entries:
+        .res    kMaxNumWindows, 0 ; 8 entries + length
+
+;;; Mapping from position in above table to FileRecord entry
+window_filerecord_table:
+        .res    kMaxNumWindows*2
+
+;;; ============================================================
+
+startup_slot_table:
+        .res    7, 0            ; maps menu item index (0-based) to slot number
+
+;;; ============================================================
+
+;;; Computed during startup
+width_items_label_padded:
+        .word   0
+width_left_labels:
+        .word   0
+
+;;; Computed during startup
+width_items_label:      .word   0
+width_k_in_disk_label:  .word   0
+width_k_available_label:        .word   0
+width_right_labels:     .word   0
+
+;;; Assigned during startup
+trash_icon_num:  .byte   0
+
+;;; ============================================================
+
+hex_digits:
+        .byte   "0123456789ABCDEF"
+
+;;; ============================================================
+
+;;; High bit set if menu dispatch via keyboard accelerator, clear otherwise.
+menu_kbd_flag:
+        .byte   0
+
+;;; ============================================================
+
 ;;; Map ProDOS file type to string (for listings/Get Info).
 ;;; If not found, $XX is used (like CATALOG).
 
@@ -16046,6 +16142,51 @@ type_names_table:
         ASSERT_RECORD_TABLE_SIZE type_names_table, kNumFileTypes, 4
 
 ;;; ============================================================
+;;; Map IconType to other icon/details
+
+icontype_iconentryflags_table:
+        .byte   0                    ; generic
+        .byte   0                    ; text
+        .byte   0                    ; binary
+        .byte   0                    ; graphics
+        .byte   0                    ; music
+        .byte   0                    ; font
+        .byte   0                    ; relocatable
+        .byte   0                    ; command
+        .byte   kIconEntryFlagsDropTarget ; folder
+        .byte   0                    ; iigs
+        .byte   0                    ; appleworks db
+        .byte   0                    ; appleworks wp
+        .byte   0                    ; appleworks sp
+        .byte   0                    ; archive
+        .byte   0                    ; desk accessory
+        .byte   0                    ; basic
+        .byte   0                    ; system
+        .byte   0                    ; application
+        ASSERT_TABLE_SIZE icontype_iconentryflags_table, IconType::COUNT
+
+type_icons_table:               ; map into definitions below
+        .addr   gen ; generic
+        .addr   txt ; text
+        .addr   bin ; binary
+        .addr   fot ; graphics
+        .addr   mus ; music
+        .addr   fnt ; font
+        .addr   rel ; relocatable
+        .addr   cmd ; command
+        .addr   dir ; folder
+        .addr   src ; iigs
+        .addr   adb ; appleworks db
+        .addr   awp ; appleworks wp
+        .addr   asp ; appleworks sp
+        .addr   arc ; archive
+        .addr   a2d ; desk accessory
+        .addr   bas ; basic
+        .addr   sys ; system
+        .addr   app ; application
+        ASSERT_ADDRESS_TABLE_SIZE type_icons_table, IconType::COUNT
+
+;;; ============================================================
 ;;; Localized strings (may change length)
 ;;; ============================================================
 
@@ -16074,5 +16215,6 @@ str_volume:
 ;;; ============================================================
 ;;; "Exports"
 
+        Bell := main::Bell
         Multiply_16_8_16 := main::Multiply_16_8_16
         Divide_16_8_16 := main::Divide_16_8_16

desktop/auxmem.s
@@ -382,7 +382,6 @@ special_menu:
 ;;; ============================================================
 
         .include "../lib/drawstring.s"
-        .include "../lib/bell.s"
 
 ;;; ============================================================
 
@@ -787,28 +786,8 @@ start:
         .define AD_WRAP 1
         .define AD_EJECTABLE 0
 
+        Bell := BellFromAux
         .include "../lib/alert_dialog.s"
-
-;;; ============================================================
-;;; Copy current GrafPort MapInfo into target buffer
-;;; Inputs: A,X = mapinfo address
-
-.proc GetPortBits
-        port_ptr := $06
-
-        stax    dest_ptr
-
-        MGTK_CALL MGTK::GetPort, port_ptr
-
-        ldy     #.sizeof(MGTK::MapInfo)-1
-:       lda     (port_ptr),y
-        dest_ptr := *+1
-        sta     SELF_MODIFIED,y
-        dey
-        bpl     :-
-
-        rts
-.endproc
 
 ;;; ============================================================
 

desktop/init.s
@@ -362,7 +362,7 @@ trash_name:  PASCAL_STRING res_string_trash_icon_name
         sta     cached_window_entry_count
         sta     icon_count
         jsr     AllocateIcon
-        sta     trash_icon_num
+        sta     main::trash_icon_num
         sta     cached_window_entry_list
         jsr     main::IconEntryLookup
         stax    ptr
@@ -571,20 +571,20 @@ done:   rts
 
         ;; Width of "123,456 Items"
         param_call main::MeasureLCString, str_items_suffix
-        addax   dx, width_items_label
+        addax   dx, main::width_items_label
 
         ;; Width of "123,456K in disk"
         param_call main::MeasureLCString, str_k_in_disk
-        addax   dx, width_k_in_disk_label
+        addax   dx, main::width_k_in_disk_label
 
         ;; Width of "123,456K available"
         param_call main::MeasureLCString, str_k_available
-        addax   dx, width_k_available_label
+        addax   dx, main::width_k_available_label
 
-        add16   width_k_in_disk_label, width_k_available_label, width_right_labels
-        add16   width_items_label, #5, width_items_label_padded
-        add16   width_items_label_padded, width_k_in_disk_label, width_left_labels
-        add16   width_left_labels, #3, width_left_labels
+        add16   main::width_k_in_disk_label, main::width_k_available_label, main::width_right_labels
+        add16   main::width_items_label, #5, main::width_items_label_padded
+        add16   main::width_items_label_padded, main::width_k_in_disk_label, main::width_left_labels
+        add16   main::width_left_labels, #3, main::width_left_labels
         jmp     end
 
 dx:     .word   0
@@ -945,7 +945,7 @@ cvi_result:
         dex
 :       inx
         copy    DEVLST+1,x, DEVLST,x
-        copy    device_to_icon_map+1,x, device_to_icon_map,x
+        copy    main::device_to_icon_map+1,x, main::device_to_icon_map,x
         cpx     DEVCNT
         bne     :-
         dec     DEVCNT
@@ -986,7 +986,7 @@ loop:   lda     slot
 
         ;; It is a ProDOS device - prepare menu item.
         lda     slot
-        sta     startup_slot_table,x
+        sta     main::startup_slot_table,x
 
         txa                     ; pointer to nth sNN string
         pha

desktop/lc.s
@@ -268,12 +268,12 @@ op:     lda     SELF_MODIFIED
 
 ;;; ============================================================
 ;;; Bell
-;;; Assert: Main is banked in
+;;; Assert: Aux is banked in
 
-.proc Bell
-        jsr     BankInAux
-        jsr     aux::Bell
-        jmp     BankInMain
+.proc BellFromAux
+        jsr     BankInMain
+        jsr     Bell
+        jmp     BankInAux
 .endproc
 
 ;;; ============================================================
@@ -283,8 +283,23 @@ op:     lda     SELF_MODIFIED
 
 .proc GetPortBits
         jsr     BankInAux
-        jsr     aux::GetPortBits
+
+        port_ptr := $06
+
+        stax    dest_ptr
+
+        MGTKEntry := MGTKAuxEntry
+        MGTK_CALL MGTK::GetPort, port_ptr
+
+        ldy     #.sizeof(MGTK::MapInfo)-1
+:       lda     (port_ptr),y
+        dest_ptr := *+1
+        sta     SELF_MODIFIED,y
+        dey
+        bpl     :-
+
         jmp     BankInMain
+        rts
 .endproc
 
 ;;; ============================================================

desktop/ovl_file_copy.s
@@ -123,6 +123,9 @@ done:   jsr     file_dialog::Update ; string changed
         rts
 .endproc
 
+saved_src_index:
+        .byte   0
+
 ;;; ============================================================
 
 .proc HandleOkDestination

desktop/res.s
@@ -545,9 +545,6 @@ input1_dirty_flag:              ; stash dirty flag when input2 is active
 input2_dirty_flag:              ; stash dirty flag when input1 is active
         .byte   0
 
-saved_src_index:
-        .byte   0
-
         FONT := DEFAULT_FONT
         .define FD_EXTENDED 1
         buf_input1 := path_buf0
@@ -864,66 +861,6 @@ str_file_type:
         PASCAL_STRING " $00"
 
 ;;; ============================================================
-
-path_buf4:
-        .res    kPathBufferSize, 0
-path_buf3:
-        .res    kPathBufferSize, 0
-filename_buf:
-        .res    16, 0
-
-        ;; Set to $80 for Copy, $FF for Run
-copy_run_flag:
-        .byte   0
-
-delete_skip_decrement_flag:     ; always set to 0 ???
-        .byte   0
-
-op_ref_num:
-        .byte   0
-
-process_depth:
-        .byte   0               ; tracks recursion depth
-
-;;; Number of file entries per directory block
-num_entries_per_block:
-        .byte   13
-
-entries_read:
-        .word   0
-entries_to_skip:
-        .word   0
-
-;;; During directory traversal, the number of file entries processed
-;;; at the current level is pushed here, so that following a descent
-;;; the previous entries can be skipped.
-entry_count_stack:
-        .res    kDirStackBufferSize, 0
-
-entry_count_stack_index:
-        .byte   0
-
-entries_read_this_block:
-        .byte   0
-
-;;; ============================================================
-
-        ;; index is device number (in DEVLST), value is icon number
-device_to_icon_map:
-        .res    kMaxVolumes, 0
-
-;;; Window to file record mapping list. Each entry is a window
-;;; id. Position in the list is the same as position in the
-;;; subsequent file record list.
-window_id_to_filerecord_list_count:
-        .byte   0
-window_id_to_filerecord_list_entries:
-        .res    kMaxNumWindows, 0 ; 8 entries + length
-
-;;; Mapping from position in above table to FileRecord entry
-window_filerecord_table:
-        .res    kMaxNumWindows*2
-
 ;;; ============================================================
 
 ;;; IconTK initialization parameters
@@ -1017,9 +954,6 @@ startup_menu_item_5:    PASCAL_STRING res_string_menu_item_slot_pattern ; menu i
 startup_menu_item_6:    PASCAL_STRING res_string_menu_item_slot_pattern ; menu item
 startup_menu_item_7:    PASCAL_STRING res_string_menu_item_slot_pattern ; menu item
         kStartupMenuItemSlotOffset = res_const_menu_item_slot_pattern_offset1
-
-startup_slot_table:
-        .res    7, 0            ; maps menu item index (0-based) to slot number
 
 ;;; ============================================================
 
@@ -1173,24 +1107,9 @@ str_k_available:
 str_from_int:                   ; populated by IntToString
         PASCAL_STRING "000,000" ; 6 digits plus thousands separator
 
-;;; Computed during startup
-width_items_label_padded:
-        .word   0
-width_left_labels:
-        .word   0
-
 ;;; Computed when painted
         DEFINE_POINT pos_k_in_disk, 0, 0
         DEFINE_POINT pos_k_available, 0, 0
-
-;;; Computed during startup
-width_items_label:      .word   0
-width_k_in_disk_label:  .word   0
-width_k_available_label:        .word   0
-width_right_labels:     .word   0
-
-;;; Assigned during startup
-trash_icon_num:  .byte   0
 
 ;;; Selection drag/drop icon/result, and coords
 .params drag_drop_params
@@ -1230,16 +1149,7 @@ window_to_dir_icon_table:
 num_open_windows:
         .byte   0
 
-;;; --------------------------------------------------
-
-hex_digits:
-        .byte   "0123456789ABCDEF"
-
-;;; High bit set if menu dispatch via keyboard accelerator, clear otherwise.
-menu_kbd_flag:
-        .byte   0
-
-;;; --------------------------------------------------
+;;; ============================================================
 
 checkerboard_pattern:
         .byte   %01010101
@@ -1342,51 +1252,6 @@ icon_entries:
 ;;; ============================================================
 
         .org ::kSegmentDeskTopLC1BAddress
-
-
-;;; Map IconType to other icon/details
-
-icontype_iconentryflags_table:
-        .byte   0                    ; generic
-        .byte   0                    ; text
-        .byte   0                    ; binary
-        .byte   0                    ; graphics
-        .byte   0                    ; music
-        .byte   0                    ; font
-        .byte   0                    ; relocatable
-        .byte   0                    ; command
-        .byte   kIconEntryFlagsDropTarget ; folder
-        .byte   0                    ; iigs
-        .byte   0                    ; appleworks db
-        .byte   0                    ; appleworks wp
-        .byte   0                    ; appleworks sp
-        .byte   0                    ; archive
-        .byte   0                    ; desk accessory
-        .byte   0                    ; basic
-        .byte   0                    ; system
-        .byte   0                    ; application
-        ASSERT_TABLE_SIZE icontype_iconentryflags_table, IconType::COUNT
-
-type_icons_table:               ; map into definitions below
-        .addr   gen ; generic
-        .addr   txt ; text
-        .addr   bin ; binary
-        .addr   fot ; graphics
-        .addr   mus ; music
-        .addr   fnt ; font
-        .addr   rel ; relocatable
-        .addr   cmd ; command
-        .addr   dir ; folder
-        .addr   src ; iigs
-        .addr   adb ; appleworks db
-        .addr   awp ; appleworks wp
-        .addr   asp ; appleworks sp
-        .addr   arc ; archive
-        .addr   a2d ; desk accessory
-        .addr   bas ; basic
-        .addr   sys ; system
-        .addr   app ; application
-        ASSERT_ADDRESS_TABLE_SIZE type_icons_table, IconType::COUNT
 
         DEFINE_ICON_RESOURCE gen, generic_icon, 4, 27, 15, generic_mask
         DEFINE_ICON_RESOURCE src, aux::iigs_file_icon, 4, 27, 15, generic_mask

commit a09dd65c5684d213a28276779d5c84b39d3566b1

DeskTop: Fix Get Info display of file/volume sizes.
Broken in 2cc73d01.

desktop/main.s
@@ -11200,10 +11200,10 @@ append_size:
         bne     :-
 :       stx     buf
 
-        ;; TODO: Compose directly into `path_buf4`.
-        COPY_STRING buf, path_buf4
+        ;; TODO: Compose directly into `path_buf1`.
+        COPY_STRING buf, path_buf1
 
-        copy16  #path_buf4, get_info_dialog_params::a_str
+        copy16  #path_buf1, get_info_dialog_params::a_str
         jsr     RunGetInfoDialogProc
 
         ;; --------------------------------------------------