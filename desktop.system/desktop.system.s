a2stuff/a2d

commit e93f03f9f9d3cb18a307bd48b46bf75141897b0c

Resolve a couple of TODOs: eschew hardcoded entries-per-block value
Init "entries per block" from the directory header when doing a
recursive copy, rather than hardcoding it.

desktop.system/desktop.system.s
@@ -504,7 +504,7 @@ close:  MLI_CALL CLOSE, close_dstfile_params
 ;;; ============================================================
 
 recursion_depth:        .byte   0 ; How far down the directory structure are we
-entries_per_block:      .byte   13 ; TODO: Read this from directory header
+entries_per_block:      .byte   0
 ref_num:                .byte   0
 entry_index_in_dir:     .word   0
 target_index:           .word   0
@@ -569,6 +569,8 @@ entry_index_in_block:   .byte   0
         ;; Header size is next/prev blocks + a file entry
         .assert .sizeof(SubdirectoryHeader) = .sizeof(FileEntry) + 4, error, "incorrect struct size"
 :       jsr     ReadFileEntry   ; read the rest of the header
+
+        copy    filename + SubdirectoryHeader::entries_per_block - 4, entries_per_block
 
         rts
 .endproc

selector/ovl_file_copy.s
@@ -96,7 +96,7 @@ LA1F6:  .res    16, 0
 ;;; ============================================================
 
 recursion_depth:        .byte   0 ; How far down the directory structure are we
-entries_per_block:      .byte   13 ; TODO: Read this from directory header
+entries_per_block:      .byte   0
 ref_num:                .byte   0
 entry_index_in_dir:     .word   0
 target_index:           .word   0
@@ -155,6 +155,9 @@ l1:     lda     open_params::ref_num
         jmp     HandleErrorCode
 
 l2:     jsr     ReadFileEntry
+
+        copy    buf_dir_header + SubdirectoryHeader::entries_per_block - 4, entries_per_block
+
         rts
 .endproc
 

commit 390c843549aea6ea1319a77f4e8b9089808a8da4

Launcher & Selector: Fix initial recursive copy of directories
Broken in e93f03f9 which tried to correctly use the dir header's count
of entries-per-block... but if it was not initialized to non-zero the
subsequent reads would be offset incorrectly.

desktop.system/desktop.system.s
@@ -567,7 +567,8 @@ entry_index_in_block:   .byte   0
 
         ;; Header size is next/prev blocks + a file entry
         .assert .sizeof(SubdirectoryHeader) = .sizeof(FileEntry) + 4, error, "incorrect struct size"
-:       jsr     ReadFileEntry   ; read the rest of the header
+:       copy    #13, entries_per_block ; so ReadFileEntry doesn't immediately advance
+        jsr     ReadFileEntry          ; read the rest of the header
 
         copy    file_entry-4 + SubdirectoryHeader::entries_per_block, entries_per_block
 

selector/ovl_file_copy.s
@@ -157,7 +157,8 @@ l1:     lda     open_params::ref_num
         beq     l2
         jmp     HandleErrorCode
 
-l2:     jsr     ReadFileEntry
+l2:     copy    #13, entries_per_block ; so ReadFileEntry doesn't immediately advance
+        jsr     ReadFileEntry          ; read the rest of the header
 
         copy    file_entry-4 + SubdirectoryHeader::entries_per_block, entries_per_block
 