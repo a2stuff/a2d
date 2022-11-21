a2stuff/a2d

commit 66decf8048653ffacb0ea53ea2f9e85d62472a67

DeskTop: Make initial window viewport/size more accurate to icon bounds
Before the icon bounding box was computed accurately, initial window
sizes and icon positions were set up so that the icons on the left
would have sufficient padding even if they had very long names and not
be cut off. For most windows this would appear as extra space.

Since we now have accurate bounding box calculations, use the actual
bounding box width to guide the window width, and adjust the viewport
to match the bounding box left edge (with padding) to eliminate this
extra spacing. This allows most windows to lose some extra padding on
both sides, while still not clipping icon names.

desktop/main.s
@@ -7334,12 +7334,8 @@ CreateIconsForWindow := CreateIconsForWindowImpl::Start
         ;; convert right/bottom to width/height
         bbox_dx := iconbb_rect+MGTK::Rect::x2
         bbox_dy := iconbb_rect+MGTK::Rect::y2
-
-        ;; Include offset on left/top
-        ldy     #MGTK::Winfo::port + MGTK::GrafPort::viewloc + MGTK::Point::xcoord
-        sub16in bbox_dx, (winfo_ptr),y, bbox_dx
-        ldy     #MGTK::Winfo::port + MGTK::GrafPort::viewloc + MGTK::Point::ycoord
-        sub16in bbox_dy, (winfo_ptr),y, bbox_dy
+        sub16   bbox_dx, iconbb_rect+MGTK::Rect::x1, bbox_dx
+        sub16   bbox_dy, iconbb_rect+MGTK::Rect::y1, bbox_dy
 
         ;; --------------------------------------------------
         ;; Width
@@ -7368,6 +7364,20 @@ assign_width:
         txa
         iny
         sta     (winfo_ptr),y
+
+        ;; Adjust view bounds of new window so it matches icon bounding box.
+        ;; (Only done for width because height is treated as fixed.)
+        lda     cached_window_entry_count
+    IF_NE
+        jsr     CachedIconsScreenToWindow
+        ldy     #MGTK::Winfo::port + MGTK::GrafPort::viewloc + MGTK::Point::xcoord
+        sub16in iconbb_rect+MGTK::Rect::x1, (winfo_ptr),y, tmpw
+        ldy     #MGTK::Winfo::port + MGTK::GrafPort::maprect + MGTK::Rect::x1
+        add16in (winfo_ptr),y, tmpw, (winfo_ptr),y
+        ldy     #MGTK::Winfo::port + MGTK::GrafPort::maprect + MGTK::Rect::x2
+        add16in (winfo_ptr),y, tmpw, (winfo_ptr),y
+        jsr     CachedIconsWindowToScreen
+    END_IF
 
         ;; --------------------------------------------------
         ;; Height
@@ -7400,6 +7410,8 @@ assign_height:
         ;; Finished
         jsr     PopPointers     ; do not tail-call optimise!
         rts
+
+tmpw:   .word   0
 .endproc
 
 

desktop/internal.inc
@@ -88,7 +88,7 @@ kIconLabelGap           = 2
 kMaxIconTotalHeight     = kMaxIconBitmapHeight + kIconLabelGap + kIconLabelHeight
 kMaxIconTotalWidth      = 115   ; width of " MMMMMMMMMMMMMMM "
 
-kIconViewInitialLeft = kIconBBoxPaddingLeft + (kMaxIconTotalWidth - kIconBitmapWidth) / 2
+kIconViewInitialLeft = kIconBBoxPaddingLeft
 kIconViewInitialTop  = kWindowHeaderHeight + kIconBBoxPaddingTop + kMaxIconBitmapHeight
 kIconViewIconsPerRow = 5
 

commit 6b0fed2cbd056150390e6db20ca68d1a68e891e4

DeskTop: Fix initial window height when not min/max out
Regressed in 66decf80. While the horizontal sizing code was changed to
be smart and use the boundng box, the vertical sizing code was not.
But the bbox initialization was changed for both.

Revert the vertical bbox initialization. This asymmetry makes the code
a little more difficult to understand, but it's the simplest fix.

desktop/main.s
@@ -7335,7 +7335,9 @@ CreateIconsForWindow := CreateIconsForWindowImpl::Start
         bbox_dx := iconbb_rect+MGTK::Rect::x2
         bbox_dy := iconbb_rect+MGTK::Rect::y2
         sub16   bbox_dx, iconbb_rect+MGTK::Rect::x1, bbox_dx
-        sub16   bbox_dy, iconbb_rect+MGTK::Rect::y1, bbox_dy
+        ldy     #MGTK::Winfo::port + MGTK::GrafPort::viewloc + MGTK::Point::ycoord
+        sub16in bbox_dy, (winfo_ptr),y, bbox_dy
+
 
         ;; --------------------------------------------------
         ;; Width