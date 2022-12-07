08e4c2771f91104bcc963a66f919c9f1af60ef72
apple2geek <54514423+apple2geek@users.noreply.github.com>
2022-12-06 19:11:37 -0700

    Align version number string reported by Selector and Desktop (#733)
    * Align version number string reported by Selector and Desktop
    
    * Align version number string reported by Disk Copy as well

--- disk_copy/auxlc.s
+++ disk_copy/auxlc.s
@@ -124,7 +124,7 @@ label_facilities:
         PASCAL_STRING res_string_menu_bar_item_facilities ; menu bar item
 
 label_desktop:
-        PASCAL_STRING .sprintf(res_string_version_format_short, kDeskTopProductName, ::kDeskTopVersionMajor, ::kDeskTopVersionMinor) ; menu item
+        PASCAL_STRING .sprintf(res_string_version_format_long, kDeskTopProductName, ::kDeskTopVersionMajor, ::kDeskTopVersionMinor, kDeskTopVersionSuffix)
 
 label_blank:
         PASCAL_STRING " "
