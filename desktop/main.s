a2stuff/a2d

commit c31240de61252e621f2f63a1e878bda394aaee85

DeskTop: Fix some edge cases with renaming icons
* If renaming an icon should activate or deactivate scrollbars, make
  sure that happens.

* If renaming an icon should rename a window too, make sure that
  happens even if the icon is in a View > by Name window.

desktop/main.s
@@ -2942,10 +2942,13 @@ CmdLock         := DoLock
         ;; If selection is in a window with View > by Name, refresh
         jsr     GetSelectionViewBy
         cmp     #kViewByName
-        bne     :+
-        txa
-        jmp     ViewByCommon::entry2
-:
+    IF_EQ
+        txa                     ; X = window id
+        jsr     ViewByCommon::entry2
+    ELSE
+        ;; Scrollbars may need adjusting
+        jsr     ScrollUpdate
+    END_IF
 
         bit     result
         bpl     :+              ; N = window renamed
@@ -15008,7 +15011,7 @@ done:   rts
 .endproc
 
 ;;; ============================================================
-;;; Outputs: A = kViewBy* value for active window
+;;; Outputs: A = kViewBy* value for active window, X = window id
 ;;; If kViewByIcon, Z=1 and N=0; otherwise Z=0 and N=1
 
 ;;; Assert: There is an active window

commit ef042630ef1109ab85155230e914a5664e771a7e

DeskTop: Fix scrollbar glitch when a volume is renamed
Regressed in c31240de - when an icon is renamed, if the window is View
> By Name, it must be repopulated. Otherwise, scrollbar may need
adjusting if the name changed size. But if the selection isn't in a
window (e.g. there is no window) we'd better not try to adjust the
scrollbar!

desktop/main.s
@@ -3002,14 +3002,17 @@ CmdLock         := DoLock
         pha                     ; A = result
 
         ;; If selection is in a window with View > by Name, refresh
+        lda     selected_window_id
+    IF_NE
         jsr     GetSelectionViewBy
         cmp     #kViewByName
-    IF_EQ
+      IF_EQ
         txa                     ; X = window id
         jsr     ViewByCommon::entry2
-    ELSE
+      ELSE
         ;; Scrollbars may need adjusting
         jsr     ScrollUpdate
+      END_IF
     END_IF
 
         pla                     ; A = result