a2stuff/a2d

commit 28fc777bda2074afb430ea128425a34d8b3d40fc

DeskTop: Keep mouse responsive during I/O operations
Since ProDOS MLI calls (which are mostly I/O bound) do bounce through
a relay already, slip in a call to MGTK's CheckEvents which allows the
mouse to move during e.g. file copies.

This might need tweaking if performance is impacted in a couple of
scenarios, e.g. GET_TIME or READ where the data is already buffered.
Doesn't seem to be a problem yet, though.

desktop/main.s
@@ -14137,6 +14137,10 @@ cursor_ibeam_flag:          ; high bit set if I-beam, clear if pointer
 .proc MLIRelayImpl
         params_src := $7E
 
+        ;; Since this is likely to be I/O bound, process events
+        ;; so the mouse stays responsive.
+        MGTK_CALL MGTK::CheckEvents
+
         ;; Adjust return address on stack, compute
         ;; original params address.
         pla

commit bfcf8555f2de1d231560aa838a778fdcc4c7012f

DeskTop: Fix crash on start introduced in 28fc777b
MGTK's CheckEvents was being called before StartDeskTop, which caused
a crash. At least for me, starting DeskTop from Selector after being
copied to RAMDisk was a reliable repro, but it probably has other
triggers.

Make the CheckEvents call conditional, only after StartDeskTop is
invoked and disable it once we're shutting down.

desktop/init.s
@@ -287,6 +287,8 @@ done:
         sta     SETTINGS+DeskTopSettings::options
     END_IF
 
+        copy    #$80, main::mli_relay_checkevents_flag
+
         ;; --------------------------------------------------
         ;; Cursor tracking
 

desktop/main.s
@@ -2685,6 +2685,8 @@ ResetHandler    := CmdQuitImpl::ResetHandler
 ;;; Returns with ALTZPOFF and ROM banked in.
 
 .proc RestoreSystem
+        copy    #0, main::mli_relay_checkevents_flag
+
         jsr     SaveWindows
 
         ;; Switch back to main ZP/LC, preserving return address.
@@ -14141,13 +14143,18 @@ cursor_ibeam_flag:          ; high bit set if I-beam, clear if pointer
 
 ;;; ============================================================
 
+mli_relay_checkevents_flag:
+        .byte   0
+
 .proc MLIRelayImpl
         params_src := $7E
 
         ;; Since this is likely to be I/O bound, process events
         ;; so the mouse stays responsive.
+        bit     mli_relay_checkevents_flag
+        bpl     :+
         MGTK_CALL MGTK::CheckEvents
-
+:
         ;; Adjust return address on stack, compute
         ;; original params address.
         pla