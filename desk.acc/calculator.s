a2stuff/a2d

commit 0fb5718c5e3f7be39f85d6d668ffa056d1ea55b3

Desk Accessories: Make DeskTop handle Aux/Main segments
Previously, DAs were loaded into $800-$1C00 Main, and if they needed
to do anything with MGTK they needed to copy the data into Aux. Most
DAs were lazy and simply cloned themselves into Aux, which also made
bank switching very convenient - after a `STA RAMRDxxx` the other copy
of the code is right there! But this limited the overall size of the
DA to $1400 bytes.

Introduce a new file format for DAs (now auxtype $642) that has a
4-byte header, indicating the size of the Aux and Main segments. The
Aux segment comes first, and is loaded into aux by DeskTop, then the
Main segment is loaded. As before, execution starts at $800 Main.

If the DA doesn't need anything in Aux, then just the header addition
is needed (although it's populatedfor you by macros - see
desk.acc/API.md). And a DA could be written exactly as before to
manually clone itself.

But more interestingly, now DAs can separate code as appropriate. For
example, the Find Files DA has the UI code and resources in Aux, and
the directory traversal code in Main. This was already the case, but
the DA had to manage that before.

This does mean that transferring control between Main is no longer as
simple as hitting softswitches; so new macros JSR_TO_MAIN and
JSR_TO_AUX are introduced that will let you call subroutines in the
other bank. Data moves (e.g. file data loaded in Main, events from
Aux) can be handled with `AUXMOVE`.

A new tool `bin/audit_das` will dump the Main/Aux segment sizes for
all DAs and show the % of the total segment size being used.

The "This Apple" DA gets more model images/names back as easter eggs.

desk.acc/calculator.s
@@ -18,60 +18,20 @@
 
 ;;; ============================================================
 
-        .org DA_LOAD_ADDRESS
-
-;;; ============================================================
-;;; Start of the code
-
-start:  jmp     Copy2Aux
-
-save_stack:  .byte   0
-
-;;; ============================================================
-;;; Duplicate the DA (code and data) to AUX memory,
-;;; then invoke the code in AUX.
-
-.proc Copy2Aux
-        lda     SETTINGS + DeskTopSettings::intl_deci_sep
-        sta     decimal_label
-        sta     decimal_lookup
-
-        tsx
-        stx     save_stack
-
-        ;; Copy the DA to AUX memory.
-        copy16  #start, STARTLO
-        copy16  #da_end, ENDLO
-        copy16  #start, DESTINATIONLO
-        sec                     ; main>aux
-        jsr     AUXMOVE
-
-        FALL_THROUGH_TO InitDA
-.endproc
+        DA_HEADER
+        DA_START_AUX_SEGMENT
 
 ;;; ============================================================
 
 .proc InitDA
-        ;; Run DA from Aux
-        sta     RAMRDON
-        sta     RAMWRTON
-
         ;; Mostly use ZP preservation mode, since we use ROM FP routines.
         MGTK_CALL MGTK::SetZP1, setzp_params_preserve
-
         jmp     Init
 .endproc
 
 
 .proc ExitDA
         MGTK_CALL MGTK::SetZP1, setzp_params_nopreserve
-
-        ;; Return to DeskTop running in Main
-        sta     RAMRDOFF
-        sta     RAMWRTOFF
-
-        ldx     save_stack
-        txs
         rts
 .endproc
 
@@ -584,7 +544,7 @@ loop:   lda     chrget_routine-1,x
 ;;; Input Loop
 
 .proc InputLoop
-        param_call JTRelay, JUMP_TABLE_YIELD_LOOP
+        JSR_TO_MAIN JUMP_TABLE_YIELD_LOOP
         MGTK_CALL MGTK::GetEvent, event_params
         lda     event_params::kind
         cmp     #MGTK::EventKind::button_down
@@ -596,21 +556,6 @@ loop:   lda     chrget_routine-1,x
         bne     InputLoop
         jsr     OnKeyPress
         jmp     InputLoop
-.endproc
-
-;;; ============================================================
-;;; Make call into Main from Aux (for JUMP_TABLE calls)
-;;; Inputs: A,X = address
-
-.proc JTRelay
-        sta     RAMRDOFF
-        sta     RAMWRTOFF
-        stax    @addr
-        @addr := *+1
-        jsr     SELF_MODIFIED
-        sta     RAMRDON
-        sta     RAMWRTON
-        rts
 .endproc
 
 ;;; ============================================================
@@ -642,8 +587,10 @@ ignore_click:
         lda     trackgoaway_params::goaway
         beq     ignore_click
 
-exit:   MGTK_CALL MGTK::CloseWindow, closewindow_params
-        param_call JTRelay, JUMP_TABLE_CLEAR_UPDATES
+exit:   pla                     ; pop OnClick / OnKeyPress
+        pla
+        MGTK_CALL MGTK::CloseWindow, closewindow_params
+        JSR_TO_MAIN JUMP_TABLE_CLEAR_UPDATES
         jmp     ExitDA
 
 :       cmp     #MGTK::Area::dragbar ; Title bar?
@@ -651,7 +598,7 @@ exit:   MGTK_CALL MGTK::CloseWindow, closewindow_params
         lda     #kDAWindowId
         sta     dragwindow_params::window_id
         MGTK_CALL MGTK::DragWindow, dragwindow_params
-        param_call JTRelay, JUMP_TABLE_CLEAR_UPDATES
+        JSR_TO_MAIN JUMP_TABLE_CLEAR_UPDATES
         jmp     DrawContent
 .endproc
 exit := OnClick::exit
@@ -1476,4 +1423,15 @@ END_PROC_AT
 
 ;;; ============================================================
 
-da_end := *
+        DA_END_AUX_SEGMENT
+
+;;; ============================================================
+
+        DA_START_MAIN_SEGMENT
+
+        JSR_TO_AUX InitDA
+        rts
+
+        DA_END_MAIN_SEGMENT
+
+;;; ============================================================

bin/audit_das
@@ -1,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Does a build, then shows the sizes of the Main and Aux segments
+# for all of the desk accessories.
+
+max_seg_size=$(( 0x1C00 - 0x800 ))
+
+make
+printf "                       Main    Aux  Main%%  Aux%% \n"
+for file in $(cat desk.acc/TARGETS | bin/targets.pl targets | sort); do
+    aux_lo=$(xxd -p -l1 -s0 "out/${file}.da")
+    aux_hi=$(xxd -p -l1 -s1 "out/${file}.da")
+    aux=$(( 0x${aux_hi}${aux_lo} ))
+    main_lo=$(xxd -p -l1 -s2 "out/${file}.da")
+    main_hi=$(xxd -p -l1 -s3 "out/${file}.da")
+    main=$(( 0x${main_hi}${main_lo} ))
+
+    aux_p=$(( $aux * 100 / $max_seg_size ))
+    main_p=$(( $main * 100 / $max_seg_size ))
+
+    printf "%-20s  $%04X  $%04X   %3d%%  %3d%%\n" $file $main $aux $main_p $aux_p
+done

bin/manifest
@@ -15,7 +15,7 @@
 #     src_file    = e.g. "out/calculator.da"
 #     folder      = e.g. "apple.menu"
 #     dst_file    = e.g. "calculator"
-#     suffix      = e.g. "F10641"
+#     suffix      = e.g. "F10642"
 
 set -e
 source "bin/util.sh"
@@ -119,7 +119,7 @@ for line in $(cat desk.acc/TARGETS | bin/targets.pl); do
     path="${array[1]}"
     ploc=$(get_da_loc_path "$path")
     disposition="${array[2]}"
-    emit add_file $disposition "out/$file.da" "$ploc" "$loc" F10641
+    emit add_file $disposition "out/$file.da" "$ploc" "$loc" F10642
 done
 
 # --------------------------------------------------

desk.acc/analog.clock.s
@@ -18,43 +18,10 @@
 
 ;;; ============================================================
 
-        .org DA_LOAD_ADDRESS
-
-da_start:
-        jmp     Start
-
-save_stack:.byte   0
-
-.proc Start
-        tsx
-        stx     save_stack
-
-        ;; Copy DA to AUX
-        copy16  #da_start, STARTLO
-        copy16  #da_start, DESTINATIONLO
-        copy16  #da_end, ENDLO
-        sec                     ; main>aux
-        jsr     AUXMOVE
-
-        ;; Transfer control to aux
-        sta     RAMWRTON
-        sta     RAMRDON
-
-        ;; run the DA
-        jsr     Init
-
-        ;; tear down/exit
-        sta     RAMRDOFF
-        sta     RAMWRTOFF
-
-        ldx     save_stack
-        txs
-
-        rts
-.endproc
+        DA_HEADER
+        DA_START_AUX_SEGMENT
 
 ;;; ============================================================
-;;; Main Memory Relay
 
 ;;; Copy of DATELO/HI and TIMELO/HI
 datetime:
@@ -62,18 +29,13 @@ datetime:
 
 ;;; Assert: Called from Aux
 .proc GetDateTime
-        ;; Back to Main, temporarily
-        sta     RAMRDOFF
-        sta     RAMWRTOFF
+        JSR_TO_MAIN DoMLIGetTime
 
-        ;; Poke ProDOS to get the latest from clock driver
-        JUMP_TABLE_MLI_CALL GET_TIME, 0
-
-        ;; Copy from ProDOS global page to Aux
-        sta     RAMWRTON
-        COPY_BYTES 4, DATELO, datetime
-        sta     RAMRDON
-        rts
+        copy16  #DATELO, STARTLO
+        copy16  #DATELO+.sizeof(DateTime)-1, ENDLO
+        copy16  #datetime, DESTINATIONLO
+        sec                     ; main>aux
+        jmp     AUXMOVE
 .endproc
 
 ;;; ============================================================
@@ -135,11 +97,7 @@ exit:
         MGTK_CALL MGTK::RedrawDeskTop
 
         MGTK_CALL MGTK::DrawMenu
-        sta     RAMWRTOFF
-        sta     RAMRDOFF
-        jsr     JUMP_TABLE_HILITE_MENU
-        sta     RAMWRTON
-        sta     RAMRDON
+        JSR_TO_MAIN JUMP_TABLE_HILITE_MENU
 
         MGTK_CALL MGTK::ShowCursor
         rts                     ; exits input loop
@@ -286,4 +244,20 @@ min_offset:
 
 ;;; ============================================================
 
-da_end:
+        DA_END_AUX_SEGMENT
+
+;;; ============================================================
+
+        DA_START_MAIN_SEGMENT
+
+        JSR_TO_AUX Init
+        rts
+
+.proc DoMLIGetTime
+        JUMP_TABLE_MLI_CALL GET_TIME
+        rts
+.endproc
+
+        DA_END_MAIN_SEGMENT
+
+;;; ============================================================

desk.acc/calendar.s
@@ -22,42 +22,8 @@
 
 ;;; ============================================================
 
-        .org DA_LOAD_ADDRESS
-
-da_start:
-
-.scope
-;;; Stuff that we need to run from main
-        lda     DATELO
-        ora     DATEHI
-    IF_NOT_ZERO
-        copy16  #datetime, $A   ; populate this struct
-        ldax    #DATELO         ; use current date
-        jsr     ParseDatetime
-    END_IF
-.endscope
-
-;;; Copy the DA to AUX for easy bank switching
-.scope
-        copy16  #da_start, STARTLO
-        copy16  #da_end, ENDLO
-        copy16  #da_start, DESTINATIONLO
-        sec                     ; main>aux
-        jsr     AUXMOVE
-.endscope
-
-.scope
-        ;; run the DA
-        sta     RAMRDON         ; Run from Aux
-        sta     RAMWRTON
-        jsr     Init
-
-        ;; tear down/exit
-        sta     RAMRDOFF        ; Back to Main
-        sta     RAMWRTOFF
-
-        rts
-.endscope
+        DA_HEADER
+        DA_START_AUX_SEGMENT
 
 ;;; ============================================================
 
@@ -237,6 +203,15 @@ grafport:       .tag    MGTK::GrafPort
 ;;; ============================================================
 ;;; Common Resources
 
+;;; Copied from ProDOS
+.params auxdt
+DATELO: .byte   0
+DATEHI: .byte   0
+TIMELO: .byte   0
+TIMEHI: .byte   0
+.endparams
+
+;;; Parsed
 .params datetime
 year:   .word   kBuildYYYY
 month:  .byte   kBuildMM
@@ -249,6 +224,22 @@ minute: .byte   0
 ;;; ============================================================
 
 .proc Init
+        ;; Grab current ProDOS date/time
+        copy16  #DATELO, STARTLO
+        copy16  #DATELO+.sizeof(DateTime)-1, ENDLO
+        copy16  #auxdt, DESTINATIONLO
+        sec                     ; main>aux
+        jsr     AUXMOVE
+
+        ;; If it is valid, parse it
+        lda     auxdt::DATELO
+        ora     auxdt::DATEHI
+    IF_NOT_ZERO
+        copy16  #datetime, $A   ; populate this struct
+        ldax    #auxdt          ; use current date
+        jsr     ParseDatetime
+    END_IF
+
         MGTK_CALL MGTK::OpenWindow, winfo
         jsr     DrawWindow
         MGTK_CALL MGTK::FlushEvents
@@ -256,7 +247,7 @@ minute: .byte   0
 .endproc
 
 .proc InputLoop
-        param_call JTRelay, JUMP_TABLE_YIELD_LOOP
+        JSR_TO_MAIN JUMP_TABLE_YIELD_LOOP
         MGTK_CALL MGTK::GetEvent, event_params
         lda     event_params::kind
         cmp     #MGTK::EventKind::button_down
@@ -269,7 +260,8 @@ minute: .byte   0
 
 .proc Exit
         MGTK_CALL MGTK::CloseWindow, winfo
-        param_jump JTRelay, JUMP_TABLE_CLEAR_UPDATES
+        JSR_TO_MAIN JUMP_TABLE_CLEAR_UPDATES
+        rts
 .endproc
 
 ;;; ============================================================
@@ -394,7 +386,7 @@ common: bit     dragwindow_params::moved
         bpl     :+
 
         ;; Draw DeskTop's windows and icons.
-        param_call JTRelay, JUMP_TABLE_CLEAR_UPDATES
+        JSR_TO_MAIN JUMP_TABLE_CLEAR_UPDATES
 
         ;; Draw DA's window
         jsr     DrawWindow
@@ -736,21 +728,6 @@ tmp:    .word   0
 .endproc
 
 ;;; ============================================================
-;;; Make call into Main from Aux (for JUMP_TABLE calls)
-;;; Inputs: A,X = address
-
-.proc JTRelay
-        sta     RAMRDOFF
-        sta     RAMWRTOFF
-        stax    @addr
-        @addr := *+1
-        jsr     SELF_MODIFIED
-        sta     RAMRDON
-        sta     RAMWRTON
-        rts
-.endproc
-
-;;; ============================================================
 
         .include "../lib/drawstring.s"
         .include "../lib/measurestring.s"
@@ -760,7 +737,15 @@ tmp:    .word   0
 
 ;;; ============================================================
 
-da_end  := *
-.assert * < DA_IO_BUFFER, error, .sprintf("DA too big (at $%X)", *)
+        DA_END_AUX_SEGMENT
+
+;;; ============================================================
+
+        DA_START_MAIN_SEGMENT
+
+        JSR_TO_AUX Init
+        rts
+
+        DA_END_MAIN_SEGMENT
 
 ;;; ============================================================

commit cc2828d15aa1e4bb59cdfe9008a135372c187a7c

Calculator: Show correct glyph for decimal. Fixes #745
Broken by 0fb5718c, which deleted some necessary code.

desk.acc/calculator.s
@@ -486,6 +486,8 @@ intl_deci_sep:  .byte   0
         ldx     #DeskTopSettings::intl_deci_sep
         jsr     ReadSetting
         sta     intl_deci_sep
+        sta     decimal_label
+        sta     decimal_lookup
 
         MGTK_CALL MGTK::OpenWindow, winfo
         MGTK_CALL MGTK::InitPort, grafport