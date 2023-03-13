a2stuff/a2d

commit bd549bb670f196f216f76d178caac09f89889af5

DeskTop: Make Lock/Unlock work on folders
Also, fix the access mask being used, which was strange.

desktop/main.s
@@ -12468,14 +12468,19 @@ a_path: .addr   src_path_buf
         jmp     @retry
 
 :       lda     src_file_info_params::storage_type
+        sta     storage_type
         cmp     #ST_VOLUME_DIRECTORY
         beq     is_dir
         cmp     #ST_LINKED_DIRECTORY
         bne     do_lock
 
 is_dir:
-        jmp     ProcessDir
-        ;; Subdirectories and Volume directories are not locked
+        jsr     ProcessDir
+        storage_type := *+1
+        lda     #SELF_MODIFIED_BYTE
+        cmp     #ST_VOLUME_DIRECTORY
+        bne     do_lock
+        rts
 
 do_lock:
         jsr     LockFileCommon
@@ -12498,19 +12503,20 @@ LockProcessDirectoryEntry:
         lda     src_file_info_params::storage_type
         cmp     #ST_VOLUME_DIRECTORY
         beq     ok
-        cmp     #ST_LINKED_DIRECTORY
-        beq     ok
-        bit     unlock_flag
-        bpl     :+
-        lda     #ACCESS_DEFAULT
-        bne     set             ; always
-:       lda     #ACCESS_LOCKED
-set:    sta     src_file_info_params::access
 
-:       jsr     SetSrcFileInfo
+        lda     src_file_info_params::access
+        bit     unlock_flag
+    IF_NS
+        ora     #LOCKED_MASK    ; grant access
+    ELSE
+        and     #AS_BYTE(~LOCKED_MASK) ; revoke access
+    END_IF
+        sta     src_file_info_params::access
+
+retry:  jsr     SetSrcFileInfo
         beq     ok
         jsr     ShowErrorAlert
-        jmp     :-
+        jmp     retry
 
 ok:     jmp     RemoveSrcPathSegment
 

inc/prodos.inc
@@ -139,7 +139,7 @@ PRODOS_SYS_PATH         := $280
 ;;; ============================================================
 
 ACCESS_DEFAULT  = %11000011
-ACCESS_LOCKED   = %00100001
+LOCKED_MASK     = %11000010     ; all 0 = locked, all 1 = unlocked
 
 ;;; ============================================================
 ;;; Storage Types

commit dc17b36e8454588d117fe7d20478b6ca2d883544

DeskTop: Fix Lock/Unlock operations when recursing
Regression in bd549bb6; need to ensure the right file info
is around.

desktop/main.s
@@ -12479,8 +12479,10 @@ is_dir:
         storage_type := *+1
         lda     #SELF_MODIFIED_BYTE
         cmp     #ST_VOLUME_DIRECTORY
-        bne     do_lock
+        bne     :+
         rts
+:       jsr     GetSrcFileInfo
+        FALL_THROUGH_TO do_lock
 
 do_lock:
         jsr     LockFileCommon