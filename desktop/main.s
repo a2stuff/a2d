a2stuff/a2d

commit 3aceaa793c03fd76d36a8da5588671dcdca0575b

DeskTop: Simplify Get Size logic
Always use the src_file_info_params::block_count since we always have
it. Reduce silly branching and a potential extra call.

desktop/main.s
@@ -12527,8 +12527,6 @@ is_dir:
 :       rts
 
 do_sum_file_size:
-        ;; Make subsequent call to `AppendFileEntryToSrcPath` a no-op
-        copy    #0, file_entry_buf + FileEntry::storage_type_name_length
         FALL_THROUGH_TO EnumerationProcessDirectoryEntry
 .endproc ; EnumerationProcessSelectedFile
 
@@ -12539,17 +12537,8 @@ do_sum_file_size:
         ;; If operation is "get size" or "download", add the block count to the sum
         bit     operation_flags
     IF_VS
-        lda     file_entry_buf + FileEntry::storage_type_name_length
-      IF_NOT_ZERO
-        ;; If we have a valid file entry, use its block count
-        add16   op_block_count, file_entry_buf+FileEntry::blocks_used, op_block_count
-      ELSE
-        ;; Otherwise, query the existing path
-        jsr     GetSrcFileInfo
-       IF_ZERO
+        ;; Called with `src_file_info_params` pre-populated
         add16   op_block_count, src_file_info_params::blocks_used, op_block_count
-       END_IF
-      END_IF
     END_IF
 
         inc16   op_file_count

commit 94187afe3e26b5732940ba306634f9321c7d0273

DeskTop: Fix Get Size tallying of folder size
Regressed in 3aceaa79 - the change assumed that the specified file's
info was previously grabbed. This is true, but for folders it was
overwritten by the traversal.

Fix by tallying the folder's size first, before descending.

desktop/main.s
@@ -12534,12 +12534,16 @@ callbacks_for_size_or_count:
         cmp     #ST_LINKED_DIRECTORY
         bne     do_sum_file_size
 
+        ;; For linked directory - tally it now while we have
+        ;; `src_file_info_params` populated.
+        jsr     EnumerationProcessDirectoryEntry
+
 is_dir:
         jsr     ProcessDir
         storage_type := *+1
         lda     #SELF_MODIFIED_BYTE
         cmp     #ST_VOLUME_DIRECTORY
-        bne     do_sum_file_size           ; if a subdirectory
+        bne     :+              ; if a subdirectory
 
         ;; If copying a volume dir to RAMCard, the volume dir
         ;; will not be counted as a file during enumeration but