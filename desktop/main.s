a2stuff/a2d

commit e5981cd862c0d59d478f8dbfbc8d40f44b8f1dd5

DeskTop: No need to zero out list items on remove. Saves 21 bytes.
A few places were zero-ing out cached_window_entry_list entries
when they had been removed, but this is not necessary - the list
has a length, and anything beyond that is ignored.

desktop/main.s
@@ -3108,10 +3108,6 @@ loop:   ldx     #SELF_MODIFIED_BYTE
         copy    #$FF, window_to_dir_icon_table,x ; $FF = dir icon freed
     END_IF
 
-        ;; TODO: Unnecessary - remove this!
-        ldx     index
-        copy    #0, cached_window_entry_list,x
-
         inc     index
         bne     loop
 
@@ -4285,7 +4281,6 @@ loop:   lda     cached_window_entry_list,x
         pha
         lda     cached_window_entry_list,x
         sta     icon_param
-        copy    #0, cached_window_entry_list,x
         ITK_CALL IconTK::EraseIcon, icon_param ; CHECKED (desktop)
         ITK_CALL IconTK::RemoveIcon, icon_param
         lda     icon_param
@@ -9591,9 +9586,6 @@ remove: lda     cached_window_entry_list+1,x
         cpx     cached_window_entry_count
         bne     remove
         dec     cached_window_entry_count
-        ldx     cached_window_entry_count
-        lda     #0
-        sta     cached_window_entry_list,x
         rts
 .endproc ; RemoveIconFromWindow
 

commit 49e2be013c926bec3c915b7c34503ab1a39140cb

DeskTop: Fix bogus duplicate disk error from Check All Drives
Regressed in e5981cd8, which didn't zero out cached_window_entry_list
entries when icons were deleted. This revealed a latent bug in the
code to check for duplicate icon names. The "is there already a volume
icon with this name?" CompareNames check looks at all entries in the
list, but at the point where it is called (1) the list size has been
incremented already but (2) the new icon number has not been emplaced.
When the list entries were zeroed, this would see an icon number of 0
and... well, remarkably not crash. When the list is not zeroed a
deleted icon number is still in the list, and so it would detect a
duplicate.

Just ensure the last (and not yet valid) list entry is not compared.

desktop/main.s
@@ -9505,12 +9505,12 @@ offset:         .word   0
         copy16  #cvi_data_buffer, $08
 
         ldx     cached_window_entry_count
-        dex
+        dex                     ; skip the newly created icon
         stx     index
 
         index := *+1
 loop:   ldx     #SELF_MODIFIED_BYTE
-        lda     cached_window_entry_list,x
+        lda     cached_window_entry_list-1,x
         cmp     trash_icon_num
         beq     next
         jsr     GetIconName
@@ -9525,7 +9525,7 @@ loop:   ldx     #SELF_MODIFIED_BYTE
 
         ;; Doesn't match, try again
 next:   dec     index
-        bpl     loop
+        bne     loop
 
         ;; All done, clean up and report no duplicates.
         lda     #0