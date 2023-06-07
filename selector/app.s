a2stuff/a2d

commit 004bdc393c13cdf17dbee29e3598a20cb0eba8e7

Selector & Disk Copy: Call StopDeskTop when exiting
Same as previous commit for DeskTop itself.

Selector: Also, remove some unneeded bank switching calls - Selector
always runs with ROM and Main ZP/LC banked in.

Disk Copy: Also call StopDeskTop since we QUIT back to ProDOS; DeskTop
will start back up, but for a while MGTK yields the mouse so we should
be good citizens.

selector/app.s
@@ -984,8 +984,6 @@ noop:   rts
 ;;; ============================================================
 
 .proc RunDesktop
-        sta     ALTZPOFF
-        bit     ROMIN2
         jsr     RestoreSystem
 
         MLI_CALL OPEN, open_desktop_params
@@ -1000,6 +998,7 @@ noop:   rts
 ;;; Assert: ROM banked in, ALTZP/LC is OFF
 
 .proc RestoreSystem
+        MGTK_CALL MGTK::StopDeskTop
         jsr     RestoreTextMode
         jsr     ReconnectRAM
         jmp     RestoreDeviceList
@@ -1498,8 +1497,6 @@ common: lda     #winfo::kDialogId
         lda     #<$C000
         sta     @addr
 
-        sta     ALTZPOFF
-        bit     ROMIN2
         jsr     RestoreSystem
 
         @addr := * + 1
@@ -1703,8 +1700,6 @@ check_path:
         ;; --------------------------------------------------
         ;; Invoke
 
-        sta     ALTZPOFF
-        bit     ROMIN2
         jsr     RestoreSystem
 
         jsr     INVOKER

disk_copy/auxlc.s
@@ -2457,6 +2457,13 @@ Alert := alert_dialog::Alert
 
 ;;; ============================================================
 
+.proc StopDeskTop
+        MGTK_CALL MGTK::StopDeskTop
+        rts
+.endproc ; StopDeskTop
+
+;;; ============================================================
+
         .assert * <= $F600, error, "Update memory_bitmap if code extends past $F600"
 .endscope ; auxlc
         auxlc__start := auxlc::start

disk_copy/main.s
@@ -90,6 +90,8 @@ params:  .res    3
         ;; Override in this scope
         MLIEntry := MLI
 
+        jsr     auxlc::CheckEvents
+
         sta     ALTZPOFF
         bit     ROMIN2
 

commit 11728d071194e7186b2d0854976a23528a210baa

Selector: Fix crash auto-running DeskTop w/o SELECTOR.LIST
Regressed in 004bdc39. MGTK's StopDeskTop was called unconditionally,
but in the case where SELECTOR.LIST was missing or empty, MGTK's
StartDeskTop had not be called and boom. Fixed with a flag.

selector/app.s
@@ -185,6 +185,9 @@ savearea:       .addr   $800
 savesize:       .word   $800
 .endparams
 
+desktop_started_flag:
+        .byte   0
+
 .params scalemouse_params
 x_exponent:     .byte   1       ; MGTK default is x 2:1 and y 1:1
 y_exponent:     .byte   0       ; ... doubled on IIc / IIc+
@@ -596,6 +599,7 @@ set_startup_menu_items:
 
         MGTK_CALL MGTK::SetZP1, setzp_params
         MGTK_CALL MGTK::StartDeskTop, startdesktop_params
+        copy    #$80, desktop_started_flag
         jsr     SetRGBMode
         MGTK_CALL MGTK::SetMenu, menu
         jsr     ShowClock
@@ -998,7 +1002,10 @@ noop:   rts
 ;;; Assert: ROM banked in, ALTZP/LC is OFF
 
 .proc RestoreSystem
+        bit     desktop_started_flag
+    IF_NS
         MGTK_CALL MGTK::StopDeskTop
+    END_IF
         jsr     RestoreTextMode
         jsr     ReconnectRAM
         jmp     RestoreDeviceList