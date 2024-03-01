a2stuff/a2d

commit c2975b5a185e71d1810c9dc09e7a76c865119d68

DeskTop: Rearrange overlay locations to simplify management
Previously, DeskTop had these overlays

* Format/Erase at $800 - $1A00
* File Dialog at $5000 - $7000
* Shortcut Picker at $9000 - $A000
* File Copy and Shortcut Edit at $7000 - $7800

This meant DeskTop needed to ensure that any routines used by overlays
were in $4000-$5000, or between $7800 and $9000, or after $A000.
Maintaining that intermediate hole was a pain as we've been steadily
reducing the code size.

The "good news everyone!" is that the file dialog is way, way smaller
now so we can crunch everything. The new scheme is:

* Format/Erase at $800 - $1A00 - unchanged
* Shortcut Picker at $5000 - $6000
* File Dialog at $6000 - $7000
* File Copy and Shortcut Edit at $7000 - $7800

The Shortcut Picker logic moves down so that the File Dialog routine
and the routines that drive the file dialog are adjacent. This is
important, as shortcut operations restore the latter in one go -
previously $5000-$7800, now $6000-$7800.

The only real complexity is that modal Desk Accessories were allowed
to use $5000-$7800 as a 10K buffer, so that's now added as a dedicated
restore chunk.

No functional changes.

desktop/main.s
@@ -193,7 +193,7 @@ ClearUpdates := ClearUpdatesImpl::clear
 ;;; Called by main and nested event loops to do periodic tasks.
 ;;; Returns 0 if the periodic tasks were run.
 
-        .assert * < $5000 || (* >= $7800 && * < $9000) || * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 .proc YieldLoop
         inc     loop_counter
         inc     loop_counter
@@ -673,7 +673,7 @@ done:   rts
 .endproc ; UnsafeSetPortFromWindowId
 
 ;;; Used for windows that can never be obscured (e.g. dialogs)
-        .assert * < $5000 || (* >= $7800 && * < $9000) || * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 .proc SafeSetPortFromWindowId
         sta     getwinport_params::window_id
         MGTK_CALL MGTK::GetWinPort, getwinport_params
@@ -1127,7 +1127,7 @@ CheckBasisSystem        := CheckBasixSystemImpl::basis
 
 ;;; ============================================================
 
-        .assert * < $5000 || (* >= $7800 && * < $9000) || * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 .proc UpcaseChar
         cmp     #'a'
         bcc     done
@@ -1280,7 +1280,7 @@ devlst_backup:
 :
     END_IF
 
-        lda     #kDynamicRoutineSelector1 ; selector picker dialog
+        lda     #kDynamicRoutineShortcutPick
         jsr     LoadDynamicRoutine
         bmi     done
 
@@ -1288,7 +1288,7 @@ devlst_backup:
         cmp     #SelectorAction::delete
         bcs     invoke     ; delete or run (no need for more overlays)
 
-        lda     #kDynamicRoutineSelector2 ; file dialog driver
+        lda     #kDynamicRoutineShortcutEdit
         jsr     LoadDynamicRoutine
         bmi     done
         lda     #kDynamicRoutineFileDialog ; file dialog
@@ -1303,7 +1303,7 @@ invoke:
 
         ;; Restore from overlays
         ;; (restore from file dialog overlay handled in picker overlay)
-        lda     #kDynamicRoutineRestore9000 ; restore from picker dialog
+        lda     #kDynamicRoutineRestoreSP ; restore from picker dialog
         jsr     RestoreDynamicRoutine
 
         bit     result
@@ -1745,7 +1745,7 @@ main_length:    .word   0
         lda     #$00
         jsr     file_dialog__Exec
         pha                     ; A = dialog result
-        lda     #kDynamicRoutineRestore5000
+        lda     #kDynamicRoutineRestoreFD
         jsr     RestoreDynamicRoutine
         jsr     PushPointers    ; $06 = dst
         jsr     ClearUpdates    ; following picker dialog close
@@ -8369,7 +8369,7 @@ min     := parsed_date + ParsedDateTime::minute
 
 ;;; ============================================================
 
-        .assert * < $5000 || (* >= $7800 && * < $9000) || * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 
 ;;; A,X = A * 16
 .proc ATimes16
@@ -9708,41 +9708,42 @@ next:   dec     step
 
 ;;; Routines are:
 ;;;  0 = format/erase disk        - A$ 800,L$1400 call w/ A = 4 = format, A = 5 = erase
-;;;  1 = selector actions (all)   - A$9000,L$1000
-;;;  2 = common file dialog       - A$5000,L$2000
+;;;  1 = shortcut picker          - A$9000,L$1000
+;;;  2 = common file dialog       - A$6000,L$1000
 ;;;  3 = part of copy file        - A$7000,L$ 800
-;;;  4 = selector add/edit        - L$7000,L$ 800
-;;;  5 = restore 1                - A$5000,L$2800 (restore $5000...$77FF)
-;;;  6 = restore 2                - A$9000,L$1000 (restore $9000...$9FFF)
+;;;  4 = shortcut editor          - L$7000,L$ 800
+;;;  5 = restore shortcut picker  - A$5000,L$1000 (restore $5000...$5FFF)
+;;;  6 = restore file dialog      - A$6000,L$1400 (restore $6000...$73FF)
+;;;  7 = restore 10K buffer       - A$5000,L$2800 (restore $5000...$77FF)
 ;;;
 ;;; Routines 1-5 need appropriate "restore routines" applied when complete.
 
 .proc LoadDynamicRoutineImpl
 
-kNumOverlays = 7
+kNumOverlays = 8
 
 pos_table:
         .dword  kOverlayFormatEraseOffset
         .dword  kOverlayShortcutPickOffset, kOverlayFileDialogOffset
         .dword  kOverlayFileCopyOffset
-        .dword  kOverlayShortcutEditOffset, kOverlayDeskTopRestore1Offset
-        .dword  kOverlayDeskTopRestore2Offset
+        .dword  kOverlayShortcutEditOffset, kOverlayDeskTopRestoreSPOffset
+        .dword  kOverlayDeskTopRestoreFDOffset, kOverlayDeskTopRestore10KOffset
         ASSERT_RECORD_TABLE_SIZE pos_table, kNumOverlays, 4
 
 len_table:
         .word   kOverlayFormatEraseLength
         .word   kOverlayShortcutPickLength, kOverlayFileDialogLength
         .word   kOverlayFileCopyLength
-        .word   kOverlayShortcutEditLength, kOverlayDeskTopRestore1Length
-        .word   kOverlayDeskTopRestore2Length
+        .word   kOverlayShortcutEditLength, kOverlayDeskTopRestoreSPLength
+        .word   kOverlayDeskTopRestoreFDLength, kOverlayDeskTopRestore10KLength
         ASSERT_RECORD_TABLE_SIZE len_table, kNumOverlays, 2
 
 addr_table:
         .word   kOverlayFormatEraseAddress
         .word   kOverlayShortcutPickAddress, kOverlayFileDialogAddress
         .word   kOverlayFileCopyAddress
-        .word   kOverlayShortcutEditAddress, kOverlayDeskTopRestore1Address
-        .word   kOverlayDeskTopRestore2Address
+        .word   kOverlayShortcutEditAddress, kOverlayDeskTopRestoreSPAddress
+        .word   kOverlayDeskTopRestoreFDAddress, kOverlayDeskTopRestore10KAddress
         ASSERT_ADDRESS_TABLE_SIZE addr_table, kNumOverlays
 
         DEFINE_OPEN_PARAMS open_params, str_desktop, IO_BUFFER
@@ -14063,7 +14064,7 @@ SaveWindows := save_restore_windows::Save
 ;;;
 ;;; ============================================================
 
-        .assert * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 
 ;;; ============================================================
 
@@ -14118,7 +14119,7 @@ params:  .res    3
 ;;; ============================================================
 
 ;;; Preserves A
-        .assert * < $5000 || (* >= $7800 && * < $9000) || * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 .proc SetCursorWatch
         pha
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::watch
@@ -14126,13 +14127,13 @@ params:  .res    3
         rts
 .endproc ; SetCursorWatch
 
-        .assert * < $5000 || (* >= $7800 && * < $9000) || * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 .proc SetCursorPointer
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::pointer
         rts
 .endproc ; SetCursorPointer
 
-        .assert * < $5000 || (* >= $7800 && * < $9000) || * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 .proc SetCursorIBeam
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::ibeam
         rts
@@ -14142,7 +14143,7 @@ params:  .res    3
 ;;; Double Click Detection
 ;;; Returns with A=0 if double click, A=$FF otherwise.
 
-        .assert * < $5000 || (* >= $7800 && * < $9000) || * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 .proc StashCoordsAndDetectDoubleClick
         ;; Stash coords for double-click in windows
         COPY_STRUCT MGTK::Point, event_params::coords, drag_drop_params::coords
@@ -14608,7 +14609,7 @@ done:   rts
 ;;; Determine if mouse moved (returns w/ carry set if moved)
 ;;; Used in dialogs to possibly change cursor
 
-        .assert * < $5000 || (* >= $7800 && * < $9000) || * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 .proc CheckMouseMoved
         ldx     #.sizeof(MGTK::Point)-1
 :       lda     event_params::coords,x
@@ -14632,7 +14633,7 @@ diff:   COPY_STRUCT MGTK::Point, event_params::coords, coords
 ;;; Test if either modifier (Open-Apple or Solid-Apple) is down.
 ;;; Output: A=high bit/N flag set if either is down.
 
-        .assert * < $5000 || (* >= $7800 && * < $9000) || * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 .proc ModifierDown
         lda     BUTN0
         ora     BUTN1
@@ -14663,7 +14664,7 @@ ret:    rts
 ;;; Test if shift is down (if it can be detected).
 ;;; Output: A=high bit/N flag set if down.
 
-        .assert * < $5000 || (* >= $7800 && * < $9000) || * >= $A000, error, "Routine used by overlays in overlay zone"
+        .assert * < $5000 || * >= $7800, error, "Routine used by overlays in overlay zone"
 .proc ShiftDown
         bit     machine_config::iigs_flag
         bpl     TestShiftMod    ; no, rely on shift key mod

desk.acc/show.duet.file.s
@@ -285,7 +285,7 @@ ret:    rts
         JSR_TO_AUX aux::Init
 
         ;; Page DeskTop's code back in.
-        lda     #kDynamicRoutineRestore5000
+        lda     #kDynamicRoutineRestore10K
         jsr     JUMP_TABLE_RESTORE_OVL
 
         jsr     JUMP_TABLE_CLEAR_UPDATES

desk.acc/sort.directory.s
@@ -98,7 +98,7 @@ start:  tsx
         lda     #kErrNoWindowsOpen
         jmp     JUMP_TABLE_SHOW_ALERT ; NOTE: Trashes AUX $800-$1AFF
 
-:       lda     #kDynamicRoutineRestore5000
+:       lda     #kDynamicRoutineRestore10K
         jsr     JUMP_TABLE_RESTORE_OVL
 
         lda     window_id

desktop/desktop.inc
@@ -28,7 +28,7 @@ kDAMaxSize      = DA_IO_BUFFER - DA_LOAD_ADDRESS
 DEFAULT_FONT    := $8680
 
 ;;; A 10k buffer that can be restored using overlays; load
-;;; `kDynamicRoutineRestore5000` and call `JUMP_TABLE_RESTORE_OVL`
+;;; `kDynamicRoutineRestore10K` and call `JUMP_TABLE_RESTORE_OVL`
 ;;; to restore DeskTop afterwards.
 OVERLAY_10K_BUFFER      := $5000
 kOverlay10KBufferSize   = $2800
@@ -153,12 +153,13 @@ JT_ENTRY JUMP_TABLE_LAST
 ;;; Overlay routines, for JUMP_TABLE_LOAD_OVL
 
 kDynamicRoutineFormatErase      = 0
-kDynamicRoutineSelector1        = 1
+kDynamicRoutineShortcutPick     = 1
 kDynamicRoutineFileDialog       = 2
 kDynamicRoutineFileCopy         = 3
-kDynamicRoutineSelector2        = 4
-kDynamicRoutineRestore5000      = 5
-kDynamicRoutineRestore9000      = 6
+kDynamicRoutineShortcutEdit     = 4
+kDynamicRoutineRestoreSP        = 5
+kDynamicRoutineRestoreFD        = 6
+kDynamicRoutineRestore10K       = 7
 
 ;;; ============================================================
 ;;; Error codes used for alerts which are not ProDOS errors

desktop/desktop.s
@@ -35,20 +35,29 @@
 
         ;; Dynamically loaded overlays
         DEFSEG OverlayFormatErase,  $0800, $1200
-        DEFSEG OverlayShortcutPick, $9000, $0A00
-        DEFSEG OverlayFileDialog,   $5000, $0F00
+        DEFSEG OverlayShortcutPick, $5000, $0A00
+        DEFSEG OverlayFileDialog,   $6000, $0F00
         DEFSEG OverlayFileCopy,     $7000, $0100
         DEFSEG OverlayShortcutEdit, $7000, $0300
 
 ;;; These pseudo-overlays restore DeskTop after overlays are used
 
-kOverlayDeskTopRestore1Length = $2800
-kOverlayDeskTopRestore1Address = $5000
-kOverlayDeskTopRestore1Offset = kSegmentDeskTopMainOffset + (kOverlayDeskTopRestore1Address - kSegmentDeskTopMainAddress)
+;;; Restore after OverlayShortcutPick has been used
+kOverlayDeskTopRestoreSPLength = kOverlayShortcutPickLength
+kOverlayDeskTopRestoreSPAddress = kOverlayShortcutPickAddress
+kOverlayDeskTopRestoreSPOffset = kSegmentDeskTopMainOffset + (kOverlayDeskTopRestoreSPAddress - kSegmentDeskTopMainAddress)
 
-kOverlayDeskTopRestore2Length = $1000
-kOverlayDeskTopRestore2Address = $9000
-kOverlayDeskTopRestore2Offset = kSegmentDeskTopMainOffset + (kOverlayDeskTopRestore2Address - kSegmentDeskTopMainAddress)
+;;; Restore after OverlayFileDialog and either OverlayFileCopy or OverlayShortcutEdit has been used
+kOverlayDeskTopRestoreFDLength = $1400
+kOverlayDeskTopRestoreFDAddress = kOverlayFileDialogAddress
+kOverlayDeskTopRestoreFDOffset = kSegmentDeskTopMainOffset + (kOverlayDeskTopRestoreFDAddress - kSegmentDeskTopMainAddress)
+.assert (kOverlayFileCopyAddress + kOverlayFileCopyLength) <= (kOverlayDeskTopRestoreFDAddress + kOverlayDeskTopRestoreFDLength), error, "restore coverage"
+.assert (kOverlayShortcutEditAddress + kOverlayShortcutEditLength) <= (kOverlayDeskTopRestoreFDAddress + kOverlayDeskTopRestoreFDLength), error, "restore coverage"
+
+;;; Restore after 10K buffer is used by Desk Accessories
+kOverlayDeskTopRestore10KLength = kOverlay10KBufferSize
+kOverlayDeskTopRestore10KAddress = OVERLAY_10K_BUFFER
+kOverlayDeskTopRestore10KOffset = kSegmentDeskTopMainOffset + (kOverlayDeskTopRestore10KAddress - kSegmentDeskTopMainAddress)
 
 ;;; ============================================================
 ;;; DeskTop module

desktop/ovl_selector_pick.s
@@ -61,7 +61,7 @@ L9052:  lda     #$00
         pha
         tya
         pha
-        lda     #kDynamicRoutineRestore5000
+        lda     #kDynamicRoutineRestoreFD
         jsr     main::RestoreDynamicRoutine
         jsr     main::ClearUpdates ; Add File Dialog close
         pla
@@ -214,7 +214,7 @@ l3:     clc
         pha
         tya
         pha
-        lda     #kDynamicRoutineRestore5000
+        lda     #kDynamicRoutineRestoreFD
         jsr     main::RestoreDynamicRoutine
         jsr     main::ClearUpdates ; Edit File Dialog close
         pla
@@ -314,7 +314,7 @@ flags:  .byte   0
         cmp     #SelectorAction::edit
         bne     :+
 
-        lda     #kDynamicRoutineRestore5000
+        lda     #kDynamicRoutineRestoreFD
         jsr     main::RestoreDynamicRoutine
 
 :       jsr     CloseWindow

commit 16df426037e8818d6885f0862db55348157b2ac8

DeskTop: Fix corruption when exiting Shortcuts dialogs
Introduced in c2975b5 (back in March 2023); rearranging the shortcut
overlay from $9000-$A000 down to $5000-$6000 put at least one routine
into the "danger zone" so that when the shortcuts routine (1) closed
the file picker and (2) cleared updates but (3) the overlay was not
restored, the update clearing would call into bad code leading to
corruption or a crash.

Move the routine into a safe zone. Also, from an audit of the routines
invoked by ClearUpdates, drop in several more asserts. This is
definitely not a perfect solution.

Fixes #790.

desktop/main.s
@@ -3003,21 +3003,6 @@ selection_preserved_count:
 .endproc ; FindIconForRecordNum
 
 ;;; ============================================================
-;;; Retrieve the `IconEntry::record_num` for a given icon.
-;;; Input: A = icon id
-;;; Output: A = icon's record index in its window
-;;; Trashes $06
-
-.proc GetIconRecordNum
-        jsr     GetIconEntry
-        ptr := $06
-        stax    ptr
-        ldy     #IconEntry::record_num
-        lda     (ptr),y
-        rts
-.endproc ; GetIconRecordNum
-
-;;; ============================================================
 ;;; Retrieve the window id for a given icon.
 ;;; Input: A = icon id
 ;;; Output: A = window id (0=desktop)
@@ -6172,6 +6157,7 @@ UncheckViewMenuItem := CheckViewMenuItemImpl::uncheck
 ;;; ============================================================
 ;;; Draw all entries (icons or list items) in (cached) window
 
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc DrawWindowEntries
         ;; --------------------------------------------------
         ;; Icons
@@ -6226,6 +6212,22 @@ rloop:  lda     #SELF_MODIFIED_BYTE
 done:
         rts
 .endproc ; DrawWindowEntries
+
+;;; ============================================================
+;;; Retrieve the `IconEntry::record_num` for a given icon.
+;;; Input: A = icon id
+;;; Output: A = icon's record index in its window
+;;; Trashes $06
+
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
+.proc GetIconRecordNum
+        jsr     GetIconEntry
+        ptr := $06
+        stax    ptr
+        ldy     #IconEntry::record_num
+        lda     (ptr),y
+        rts
+.endproc ; GetIconRecordNum
 
 ;;; ============================================================
 
@@ -6327,12 +6329,14 @@ skip:
 
 ;;; ============================================================
 
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc CachedIconsScreenToWindow
         param_jump CachedIconsXToY, IconPtrScreenToWindow
 .endproc ; CachedIconsScreenToWindow
 
 ;;; ============================================================
 
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc CachedIconsWindowToScreen
         param_jump CachedIconsXToY, IconPtrWindowToScreen
 .endproc ; CachedIconsWindowToScreen
@@ -6340,6 +6344,7 @@ skip:
 ;;; ============================================================
 
 ;;; Inputs: A,X = proc to call for each icon
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc CachedIconsXToY
         stax    proc
 
@@ -7766,6 +7771,7 @@ flags:  .byte   0
 ;;; ============================================================
 ;;; Draw header (items/K in disk/K available/lines) for active window
 
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc DrawWindowHeader
         ;; --------------------------------------------------
         ;; Separator Lines
@@ -8340,6 +8346,7 @@ CompareFileRecords_sort_by := CompareFileRecords::sort_by
 ;;; ============================================================
 ;;; A = entry number
 
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc DrawListViewRow
 
         ptr := $06
@@ -8396,7 +8403,6 @@ set_pos:
         stax    pos_col::xcoord
         MGTK_CALL MGTK::MoveTo, pos_col
         rts
-.endproc ; DrawListViewRow
 
 ;;; ============================================================
 
@@ -8443,9 +8449,12 @@ set_pos:
         FALL_THROUGH_TO ComposeSizeString
 .endproc ; PrepareColSize
 
+.endproc ; DrawListViewRow
+
 ;;; ============================================================
 ;;; Populate `text_buffer2` with "12,345K"
 
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc ComposeSizeString
         stax    value           ; size in 512-byte blocks
 
@@ -8509,6 +8518,7 @@ value:  .word   0
 
 ;;; ============================================================
 
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc ComposeDateString
         copy    #0, text_buffer2
         copy16  #text_buffer2, $8
@@ -14721,6 +14731,7 @@ done:   rts
 ;;; Inputs: A = window id
 ;;; Outputs: Z = 1 if found, and X = index in `window_id_to_filerecord_list_entries`
 
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc FindIndexInFilerecordListEntries
         ldx     window_id_to_filerecord_list_count
         dex
@@ -14734,6 +14745,7 @@ done:   rts
 ;;; Input: A = window_id
 ;;; Output: A,X = address of FileRecord list (first entry is length)
 ;;; Assert: Window is found in list.
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc GetFileRecordListForWindow
         jsr     FindIndexInFilerecordListEntries
         txa
@@ -14759,6 +14771,7 @@ done:   rts
 .endproc ; GetActiveWindowViewBy
 
 ;;; Assert: There is a cached window
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc GetCachedWindowViewBy
         ldx     cached_window_id
         lda     win_view_by_table-1,x
@@ -14856,6 +14869,7 @@ ret:    rts
 ;;; ============================================================
 
 
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 ;;; Input: A = window_id (0=desktop)
 .proc LoadWindowEntryTable
         sta     cached_window_id
@@ -14978,6 +14992,7 @@ window_entry_table:             .res    ::kMaxIconCount+1, 0
 ;;; logic with 127 icons. A simpler fix may be possible, see commit
 ;;; 41ebde49 for another attempt, but that introduces other issues.
 
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 .proc LoadActiveWindowEntryTable
         lda     active_window_id
         jmp     LoadWindowEntryTable
@@ -15212,6 +15227,7 @@ desktop_icon_usage_table:
 
 ;;; ============================================================
 
+        .assert * < $5000 || * >= $6000, error, "Routine used when clearing updates in overlay zone"
 ;;; FileRecord for list view
 list_view_filerecord:
         .tag FileRecord