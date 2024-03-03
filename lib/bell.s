38ed35ce2ebd00c4bcfdc3e44d50c465988ecde7
Joshua Bell <inexorabletash@gmail.com>
2024-03-02 16:53:38 -0800

    Bell: Make Silent use the "touch slot 6 to slow accelerator" trick
    Previously this hardcoded a delay and hoped for the best. While this
    is not as good as using interrupts, this should be better at least on
    real hardware.

--- lib/bell.s
+++ lib/bell.s
@@ -18,58 +18,17 @@
     IF_ZERO
         MGTK_CALL MGTK::FlashMenuBar
 
-        ;; Delay using some heuristic.
-
-        ;; Option #1: Slow accelerator down to 1MHz
-        ;;
-        ;; 1a: Using documented accelerator access for each type of
-        ;;     hardware (IIgs, Mac IIe Card, IIc+, Laser, add-ons, ...)
-        ;;     like NORMFAST does it.
-        ;;
-        ;;   * This now happens by default on some systems, c/o
-        ;;     general beep slowdown. See `lib/sounds.s`
-        ;;   * This doesn't slow emulators.
-        ;;
-        ;; 1b: Touching the speaker quickly (i.e. STA SPKR twice), to
-        ;;      temporarily slow accelerators; this is inaudible on real
-        ;;      hardware.
-        ;;
-        ;;   * This causes Virtual ][ to stop playing sounds for a bit.
-        ;;   * This has an audible click in MAME.
-        ;;
-        ;; 1c: Hit slot 6 (e.g. BIT $C0EC) to temporarily slow accelerator.
-        ;; 1d: Hit PTRIG to temporarily slow accelerator.
-        ;;
-        ;;   * These doesn't slow emulators like Virtual ][.
-
-        ;; Option #2: Wait based on double-click setting.
-        ;;
-        ;; The assumption is that users will adjust the setting based on
-        ;; factors such as acceleration/emulation speed, so use it as
-        ;; the basis of a timing loop.
-        ;;
-        ;;  * Since we slow the machine to 1MHz while playing sound,
-        ;;    this ends up being unacceptably slow.
-
+        ;; Hit Slot 6, which causes accelerators e.g. Zip Chip
+        ;; to slow down.
+        ;; NOTE: $C0E0 causes Virtual ][ emulator to make sound;
+        ;; $C0EC (data read location) does not.
+        bit     $C0EC
         ptr := $06
-        copy16  #kDefaultDblClickSpeed, ptr
-loop:   ldx     #48
-:       dex
-        bne     :-
-        dec     ptr
+        copy16  #20000, ptr
+loop:   dec     ptr
         bne     loop
         dec     ptr+1
         bne     loop
-
-        ;; Option #3: Use VBL on Enh IIe/IIc/IIgs
-        ;; https://comp.sys.apple2.narkive.com/dHkvl39d/vblank-apple-iic
-        ;;
-        ;; * Emulators don't throttle to 60/50Hz, and rather sync VBL to
-        ;;   cycle counts.
-
-        ;; Option #4: Use interrupt timer from mouse card
-        ;;
-        ;; * Interrupts are hard. PRs welcome!
 
         MGTK_CALL MGTK::FlashMenuBar
     ELSE
