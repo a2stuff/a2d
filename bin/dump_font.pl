5d0cf90a2c32c6ca41937a9647b1a070914e486e
Joshua Bell <inexorabletash@gmail.com>
2024-03-12 19:44:07 -0700

    Fonts: Fix stray bits in two font files, improve tooling
    The Message DA was showing glitches following the glyph for '2' in the
    Athens font. Inspection revealed that the font had pixels in the
    bitmap set beyond the character's width; apparently MGTK doesn't
    handle this as expected.
    
    * Add support for two-byte wide fonts to bin/make_font.pl
    * Add validation support for these stray bits to bin/dump_font.pl
    * Update the Athens and Toronto fonts which had stray bits.

--- bin/dump_font.pl
+++ bin/dump_font.pl
@@ -11,6 +11,8 @@ sub getbyte {
 }
 
 my $type = getbyte();
+die "only types 0x00/0x80 supported\n" unless $type == 0x00 || $type == 0x80;
+
 my $last = getbyte();
 my $height = getbyte();
 my $num = $last + 1;
@@ -41,6 +43,16 @@ for (my $row = 0; $row < $height; ++$row) {
         }
     }
     for (my $c = 0; $c < $num; ++$c) {
+        # Validate that no extra bits are set; MGTK will render these
+        # with glitches. The bits will not appear in the output from
+        # this utility, however, so fonts can be "cleaned" by dumping
+        # and re-making the font.
+        my $last = (split(/\n/,$chars[$c]))[-1];
+        if (substr($last, $widths[$c]) =~ m/#.*$/) {
+            warn sprintf("extra bits in char 0x%02x '%c', row %d (of %d)",
+                         $c, $c, $row+1, $height);
+        }
+
         $chars[$c] .= "\n";
     }
 }
@@ -53,5 +65,5 @@ for (my $i = 0; $i < $num; ++$i) {
 }
 
 for (my $i = 0; $i < $num; ++$i) {
-    print "== 0x".sprintf("%02x",$i)." ==\n$chars[$i]\n";
+    printf("== 0x%02x ==\n%s\n", $i, $chars[$i]);
 }
--- bin/make_font.pl
+++ bin/make_font.pl
@@ -11,7 +11,7 @@ sub from2 { unpack("N", pack("B32", substr("0" x 32 . shift, -32))); }
 $_ = <STDIN>; chomp;
 die "expected type (line $.)\n" unless m/^type: (\d+)$/;
 my $type = $1;
-die "only type 0 supported (line $.)\n" unless $type == 0;
+die "only types 0x00/0x80 supported (line $.)\n" unless $type == 0x00 || $type == 0x80;
 
 $_ = <STDIN>; chomp;
 die "expected type (line $.)\n" unless m/^chars: (\d+)$/;
@@ -30,7 +30,7 @@ for (my $c = 0; $c < $chars; ++$c) {
 
     for (my $r = 0; $r < $height; ++$r) {
         $_ = <STDIN>; chomp;
-        die "expected bitmap, saw $_ (line $.)\n" unless m/^[.#]+$/;
+        die "expected bitmap, saw $_ (line $.)\n" unless m/^[.#]*$/;
         my $len = length($_);
         if (defined $widths[$c]) {
             die sprintf("changed width: 0x%02x (line $.)\n", $c) unless $widths[$c] == $len;
@@ -50,6 +50,11 @@ print chr($type), chr($chars-1), chr($height);
 print pack('C*', @widths);
 for (my $r = 0; $r < $height; ++$r) {
     for (my $c = 0; $c < $chars; ++$c) {
-        print chr($chars[$c][$r]);
+        print chr($chars[$c][$r] & 0x7F);
+    }
+    if ($type == 0x80) {
+        for (my $c = 0; $c < $chars; ++$c) {
+            print chr($chars[$c][$r] >> 7);
+        }
     }
 }
