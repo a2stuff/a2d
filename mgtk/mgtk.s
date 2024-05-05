a2stuff/a2d

commit 258ecee4744ac0cac8cde2dfa85aed1b2f75f542

MGTK: Improve window title bar appearance
Rather than filling the entire title bar with stripes and making the
"go away" box full height, add a pixel of spacing above/below. This is
closer to the classic Macintosh and IIgs style. I was skeptical at
first but at least subjectively it does make the the title bars less
visually noisy.

mgtk/mgtk.s
@@ -4696,8 +4696,11 @@ savesize        .word
         tax
         stx     sysfont_height
         dex
-        stx     goaway_height                 ; goaway height = font height - 1
-        inx
+        dex
+        dex
+        stx     goaway_height                 ; goaway height = font height - 3
+
+        tax
         inx
         inx
         stx     hilite_menu_rect_height       ; menu bar height = font height + 2
@@ -7481,14 +7484,14 @@ get_rect:
         inx
 :       stax    winrect::x1
         clc
-        adc     #14
+        adc     #12
         bcc     :+
         inx
 :       stax    winrect::x2
 
         ldax    winrect::y1
         clc
-        adc     #2
+        adc     #3
         bcc     :+
         inx
 :       stax    winrect::y1
@@ -7572,7 +7575,6 @@ stripes_pattern_alt := *+1
         .byte   %11111111
         .byte   %00000000
         .byte   %11111111
-
 
 .proc SetStripesPattern
         jsr     GetWinGoAwayRect
@@ -8706,7 +8708,7 @@ plp_ret:
 .endproc ; EraseWindow
 
 
-goaway_height:  .word   8       ; font height - 1
+goaway_height:  .word   6       ; font height - 3
 wintitle_height:.word  12       ; font height + 3
 winframe_top:   .word  13       ; font height + 4
 

commit 206b19d9271624bb97ca1a03f0ff6040323d7fbf

MGTK: Fix title bar rendering when no "go away" box is present.
This doesn't affect any DeskTop modules or DAs, but the calculation
for drawing the title bar stripes if the window didn't have a "go
away" box was broken by 258ecee.

No functional changes.

mgtk/mgtk.s
@@ -7657,7 +7657,7 @@ no_goaway:
         ;; There was no "go away" box, so further offset left
         tya
         sec
-        sbc     #kGoAwayLeft + kGoAwayWidth + 2 ; TODO: Fix this!
+        sbc     #kGoAwayLeft + kGoAwayWidth
         bcs     :+
         dex
 :       tay