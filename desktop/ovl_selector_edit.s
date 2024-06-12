a2stuff/a2d

commit d01be1d859f1713d50357dfa87208109b422d6d6

DeskTop: Remove some obsolete code from Add/Edit a Shortcut dialog
Remove a set of calls that previously drew the radio buttons. These
should have been removed when BTK was introduced, but only the
label drawing logic was replaced. Ooops!

No functional changes.

desktop/ovl_selector_edit.s
@@ -116,22 +116,6 @@ buffer: .res 16, 0
         BTK_CALL BTK::RadioDraw, at_first_use_params
         BTK_CALL BTK::RadioDraw, never_params
 
-        lda     #kRunListPrimary
-        clc
-        jsr     UpdateRunListButton
-        lda     #kRunListSecondary
-        clc
-        jsr     UpdateRunListButton
-        lda     #kCopyOnBoot
-        clc
-        jsr     DrawCopyWhenButton
-        lda     #kCopyOnUse
-        clc
-        jsr     DrawCopyWhenButton
-        lda     #kCopyNever
-        clc
-        jsr     DrawCopyWhenButton
-
         rts
 .endproc ; DrawControls
 

commit c4ea033d80f5de75c70b5cb3c7b59ba73a7f1ddb

DeskTop: Fix initialization of Add/Edit a Shortcut radio buttons
The button state was accidentally retained across invocations of the
dialog. Regressed in d01be1d in what was claimed was just an
optimization to avoid repainting.

desktop/ovl_selector_edit.s
@@ -39,6 +39,13 @@ kCopyNever  = 3                 ; corresponds to `kSelectorEntryCopyNever`
         jsr     file_dialog::Init
         copy    #$80, file_dialog::extra_controls_flag
 
+        lda     #BTK::kButtonStateNormal
+        sta     primary_run_list_button::state
+        sta     secondary_run_list_button::state
+        sta     at_first_boot_button::state
+        sta     at_first_use_button::state
+        sta     never_button::state
+
         ldax    #label_edit
         bit     is_add_flag
     IF_NS