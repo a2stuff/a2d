a2stuff/a2d

commit 1bdfff31e8ad6bfcb35342ca3847f10170e60fec

DeskTop: Allow selection to remain in inactive windows.
Since vintage versions, a distinction has been made in the code
between the active (i.e. top-most) window (if any) and the "selected"
window i.e. the window containing the selected icons (or the desktop).
This came with limitations, as only the active window could be drawn
into, so many, many fixes have been made over time to limit selection
to the active window or desktop, clearing selection in other cases.

But since recent changes to IconTK allow painting arbitrarily clipped
icons in windows, this restriction can be relaxed. Various places that
would clear selection can now allow selection to be maintained, and a
few places where volume icon behavior was special-cased can be
relaxed, e.g.:

* If you click on a window's content area the owning icon folder icon
  can be selected even if it's in a background window.

* If you select multiple folder icons then File > Open, the icons can
  stay selected.

This revises how several issues were treated, including #364, #415,
(e.g. System 7.5.3) but not identical. For example, clicking on the
non-content parts of a window (title bar, scroll bars, header) will
activate it but will not clear selection and select the owning icon,
or the volume icon if not present.

Testers should watch out for any confusion that's crept in between the
selected and active window, e.g. when an operation on selection (in an
inactive window) causes the active window to be refreshed instead.

toolkits/icontk.s
@@ -467,8 +467,9 @@ done:   rts
         ;; Pointer to IconEntry
         ldy     #EraseIconParams::icon
         lda     (params),y
-        sec                     ; redraw highlighted
-        jmp     EraseIconCommon ; A = icon id, C = redraw flag
+        ldx     #$80            ; do clip
+        sec                     ; do redraw highlighted
+        jmp     EraseIconCommon ; A = icon id, X = clip flag, C = redraw flag
 .endproc ; EraseIconImpl
 
 ;;; ============================================================
@@ -887,8 +888,9 @@ same_window:
 move_ok:
 
         INVOKE_WITH_LAMBDA IterateHighlightedIcons
+        ldx     #0              ; don't clip
         clc                     ; don't redraw highlighted
-        jmp     EraseIconCommon ; A = icon id, C = redraw flag
+        jmp     EraseIconCommon ; A = icon id, X = clip flag, C = redraw flag
         END_OF_LAMBDA
 
         ;; --------------------------------------------------
@@ -1278,7 +1280,7 @@ inside:
 more_drawing_needed_flag:
         .byte   0
 
-;;; Set by some callers of DrawIcon
+;;; Set by some callers of `DrawIconCommon` and `EraseIconCommon`
 clip_icons_flag:
         .byte   0
 
@@ -1843,9 +1845,10 @@ rect:   .tag    MGTK::Rect
 
 ;;; ============================================================
 ;;; Erase an icon; redraws overlapping icons as needed
-;;; Inputs: A = icon id, C = redraw highlighted flag
+;;; Inputs: A = icon id, X = clip_icons_flag, C = redraw highlighted flag
 .proc EraseIconCommon
         sta     erase_icon_id
+        stx     clip_icons_flag
         ror     redraw_highlighted_flag ; shift C into high bit
 
         ptr := $06
@@ -1861,27 +1864,28 @@ rect:   .tag    MGTK::Rect
         and     #kIconEntryWinIdMask
         sta     clip_window_id
         sta     getwinport_params::window_id
-        beq     volume
 
-        ;; File (i.e. icon in window)
-        MGTK_CALL MGTK::SetPattern, white_pattern
-        jsr     SetPortForWinIcon
-        bne     ret             ; obscured!
-        MGTK_CALL MGTK::PaintPoly, poly
-        jmp     RedrawIconsAfterErase
-ret:    rts
-
-        ;; Volume (i.e. icon on desktop)
-volume:
+    IF_ZERO
+        jsr     SetPortForVolIcon
         MGTK_CALL MGTK::GetDeskPat, addr
         MGTK_CALL MGTK::SetPattern, 0, addr
-        jsr     SetPortForVolIcon
+    ELSE
+        jsr     SetPortForWinIcon
+        RTS_IF_NE               ; obscured!
+        MGTK_CALL MGTK::SetPattern, white_pattern
+    END_IF
+
+        bit     clip_icons_flag
+    IF_NC
+        MGTK_CALL MGTK::PaintPoly, poly
+    ELSE
 :       jsr     CalcWindowIntersections
         bcs     :+              ; nothing remaining to draw
         MGTK_CALL MGTK::PaintPoly, poly
         lda     more_drawing_needed_flag
         bne     :-
 :
+    END_IF
         FALL_THROUGH_TO RedrawIconsAfterErase
 
 ;;; ============================================================

desktop/main.s
@@ -488,7 +488,6 @@ not_menu:
         lda     active_window_id
         cmp     findwindow_params::window_id
     IF_NE
-        jsr     ClearSelection
         jsr     ActivateWindow
     END_IF
 
@@ -497,9 +496,7 @@ not_menu:
 
         lda     selected_icon_count
     IF_ZERO
-        ;; Try to select the window's parent icon. (Only works
-        ;; for volume icons, otherwise it would put selection
-        ;; in an inactive window.)
+        ;; Try to select the window's parent icon.
         lda     active_window_id
         jne     SelectIconForWindow
     END_IF
@@ -590,27 +587,18 @@ dispatch_click:
 ;;; Selection should be cleared before calling
 
 .proc SelectIconForWindow
-        ;; Select window's corresponding volume icon.
-        ;; (Doesn't work for folder icons as only the active
-        ;; window and desktop can have selections.)
-        tax
-        lda     window_to_dir_icon_table-1,x
-        bmi     done            ; $FF = dir icon freed
-
+        jsr     GetWindowPath
+        jsr     IconToAnimate
         sta     icon_param
 
         jsr     GetIconWindow
-        beq     :+               ; desktop - selection ok
-        cmp     active_window_id ; This should never be true
-        bne     done
-:
         sta     selected_window_id
         copy    #1, selected_icon_count
         copy    icon_param, selected_icon_list
         ITK_CALL IconTK::HighlightIcon, icon_param
         ITK_CALL IconTK::DrawIcon, icon_param
 
-done:   rts
+        rts
 .endproc ; SelectIconForWindow
 
 ;;; ============================================================
@@ -2091,8 +2079,9 @@ loop:   cpx     selected_icon_count_copy
         ;; Finish up...
 
         ;; Were any directories opened?
-        lda     dir_flag
-        beq     done
+        dir_flag := *+1
+        lda     #SELF_MODIFIED_BYTE
+        bpl     done
 
         ;; Maybe close the previously active window, depending on source/modifiers
         jsr     MaybeCloseWindowAfterOpen
@@ -2119,18 +2108,8 @@ next:   txa
         beq     maybe_open_file       ; nope
 
         ;; Directory
+        copy    #$80, dir_flag
 
-        ;; Set when we see the first vol/folder icon, so we can
-        ;; clear selection (if it's a folder).
-        dir_flag := *+1         ; first one seen?
-        lda     #SELF_MODIFIED_BYTE
-
-        bne     :+              ; not the first
-        inc     dir_flag        ; only do this once
-        lda     selected_window_id ; selection in a window?
-        beq     :+                 ; no
-        jsr     ClearSelection
-:
         pla                     ; A = icon id
         jsr     OpenWindowForIcon
 
@@ -3236,7 +3215,7 @@ CmdGetInfo      := DoGetInfo
         param_call UpdateUsedFreeViaPath, path_buf3
 
         ;; Select/refresh window if there was one
-        lda     active_window_id
+        lda     selected_window_id
         jne     SelectAndRefreshWindowOrClose
 
 ret:    rts
@@ -3727,7 +3706,6 @@ finish: rts
 ;;; Cycle Through Windows
 ;;; Input: A = Key used; '~' is reversed
 
-
 .proc CmdCycleWindows
         tay
 
@@ -3774,7 +3752,8 @@ reverse:
 
 found:  inx
         stx     findwindow_params::window_id
-        jmp     ActivateWindowAndSelectIcon
+
+        jmp     ActivateWindow
 
 done:   rts
 .endproc ; CmdCycleWindows
@@ -4924,7 +4903,7 @@ failure:
     IF_NS
         ;; Update source vol's contents
         jsr     MaybeStashDropTargetName ; in case target is in window...
-        jsr     UpdateActiveWindow
+        jsr     UpdateSelectedWindow
         jsr     MaybeUpdateDropTargetFromName ; ...restore after update.
     END_IF
 
@@ -4938,7 +4917,7 @@ failure:
         ;; Update used/free for same-vol windows
     IF_EQ
         copy    #$80, validate_windows_flag
-        bne     UpdateActiveWindow ; always
+        bne     UpdateSelectedWindow ; always
     END_IF
 
         ;; --------------------------------------------------
@@ -4968,12 +4947,12 @@ failure:
         pla
         jmp     SelectAndRefreshWindowOrClose
 
-.proc UpdateActiveWindow
-        lda     active_window_id
+.proc UpdateSelectedWindow
+        lda     selected_window_id
         jsr     UpdateUsedFreeViaWindow
-        lda     active_window_id
+        lda     selected_window_id
         jmp     SelectAndRefreshWindowOrClose
-.endproc ; UpdateActiveWindow
+.endproc ; UpdateSelectedWindow
 
 .endproc ; PerformPostDropUpdates
 
@@ -5835,12 +5814,8 @@ ignore: rts
         sta     icon_param
 
         ;; Already an open window for the icon?
-        ldx     #kMaxDeskTopWindows-1
-:       cmp     window_to_dir_icon_table,x
-        beq     found_win
-        dex
-        bpl     :-
-        jmp     no_linked_win
+        jsr     FindWindowIndexForDirIcon
+        bne     no_linked_win
 
         ;; --------------------------------------------------
         ;; There is an existing window associated with icon.
@@ -6215,10 +6190,10 @@ done:
         ;; Desktop
         jsr     RedrawSelectedIcons
     ELSE
-        ;; Windowed - use a clipped port for the window
-        cmp     active_window_id ; in the active window?
-        bne     skip             ; TODO: This should not be possible
-
+        ;; Windowed
+        cmp     active_window_id
+      IF_EQ
+        ;; Fast path - use a clipped port
         jsr     UnsafeOffsetAndSetPortFromWindowId ; CHECKED
         bne     skip             ; obscured
 
@@ -6242,6 +6217,19 @@ done:
         cpx     selected_icon_count
         bne     :-
         jsr     PopPointers     ; do not tail-call optimize!
+      ELSE
+        ldx     #0
+:       txa
+        pha                     ; A = index
+        copy    selected_icon_list,x, icon_param
+        ITK_CALL IconTK::DrawIcon, icon_param ; CHECKED
+        pla                     ; A = index
+        tax
+        inx
+        cpx     selected_icon_count
+        bne     :-
+      END_IF
+
 
 skip:
     END_IF

commit 7be958972aeaf105c24dc3da2c80d7e55c81bd25

DeskTop: Fix repaint of icons dragged on desktop
Regressed in 1bdfff3. When optimizing repaints after dragging icons
within the some window (or desktop), it failed to specify that erasing
required clipping when for desktop icons moves.

toolkits/icontk.s
@@ -873,6 +873,7 @@ includes_trash:
 
         ;; Drag within same window (or desktop)
 same_window:
+        ldx     #$80            ; clip (if desktop)
         lda     findwindow_params::which_area
         .assert MGTK::Area::desktop = 0, error, "enum mismatch"
         beq     move_ok
@@ -888,10 +889,12 @@ same_window:
 
         ;; --------------------------------------------------
 
+        ldx     #0              ; don't clip (not desktop; unnecessary)
 move_ok:
-
+        stx     ::drag_highlighted_lambda_clip_flag
         INVOKE_WITH_LAMBDA IterateHighlightedIcons
-        ldx     #0              ; don't clip
+        ::drag_highlighted_lambda_clip_flag := *+1
+        ldx     #SELF_MODIFIED_BYTE
         clc                     ; don't redraw highlighted
         jmp     EraseIconCommon ; A = icon id, X = clip flag, C = redraw flag
         END_OF_LAMBDA