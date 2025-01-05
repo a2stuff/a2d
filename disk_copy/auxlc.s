eebd4867ed6a65ff0615db9cd27fe86fde311f2f
Joshua Bell <inexorabletash@gmail.com>
2025-01-05 10:34:34 -0800

    Revert "Fix FR build - disk_copy/auxlc has grown a bit"
    This reverts commit 1186fa439232f82b46199b7e3777549594affc78.

--- disk_copy/auxlc.s
+++ disk_copy/auxlc.s
@@ -2384,7 +2384,7 @@ Alert := alert_dialog::Alert
 
 ;;; ============================================================
 
-        .assert * <= $F400, error, "Update memory_bitmap if code extends past $F400"
+        .assert * <= $F200, error, "Update memory_bitmap if code extends past $F200"
 .endscope ; auxlc
         auxlc__start := auxlc::start
 
