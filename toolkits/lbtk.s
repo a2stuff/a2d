a2stuff/a2d

commit 5f615cfe3395ef09f7cc96fe004c0f20aaba5bba

Toolkits: Use ZP scratch space in a few places
Shaves about 32 bytes off of DeskTop's auxmem segment.

toolkits/lbtk.s
@@ -121,6 +121,7 @@ END_PARAM_BLOCK
 .proc Dispatch
 
         params_addr := lbr_ptr
+        jump_addr := tmp_space
 
         ;; Adjust stack/stash at `params_addr`
         pla
@@ -165,8 +166,7 @@ END_PARAM_BLOCK
         dey
         bpl     :-
 
-        jump_addr := *+1
-        jmp     SELF_MODIFIED
+        jmp     (tmp_space)
 
 jump_table:
         .addr   InitImpl
@@ -245,16 +245,14 @@ coords          .tag MGTK::Point
         cmp     lbr_copy + LBTK::ListBoxRecord::num_items
     IF_GE
         lda     #$FF
-        jsr     _SetSelection
-        jsr     OnSelChange
+        jsr     _SetSelectionAndNotify
         return  #$FF            ; not an item
     END_IF
 
         ;; Update selection (if different)
         cmp     lbr_copy + LBTK::ListBoxRecord::selected_index
     IF_NE
-        jsr     _SetSelection
-        jsr     OnSelChange
+        jsr     _SetSelectionAndNotify
     ELSE
         jsr     OnNoChange
     END_IF
@@ -274,8 +272,9 @@ coords          .tag MGTK::Point
         ;; Ignore unless vscroll is enabled
         ldy     #MGTK::Winfo::vscroll
         lda     (winfo_ptr),y
-        and     #MGTK::Scroll::option_active
-        bne     :+
+        .assert MGTK::Scroll::option_active = %00000001, error, "flag mismatch"
+        ror                     ; C = "active?"
+        bcc     :+
 ret:    rts
 :
         lda     findcontrol_params::which_part
@@ -358,8 +357,7 @@ repeat:
 
         ;; --------------------------------------------------
 
-update: jsr     _UpdateThumb
-        jmp     _Draw
+update: jmp     _UpdateThumbAndDraw
 .endproc ; _HandleListScroll
 
 ;;; ============================================================
@@ -427,19 +425,19 @@ ret:    rts
        END_IF
         dex
         txa
-        bpl     SetSelection    ; always
+        bpl     _SetSelectionAndNotify ; always
       END_IF
         ;; CHAR_DOWN
         ldx     lbr_copy + LBTK::ListBoxRecord::selected_index
       IF_NS
         lda     #0
-        beq     SetSelection    ; always
+        beq     _SetSelectionAndNotify ; always
       END_IF
         inx
         cpx     lbr_copy + LBTK::ListBoxRecord::num_items
         beq     ret
         txa
-        bpl     SetSelection    ; always
+        bpl     _SetSelectionAndNotify ; always
     END_IF
 
         ;; --------------------------------------------------
@@ -453,7 +451,7 @@ ret:    rts
         lda     lbr_copy + LBTK::ListBoxRecord::selected_index
         beq     ret
         lda     #0
-        bpl     SetSelection    ; always
+        bpl     _SetSelectionAndNotify ; always
       END_IF
         ;; CHAR_DOWN
         ldx     lbr_copy + LBTK::ListBoxRecord::selected_index
@@ -465,7 +463,7 @@ ret:    rts
         ldx     lbr_copy + LBTK::ListBoxRecord::num_items
         dex
         txa
-        bpl     SetSelection    ; always
+        bpl     _SetSelectionAndNotify ; always
     END_IF
 
         ;; --------------------------------------------------
@@ -478,11 +476,14 @@ ret:    rts
         ;; CHAR_DOWN
         lda     #MGTK::Part::page_down
         jmp     _HandleListScrollWithPart
+.endproc ; KeyImpl
 
-SetSelection:
+;;; ============================================================
+
+.proc _SetSelectionAndNotify
         jsr     _SetSelection
         jmp     OnSelChange
-.endproc ; KeyImpl
+.endproc ; _SetSelectionAndNotify
 
 ;;; ============================================================
 
@@ -599,6 +600,8 @@ activate:
 ;;; Assert: `LBTK::ListBoxRecord::winfo`'s `MGTK::Winfo::vthumbpos` is set.
 
 .proc _ScrollIntoView
+        force_draw_flag := tmp_space
+
         sta     force_draw_flag
         and     #$7F            ; A = index
 
@@ -616,16 +619,14 @@ activate:
         beq     skip
         bcs     update
 skip:
-        force_draw_flag := *+1
-        lda     #SELF_MODIFIED_BYTE
+        lda     force_draw_flag
         bmi     _Draw ; will highlight selection
 
         lda     lbr_copy + LBTK::ListBoxRecord::selected_index
         jmp     _HighlightIndex
 
 update:
-        jsr     _UpdateThumb
-        jmp     _Draw
+        jmp     _UpdateThumbAndDraw
 .endproc ; _ScrollIntoView
 
 ;;; ============================================================
@@ -670,7 +671,6 @@ update:
         tay
         ldax    #kListItemHeight
         jsr     _Multiply
-        ldy     #MGTK::Winfo::port+MGTK::GrafPort::maprect+MGTK::Rect::y1
         stax    tmp_rect+MGTK::Rect::y1
 
         ;; Set y2 to bottom
@@ -692,6 +692,11 @@ update:
 
 ;;; ============================================================
 
+.proc _UpdateThumbAndDraw
+        jsr     _UpdateThumb
+        FALL_THROUGH_TO _Draw
+.endproc
+
 ;;; Calls `DrawListEntryProc` for each entry.
 .proc _Draw
         jsr     _SetPort
@@ -704,6 +709,8 @@ update:
 
         lda     lbr_copy + LBTK::ListBoxRecord::num_items
         beq     finish
+
+        index := tmp_point + .sizeof(MGTK::Point)
 
         lda     lbr_copy + LBTK::ListBoxRecord::num_rows
         sta     rows
@@ -718,8 +725,7 @@ update:
 loop:
         MGTK_CALL MGTK::MoveTo, tmp_point
 
-        index := *+1
-        lda     #SELF_MODIFIED_BYTE
+        lda     index
         ldxy    #tmp_point
         jsr     DrawEntryProc
 

toolkits/btk.s
@@ -25,6 +25,8 @@
         .assert BTKEntry = Dispatch, error, "dispatch addr"
 .proc Dispatch
 
+        jump_addr := zp_scratch
+
         ;; Adjust stack/stash at `params_addr`
         pla
         sta     params_addr
@@ -64,8 +66,7 @@
         dey
         bpl     :-
 
-        jump_addr := *+1
-        jmp     SELF_MODIFIED
+        jmp     (jump_addr)
 
 jump_table:
         .addr   DrawImpl

toolkits/letk.s
@@ -37,10 +37,15 @@ length  .byte
 width   .word
         END_PARAM_BLOCK
         .assert text_params = command_data + 6, error, "mismatch"
-        .assert text_params+.sizeof(text_params) <= $2F, error, "mismatch"
+
+        tmpw := text_params+.sizeof(text_params)
+
+        .assert tmpw+2 <= $30, error, "mismatch"
 
         .assert LETKEntry = Dispatch, error, "dispatch addr"
 .proc Dispatch
+
+        jump_addr := tmpw
 
         ;; Adjust stack/stash at `params_addr`
         pla
@@ -92,8 +97,7 @@ width   .word
 
         jsr     _CalcPos
 
-        jump_addr := *+1
-        jmp     SELF_MODIFIED
+        jmp     (jump_addr)
 
 jump_table:
         .addr   InitImpl
@@ -323,18 +327,17 @@ END_PARAM_BLOCK
         MGTK_CALL MGTK::MoveTo, point
 
         jsr     _PrepTextParams
-        sta     len
+        pha                     ; A = len
 
+        caret_pos := tmpw
         ldy     #LETK::LineEditRecord::caret_pos
         lda     (a_record),y
         sta     caret_pos
 
         add16_8 text_params::data, caret_pos
-        len := *+1
-        lda     #SELF_MODIFIED_BYTE
+        pla                     ; A = len
         sec
-        caret_pos := *+1
-        sbc     #SELF_MODIFIED_BYTE
+        sbc     caret_pos
         beq     :+
         sta     text_params::length
         MGTK_CALL MGTK::DrawText, text_params
@@ -369,6 +372,8 @@ ycoord  .word
         END_PARAM_BLOCK
         .assert a_record = params::a_record, error, "a_record must be first"
 
+        len := tmpw
+
         jsr     _PrepTextParams
         beq     ret
         sta     len
@@ -388,8 +393,7 @@ loop:   cmp16   text_params::width, params::xcoord
         bcs     :+
         inc     text_params::length
         lda     text_params::length
-        len := *+1
-        cmp     #SELF_MODIFIED_BYTE
+        cmp     len
         beq     :+
         MGTK_CALL MGTK::TextWidth, text_params
         beq     loop            ; always
@@ -533,12 +537,15 @@ modified:
 ;;; When a non-control key is hit - insert the passed character
 
 .proc _InsertChar
+        char := tmpw
+        caret_pos := tmpw+1
+
         sta     char
 
         ;; Stash current caret pos
         ldy     #LETK::LineEditRecord::caret_pos
         lda     (a_record),y
-        sta     @caret_pos
+        sta     caret_pos
 
         ;; Is there room?
         ldy     #0
@@ -548,8 +555,7 @@ modified:
 
         ;; Move everything to right of caret up
         tay
-        @caret_pos := *+1
-:       cpy     #SELF_MODIFIED_BYTE
+:       cpy     caret_pos
         beq     :+
         lda     (a_buf),y
         iny
@@ -559,8 +565,7 @@ modified:
         bne     :-              ; always
 :
         ;; Insert
-        char := *+1
-        lda     #SELF_MODIFIED_BYTE
+        lda     char
         iny
         sta     (a_buf),y
         ldy     #0
@@ -640,20 +645,21 @@ ret:    rts
 ;;; Common logic for DeleteLeft/DeleteRight
 
 .proc _DeleteCharCommon
+        len := tmpw
+
         ;; Shrink buffer
         ldy     #0
         lda     (a_buf),y
         sec
         sbc     #1
         sta     (a_buf),y
-        sta     @len
+        sta     len
 
         ;; Move everything to the right of the caret down
         ldy     #LETK::LineEditRecord::caret_pos
         lda     (a_record),y
         tay
-        @len := *+1
-:       cpy     #SELF_MODIFIED_BYTE
+:       cpy     len
         beq     :+
         iny
         iny

commit a22e1281e170072ad258c7b55f0c2026eb4b305c

LBTK: Fix clicking on scrollbar
Regressed in 5f615cf - messed up the sense of a test.

toolkits/lbtk.s
@@ -274,7 +274,7 @@ coords          .tag MGTK::Point
         lda     (winfo_ptr),y
         .assert MGTK::Scroll::option_active = %00000001, error, "flag mismatch"
         ror                     ; C = "active?"
-        bcc     :+
+        bcs     :+
 ret:    rts
 :
         lda     findcontrol_params::which_part