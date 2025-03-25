a2stuff/a2d

commit 5fc6822af765fa317ba856bf37553b7951758228

MGTK: Add MulDiv, retire lib
Since the routine is used so heavily, add `MulDiv` as a call to MGTK.
While it is pure math and not even used by MGTK itself, so many files
were including lib/muldiv.s that it was getting silly.

This pushed MGTK past the space reserved for it ($4000...$8600) so the
upper limit was bumped up by 2 pages to give more room for future
growth.

All clients are updated; most of them can use a dedicated param block
with constants for either the number, numerator or denominator, which
simplifies the call site. Some that use the routine from main memory
continue to place params at $10... like lib/muldiv.s did. And some
places share param blocks across calls, either because they're using
the blocks at $10 or it was just not worth it to convert.

Overall, this saves space... but it's hard to quantify since it moves
things around a lot. For example, DeskTop still pays for the proc in
aux, but no longer in main. Some param blocks move off the zero page
so it's more bytes to populate each input, but can use constants so
there are fewer inputs to populate.

No functional changes.

disk_copy/auxlc.s
@@ -1788,31 +1788,38 @@ blocks_written:
 transfer_blocks:
         .word   0
 
+.params progress_muldiv_params
+number:         .word   kProgressWidth ; (in) constant
+numerator:      .word   0              ; (in) populated dynamically
+denominator:    .word   0              ; (in) populated dynamically
+result:         .word   0              ; (out)
+remainder:      .word   0              ; (out)
+.endparams
+
 .proc DrawProgressBar
         MGTK_CALL MGTK::SetPenMode, notpencopy
         MGTK_CALL MGTK::FrameRect, progress_frame
-        copy16  transfer_blocks, z:muldiv_denominator
+        copy16  transfer_blocks, progress_muldiv_params::denominator
 
         ;; read+written will not fit in 16 bits if total is > $7FFF
         ;; so scale appropriately
-        tmp_read := muldiv_numerator
-        tmp_written := muldiv_number
+        tmp_read := progress_muldiv_params::numerator
+        tmp_written := progress_muldiv_params::number
         copy16  blocks_read, tmp_read
         copy16  blocks_written, tmp_written
-        bit     z:muldiv_denominator+1
+        bit     progress_muldiv_params::denominator+1
     IF_NC
         ;; Use (read + written) / total*2
-        asl16   z:muldiv_denominator
+        asl16   progress_muldiv_params::denominator
     ELSE
         ;; Use ((read + written) / 2) / total
-        lsr16   z:tmp_read
-        lsr16   z:tmp_written
+        lsr16   tmp_read
+        lsr16   tmp_written
     END_IF
-        add16   z:tmp_read, z:tmp_written, z:muldiv_numerator
+        add16   tmp_read, tmp_written, progress_muldiv_params::numerator
 
-        copy16  #kProgressWidth, z:muldiv_number
-        jsr     MulDiv
-        add16   progress_bar::x1, z:muldiv_result, progress_bar::x2
+        MGTK_CALL MGTK::MulDiv, progress_muldiv_params
+        add16   progress_bar::x1, progress_muldiv_params::result, progress_bar::x2
         MGTK_CALL MGTK::SetPenMode, pencopy
         MGTK_CALL MGTK::SetPattern, progress_pattern
         MGTK_CALL MGTK::PaintRect, progress_bar
@@ -2365,7 +2372,6 @@ Alert := alert_dialog::Alert
 .endproc ; SystemTask
 
         .include "../lib/is_diskii.s"
-        .include "../lib/muldiv.s"
         .include "../lib/doubleclick.s"
 
 ;;; ============================================================

desk.acc/benchmark.s
@@ -121,6 +121,22 @@ counter:        .word   0       ; set by `ProbeSpeed`
         DEFINE_RECT_SZ meter_left, kMeterLeft, kMeterTop, kMeterWidth, kMeterHeight
         DEFINE_RECT_SZ meter_right, kMeterLeft, kMeterTop, kMeterWidth, kMeterHeight
 
+.params ticks_muldiv_params
+number:         .word   kMeterWidth ; (in) constant
+numerator:      .word   0           ; (in) populated dynamically
+denominator:    .word   kSpeedMax   ; (in) constant
+result:         .word   0           ; (out)
+remainder:      .word   0           ; (out)
+.endparams
+
+.params progress_muldiv_params
+number:         .word   kMeterWidth ; (in) constant
+numerator:      .word   0           ; (in) populated dynamically
+denominator:    .word   0           ; (in) populated dynamically
+result:         .word   0           ; (out)
+remainder:      .word   0           ; (out)
+.endparams
+
 pattern_left:
         .byte   %01000100
         .byte   %00010001
@@ -163,12 +179,9 @@ pattern_right:
 
         lda     #0
 loop:   pha
-        sta     z:muldiv_numerator
-        copy    #0, z:muldiv_numerator+1
-        copy16  #kSpeedMax, z:muldiv_denominator
-        copy16  #kMeterWidth, z:muldiv_number
-        jsr     MulDiv
-        add16   meter_left::x1, z:muldiv_result, pt_tick::xcoord
+        sta     ticks_muldiv_params::numerator
+        MGTK_CALL MGTK::MulDiv, ticks_muldiv_params
+        add16   meter_left::x1, ticks_muldiv_params::result, pt_tick::xcoord
         MGTK_CALL MGTK::MoveTo, pt_tick
         MGTK_CALL MGTK::Line, pt_tickdelta
         pla
@@ -338,17 +351,16 @@ done:   jmp     InputLoop
 .proc UpdateMeter
         jsr     ProbeSpeed
 
-        copy16  counter, z:muldiv_numerator
+        copy16  counter, progress_muldiv_params::numerator
         bit     radio_60hz_button::state
     IF_NS
-        copy16  #kSpeedMax * kSpeedDefault60Hz, z:muldiv_denominator
+        copy16  #kSpeedMax * kSpeedDefault60Hz, progress_muldiv_params::denominator
     ELSE
-        copy16  #kSpeedMax * kSpeedDefault50Hz, z:muldiv_denominator
+        copy16  #kSpeedMax * kSpeedDefault50Hz, progress_muldiv_params::denominator
     END_IF
-        copy16  #kMeterWidth, z:muldiv_number
 
-        jsr     MulDiv
-        add16   meter_left::x1, z:muldiv_result, meter_left::x2
+        MGTK_CALL MGTK::MulDiv, progress_muldiv_params
+        add16   meter_left::x1, progress_muldiv_params::result, meter_left::x2
         add16   meter_left::x2, #1, meter_right::x1
 
         MGTK_CALL MGTK::SetPenMode, pencopy
@@ -502,7 +514,6 @@ loop2c:
 
 ;;; ============================================================
 
-        .include "../lib/muldiv.s"
         .include "../lib/inttostring.s"
         .include "../lib/uppercase.s"
 

desk.acc/dos33.import.s
@@ -472,6 +472,22 @@ port:           .addr   grafport_win
 
         DEFINE_LABEL disk_vol, res_string_disk_volume_prefix, 20, 18
 
+.params entry_muldiv_params
+number:         .word   0                     ; (in) populated dynamically
+numerator:      .word   .sizeof(CatalogEntry) ; (in) constant
+denominator:    .word   1                     ; (in) constant
+result:         .word   0                     ; (out)
+remainder:      .word   0                     ; (out)
+.endparams
+
+.params progress_muldiv_params
+number:         .word   kProgressBarWidth ; (in) constant
+numerator:      .word   0                 ; (in) populated dynamically
+denominator:    .word   0                 ; (in) populated dynamically
+result:         .word   0                 ; (out)
+remainder:      .word   0                 ; (out)
+.endparams
+
 ;;; ============================================================
 
 .proc Init
@@ -664,12 +680,9 @@ done:   rts
 
         ;; Calculate address of `CatalogEntry`
         ptr := $04
-        sta     z:muldiv_number
-        copy    #0, z:muldiv_number+1
-        copy16  #.sizeof(CatalogEntry), z:muldiv_numerator
-        copy16  #1, z:muldiv_denominator
-        jsr     MulDiv
-        add16   z:muldiv_result, #EntryBuffer, ptr
+        sta     entry_muldiv_params::number
+        MGTK_CALL MGTK::MulDiv, entry_muldiv_params
+        add16   entry_muldiv_params::result, #EntryBuffer, ptr
 
         kLockedX = 8
         kTypeX   = 20
@@ -749,11 +762,10 @@ ret:    rts
         ;; ASSERT: Not obscured.
         MGTK_CALL MGTK::SetPort, grafport_win
 
-        copy16  control_block+ControlBlock::progress_num, z:muldiv_numerator
-        copy16  control_block+ControlBlock::progress_denom, z:muldiv_denominator
-        copy16  #kProgressBarWidth, z:muldiv_number
-        jsr     MulDiv
-        add16   z:muldiv_result, progress_meter::x1, progress_meter::x2
+        copy16  control_block+ControlBlock::progress_num, progress_muldiv_params::numerator
+        copy16  control_block+ControlBlock::progress_denom, progress_muldiv_params::denominator
+        MGTK_CALL MGTK::MulDiv, progress_muldiv_params
+        add16   progress_muldiv_params::result, progress_meter::x1, progress_meter::x2
 
         MGTK_CALL MGTK::SetPattern, progress_pattern
         MGTK_CALL MGTK::SetPenMode, pencopy
@@ -818,7 +830,6 @@ str_from_int:   PASCAL_STRING "000000" ; filled in by IntToString
         .include "../lib/drawstring.s"
         .include "../lib/get_next_event.s"
         .include "../lib/doubleclick.s"
-        .include "../lib/muldiv.s"
         .include "../lib/inttostring.s"
 
         .assert * < EntryBuffer, error, "DA too large"
@@ -1073,16 +1084,24 @@ control_block:
 set_eof_flag:   .byte   0
 tslist_offset:  .byte   0
 
+PARAM_BLOCK muldiv_params, $10
+number          .word           ; (in)
+numerator       .word           ; (in)
+denominator     .word           ; (in)
+result          .word           ; (out)
+remainder       .word           ; (out)
+END_PARAM_BLOCK
+
 start:
         jsr     FetchControlBlock
 
         ;; Fetch `CatalogEntry`
-        copy    control_block+ControlBlock::selected_index, z:muldiv_number
-        copy    #0, z:muldiv_number+1
-        copy16  #.sizeof(aux::CatalogEntry), z:muldiv_numerator
-        copy16  #1, z:muldiv_denominator
-        jsr     MulDiv
-        add16   z:muldiv_result, #aux::EntryBuffer, STARTLO
+        copy    control_block+ControlBlock::selected_index, muldiv_params::number
+        copy    #0, muldiv_params::number+1
+        copy16  #.sizeof(aux::CatalogEntry), muldiv_params::numerator
+        copy16  #1, muldiv_params::denominator
+        JUMP_TABLE_MGTK_CALL MGTK::MulDiv, muldiv_params
+        add16   muldiv_params::result, #aux::EntryBuffer, STARTLO
         add16   STARTLO, #.sizeof(aux::CatalogEntry)-1, ENDLO
         copy16  #entry_buf, DESTINATIONLO
         clc                     ; aux>main
@@ -1554,7 +1573,6 @@ RWTSWrite := RWTSImpl::Write
 ;;; ============================================================
 
         .include "../lib/is_diskii.s"
-        .include "../lib/muldiv.s"
 
 ;;; ============================================================
 

desk.acc/map.s
@@ -29,6 +29,39 @@ kMapTop = 5
 kMapWidth = 230
 kMapHeight = 52
 
+.params y_to_lat
+number:         .word   180        ; (in) constant
+numerator:      .word   0          ; (in) populated dynamically
+denominator:    .word   kMapHeight ; (in) constant
+result:         .word   0          ; (out)
+remainder:      .word   0          ; (out)
+.endparams
+
+.params x_to_long
+number:         .word   360       ; (in) constant
+numerator:      .word   0         ; (in) populated dynamically
+denominator:    .word   kMapWidth ; (in) constant
+result:         .word   0         ; (out)
+remainder:      .word   0         ; (out)
+.endparams
+
+.params lat_to_y
+number:         .word   kMapHeight ; (in) constant
+numerator:      .word   0          ; (in) populated dynamically
+denominator:    .word   180        ; (in) constant
+result:         .word   0          ; (out)
+remainder:      .word   0          ; (out)
+.endparams
+
+.params long_to_x
+number:         .word   kMapWidth ; (in) constant
+numerator:      .word   0         ; (in) populated dynamically
+denominator:    .word   360       ; (in) constant
+result:         .word   0         ; (out)
+remainder:      .word   0         ; (out)
+.endparams
+
+
 pensize_normal: .byte   1, 1
 pensize_frame:  .byte   2, 1
 
@@ -465,22 +498,16 @@ index:  .byte   0
     END_IF
 
         ;; Compute new position
-        sub16   screentowindow_params::windowx, #kMapLeft+1, long
-        sub16   screentowindow_params::windowy, #kMapTop, lat
 
         ;; Map latitude to +90...-90
-        copy16  lat, z:muldiv_numerator
-        copy16  #kMapHeight, z:muldiv_denominator
-        copy16  #180, z:muldiv_number
-        jsr     MulDiv
-        sub16   #90, z:muldiv_result, lat
+        sub16   screentowindow_params::windowy, #kMapTop, y_to_lat::numerator
+        MGTK_CALL MGTK::MulDiv, y_to_lat
+        sub16   #90, y_to_lat::result, lat
 
         ;; Map longitude to -180...+180
-        copy16  long, z:muldiv_numerator
-        copy16  #kMapWidth, z:muldiv_denominator
-        copy16  #360, z:muldiv_number
-        jsr     MulDiv
-        sub16   z:muldiv_result, #180, long
+        sub16   screentowindow_params::windowx, #kMapLeft+1, x_to_long::numerator
+        MGTK_CALL MGTK::MulDiv, x_to_long
+        sub16   x_to_long::result, #180, long
 
         ;; Update display
         jsr     SetPort
@@ -680,21 +707,15 @@ blink_counter:
 
 .proc UpdateCoordsFromLatLong
         ;; Map latitude from +90...-90
-        sub16   #90, lat, z:muldiv_numerator ; 90...-90 to 0...180
-        copy16  #180, z:muldiv_denominator
-        copy16  #kMapHeight, z:muldiv_number
-        jsr     MulDiv
-        copy16  z:muldiv_result, ycoord
+        sub16   #90, lat, lat_to_y::numerator ; 90...-90 to 0...180
+        MGTK_CALL MGTK::MulDiv, lat_to_y
+        add16   lat_to_y::result, #kMapTop    - (kPositionMarkerHeight/2), ycoord
 
         ;; Map longitude from -180...+180
-        add16   long, #180, z:muldiv_numerator ; -180...180 to 0...360
-        copy16  #360, z:muldiv_denominator
-        copy16  #kMapWidth, z:muldiv_number
-        jsr     MulDiv
-        copy16  z:muldiv_result, xcoord
+        add16   long, #180, long_to_x::numerator ; -180...180 to 0...360
+        MGTK_CALL MGTK::MulDiv, long_to_x
+        add16   long_to_x::result, #kMapLeft+1 - (kPositionMarkerWidth/2), xcoord
 
-        add16   xcoord, #kMapLeft+1 - (kPositionMarkerWidth/2), xcoord
-        add16   ycoord, #kMapTop    - (kPositionMarkerHeight/2), ycoord
         rts
 .endproc ; UpdateCoordsFromLatLong
 
@@ -702,7 +723,6 @@ blink_counter:
 
         .include "../lib/drawstring.s"
         .include "../lib/inttostring.s"
-        .include "../lib/muldiv.s"
         .include "../lib/get_next_event.s"
 
 ;;; ============================================================

desk.acc/neko.s
@@ -1011,14 +1011,22 @@ result: .byte   1
 
 ;;; ============================================================
 
+PARAM_BLOCK muldiv_params, $10
+number          .word           ; (in)
+numerator       .word           ; (in)
+denominator     .word           ; (in)
+result          .word           ; (out)
+remainder       .word           ; (out)
+END_PARAM_BLOCK
+
 ;;; A,X = A,X * Y
 .proc Multiply_16_8_16
-        stax    z:muldiv_number
-        sty     z:muldiv_numerator
-        copy    #0, z:muldiv_numerator+1
-        copy16  #1, z:muldiv_denominator
-        jsr     MulDiv
-        ldax    z:muldiv_result
+        stax    muldiv_params::number
+        sty     muldiv_params::numerator
+        copy    #0, muldiv_params::numerator+1
+        copy16  #1, muldiv_params::denominator
+        JUMP_TABLE_MGTK_CALL MGTK::MulDiv, muldiv_params
+        ldax    muldiv_params::result
         rts
 .endproc ; Multiply_16_8_16
 
@@ -1026,20 +1034,19 @@ result: .byte   1
 
 ;;; A,X = A,X / Y, Y = remainder
 .proc Divide_16_8_16
-        stax    z:muldiv_numerator
-        sty     z:muldiv_denominator
-        copy    #0, z:muldiv_denominator+1
-        copy16  #1, z:muldiv_number
-        jsr     MulDiv
-        ldax    z:muldiv_result
-        ldy     z:muldiv_remainder
+        stax    muldiv_params::numerator
+        sty     muldiv_params::denominator
+        copy    #0, muldiv_params::denominator+1
+        copy16  #1, muldiv_params::number
+        JUMP_TABLE_MGTK_CALL MGTK::MulDiv, muldiv_params
+        ldax    muldiv_params::result
+        ldy     muldiv_params::remainder
         rts
 .endproc ; Divide_16_8_16
 
 ;;; ============================================================
 
         .include "../lib/uppercase.s"
-        .include "../lib/muldiv.s"
 
 ;;; ============================================================
 

desk.acc/show.text.file.s
@@ -178,6 +178,14 @@ reserved:       .byte   0
         REF_MAPINFO_MEMBERS
 .endparams
 
+.params muldiv_params
+number:         .word   0       ; (in)
+numerator:      .word   0       ; (in)
+denominator:    .word   0       ; (in)
+result:         .word   0       ; (out)
+remainder:      .word   0       ; (out)
+.endparams
+
 ;;; ============================================================
 ;;; Open the file and create the DA window
 
@@ -377,12 +385,12 @@ end:    rts
         beq     end
 
         ;; `first_visible_line` = `trackthumb_params::thumbpos` * `max_visible_line` / `kVScrollMax`
-        copy16  max_visible_line, z:muldiv_number
-        copy    trackthumb_params::thumbpos, z:muldiv_numerator ; lo
-        copy    #0, z:muldiv_numerator+1                        ; hi
-        copy16  #kVScrollMax, z:muldiv_denominator
-        jsr     MulDiv
-        copy16  z:muldiv_result, first_visible_line
+        copy16  max_visible_line, muldiv_params::number
+        copy    trackthumb_params::thumbpos, muldiv_params::numerator ; lo
+        copy    #0, muldiv_params::numerator+1                        ; hi
+        copy16  #kVScrollMax, muldiv_params::denominator
+        MGTK_CALL MGTK::MulDiv, muldiv_params
+        copy16  muldiv_params::result, first_visible_line
 
         jsr     UpdateScrollPos
 
@@ -509,11 +517,11 @@ ForceScrollBottom := ScrollBottom::force
 
 .proc UpdateScrollPos
         ;; Update viewport
-        copy16  first_visible_line, z:muldiv_number
-        copy16  #kLineScrollDelta * kLineHeight, z:muldiv_numerator
-        copy16  #1, z:muldiv_denominator
-        jsr     MulDiv
-        ldax    z:muldiv_result
+        copy16  first_visible_line, muldiv_params::number
+        copy16  #kLineScrollDelta * kLineHeight, muldiv_params::numerator
+        copy16  #1, muldiv_params::denominator
+        MGTK_CALL MGTK::MulDiv, muldiv_params
+        ldax    muldiv_params::result
         stax    winfo::maprect::y1
         addax   #kDAHeight, winfo::maprect::y2
         MGTK_CALL MGTK::SetPort, winfo::port
@@ -521,15 +529,15 @@ ForceScrollBottom := ScrollBottom::force
         ;; Update thumb position
 
         ;; `updatethumb_params::thumbpos` = `first_visible_line` * `kVScrollMax` / `max_visible_line`
-        copy16  #kVScrollMax, z:muldiv_number
-        copy16  first_visible_line, z:muldiv_numerator
-        copy16  max_visible_line, z:muldiv_denominator
-        jsr     MulDiv
-        lda     z:muldiv_result+1
+        copy16  #kVScrollMax, muldiv_params::number
+        copy16  first_visible_line, muldiv_params::numerator
+        copy16  max_visible_line, muldiv_params::denominator
+        MGTK_CALL MGTK::MulDiv, muldiv_params
+        lda     muldiv_params::result+1
     IF_NOT_ZERO
         lda     #kVScrollMax
     ELSE
-        lda     z:muldiv_result
+        lda     muldiv_params::result
     END_IF
         sta     updatethumb_params::thumbpos
 
@@ -603,11 +611,11 @@ end:    rts
         JSR_TO_MAIN SetFileMark
 
         ;; And adjust to the appropriate offset for that line in the viewport.
-        copy16  current_line, z:muldiv_number
-        copy16  #kLineHeight, z:muldiv_numerator
-        copy16  #1, z:muldiv_denominator
-        jsr     MulDiv
-        copy16  z:muldiv_result, line_pos::base
+        copy16  current_line, muldiv_params::number
+        copy16  #kLineHeight, muldiv_params::numerator
+        copy16  #1, muldiv_params::denominator
+        MGTK_CALL MGTK::MulDiv, muldiv_params
+        copy16  muldiv_params::result, line_pos::base
     END_IF
 
         ;; Select appropriate font
@@ -1056,7 +1064,6 @@ window_id:      .byte   kDAWindowId
 ;;; ============================================================
 
         .include "../lib/uppercase.s"
-        .include "../lib/muldiv.s"
 
 ;;; ============================================================
 

desktop/desktop.inc
@@ -27,7 +27,7 @@ DA_IO_BUFFER    := $1C00        ; ...through $1FFF
 kDAMaxSize      = DA_IO_BUFFER - DA_LOAD_ADDRESS
 
 ;;; Used by DAs for window definitions
-DEFAULT_FONT    := $8600
+DEFAULT_FONT    := $8800
 
 ;;; A 16k buffer that can be restored using overlays; load
 ;;; `kDynamicRoutineRestoreBuffer` and call `JUMP_TABLE_RESTORE_OVL`

desktop/main.s
@@ -4661,6 +4661,8 @@ old     .tag    MGTK::Point
 ;;; Increment/decrement sizes (depends on view type)
 tick_h  .byte
 tick_v  .byte
+
+tmpw    .word
 END_PARAM_BLOCK
 
 ;;; --------------------------------------------------
@@ -4780,29 +4782,28 @@ _Preamble:
 
 .proc TrackHThumb
         jsr     _Preamble
-        sub16   ubox+MGTK::Rect::x2, ubox+MGTK::Rect::x1, z:muldiv_number
-        sub16   z:muldiv_number, width, z:muldiv_number
+        sub16   ubox+MGTK::Rect::x2, ubox+MGTK::Rect::x1, tmpw
+        sub16   tmpw, width, track_muldiv_params::number
         jsr     _TrackMulDiv
-        add16   z:muldiv_result, ubox+MGTK::Rect::x1, viewport+MGTK::Rect::x1
+        add16   track_muldiv_params::result, ubox+MGTK::Rect::x1, viewport+MGTK::Rect::x1
         add16   viewport+MGTK::Rect::x1, width, viewport+MGTK::Rect::x2
         jmp     _MaybeUpdateHThumb
 .endproc ; TrackHThumb
 
 .proc TrackVThumb
         jsr     _Preamble
-        sub16   ubox+MGTK::Rect::y2, ubox+MGTK::Rect::y1, z:muldiv_number
-        sub16   z:muldiv_number, height, z:muldiv_number
+        sub16   ubox+MGTK::Rect::y2, ubox+MGTK::Rect::y1, tmpw
+        sub16   tmpw, height, track_muldiv_params::number
         jsr     _TrackMulDiv
-        add16   z:muldiv_result, ubox+MGTK::Rect::y1, viewport+MGTK::Rect::y1
+        add16   track_muldiv_params::result, ubox+MGTK::Rect::y1, viewport+MGTK::Rect::y1
         add16   viewport+MGTK::Rect::y1, height, viewport+MGTK::Rect::y2
         jmp     _MaybeUpdateVThumb
 .endproc ; TrackVThumb
 
 .proc _TrackMulDiv
-        copy    trackthumb_params::thumbpos, z:muldiv_numerator
-        copy    #0, z:muldiv_numerator+1
-        copy16  #kScrollThumbMax, z:muldiv_denominator
-        jmp     MulDiv
+        copy    trackthumb_params::thumbpos, track_muldiv_params::numerator
+        MGTK_CALL MGTK::MulDiv, track_muldiv_params
+        rts
 .endproc ; _TrackMulDiv
 
 ;;; --------------------------------------------------
@@ -4906,24 +4907,22 @@ _Preamble:
 
 ;;; Set hthumb position relative to `maprect` and `ubox`.
 .proc _SetHThumbFromViewport
-        sub16   viewport+MGTK::Rect::x1, ubox+MGTK::Rect::x1, z:muldiv_number
-        copy16  #kScrollThumbMax, z:muldiv_numerator
-        sub16   ubox+MGTK::Rect::x2, ubox+MGTK::Rect::x1, z:muldiv_denominator
-        sub16   z:muldiv_denominator, width, z:muldiv_denominator
-        jsr     MulDiv
-        lda     z:muldiv_result
+        sub16   viewport+MGTK::Rect::x1, ubox+MGTK::Rect::x1, setthumb_muldiv_params::number
+        sub16   ubox+MGTK::Rect::x2, ubox+MGTK::Rect::x1, tmpw
+        sub16   tmpw, width, setthumb_muldiv_params::denominator
+        MGTK_CALL MGTK::MulDiv, setthumb_muldiv_params
+        lda     setthumb_muldiv_params::result
         ldx     #MGTK::Ctl::horizontal_scroll_bar
         jmp     _UpdateThumb
 .endproc ; _SetHThumbFromViewport
 
 ;;; Set vthumb position relative to `maprect` and `ubox`.
 .proc _SetVThumbFromViewport
-        sub16   viewport+MGTK::Rect::y1, ubox+MGTK::Rect::y1, z:muldiv_number
-        copy16  #kScrollThumbMax, z:muldiv_numerator
-        sub16   ubox+MGTK::Rect::y2, ubox+MGTK::Rect::y1, z:muldiv_denominator
-        sub16   z:muldiv_denominator, height, z:muldiv_denominator
-        jsr     MulDiv
-        lda     z:muldiv_result
+        sub16   viewport+MGTK::Rect::y1, ubox+MGTK::Rect::y1, setthumb_muldiv_params::number
+        sub16   ubox+MGTK::Rect::y2, ubox+MGTK::Rect::y1, tmpw
+        sub16   tmpw, height, setthumb_muldiv_params::denominator
+        MGTK_CALL MGTK::MulDiv, setthumb_muldiv_params
+        lda     setthumb_muldiv_params::result
         ldx     #MGTK::Ctl::vertical_scroll_bar
         jmp     _UpdateThumb
 .endproc ; _SetVThumbFromViewport
@@ -11698,11 +11697,10 @@ UpdateDownloadDialogProgress := UpdateCopyDialogProgress
         param_call DrawString, str_2_spaces
 
         ;; Update progress bar
-        sub16   total_count, file_count, z:muldiv_numerator
-        copy16  total_count, z:muldiv_denominator
-        copy16  #kProgressBarWidth, z:muldiv_number
-        jsr     MulDiv
-        add16   z:muldiv_result, progress_dialog_bar_meter::x1, progress_dialog_bar_meter::x2
+        sub16   total_count, file_count, progress_muldiv_params::numerator
+        copy16  total_count, progress_muldiv_params::denominator
+        MGTK_CALL MGTK::MulDiv, progress_muldiv_params
+        add16   progress_muldiv_params::result, progress_dialog_bar_meter::x1, progress_dialog_bar_meter::x2
         jsr     SetPenModeCopy
         MGTK_CALL MGTK::SetPattern, progress_pattern
         MGTK_CALL MGTK::PaintRect, progress_dialog_bar_meter
@@ -15093,12 +15091,20 @@ window_entry_table:             .res    ::kMaxIconCount+1, 0
 
 ;;; A,X = A,X * Y
 .proc Multiply_16_8_16
-        stax    z:muldiv_number
-        sty     z:muldiv_numerator
-        copy    #0, z:muldiv_numerator+1
-        copy16  #1, z:muldiv_denominator
-        jsr     MulDiv
-        ldax    z:muldiv_result
+PARAM_BLOCK muldiv_params, $10
+number          .word           ; (in)
+numerator       .word           ; (in)
+denominator     .word           ; (in)
+result          .word           ; (out)
+remainder       .word           ; (out)
+END_PARAM_BLOCK
+
+        stax    muldiv_params::number
+        sty     muldiv_params::numerator
+        copy    #0, muldiv_params::numerator+1
+        copy16  #1, muldiv_params::denominator
+        MGTK_CALL MGTK::MulDiv, muldiv_params
+        ldax    muldiv_params::result
         rts
 .endproc ; Multiply_16_8_16
 
@@ -15126,7 +15132,6 @@ window_entry_table:             .res    ::kMaxIconCount+1, 0
         .include "../lib/reconnect_ram.s"
         .include "../lib/readwrite_settings.s"
         .include "../lib/get_next_event.s"
-        .include "../lib/muldiv.s"
         .include "../lib/monocolor.s"
         .include "../lib/speed.s"
         .include "../lib/bell.s"
@@ -15516,12 +15521,6 @@ str_device_type_vdrive:
         DetectDoubleClick := main::DetectDoubleClick
         AdjustOnLineEntryCase := main::AdjustOnLineEntryCase
         AdjustFileEntryCase := main::AdjustFileEntryCase
-        MulDiv := main::MulDiv
-        muldiv_number := main::muldiv_number
-        muldiv_numerator := main::muldiv_numerator
-        muldiv_denominator := main::muldiv_denominator
-        muldiv_result := main::muldiv_result
-        muldiv_remainder := main::muldiv_remainder
 
 ;;; ============================================================
 

commit a19fa1941cb49a648aa6757059e7d04157378543

Disk Copy: Fix progress bar updates during copies
A regression introduced in 5fc6822 made the progress bar update
incorrectly. An overloaded field was used both as temp space and
as a constant, so it wasn't very constant.

disk_copy/auxlc.s
@@ -1817,8 +1817,8 @@ remainder:      .word   0              ; (out)
 
         ;; read+written will not fit in 16 bits if total is > $7FFF
         ;; so scale appropriately
-        tmp_read := progress_muldiv_params::numerator
-        tmp_written := progress_muldiv_params::number
+        tmp_read := $06
+        tmp_written := $08
         copy16  blocks_read, tmp_read
         copy16  blocks_written, tmp_written
         bit     progress_muldiv_params::denominator+1