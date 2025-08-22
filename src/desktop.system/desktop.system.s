a2stuff/a2d

commit e648ec7994601fc99f6a0a5cb818651c8ea8e463

Launcher: Consume Esc keypress when canceling shortcut copy to RAMCard
KBD was read but KBDSTRB wasn't hit to consume the keypress. Do it!

Also: leave high bit alone and adjust nearby compares.

Also also: leave some breadcrumbs in MGTK source about potentially
resolving similar issues once the GUI is active.

Fixes #868

src/desktop.system/desktop.system.s
@@ -698,6 +698,7 @@ close:  MLI_CALL CLOSE, close_dstfile_params
 .proc CheckCancel
         lda     KBD
         bpl     ret
+        sta     KBDSTRB
         cmp     #$80|CHAR_ESCAPE
         beq     cancel
 ret:    rts
@@ -2108,15 +2109,14 @@ str_not_completed:
         sta     KBDSTRB
 loop:   lda     KBD
         bpl     loop
-        and     #CHAR_MASK
         sta     KBDSTRB
 
-        cmp     #kShortcutMonitor ; Easter Egg: If 'M', enter monitor
+        cmp     #$80|kShortcutMonitor ; Easter Egg: If 'M', enter monitor
         beq     monitor
-        cmp     #TO_LOWER(kShortcutMonitor)
+        cmp     #$80|TO_LOWER(kShortcutMonitor)
         beq     monitor
 
-        cmp     #CHAR_RETURN
+        cmp     #$80|CHAR_RETURN
         bne     loop
 
         jmp     FinishAndInvoke
@@ -2369,10 +2369,9 @@ done:   rts
 :       lda     KBD
         bpl     :-
         sta     KBDSTRB
-        and     #CHAR_MASK
-        cmp     #CHAR_ESCAPE
+        cmp     #$80|CHAR_ESCAPE
         beq     done
-        cmp     #CHAR_RETURN
+        cmp     #$80|CHAR_RETURN
         bne     :-
 done:   rts
 .endproc ; WaitEnterEscape

src/mgtk/mgtk.s
@@ -4812,6 +4812,8 @@ reset_desktop:
         jsr     SaveParamsAndStack
         jsr     SetDesktopPort
 
+        ;; TODO: Consider clearing `KBDSTRB` to avoid lingering keypresses.
+
         ;; Fills the desktop background on startup (menu left black)
         MGTK_CALL MGTK::SetPattern, desktop_pattern
         MGTK_CALL MGTK::PaintRect, desktop_mapinfo+MGTK::MapInfo::maprect
@@ -5377,16 +5379,18 @@ eventbuf:
 
         .res    kEventBufSize*MGTK::short_event_size
 
-
 .proc FlushEventsImpl
         php
         sei
         lda     #0
         sta     eventbuf_tail
         sta     eventbuf_head
+        ;; TODO: Consider clearing `KBDSTRB` to avoid a race where the
+        ;; keypress hasn't been seen yet when `FlushEvents` is called.
         plp
         rts
 .endproc ; FlushEventsImpl
+
         ;; called during PostEvent and a few other places
 .proc PutEvent
         lda     eventbuf_head

commit c1ce5ec118d10a5d5d16d454e4b2a18f3d8b287e

Launcher: Fix ESC keypress to cancel overwriting RAM
Regressed in e648ec7.

src/desktop.system/desktop.system.s
@@ -2132,7 +2132,7 @@ str_not_completed:
         sta     OURCH
         param_call CoutString, str_insert
         jsr     WaitEnterEscape
-        cmp     #CHAR_ESCAPE
+        cmp     #$80|CHAR_ESCAPE
         bne     :+
 
         ldx     saved_stack
@@ -2399,7 +2399,7 @@ found:
         jsr     VTABZ
         param_call CoutString, str_ram_not_empty
         jsr     WaitEnterEscape
-        cmp     #CHAR_ESCAPE
+        cmp     #$80|CHAR_ESCAPE
         beq     quit
         jsr     HOME
 