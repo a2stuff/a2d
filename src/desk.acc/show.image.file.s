a2stuff/a2d

commit 54aef06725a18f0a57b3fc66afc28fd9157f738e

Use SET_BIT7_FLAG/CLEAR_BIT7_FLAG macros
Update many places that had "bit 7" flags to set/clear them using
the macros. Saves bytes here and there.

No functional changes.

src/desk.acc/show.image.file.s
@@ -1,6 +1,7 @@
         .include "../config.inc"
 
         .include "apple2.inc"
+        .include "opcodes.inc"
         .include "../inc/apple2.inc"
         .include "../inc/macros.inc"
         .include "../inc/prodos.inc"
@@ -110,7 +111,7 @@ event_params:   .tag MGTK::Event
 ;;; ============================================================
 
 .proc Init
-        copy8   #0, mode
+        CLEAR_BIT7_FLAG color_mode_flag
 
         ;; In case we're re-entered c/o switching to another file
         jsr     MaybeCallExitHook
@@ -169,7 +170,7 @@ event_params:   .tag MGTK::Event
 on_key:
         ;; Stop slideshow on any keypress
         ldy     slideshow_flag  ; Y = previous `slideshow_flag` state
-        copy8   #0, slideshow_flag
+        CLEAR_BIT7_FLAG slideshow_flag
 
         lda     event_params + MGTK::Event::key
         jsr     ToUpperCase
@@ -210,7 +211,7 @@ on_key:
 ;;; ============================================================
 
 .proc SetSlideshowMode
-        copy8   #$80, slideshow_flag
+        SET_BIT7_FLAG slideshow_flag
         jsr     InitSlideshowCounter
         jmp     InputLoop
 .endproc ; SetSlideshowMode
@@ -398,7 +399,7 @@ signature:
 .proc ShowLZ4FHFile
         sta     PAGE2OFF
 
-        copy8   #$80, restore_buffer_overlay_flag
+        SET_BIT7_FLAG restore_buffer_overlay_flag
         copy16  #OVERLAY_BUFFER, read_params::data_buffer
         JUMP_TABLE_MLI_CALL READ, read_params
         copy16  #$2000, read_params::data_buffer
@@ -701,32 +702,30 @@ END_PROC_AT
 ;;; ============================================================
 ;;; Color/B&W Toggle
 
-mode:   .byte   0               ; 0 = B&W, $80 = color
+color_mode_flag:   .byte   0    ; bit7=0 = B&W, bit7=1 = color
 
 .proc ToggleMode
-        lda     mode
-        bne     SetBWMode
+        bit     color_mode_flag
+        bpl     SetBWMode
         FALL_THROUGH_TO SetColorMode
 .endproc ; ToggleMode
 
 .proc SetColorMode
-        lda     mode
-        bne     done
-        copy8   #$80, mode
-
+        bit     color_mode_flag
+    IF_NC
+        SET_BIT7_FLAG color_mode_flag
         jsr     JUMP_TABLE_COLOR_MODE
-
-done:   rts
+    END_IF
+        rts
 .endproc ; SetColorMode
 
 .proc SetBWMode
-        lda     mode
-        beq     done
-        copy8   #0, mode
-
+        bit     color_mode_flag
+    IF_NS
+        CLEAR_BIT7_FLAG color_mode_flag
         jsr     JUMP_TABLE_MONO_MODE
-
-done:   rts
+    END_IF
+        rts
 .endproc ; SetBWMode
 
 ;;; ============================================================
@@ -867,14 +866,14 @@ dhr_file:
         lda     #$C0            ; S = is dhr?, V = is aux page?
         SKIP_NEXT_2_BYTE_INSTRUCTION
 hr_file:
-        copy8   #0, dhr_flag
+        copy8   #0, dhr_flags
         copy16  #hires, ptr
 
         sta     PAGE2OFF
 
         param_call UnpackRead, Write
 
-        bit     dhr_flag        ; if hires, need to convert
+        bit     dhr_flags       ; if hires, need to convert
     IF_NC
         jsr     HRToDHR
     END_IF
@@ -901,9 +900,9 @@ hr_file:
         bne     exit
         copy8   #$20, ptr+1     ; yes, back to page 1
 
-        bit     dhr_flag        ; if DHR aux half, need to copy page to aux
+        bit     dhr_flags       ; if DHR aux half, need to copy page to aux
         bvc     exit            ; nope
-        copy8   #$80, dhr_flag
+        copy8   #$80, dhr_flags
 
         ;; Save ptr, X, Y
         lda     ptr
@@ -933,8 +932,8 @@ exit:   pla
 
         ;; --------------------------------------------------
 
-dhr_flag:
-        .byte   0
+dhr_flags:
+        .byte   0               ; bit7 = is dhr, bit6 = is aux page
 
 .endproc ; ShowPackedHRDHRFileImpl
 ShowPackedHRFile     := ShowPackedHRDHRFileImpl::hr_file
@@ -1008,11 +1007,11 @@ yes:    clc                     ; match!
 .proc ShowSHRImpl
 
 packed:
-        lda     #$80
-        SKIP_NEXT_2_BYTE_INSTRUCTION
+        sec
+        .byte   OPC_BCC         ; mask next byte (CLC)
 unpacked:
-        lda     #0
-        sta     packed_flag
+        clc
+        ror     packed_flag     ; set bit7
 
         ;; IIgs?
         bit     ROMIN2
@@ -1044,7 +1043,7 @@ is_iigs:
         clc                     ; success
         rts
 
-packed_flag:
+packed_flag:                    ; bit7
         .byte   0
 
         ;; --------------------------------------------------
@@ -1522,7 +1521,7 @@ fail:   jmp     Init
         dex
     WHILE_NOT_ZERO
 
-        copy8   #$80, seen_flag
+        SET_BIT7_FLAG seen_flag
         rts
 
 not_cur:
@@ -1548,7 +1547,7 @@ tmp:    .addr   0
 
 flags:                          ; bit 6 = modified, bit 7 = advance
         .byte   0
-seen_flag:
+seen_flag:                      ; bit7
         .byte   0
 cur_filename:
         .res    16

src/desk.acc/calendar.s
@@ -8,6 +8,7 @@
         RESOURCE_FILE "calendar.res"
 
         .include "apple2.inc"
+        .include "opcodes.inc"
         .include "../inc/apple2.inc"
         .include "../inc/macros.inc"
         .include "../inc/prodos.inc"
@@ -467,11 +468,10 @@ notpenXOR:      .byte   MGTK::notpenXOR
 
 .proc PaintWindow
 
-draw:   lda     #$80
-        SKIP_NEXT_2_BYTE_INSTRUCTION
-update: lda     #0
-        sta     full_flag
-
+draw:   sec
+        .byte   OPC_BCC         ; mask next byte (CLC)
+update: clc
+        ror     full_flag       ; set bit7
 
         ;; Defer if content area is not visible
         MGTK_CALL MGTK::GetWinPort, getwinport_params
@@ -515,7 +515,7 @@ update: lda     #0
 
         ;; Erase background if needed
         bit     full_flag
-    IF_PLUS
+    IF_NC
         MGTK_CALL MGTK::SetPenMode, notpencopy
         MGTK_CALL MGTK::PaintRect, rect_month_year
     END_IF
@@ -531,7 +531,7 @@ update: lda     #0
         ;; Grid lines
 
         bit     full_flag
-    IF_MINUS
+    IF_NS
         MGTK_CALL MGTK::SetPenMode, pencopy
         MGTK_CALL MGTK::SetPenSize, grid_pen
 
@@ -569,7 +569,7 @@ update: lda     #0
         ;; Day names
 
         bit full_flag
-    IF_MINUS
+    IF_NS
         copy8   #6, index
       DO
         lda     index
@@ -691,7 +691,7 @@ draw_date:
         ;; Left/right arrow buttons
 
         bit     full_flag
-    IF_MINUS
+    IF_NS
         BTK_CALL BTK::Draw, left_button
         BTK_CALL BTK::Draw, right_button
     END_IF

src/desk.acc/change.type.s
@@ -234,7 +234,7 @@ cursor_ibeam_flag: .byte   0
         RTS_IF_NS
 
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::ibeam
-        copy8   #$80, cursor_ibeam_flag
+        SET_BIT7_FLAG cursor_ibeam_flag
         rts
 .endproc ; SetCursorIBeam
 
@@ -243,7 +243,7 @@ cursor_ibeam_flag: .byte   0
         RTS_IF_NC
 
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::pointer
-        copy8   #0, cursor_ibeam_flag
+        CLEAR_BIT7_FLAG cursor_ibeam_flag
         rts
 .endproc ; SetCursorPointer
 .endproc ; HandleMouseMoved
@@ -389,24 +389,22 @@ yes:    clc
 ;;; No-op if type already focused
 .proc FocusType
         bit     auxtype_focused_flag
-        RTS_IF_NC
-
+    IF_NS
         LETK_CALL LETK::Deactivate, auxtype_le_params
         LETK_CALL LETK::Activate, type_le_params
-        copy8   #0, auxtype_focused_flag
-
+        CLEAR_BIT7_FLAG auxtype_focused_flag
+    END_IF
         rts
 .endproc ; FocusType
 
 ;;; No-op if auxtype already focused
 .proc FocusAuxtype
         bit     auxtype_focused_flag
-        RTS_IF_NS
-
+    IF_NC
         LETK_CALL LETK::Deactivate, type_le_params
         LETK_CALL LETK::Activate, auxtype_le_params
-        copy8   #$80, auxtype_focused_flag
-
+        SET_BIT7_FLAG auxtype_focused_flag
+    END_IF
         rts
 .endproc ; FocusAuxtype
 
@@ -455,9 +453,9 @@ yes:    clc
 
         lda     str_type
     IF_ZERO
-        copy8   #0, data::type_valid
+        CLEAR_BIT7_FLAG data::type_valid
     ELSE
-        copy8   #$80, data::type_valid
+        SET_BIT7_FLAG data::type_valid
         jsr     PadType
         lda     str_type+1
         ldx     str_type+2
@@ -467,9 +465,9 @@ yes:    clc
 
         lda     str_auxtype
     IF_ZERO
-        copy8   #0, data::auxtype_valid
+        CLEAR_BIT7_FLAG data::auxtype_valid
     ELSE
-        copy8   #$80, data::auxtype_valid
+        SET_BIT7_FLAG data::auxtype_valid
         jsr     PadAuxtype
         lda     str_auxtype+1
         ldx     str_auxtype+2
@@ -608,10 +606,10 @@ digits: .byte   "0123456789ABCDEF"
 
 ;;; Copied to/from aux
 .params data
-type_valid:     .byte   0
+type_valid:     .byte   0       ; bit7
 type:           .byte   SELF_MODIFIED_BYTE
 
-auxtype_valid:  .byte   0
+auxtype_valid:  .byte   0       ; bit7
 auxtype:        .word   SELF_MODIFIED
 .endparams
 .assert .sizeof(data) = .sizeof(aux::data), error, "size mismatch"
@@ -692,12 +690,12 @@ callback:
         ;; Rest - determine if same type/auxtype
         lda     gfi_params::file_type
       IF_A_NE   data::type
-        copy8   #0, data::type_valid
+        CLEAR_BIT7_FLAG data::type_valid
       END_IF
 
         ecmp16  gfi_params::aux_type, data::auxtype
       IF_NE
-        copy8   #0, data::auxtype_valid
+        CLEAR_BIT7_FLAG data::auxtype_valid
       END_IF
 
     END_IF

src/desk.acc/dos33.import.s
@@ -833,7 +833,7 @@ start:
         jmp     JUMP_TABLE_SHOW_ALERT
     END_IF
 
-        copy8   #0, dirty_flag
+        CLEAR_BIT7_FLAG dirty_flag
 
         JUMP_TABLE_MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::watch
         jsr     EnumerateDrives
@@ -1179,7 +1179,7 @@ start:
         ;; +$00 WORD address (low/high)
         ;; +$02 WORD length (low/high)
         copy16  RWTS_SECTOR_BUF+0, create_params::aux_type
-        copy8   #$80, set_eof_flag
+        SET_BIT7_FLAG set_eof_flag
         copy16  RWTS_SECTOR_BUF+2, set_eof_params::eof
         copy16  #RWTS_SECTOR_BUF+4, write_params::data_buffer
         copy16  #256-4, write_params::request_count
@@ -1191,7 +1191,7 @@ start:
         ;; +$00 WORD length (low/high)
         copy16  #$0801, create_params::aux_type
         copy16  RWTS_SECTOR_BUF+0, set_eof_params::eof
-        copy8   #$80, set_eof_flag
+        SET_BIT7_FLAG set_eof_flag
         copy16  #RWTS_SECTOR_BUF+2, write_params::data_buffer
         copy16  #256-2, write_params::request_count
         jmp     translate_type
@@ -1202,14 +1202,14 @@ start:
         ;; +$00 WORD length (low/high)
         copy16  #$0000, create_params::aux_type
         copy16  RWTS_SECTOR_BUF+0, set_eof_params::eof
-        copy8   #$80, set_eof_flag
+        SET_BIT7_FLAG set_eof_flag
         copy16  #RWTS_SECTOR_BUF+2, write_params::data_buffer
         copy16  #256-2, write_params::request_count
         jmp     translate_type
     END_IF
 
         copy16  #0, create_params::aux_type
-        copy8   #0, set_eof_flag
+        CLEAR_BIT7_FLAG set_eof_flag
         copy16  #RWTS_SECTOR_BUF, write_params::data_buffer
         copy16  #256, write_params::request_count
         FALL_THROUGH_TO translate_type
@@ -1288,7 +1288,7 @@ finish:
     END_IF
         JUMP_TABLE_MLI_CALL CLOSE, close_params
 
-        copy8   #$80, dirty_flag
+        SET_BIT7_FLAG dirty_flag
         return  #0              ; success
 
 ;;; C=1 if false
@@ -1357,7 +1357,7 @@ prodos_type_table:
 
 ;;; ============================================================
 
-dirty_flag:     .byte   0
+dirty_flag:     .byte   0       ; bit7
 
 prefix_path:    .res    ::kPathBufferSize, 0
 

src/desk.acc/find.files.s
@@ -145,7 +145,7 @@ pencopy:        .byte   MGTK::pencopy
 pensize_normal: .byte   1, 1
 pensize_frame:  .byte   kBorderDX, kBorderDY
 
-cursor_ibeam_flag: .byte   0
+cursor_ibeam_flag: .byte   0    ; bit7
 
 kBufSize = ::kMaxFilenameLength+1     ; max length = 15, length
 buf_search:     .res    kBufSize, 0 ; search term
@@ -417,7 +417,7 @@ search:
 
 finish:
         bit     cursor_ibeam_flag
-    IF_PLUS
+    IF_NC
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::pointer
     ELSE
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::ibeam
@@ -495,14 +495,14 @@ done:   jmp     InputLoop
 outside:
         bit     cursor_ibeam_flag
         bpl     done
-        copy8   #0, cursor_ibeam_flag
+        CLEAR_BIT7_FLAG cursor_ibeam_flag
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::pointer
         jmp     done
 
 inside:
         bit     cursor_ibeam_flag
         bmi     done
-        copy8   #$80, cursor_ibeam_flag
+        SET_BIT7_FLAG cursor_ibeam_flag
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::ibeam
 
 done:   jmp     InputLoop

src/desk.acc/joystick.s
@@ -313,7 +313,7 @@ notpencopy:     .byte   MGTK::notpencopy
         BTK_CALL BTK::RadioDraw, joy_btn1_button
         BTK_CALL BTK::RadioDraw, joy_btn2_button
 
-        copy8   #$80, force_draw_flag
+        SET_BIT7_FLAG force_draw_flag
         MGTK_CALL MGTK::ShowCursor
     END_IF
 
@@ -379,7 +379,7 @@ notpencopy:     .byte   MGTK::notpencopy
         lda     curr+InputState::pdl3
         cmp     last+InputState::pdl3
         beq     :+
-set:    copy8   #$80, joy2_valid_flag
+set:    SET_BIT7_FLAG joy2_valid_flag
 :
     END_IF
 
@@ -400,7 +400,7 @@ set:    copy8   #$80, joy2_valid_flag
         ;; --------------------------------------------------
 
         COPY_STRUCT InputState, curr, last
-        copy8   #0, force_draw_flag
+        CLEAR_BIT7_FLAG force_draw_flag
 
         ;; Defer if content area is not visible
         MGTK_CALL MGTK::GetWinPort, getwinport_params
@@ -483,11 +483,11 @@ last:   .tag InputState
 penOR:          .byte   MGTK::penOR
 notpencopy:     .byte   MGTK::notpencopy
 
-joy2_valid_flag:
+joy2_valid_flag:                ; bit7
         .byte   0
 .endproc ; DoJoystick
 
-force_draw_flag:
+force_draw_flag:                ; bit7
         .byte   0
 
 ;;; ============================================================

src/desk.acc/key.caps.s
@@ -532,7 +532,7 @@ tmp_poly:
         bcc     continue
 
         ;; Swap in alternate layout
-        copy8   #$80, extended_layout_flag
+        SET_BIT7_FLAG extended_layout_flag
         COPY_BLOCK empty_rect, rect_gap ; Gap is eliminated
         COPY_BLOCK rect_new_bshl, kr5C ; [\|] replaces Solid Apple
         COPY_BLOCK rect_new_bshl, kr7C
@@ -797,16 +797,17 @@ check:  sec
         ptr := $06
 
         cmp     #CHAR_RETURN
-        bne     normal
+    IF_EQ
         bit     extended_layout_flag
-        bpl     normal
-
+      IF_NS
         ;; Special key
         COPY_BYTES      2 + 7 * .sizeof(MGTK::Point), poly_new_ret_inner, tmp_poly
         rts
+      END_IF
+    END_IF
 
         ;; Rectangular key
-normal:
+
         ;; Compute address of rect
         sta     ptr
         copy8   #0, ptr+1

src/desk.acc/map.s
@@ -257,7 +257,7 @@ position_marker_bitmap:
 ;;; ============================================================
 ;;; Line Edit
 
-cursor_ibeam_flag: .byte   0
+cursor_ibeam_flag: .byte   0    ; bit7
 
 kBufSize = 16                       ; max length = 15, length
 buf_search:     .res    kBufSize, 0 ; search term
@@ -542,14 +542,14 @@ notpencopy:     .byte   MGTK::notpencopy
 outside:
         bit     cursor_ibeam_flag
         bpl     done
-        copy8   #0, cursor_ibeam_flag
+        CLEAR_BIT7_FLAG cursor_ibeam_flag
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::pointer
         jmp     done
 
 inside:
         bit     cursor_ibeam_flag
         bmi     done
-        copy8   #$80, cursor_ibeam_flag
+        SET_BIT7_FLAG cursor_ibeam_flag
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::ibeam
 
 done:   jmp     InputLoop
@@ -600,10 +600,10 @@ done:   jmp     InputLoop
 .proc DrawLatLong
         ;; Latitude
         copy16  lat, tmp
-        copy8   #0, sflag
+        CLEAR_BIT7_FLAG sflag
         bit     tmp+1
     IF_NS
-        copy8   #$80, sflag
+        SET_BIT7_FLAG sflag
         sub16   #0, tmp, tmp
     END_IF
 
@@ -622,10 +622,10 @@ done:   jmp     InputLoop
 
         ;; Longitude
         copy16  long, tmp
-        copy8   #0, sflag
+        CLEAR_BIT7_FLAG sflag
         bit     tmp+1
     IF_NS
-        copy8   #$80, sflag
+        SET_BIT7_FLAG sflag
         sub16   #0, tmp, tmp
     END_IF
 

commit c0734aa2dac4187bc60af6f03c7dd6a160566b72

Image Preview: Fix slideshow toggling
Broken in 54aef06 - a "previous state" check was still using BNE/BEQ

src/desk.acc/show.image.file.s
@@ -203,9 +203,9 @@ on_key:
         jeq     NextFile
 
     IF A = #'S'
-        cpy     #$00             ; Y = previous `slideshow_flag` state
-        bne     InputLoop        ; Ignore (so toggle) if slideshow mode was on
-        beq     SetSlideshowMode ; always
+        tya               ; Y = previous `slideshow_flag` state
+        bmi     InputLoop        ; Ignore (so toggle) if slideshow mode was on
+        bpl     SetSlideshowMode ; always
     END_IF
 
     IF A = #' '