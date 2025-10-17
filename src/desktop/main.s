a2stuff/a2d

commit ddade2b9a9f0f41e4c7124443299e5e847119a1e

Work towards aligning recursive copy routines (14 of N)
Remove redundant GET_FILE_INFO calls

DeskTop had an extra GET_FILE_INFO call when applying case bits, just
to determine if the source was a volume, d'oh!

The common code shared by DeskTop.system and Selector had an extra
GET_FILE_INFO call for "normal" files to apply to the target, but the
info had already been fetched.

This should speed up copies a bit.

src/desktop/main.s
@@ -10448,6 +10448,7 @@ retry:  jsr     GetSrcFileInfo
 
         ;; Copy directory contents
         jsr     ProcessDirectory
+        ;; TODO: Is this redundant with call in `CopyFinishDirectory`?
         jsr     GetAndApplySrcInfoToDst ; copy modified date/time
         jsr     MaybeFinishFileMove
 
@@ -10663,7 +10664,7 @@ existing_size:
         ;; Copy `create_date`/`create_time`
         COPY_STRUCT DateTime, src_file_info_params::create_date, create_params3::create_date
 
-        jsr     ReadSrcCaseBits
+        jsr     _ReadSrcCaseBits
 
         ;; If a volume, need to create a subdir instead
         lda     create_params3::storage_type
@@ -10715,7 +10716,7 @@ success:
         lda     case_bits
         ora     case_bits+1
     IF_NOT_ZERO
-        jsr     WriteDstCaseBits
+        jsr     _WriteDstCaseBits
     END_IF
 
         clc
@@ -10726,16 +10727,13 @@ failure:
         rts
 
 cancel: jmp     CloseFilesCancelDialogWithFailedResult
-.endproc ; _CopyCreateFile
 
 ;;; ============================================================
 ;;; Case Bits
+;;; Input: `src_file_info_params` pre-populated
 
-.proc ReadSrcCaseBits
+.proc _ReadSrcCaseBits
         copy16  #0, case_bits   ; best effort
-
-        jsr     GetSrcFileInfo
-        bcs     ret
 
         lda     src_file_info_params::storage_type
     IF_A_EQ     #ST_VOLUME_DIRECTORY
@@ -10756,9 +10754,9 @@ cancel: jmp     CloseFilesCancelDialogWithFailedResult
 
         clc                     ; success
 ret:    rts
-.endproc ; ReadSrcCaseBits
+.endproc ; _ReadSrcCaseBits
 
-.proc WriteDstCaseBits
+.proc _WriteDstCaseBits
         ldax    #dst_path_buf
         jsr     GetFileEntryBlock
         bcs     ret
@@ -10777,7 +10775,9 @@ ret:    rts
         MLI_CALL WRITE_BLOCK, block_params
 
 ret:    rts
-.endproc ; WriteDstCaseBits
+.endproc ; _WriteDstCaseBits
+
+.endproc ; _CopyCreateFile
 
 ;;; ============================================================
 ;;; Relink - swaps source and target, then deletes source.

src/lib/recursive_copy.s
@@ -397,12 +397,11 @@ eof:    return  #$FF
         MLI_CALL GET_FILE_INFO, get_dst_file_info_params
     IF_A_EQ_ONE_OF #ERR_FILE_NOT_FOUND, #ERR_VOL_NOT_FOUND, #ERR_PATH_NOT_FOUND
         ;; Get source dir info
-check_src:
-        MLI_CALL GET_FILE_INFO, get_src_file_info_params
+retry:  MLI_CALL GET_FILE_INFO, get_src_file_info_params
         bcc     gfi_ok
       IF_A_EQ_ONE_OF #ERR_VOL_NOT_FOUND, #ERR_FILE_NOT_FOUND
         jsr     OpInsertSource
-        jmp     check_src       ; retry
+        jmp     retry
       END_IF
     END_IF
 
@@ -420,13 +419,15 @@ gfi_ok:
 is_dir:
         sty     is_dir_flag
 
-        ;; copy `file_type`, `aux_type`, and `storage_type`
-        COPY_BYTES get_src_file_info_params::storage_type - get_src_file_info_params::file_type + 1, get_src_file_info_params::file_type, create_params::file_type
-
         jsr     _CheckSpaceAvailable
     IF_CS
         jmp     OpHandleNoSpace
     END_IF
+
+        ;; TODO: Use `_CopyCreateFile` here
+
+        ;; copy `file_type`, `aux_type`, and `storage_type`
+        COPY_BYTES get_src_file_info_params::storage_type - get_src_file_info_params::file_type + 1, get_src_file_info_params::file_type, create_params::file_type
 
         ;; Copy `create_date`/`create_time`
         COPY_STRUCT DateTime, get_src_file_info_params::create_date, create_params::create_date
@@ -572,6 +573,7 @@ existing_blocks:          ; Blocks taken by file that will be replaced
 ;;; Copy a normal (non-directory) file. File info is copied too.
 ;;; Inputs: `open_src_params` populated
 ;;;         `open_dst_params` populated; file already created
+;;;         `src_file_info_params` populated
 ;;; Errors: `OpHandleErrorCode` is invoked
 
 .proc _CopyNormalFile
@@ -623,8 +625,6 @@ close:  MLI_CALL CLOSE, close_dst_params
         MLI_CALL CLOSE, close_src_params
 
         ;; Copy file info
-        MLI_CALL GET_FILE_INFO, get_src_file_info_params
-        bcs     fail
         COPY_BYTES $B, get_src_file_info_params::access, get_dst_file_info_params::access
 
         copy8   #7, get_dst_file_info_params ; `SET_FILE_INFO` param_count

commit 0b6bfa4e41954898d24930d0c54ae3f546872805

DeskTop: Fix casing when copying volume to another volume
Regressed in ddade2b; by removing a GET_FILE_INFO call, DEVNUM was
no longer correct for getting the source vol case bits. Stash it
instead.

src/desktop/main.s
@@ -9931,6 +9931,7 @@ stack_index             .byte
 entry_index_in_block    .byte
 dir_data_buffer_end     .byte
 
+src_vol_devnum          .byte
 dst_vol_blocks_free     .word
         END_PARAM_BLOCK
         .assert dir_data_buffer_end - dir_data_buffer <= 256, error, "too big"
@@ -10437,6 +10438,7 @@ retry:  jsr     GetSrcFileInfo
         jsr     ShowErrorAlert
         jmp     retry
     END_IF
+        copy8   DEVNUM, src_vol_devnum
 
         jsr     _RecordDestVolBlocksFree
 
@@ -10834,7 +10836,7 @@ cancel:
         lda     src_file_info_params::storage_type
     IF A = #ST_VOLUME_DIRECTORY
         ;; Volume
-        copy8   DEVNUM, vol_key_block_params::unit_num
+        copy8   src_vol_devnum, vol_key_block_params::unit_num
         MLI_CALL READ_BLOCK, vol_key_block_params
         bcs     ret
         copy16  block_buffer + VolumeDirectoryHeader::case_bits, case_bits