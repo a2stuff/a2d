a2stuff/a2d

commit cb16d04822870993445399e300bbbd535bdc9159

Issue ON_LINE call after disconnecting /RAM
This is required per ProDOS Technical Reference Manual and Tech Notes;
it apparently tidies up some ProDOS internals.

src/disk_copy/loader.s
@@ -28,6 +28,8 @@ filename:   PASCAL_STRING kPathnameDiskCopy
 ;;; ============================================================
 
 start:
+        jsr     DisconnectRAM
+
         ;; Set stack pointers to arbitrarily low values for use when
         ;; interrupts occur. DeskTop does not utilize this convention,
         ;; so the values are set low so that interrupts which do (for
@@ -116,6 +118,13 @@ loop:   lda     (src),y
         bit     ROMIN2
         rts
 .endproc ; CopyToLc
+
+;;; ============================================================
+
+        saved_ram_unitnum := main__saved_ram_unitnum
+        saved_ram_drvec   := main__saved_ram_drvec
+        saved_ram_buffer: .res 16
+        .include "../lib/disconnect_ram.s"
 
 ;;; ============================================================
 

src/selector/loader.s
@@ -1,4 +1,4 @@
-;;; ============================================================
+;; ============================================================
 ;;; Loader
 ;;;
 ;;; Compiled as part of selector.s

src/desktop/init.s
@@ -1144,6 +1144,7 @@ trash_name:  PASCAL_STRING res_string_trash_icon_name
         .include "../lib/clear_dhr.s"
         saved_ram_unitnum := main::saved_ram_unitnum
         saved_ram_drvec   := main::saved_ram_drvec
+        saved_ram_buffer  := IO_BUFFER
         .include "../lib/disconnect_ram.s"
 
 ;;; ============================================================

src/disk_copy/auxlc.s
@@ -422,8 +422,7 @@ NoOp:   rts
 
 ;;; ============================================================
 
-init:   jsr     DisconnectRAM
-
+init:
         ;; DeskTop will have left a no-longer valid port selected,
         ;; so init a new port before we do anything else.
         MGTK_CALL MGTK::InitPort, grafport
@@ -1324,9 +1323,6 @@ match:  clc
 ;;; ============================================================
 
         .include "../lib/inttostring.s"
-        saved_ram_unitnum := main__saved_ram_unitnum
-        saved_ram_drvec   := main__saved_ram_drvec
-        .include "../lib/disconnect_ram.s"
 
         ;; TODO: Move these out of the `auxlc` scope
         .include "../toolkits/btk.s"

src/lib/disconnect_ram.s
@@ -3,6 +3,7 @@
 ;;; Requires:
 ;;; * `saved_ram_unitnum` (byte) to be defined
 ;;; * `saved_ram_drvec` (word) to be defined
+;;; * `saved_ram_buffer` (16 bytes) to be defined
 ;;; ============================================================
 
 .proc DisconnectRAM
@@ -21,7 +22,10 @@
         rts
 
         ;; Remove it, shuffle everything else down.
-remove: copy8   DEVLST,x, saved_ram_unitnum
+remove: lda     DEVLST,x
+        sta     saved_ram_unitnum
+        and     #UNIT_NUM_MASK
+        sta     on_line_params__unit_num
         copy16  RAMSLOT, saved_ram_drvec
         copy16  NODEV, RAMSLOT
 
@@ -34,10 +38,16 @@ shift:  lda     DEVLST+1,x
 
 :       dec     DEVCNT
 
-        ;; TODO: Issue ON_LINE call to device after disconnecting it
-        ;; to erases the VCB entry for the disconnected device, per
+        ;; Issue ON_LINE call to device after disconnecting it to
+        ;; erase the VCB entry for the disconnected device, per
         ;; Technical Note: ProDOS #8: Dealing with /RAM
-        ;; https://web.archive.org/web/2007/http://web.pdx.edu/~heiss/technotes/pdos/tn.pdos.08.html
+        ;; https://prodos8.com/docs/technote/08/
+        MLI_CALL ON_LINE, on_line_params
+        ;; Should fail with `ERR_DEVICE_NOT_CONNECTED` ($28)
 
         rts
+
+        DEFINE_ON_LINE_PARAMS on_line_params, SELF_MODIFIED_BYTE, saved_ram_buffer
+        on_line_params__unit_num := on_line_params::unit_num
+
 .endproc ; DisconnectRAM

src/selector/app.s
@@ -2008,6 +2008,7 @@ loop_counter:
         .include "../lib/speed.s"
         .include "../lib/bell.s"
         .include "../lib/clear_dhr.s"
+        saved_ram_buffer := $1C00
         .include "../lib/disconnect_ram.s"
         .include "../lib/reconnect_ram.s"
         .include "../lib/readwrite_settings.s"

commit 1eaa45772eefc64efea559d9c6878307c4da283c

Disk Copy: Correctly reconnect /RAM on exit
Regressed in cb16d04. That change moved when the stashed /RAM unit
number and DEVADR entry were recorded prior to the module segments
being loaded, so they were stomped on by the segment itself. Move the
call later.

src/disk_copy/loader.s
@@ -28,7 +28,6 @@ filename:   PASCAL_STRING kPathnameDiskCopy
 ;;; ============================================================
 
 start:
-        jsr     DisconnectRAM
 
         ;; Set stack pointers to arbitrarily low values for use when
         ;; interrupts occur. DeskTop does not utilize this convention,
@@ -66,6 +65,10 @@ start:
         bcs     fail
         MLI_CALL CLOSE, close_params
         bcs     fail
+
+        ;; Writes to `main::saved_ram_unitnum` etc. so must be after
+        ;; the segments are loaded.
+        jsr     DisconnectRAM
 
         sta     ALTZPON
         bit     LCBANK1