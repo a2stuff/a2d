a2stuff/a2d

commit 7fdc8d16274e19ea78bcbc44fc5b973a6d0e3c7f

DeskTop: Improve type-down and sorted view performance
Both of these operations rely on sorting, and a simple but inefficient
sorting algorithm was used - selection sort, which is O(n^2).

It actually isn't that terrible - the file dialog also relies on this
and it can sort a list of a hundred files in under 2 seconds at 1MHz.
But DeskTop has a lot more constant overhead per comparison since
there are many more indirections to the data. For type-down selection,
with a hundred files the sort would take 12 seconds!

Swapping in a Quicksort drops this down to the 2 second range, which
is still not great but a significant improvement.

src/desktop/main.s
@@ -4089,51 +4089,22 @@ typedown_buf:
 
 .proc GetKeyboardSelectableIconsSorted
         buffer := $1800
-        ptr1 := $06
-        ptr2 := $08
 
         jsr     GetKeyboardSelectableIcons
 
         RTS_IF X < #2
 
-        ;; Selection sort. In each outer iteration, the highest
-        ;; remaining element is moved to the end of the unsorted
-        ;; region, and the region is reduced by one. O(n^2)
-        dex
-        stx     outer
+        copy16  #GetNthSelectableIconName, Quicksort_GetPtrProc
+        copy16  #CompareStrings, Quicksort_CompareProc
+        copy16  #_SwapProc, Quicksort_SwapProc
 
-    DO
-        outer := *+1
-        lda     #SELF_MODIFIED_BYTE
-        jsr     GetNthSelectableIconName
-        stax    ptr2
+        txa
+        jmp     Quicksort       ; A = count
 
-        copy8   #0, inner
-      DO
-        inner := *+1
-        lda     #SELF_MODIFIED_BYTE
-        jsr     GetNthSelectableIconName
-        stax    ptr1
-
-        jsr     CompareStrings
-       IF GE
-        ;; Swap
-        ldx     inner
-        ldy     outer
+.proc _SwapProc
         swap8   buffer+1,x, buffer+1,y
-        tya
-        jsr     GetNthSelectableIconName
-        stax    ptr2
-       END_IF
-
-        inc     inner
-        lda     inner
-      WHILE A <> outer
-
-        dec     outer
-    WHILE NOT_ZERO
-
         rts
+.endproc ; _SwapProc
 .endproc ; GetKeyboardSelectableIconsSorted
 
 ;;; Assuming selectable icon buffer at $1800 is populated by the
@@ -7977,7 +7948,6 @@ records_base_ptr:
 ;;; Inputs: A=DeskTopSettings::kViewBy* for `cached_window_id`
 
 .proc SortRecords
-        ptr := $06
 
 list_start_ptr  := $801
 num_records     := $803
@@ -7992,34 +7962,17 @@ scratch_space   := $804         ; can be used by comparison funcs
 
         lda     cached_window_id
         jsr     GetFileRecordListForWindow
-        stax    ptr             ; point past the count
         stax    list_start_ptr
-        inc16   ptr
         inc16   list_start_ptr
 
-        ;; --------------------------------------------------
-        ;; Selection sort
+        copy16  #_CalcPtr, Quicksort_GetPtrProc
+        copy16  #_CompareProc, Quicksort_CompareProc
+        copy16  #_SwapProc, Quicksort_SwapProc
 
-        ptr1 := $06
-        ptr2 := $08
+        lda     num_records
+        jmp     Quicksort
 
-        ldx     num_records
-        dex
-        stx     outer
-    DO
-        outer := *+1
-        lda     #SELF_MODIFIED_BYTE
-        jsr     _CalcPtr
-        stax    ptr2
-
-        copy8   #0, inner
-
-      DO
-        inner := *+1
-        lda     #SELF_MODIFIED_BYTE
-        jsr     _CalcPtr
-        stax    ptr1
-
+.proc _CompareProc
         bit     LCBANK2
         bit     LCBANK2
         jsr     _CompareFileRecords
@@ -8027,30 +7980,18 @@ scratch_space   := $804         ; can be used by comparison funcs
         bit     LCBANK1
         bit     LCBANK1
         plp
-
-       IF GE
-        ;; Swap
-        ldx     inner
-        ldy     outer
-        swap8   cached_window_entry_list,x, cached_window_entry_list,y
-
-        lda     outer
-        jsr     _CalcPtr
-        stax    ptr2
-       END_IF
-
-        inc     inner
-        lda     inner
-      WHILE A <> outer
-
-        dec     outer
-    WHILE NOT_ZERO
-
         rts
+.endproc ; _CompareProc
+
+.proc _SwapProc
+        swap8   cached_window_entry_list,x, cached_window_entry_list,y
+        rts
+.endproc ; _SwapProc
 
 ;;; --------------------------------------------------
 ;;; Input: A = index in list being sorted
 ;;; Output: A,X = pointer to FileRecord
+;;; Assert: LCBANK1 banked in so `cached_window_entry_list` is visible
 
 .proc _CalcPtr
         ;; Map from sorting list index to FileRecord index
@@ -8185,9 +8126,13 @@ done:   rts
         copy16  #scratch, $06
         copy16  #str_file_type, $08
         jsr     CompareStrings
+        php                     ; preserve Z,C
+        pla
         jsr     PopPointers
         bit     LCBANK2
         bit     LCBANK2
+        pha
+        plp                     ; restore Z,C
 
         rts
 
@@ -14903,6 +14848,7 @@ END_PARAM_BLOCK
         .include "../lib/speed.s"
         .include "../lib/bell.s"
         .include "../lib/uppercase.s"
+        .include "../lib/quicksort.s"
 
 ;;; ============================================================
 ;;; Resources (that are only used from Main, i.e. not MGTK)

src/desktop/desktop.s
@@ -33,7 +33,7 @@
         DEFSEG SegmentLoader,      $2000, $0200
         DEFSEG SegmentDeskTopAux,  $4000, $8000
         DEFSEG SegmentDeskTopLC,   $D000, $2200
-        DEFSEG SegmentDeskTopMain, $4000, $6D00
+        DEFSEG SegmentDeskTopMain, $4000, $6E00
         DEFSEG SegmentInitializer, $0800, $0800
         DEFSEG SegmentInvoker,     $0290, $0160
 

src/lib/file_dialog.s
@@ -1324,6 +1324,14 @@ finish:
         ;; --------------------------------------------------
         ;; Selection sort
 
+        ;; Why not Quicksort? It turns out that for the limited number
+        ;; of files we have here (<= 128), with the low overhead to
+        ;; access the names (simple shift and add) this can sort all
+        ;; of the names in under 2 seconds even with O(n^2). In
+        ;; contrast, DeskTop has more complex storage which
+        ;; dramatically increases the constant overhead, so a more
+        ;; efficient sort is necessary.
+
         ptr1 := $06
         ptr2 := $08
 

src/lib/quicksort.s
@@ -1,0 +1,190 @@
+;;; ============================================================
+;;; Quicksort Routine
+;;;
+;;; Pretty much uses the pseudocode from Wikipedia
+;;; * Only does true recursion on the smaller partition to avoid
+;;;   blowing out the stack. Tail recursion handles the other.
+;;; * "Fat partition" scheme is used
+;;;
+;;; Caller sets these procs:
+;;; * `Quicksort_GetPtrProc` - In: A = index; Out: A,X = ptr
+;;; * `Quicksort_CompareProc` - In: $06,$08 ptrs to compare; Out: Z, C
+;;; * `Quicksort_SwapProc` - In: X,Y
+;;;
+;;; Input: A = number of items
+;;;
+;;; Uses $06/$08 and $20... $2F
+
+.proc Quicksort
+        ;; --------------------------------------------------
+        ;; Quicksort
+
+        tay
+        dey                     ; Y = upper index
+        ldx     #0              ; X = lower index
+        FALL_THROUGH_TO quicksort
+
+        lo   := $20
+        hi   := $21
+        lt   := $22
+        eq   := $23
+        gt   := $24
+        s1   := $25
+
+        ;; Input: X = lower index, Y = upper index
+.proc quicksort
+
+        ;; X is lo, Y is hi
+        stx     lo
+        sty     hi
+
+        ;; while lo < hi
+        RTS_IF X >= hi
+
+        ;; lt, gt := partition(lo, hi)
+        jsr     partition
+
+        ldx     lo
+        ldy     hi
+
+        ;; Size of lower partition
+        ;; s1 := (lt - 1) - lo
+        lda     lt
+        sec
+        sbc     lo              ; Assert: no borrow, so leaves C=1
+    IF ZERO
+        ldx     gt              ; partition empty, so do other
+        jmp     tc1
+    END_IF
+        sbc     #1
+        sta     s1
+
+        ;; Size of upper partition
+        ;; s2 := hi - (gt + 1)
+        tya                     ; A = hi
+        sec
+        sbc     gt              ; Assert: no borrow, so leaves C=1
+    IF ZERO
+        ldy     lt              ; partition empty, so do other
+        jmp     tc2
+    END_IF
+        sbc     #1              ; A = s2
+
+        ;; Recurse on smaller partition
+        cmp     s1
+    IF GE
+        tya
+        pha                     ; A = hi
+        lda     gt
+        pha                     ; A = gt
+
+        ;; recurse quicksort(lo, lt - 1)
+        ldy     lt
+        dey                     ; Y = part - 1
+        jsr     quicksort       ; X still lo
+
+        ;; tail-call quicksort(gt + 1, hi)
+        pla                     ; A = gt
+        tax
+        pla                     ; A = hi
+        tay                     ; Y = hi
+tc1:    inx                     ; X = gt + 1
+        jmp     quicksort
+    END_IF
+
+        txa
+        pha                     ; A = lo
+        lda     lt
+        pha                     ; A = lt
+
+        ;; recurse quicksort(gt + 1, hi)
+        ldx     gt
+        inx                     ; X = gt + 1
+        jsr     quicksort       ; Y still hi
+
+        ;; tail-call quicksort(lo, lt - 1)
+        pla                     ; A = lt
+        tay
+        pla                     ; A = lo
+        tax                     ; X = lo
+tc2:    dey                     ; Y = lt - 1
+        jmp     quicksort
+
+.endproc ; quicksort
+
+        ;; Input: X = `lo` = lower index, Y = `hi` = upper index
+        ;; Output: `lt`, `gt`
+.proc partition
+        pivot := $06
+        temp := $08
+
+        ;; lt := lo
+        stx     lt
+        ;; eq := lo
+        stx     eq
+        ;; gt := hi
+        sty     gt
+
+        ;; pivot := A[(lo + hi) / 2]
+        txa
+        clc
+        adc     hi              ; 9-bit result in A,C
+        ror                     ; /= 2 yields 8 bit result
+        jsr     getptr
+        stax    pivot
+
+        ;; while eq <= gt do
+loop:
+        lda     gt
+        cmp     eq
+        RTS_IF LT
+
+        lda     eq
+        jsr     getptr
+        stax    temp
+
+        CompareProc := *+1
+        jsr     SELF_MODIFIED
+
+        ;; if A[eq] = pivot then
+    IF EQ
+        ;; eq := eq + 1
+        inc     eq
+        jmp     loop
+    END_IF
+
+        ;; if A[eq] < pivot then
+    IF GE
+        ;; swap A[eq] with A[lt]
+        ldx     eq
+        ldy     lt
+        jsr     swap
+        ;; lt := lt + 1
+        inc     lt
+        ;; eq := eq + 1
+        inc     eq
+        jmp     loop
+    END_IF
+
+        ;; if A[eq] > pivot then
+        ;; swap A[eq] with A[gt]
+        ldx     eq
+        ldy     gt
+        jsr     swap
+        ;; gt := gt - 1
+        dec     gt
+        jmp     loop
+.endproc ; partition
+
+        GetPtrProc := *+1
+getptr: jmp     SELF_MODIFIED
+
+        CompareProc := partition::CompareProc
+
+        SwapProc := *+1
+swap:   jmp     SELF_MODIFIED
+
+.endproc
+Quicksort_GetPtrProc := Quicksort::GetPtrProc
+Quicksort_CompareProc := Quicksort::CompareProc
+Quicksort_SwapProc := Quicksort::SwapProc

commit 77e53decb2ca35760e2b04637d43d65b7cd777a4

DeskTop: Fix View > by Date ordering
Regressed in 7fdc8d1 because cmp16 was used, but the Quicksort
implementation requires C and Z to be valid for the comparison,
not just C.

src/desktop/main.s
@@ -8113,7 +8113,7 @@ done:   rts
       END_IF
 
         ;; Compare!
-        cmp16   size2, size1 ; order descending
+        ecmp16  size2, size1 ; order descending, with Z and C
         rts
     END_IF
 