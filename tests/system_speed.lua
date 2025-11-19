d5003fd2ccaa1a9944fe47ca7b0737e5fcb80188
Joshua Bell <inexorabletash@gmail.com>
2025-11-18 20:45:08 -0800

    system speed

---
+++ tests/system_speed.lua
@@ -0,0 +1,96 @@
+--[[ BEGINCONFIG ========================================
+
+MODEL="apple2c4"
+MODELARGS="-ramsize 1152K -gameio joy"
+DISKARGS="-flop1 out/A2DeskTop-1.6-alpha0-en_140k_disk1.po -flop2 out/A2DeskTop-1.6-alpha0-en_140k_disk2.po"
+
+======================================== ENDCONFIG ]]--
+
+package.path = emu.subst_env("$LUA_PATH") .. ";" .. package.path
+
+-- Run in an async context
+local c = coroutine.create(function()
+    emu.wait(1/60) -- allow logging to get ready
+
+    -- "Globals"
+    local machine = manager.machine
+
+    -- Dependencies
+    local test = require("test")
+    local apple2 = require("apple2")
+    local a2d = require("a2d")
+    a2d.InitSystem() -- async; outside require
+
+    --[[============================================================
+
+      "System Speed" tests
+
+    ============================================================]]--
+
+    -- Wait for DeskTop to start
+    a2d.WaitForRestart()
+    emu.wait(50) -- floppy drives are slow
+
+    test.Step(
+      "Normal + OK doesn't crash",
+      function()
+        a2d.OpenPath("/A2.DESKTOP.2/APPLE.MENU/CONTROL.PANELS/SYSTEM.SPEED")
+        emu.wait(5) -- floppy drives are slow
+        apple2.Type("N")
+        a2d.DialogOK()
+        a2d.CloseAllWindows()
+        -- TODO: Expect still running
+        return test.PASS
+    end)
+
+    test.Step(
+      "Fast + OK doesn't crash",
+      function()
+        a2d.OpenPath("/A2.DESKTOP.2/APPLE.MENU/CONTROL.PANELS/SYSTEM.SPEED")
+        emu.wait(5) -- floppy drives are slow
+        apple2.Type("F")
+        a2d.DialogOK()
+        a2d.CloseAllWindows()
+        -- TODO: Expect still running
+        return test.PASS
+    end)
+
+    test.Step(
+      "IIc - speed doesn't affect DHR display",
+      function()
+        a2d.OpenPath("/A2.DESKTOP.2/APPLE.MENU/CONTROL.PANELS/SYSTEM.SPEED")
+        emu.wait(5) -- floppy drives are slow
+        apple2.Type("N")
+        apple2.Type("F")
+        test.Snap()
+        test.Expect(apple2.ReadSSW("RDDHIRES") < 128, "Should still be in DHR mode")
+        a2d.CloseAllWindows()
+        return test.PASS
+    end)
+
+    test.Step(
+      "Animation shields cursor correctly",
+      function()
+        a2d.OpenPath("/A2.DESKTOP.2/APPLE.MENU/CONTROL.PANELS/SYSTEM.SPEED")
+        emu.wait(5) -- floppy drives are slow
+        a2d.EnterMouseKeysMode()
+        a2d.MouseKeysGoToApproximately(115,124)
+        for i=1,15 do
+          a2d.MouseKeysUp(2)
+          emu.wait(0.15)
+          test.Snap("visually confirm no garbage")
+          a2d.MouseKeysDown(2)
+          emu.wait(0.15)
+          test.Snap("visually confirm no garbage")
+        end
+        a2d.CloseAllWindows()
+        a2d.ExitMouseKeysMode()
+        return test.PASS
+    end)
+
+
+    --------------------------------------------------
+
+    os.exit(0)
+end)
+coroutine.resume(c)
