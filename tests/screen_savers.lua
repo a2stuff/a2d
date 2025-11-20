857578458d4dce439a5a23d0ea8dbaaadf51dd67
Joshua Bell <inexorabletash@gmail.com>
2025-11-20 15:35:24 -0800

    Streamlining

--- tests/screen_savers.lua
+++ tests/screen_savers.lua
@@ -22,38 +22,29 @@ local c = coroutine.create(function()
     -- Wait for DeskTop to start
     a2d.WaitForRestart()
 
-    test.Step(
-      "Melt - File > Open does not leave File menu highlighted",
-      function()
+    test.Variants(
+      {
+        "Melt - File > Open does not leave File menu highlighted",
+        "Melt - Apple-Down does not leave File menu highlighted",
+      },
+      function(idx)
         a2d.OpenPath("/A2.DESKTOP/APPLE.MENU/SCREEN.SAVERS")
         apple2.Type("MELT")
-        a2d.OAShortcut("O")
-        emu.wait(1)
 
-        a2d.InMouseKeysMode(function(m) m.Click() end)
-        a2d.WaitForRepaint()
-
-        for i = 1,39 do
-          test.ExpectEquals(apple2.ReadRAMDevice(0x2000+i), 0x7F, "Menu should not be highlighted")
+        if idx == 1 then
+          a2d.OAShortcut("O")
+        else
+          a2d.OADown()
         end
-        a2d.CloseAllWindows()
-        return test.PASS
-    end)
 
-    test.Step(
-      "Melt - Apple-Down does not leave File menu highlighted",
-      function()
-        a2d.OpenPath("/A2.DESKTOP/APPLE.MENU/SCREEN.SAVERS")
-        apple2.Type("MELT")
-
-        a2d.OADown()
         emu.wait(1)
 
         a2d.InMouseKeysMode(function(m) m.Click() end)
         a2d.WaitForRepaint()
 
-        for i = 1,39 do
-          test.ExpectEquals(apple2.ReadRAMDevice(0x2000+i), 0x7F, "Menu should not be highlighted")
+        -- cursor is in leftmost byte
+        for col = 1,79 do
+          test.ExpectEquals(apple2.GetDoubleHiresByte(0, col), 0x7F, "Menu should not be highlighted")
         end
         a2d.CloseAllWindows()
         return test.PASS
@@ -70,51 +61,42 @@ local c = coroutine.create(function()
         apple2.EscapeKey()
         apple2.ControlKey('@') -- no-op, wait for key to be consumed
 
-        test.ExpectNotEquals(apple2.GetHiresByte(4, 38), 0x7F, "Clock should be visible already")
+        test.ExpectNotEquals(apple2.GetDoubleHiresByte(4, 78), 0x7F, "Clock should be visible already")
 
         a2d.CloseAllWindows()
         return test.PASS
     end)
 
-    test.Step(
-      "Matrix exits on click",
-      function()
+    test.Variants(
+      {
+        "Matrix exits on click",
+        "Matrix exits on key",
+      },
+      function(idx)
         a2d.OpenPath("/A2.DESKTOP/APPLE.MENU/SCREEN.SAVERS/MATRIX")
         emu.wait(1)
 
-        a2d.InMouseKeysMode(function(m) m.Click() end)
+        if idx == 1 then
+          a2d.InMouseKeysMode(function(m) m.Click() end)
+        else
+          apple2.ReturnKey()
+        end
+
         a2d.WaitForRepaint()
-
         a2d.CloseAllWindows()
         return test.PASS
     end)
 
-    test.Step(
-      "Matrix exits on key",
-      function()
-        a2d.OpenPath("/A2.DESKTOP/APPLE.MENU/SCREEN.SAVERS/MATRIX")
-        emu.wait(1)
-
-        apple2.ReturnKey()
-
-        a2d.CloseAllWindows()
-        return test.PASS
-    end)
-
-    function RemoveClockDriver()
-      a2d.OpenPath("/A2.DESKTOP/EXTRAS/BASIC.SYSTEM")
-      a2d.WaitForRestart()
-      apple2.TypeLine("DELETE /A2.DESKTOP/CLOCK.SYSTEM")
-      apple2.TypeLine("PR#7")
-      a2d.WaitForRestart()
-    end
-
+    -- Remove clock driver
+    a2d.OpenPath("/A2.DESKTOP/EXTRAS/BASIC.SYSTEM")
+    a2d.WaitForRestart()
+    apple2.TypeLine("DELETE /A2.DESKTOP/CLOCK.SYSTEM")
+    apple2.TypeLine("PR#7")
+    a2d.WaitForRestart()
 
     test.Step(
       "Analog Clock shows alert if there is no system clock",
       function()
-        RemoveClockDriver()
-
         a2d.OpenPath("/A2.DESKTOP/APPLE.MENU/SCREEN.SAVERS/ANALOG.CLOCK")
         test.Snap("verify alert shown")
         a2d.DialogOK()
@@ -125,8 +107,6 @@ local c = coroutine.create(function()
     test.Step(
       "Digital Clock shows alert if there is no system clock",
       function()
-        RemoveClockDriver()
-
         a2d.OpenPath("/A2.DESKTOP/APPLE.MENU/SCREEN.SAVERS/DIGITAL.CLOCK")
         test.Snap("verify alert shown")
         a2d.DialogOK()
