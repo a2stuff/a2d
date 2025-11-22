f0f8cca8ee258718147bf44620ee73c9d1d0c909
Joshua Bell <inexorabletash@gmail.com>
2025-11-21 21:57:07 -0800

    Add joystick reading routines to apple2

--- tests/joystick.lua
+++ tests/joystick.lua
@@ -6,31 +6,20 @@ DISKARGS="-hard1 $HARDIMG"
 
 ======================================== ENDCONFIG ]]--
 
-function SetJoy1(x,y)
-  machine.ioport.ports[":gameio:joy:joystick_1_x"].fields["P1 Joystick X"]:set_value(x)
-  machine.ioport.ports[":gameio:joy:joystick_1_y"].fields["P1 Joystick Y"]:set_value(y)
-  emu.wait(1/10)
-end
-function SetJoy2(x,y)
-  machine.ioport.ports[":gameio:joy:joystick_2_x"].fields["P2 Joystick X"]:set_value(x)
-  machine.ioport.ports[":gameio:joy:joystick_2_y"].fields["P2 Joystick Y"]:set_value(y)
-  emu.wait(1/10)
-end
-
 test.Step(
   "Joystick Limits",
   function()
     a2d.OpenPath("/A2.DESKTOP/APPLE.MENU/CONTROL.PANELS/JOYSTICK")
 
-    SetJoy1(0,0)
+    apple2.SetJoy1(0,0)
     test.Snap("verify indicator in top left")
-    SetJoy1(255,0)
+    apple2.SetJoy1(255,0)
     test.Snap("verify indicator in top right")
-    SetJoy1(255,255)
+    apple2.SetJoy1(255,255)
     test.Snap("verify indicator in bottom right")
-    SetJoy1(0,255,0)
+    apple2.SetJoy1(0,255,0)
     test.Snap("verify indicator in bottom left")
-    SetJoy1(128,128,0)
+    apple2.SetJoy1(128,128,0)
     test.Snap("verify indicator in center")
 
     a2d.CloseWindow()
@@ -41,8 +30,8 @@ test.Step(
   function()
     a2d.OpenPath("/A2.DESKTOP/APPLE.MENU/CONTROL.PANELS/JOYSTICK")
 
-    SetJoy1(64,64)
+    apple2.SetJoy1(64,64)
     test.Snap("verify single indicator")
-    SetJoy2(192, 192)
+    apple2.SetJoy2(192, 192)
     test.Snap("verify second indicator")
 end)
