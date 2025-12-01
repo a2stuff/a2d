a2stuff/a2d

commit 05ef150d0fe3666b943d9dd24b51d59f4d9cafad

DeskTop: Draw volume icons during load/check for better feedback
Previously, during initialization and during Special > Check All
Drives, the new volume icons were drawn as a batch after the device
iteration was complete.

To provide better progress feedback during startup, draw the icons
as we go.

src/desktop/init.s
@@ -286,6 +286,7 @@ done:
         lda     get_icon_entry_params::id
         sta     main::trash_icon_num
         sta     cached_window_entry_list
+        sta     icon_param
         ldax    get_icon_entry_params::addr
         stax    ptr
 
@@ -308,6 +309,8 @@ done:
         inx
     WHILE_X_NE  trash_name
         copy8   trash_name,x, (ptr),y
+
+        ITK_CALL IconTK::DrawIcon, icon_param
 
         FALL_THROUGH_TO LoadSelectorList
 .endproc ; CreateTrashIcon
@@ -671,7 +674,9 @@ end:
       END_IF
 
 done_create:
-        ;; TODO: Draw icons as we add them.
+        ldx     cached_window_entry_count
+        copy8   cached_window_entry_list-1,x, icon_param
+        ITK_CALL IconTK::DrawIcon, icon_param
 
 next:
         pla
@@ -970,19 +975,6 @@ unit_num:
         MGTK_CALL MGTK::CheckEvents
         MGTK_CALL MGTK::SetCursor, MGTK::SystemCursor::pointer
         copy8   #0, active_window_id
-
-        ;; Add desktop icons
-        ldx     #0
-    DO
-        BREAK_IF_X_EQ cached_window_entry_count
-        txa
-        pha
-        copy8   cached_window_entry_list,x, icon_param
-        ITK_CALL IconTK::DrawIcon, icon_param
-        pla
-        tax
-        inx
-    WHILE_NOT_ZERO              ; always
 
         ;; Desktop icons are cached now
         copy8   #0, cached_window_id

src/desktop/main.s
@@ -5005,30 +5005,12 @@ ScrollUpdate    := ScrollManager::ActivateCtlsSetThumbs
         copy8   #kErrDuplicateVolName, pending_alert
       END_IF
 
-        ;; TODO: Draw icons as we add them.
+        ldx     cached_window_entry_count
+        copy8   cached_window_entry_list-1,x, icon_param
+        ITK_CALL IconTK::DrawIcon, icon_param
 
         dec     devlst_index
     WHILE_POS
-.endscope
-
-        ;; --------------------------------------------------
-        ;; Add them to IconTK
-.scope
-        ;; Assert: `cached_window_entry_count` > 0, c/o trash
-        ldx     #0
-    DO
-        txa
-        pha
-        lda     cached_window_entry_list,x
-      IF_A_NE   trash_icon_num
-        sta     icon_param
-        ITK_CALL IconTK::DrawIcon, icon_param
-      END_IF
-
-        pla
-        tax
-        inx
-    WHILE_X_NE  cached_window_entry_count
 
         lda     pending_alert
     IF_NOT_ZERO

commit c0e2020691763e365ea84066acb849300bfd3369

DeskTop: Fix volume icon flicker on launch with empty Disk II drives
Regression introduced in 05ef150d (new in v1.5); a bad branch when an
empty Disk II was detected caused the most recent volume icon to be
repainted.

Reorganize the function to use better flow control constructs.

src/desktop/init.s
@@ -643,28 +643,27 @@ end:
         pha                     ; A = unmasked unit number
 
         jsr     main::CreateVolumeIcon ; A = unmasked unit number, Y = device index
-      IF A = #ERR_DEVICE_NOT_CONNECTED
+
+      IF ZERO
+        ;; Success! Draw it.
+        ldx     cached_window_icon_count
+        copy8   cached_window_icon_list-1,x, icon_param
+        ITK_CALL IconTK::DrawIcon, icon_param
+      ELSE_IF A = #ERR_DEVICE_NOT_CONNECTED
         ;; If device is not connected, remove it from DEVLST
         ;; unless it's a Disk II.
         pla                     ; A = unmasked unit number
         pha                     ; A = unmasked unit number
         ;; NOTE: Not masked with `UNIT_NUM_MASK`, `IsDiskII` handles it.
         jsr     main::IsDiskII
-        beq     done_create     ; skip
+       IF ZC
         CALL    _RemoveDevice, X=device_index
-        jmp     next
-      END_IF
-
-      IF A = #ERR_DUPLICATE_VOLUME
+       END_IF
+      ELSE_IF A = #ERR_DUPLICATE_VOLUME
+        ;; If duplicate, show a special error.
         copy8   #kErrDuplicateVolName, main::pending_alert
       END_IF
 
-done_create:
-        ldx     cached_window_icon_count
-        copy8   cached_window_icon_list-1,x, icon_param
-        ITK_CALL IconTK::DrawIcon, icon_param
-
-next:
         pla
         dec     device_index
     WHILE POS

src/desktop/main.s
@@ -67,7 +67,6 @@ JT_GET_TICKS:           jmp     GetTickCount            ; *
 ;;; Main event loop for the application
 
 .proc MainLoop
-
         ;; Poll drives every Nth time `SystemTask` does its thing.
         ;; At 1MHz on a //e this is about once every 3 seconds.
         kDrivePollFrequency = 35