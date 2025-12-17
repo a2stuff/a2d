a3ee221bae2a8f9451bca8ad3aaf05865902ac6f
Joshua Bell <inexorabletash@gmail.com>
2025-12-17 10:25:00 -0800

    Tests: Another good test case for the launcher

---
+++ tests/launcher/std_80col_card.lua
@@ -0,0 +1,27 @@
+--[[ BEGINCONFIG ========================================
+
+MODEL="apple2ee"
+MODELARGS="-sl2 mouse -aux std80"
+DISKARGS="-flop1 res/ProDOS_2_4_3.po -flop2 $FLOP1IMG"
+WAITFORDESKTOP="false"
+CHECKAUXMEMORY="false"
+
+======================================== ENDCONFIG ]]
+
+--[[
+  Configure an Apple IIe system with no card in the Aux slot. Invoke
+  `DESKTOP.SYSTEM` from a launcher (e.g. Bitsy Bye). Verify the
+  launcher is restarted and does not crash or hang.
+]]
+test.Step(
+  "Running with nothing in the Aux slot",
+  function()
+    apple2.WaitForBitsy()
+    apple2.BitsySelectSlotDrive("S6,D2")
+    apple2.BitsyInvokeFile("DESKTOP.SYSTEM")
+    while not apple2.GrabTextScreen():match("PRESS A KEY TO QUIT") do
+      emu.wait(0.25)
+    end
+    apple2.Type("A") -- too literal?
+    apple2.WaitForBitsy()
+end)
