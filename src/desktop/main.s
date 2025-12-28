a2stuff/a2d

commit 22975d4b9adcc21ff5ee766518d24e09d6e25b54

DeskTop: Make drag selection more responsive
Replace more "map icons one by one" logic (which has significant
overhead) with remapping all icons in a window. And do CheckEvents
when rejecting an icon to keep the cursor responsive, e.g. when
there are a hundred files in the window.

Factor out a helper for mapping an individual point, to save space.

src/desktop/main.s
@@ -2958,21 +2958,14 @@ concatenate:
 ;;; Assert: Icon in active window.
 
 .proc ScrollIconIntoView
-        pha
-        jsr     LoadActiveWindowEntryTable
-        pla
         sta     icon_param
-
-        ;; Map coordinates to window
-        pha                     ; A = icon
-        jsr     IconScreenToWindow
+        jsr     LoadActiveWindowEntryTable
 
         ;; Grab the icon bounds
         ITK_CALL IconTK::GetIconBounds, icon_param ; inits `tmp_rect`
-
-        ;; Restore coordinates
-        pla                     ; A = icon
-        jsr     IconWindowToScreen
+        jsr     PrepActiveWindowScreenMapping
+        CALL    MapCoordsScreenToWindow, AX=#tmp_rect::topleft
+        CALL    MapCoordsScreenToWindow, AX=#tmp_rect::bottomright
 
         viewport := window_grafport+MGTK::GrafPort::maprect
 
@@ -2992,11 +2985,11 @@ concatenate:
         copy8   #0, dirty
         ldx     #2              ; loop over dimensions
     DO
-        ;; Is left of icon beyond window? If so, adjust by delta (negative)
+        ;; Is left/top of icon beyond window? If so, adjust by delta (negative)
         sub16   tmp_rect::topleft,x, viewport+MGTK::Rect::topleft,x, delta
         bmi     adjust
 
-        ;; Is right of icon beyond window? If so, adjust by delta (positive)
+        ;; Is right/bottom of icon beyond window? If so, adjust by delta (positive)
         sub16   tmp_rect::bottomright,x, viewport+MGTK::Rect::bottomright,x, delta
         bmi     done
 
@@ -4409,15 +4402,13 @@ _Preamble:
         jsr     ComputeIconsBBox
         CALL    PrepWindowScreenMapping, A=cached_window_id
 
+        CALL    MapCoordsScreenToWindow, AX=#iconbb_rect+MGTK::Rect::topleft
+        CALL    MapCoordsScreenToWindow, AX=#iconbb_rect+MGTK::Rect::bottomright
+
         ldx     #2              ; loop over dimensions
     DO
         ;; stash bbox dimensions before union below
         sub16   iconbb_rect+MGTK::Rect::bottomright,x, iconbb_rect+MGTK::Rect::topleft,x, bbox_size,x
-
-        ;; offset bbox
-        add16   map_delta_x,x, iconbb_rect+MGTK::Rect::topleft,x, iconbb_rect+MGTK::Rect::topleft,x
-        add16   map_delta_x,x, iconbb_rect+MGTK::Rect::bottomright,x, iconbb_rect+MGTK::Rect::bottomright,x
-
         dex                     ; next dimension
         dex
     WHILE POS
@@ -5329,6 +5320,7 @@ END_PARAM_BLOCK
         lda     window_id
     IF NOT_ZERO
         ;; Map initial event coordinates
+        jsr     PrepActiveWindowScreenMapping
         jsr     _CoordsScreenToWindow
     END_IF
 
@@ -5386,48 +5378,46 @@ event_loop:
 
         jsr     FrameTmpRect
 
+        jsr     CachedIconsScreenToWindow
+
         ;; Process all icons in window
         ldx     #0              ; X = index
       DO
-        RTS_IF X = cached_window_entry_count
+       IF X = cached_window_entry_count
+        jmp     CachedIconsWindowToScreen
+       END_IF
+
         txa
         pha                     ; A = index
 
         ;; Check if icon should be selected
         copy8   cached_window_entry_list,x, icon_param
-        lda     window_id
-       IF NOT_ZERO
-        CALL    IconScreenToWindow, A=icon_param
-       END_IF
         ITK_CALL IconTK::IconInRect, icon_param
-        beq     done_icon
+       IF NOT ZERO
 
         ;; Already selected?
         CALL    IsIconSelected, A=icon_param
-       IF NE
+        IF NE
         ;; Highlight and add to selection
         ;; NOTE: Does not use `AddIconToSelection` because we perform
         ;; a more optimized drawing below.
         ITK_CALL IconTK::HighlightIcon, icon_param
         CALL    AddToSelectionList, A=icon_param
         copy8   window_id, selected_window_id
-       ELSE
+        ELSE
         ;; Unhighlight and remove from selection
         ITK_CALL IconTK::UnhighlightIcon, icon_param
         CALL    RemoveFromSelectionList, A=icon_param
-       END_IF
+        END_IF
 
         lda     window_id
-       IF ZERO
+        IF ZERO
         ITK_CALL IconTK::DrawIcon, icon_param
-       ELSE
+        ELSE
         ITK_CALL IconTK::DrawIconRaw, icon_param ; CHECKED (drag select)
-       END_IF
-
-done_icon:
-        lda     window_id
-       IF NOT_ZERO
-        CALL    IconWindowToScreen, A=icon_param
+        END_IF
+       ELSE
+        MGTK_CALL MGTK::CheckEvents
        END_IF
 
         pla                     ; A = index
@@ -5494,15 +5484,7 @@ beyond:
         jmp     event_loop
 
 .proc _CoordsScreenToWindow
-        jsr     PushPointers
-        jsr     PrepActiveWindowScreenMapping
-
-        ;; Point at an imaginary `IconEntry`, to map
-        ;; `event_params::coords` from screen to window.
-        CALL    IconPtrScreenToWindow, AX=#(event_params::coords - IconEntry::iconx)
-
-        jsr     PopPointers     ; do not tail-call optimise!
-        rts
+        TAIL_CALL MapCoordsScreenToWindow, AX=#event_params::coords
 .endproc ; _CoordsScreenToWindow
 .endproc ; DragSelect
 
@@ -6290,7 +6272,8 @@ done:
         RTS_IF NOT_ZERO         ; obscured
 
         jsr     PushPointers
-        jsr     PrepActiveWindowScreenMapping
+        jsr     LoadActiveWindowEntryTable
+        jsr     CachedIconsScreenToWindow
 
         COPY_STRUCT MGTK::Rect, window_grafport+MGTK::GrafPort::maprect, tmp_rect
 
@@ -6298,10 +6281,8 @@ done:
        DO
         txa
         pha                     ; A = index
+
         copy8   selected_icon_list,x, icon_param
-        pha                     ; A = icon id
-        jsr     GetIconEntry
-        jsr     IconPtrScreenToWindow
 
         ITK_CALL IconTK::IconInRect, icon_param
         IF_NOT_ZERO
@@ -6310,14 +6291,12 @@ done:
         MGTK_CALL MGTK::CheckEvents
         END_IF
 
-        pla                     ; A = icon id
-        jsr     GetIconEntry
-        jsr     IconPtrWindowToScreen
         pla                     ; A = index
         tax
         inx
        WHILE X <> selected_icon_count
 
+        jsr     CachedIconsWindowToScreen
         jsr     PopPointers     ; do not tail-call optimize!
         rts
       END_IF
@@ -8567,74 +8546,6 @@ ret:    rts
 .endproc ; DrawStringRight
 
 ;;; ============================================================
-;;; Convert icon's coordinates from window to screen
-;;; (icon index in A, active window)
-;;; NOTE: Avoid calling in a loop; factor out `PrepActiveWindowScreenMapping`
-
-.proc IconWindowToScreen
-        jsr     PushPointers
-        pha
-        jsr     PrepActiveWindowScreenMapping
-        pla
-        jsr     GetIconEntry
-        jsr     IconPtrWindowToScreen
-        jsr     PopPointers     ; do not tail-call optimise!
-        rts
-.endproc ; IconWindowToScreen
-
-;;; Convert icon's coordinates from window to screen
-;;; Inputs: A,X = `IconEntry`, `PrepActiveWindowScreenMapping` called
-;;; Trashes $06
-.proc IconPtrWindowToScreen
-        entry_ptr := $6
-        stax    entry_ptr
-
-        ;; iconx
-        ldy     #IconEntry::iconx
-        sub16in (entry_ptr),y, map_delta_x, (entry_ptr),y
-
-        ;; icony
-        iny
-        sub16in (entry_ptr),y, map_delta_y, (entry_ptr),y
-
-        rts
-.endproc ; IconPtrWindowToScreen
-
-;;; ============================================================
-;;; Convert icon's coordinates from screen to window
-;;; (icon index in A, active window)
-;;; NOTE: Avoid calling in a loop; factor out `PrepActiveWindowScreenMapping`
-
-.proc IconScreenToWindow
-        jsr     PushPointers
-        pha
-        jsr     PrepActiveWindowScreenMapping
-        pla
-        jsr     GetIconEntry
-        jsr     IconPtrScreenToWindow
-        jsr     PopPointers     ; do not tail-call optimise!
-        rts
-.endproc ; IconScreenToWindow
-
-;;; Convert icon's coordinates from screen to window
-;;; Inputs: A,X = `IconEntry`, `PrepActiveWindowScreenMapping` called
-;;; Trashes $06
-.proc IconPtrScreenToWindow
-        entry_ptr := $6
-        stax    entry_ptr
-
-        ;; iconx
-        ldy     #IconEntry::iconx
-        add16in (entry_ptr),y, map_delta_x, (entry_ptr),y
-
-        ;; icony
-        iny
-        add16in (entry_ptr),y, map_delta_y, (entry_ptr),y
-
-        rts
-.endproc ; IconPtrScreenToWindow
-
-;;; ============================================================
 
 map_delta_x:    .word   0
 map_delta_y:    .word   0
@@ -8684,6 +8595,22 @@ map_delta_y:    .word   0
         rts
 
 .endproc ; PrepWindowScreenMapping
+
+;;; Input: A,X = point coords
+.proc MapCoordsScreenToWindow
+        ptr := $06
+
+        jsr     PushPointers
+        stax    ptr
+
+        ldy     #MGTK::Point::xcoord
+        add16in map_delta_x, (ptr),y, (ptr),y
+        iny                     ; Y = #MGTK::Point::ycoord
+        add16in map_delta_y, (ptr),y, (ptr),y
+
+        jsr     PopPointers     ; do not tail-call optimize!
+        rts
+.endproc ; MapCoordsScreenToWindow
 
 ;;; ============================================================
 ;;; Input: A = unmasked unit number
@@ -12896,6 +12823,145 @@ ok:     RETURN  A=#0
 .endproc ; AboutDialogProc
 
 ;;; ============================================================
+;;; Given a path and a prospective name, update the filesystem with
+;;; the desired case bits, considering type and the option
+;;; `DeskTopSettings::kOptionsSetCaseBits`.
+;;;
+;;; Volume - If option set, write case bits to volume header;
+;;; otherwise clear case bits in volume header and recase the string
+;;; in memory.
+;;;
+;;; Regular File - If option set, write case bits to directory entry;
+;;; otherwise clear case bits in directory entry and recase the string
+;;; in memory.
+;;;
+;;; AppleWorks File - Write case bits to auxtype. If option set, also
+;;; write case bits to directory entry. Otherwise, clear case bits in
+;;; directory entry. (The string in memory is never recased, which
+;;; makes this not a superset of the regular file case.)
+;;;
+;;; Inputs: `src_path_buf` is file, `stashed_name` is new name
+;;; Outputs: `stashed_name` had "resulting" file case
+
+.proc ApplyCaseBits
+        CALL    CalculateCaseBits, AX=#stashed_name
+        stax    case_bits
+
+        jsr     GetSrcFileInfo
+        bcs     ret
+        copy8   DEVNUM, block_params::unit_num
+
+        lda     src_file_info_params::storage_type
+    IF A <> #ST_VOLUME_DIRECTORY
+        ;; --------------------------------------------------
+        ;; File
+
+        CALL    GetFileEntryBlock, AX=#src_path_buf
+        bcs     ret
+        stax    block_params::block_num
+
+        block_ptr := $08
+        CALL    GetFileEntryBlockOffset, AX=#block_buffer ; Y is already the entry number
+        stax    block_ptr
+
+        MLI_CALL READ_BLOCK, block_params
+        bcs     ret
+
+        ;; Is AppleWorks?
+        ldy     #FileEntry::file_type
+        lda     (block_ptr),y
+      IF A NOT_IN #FT_ADB, #FT_AWP, #FT_ASP
+
+        ;; --------------------------------------------------
+        ;; Non-AppleWorks file
+
+        jsr     get_case_bits_per_option_and_adjust_string
+      ELSE
+
+        ;; --------------------------------------------------
+        ;; AppleWorks file
+
+        ;; Per Per File Type Notes: File Type $19 (25) All Auxiliary Types (etc)
+        ;; https://web.archive.org/web/2007/http://web.pdx.edu/~heiss/technotes/ftyp/ftn.19.xxxx.html
+        ;;
+        ;; Like as GS/OS case bits, except:
+        ;; * Shifted left by one bit; low bit is clear
+        ;; * Word is stored byte-swapped
+        lda     case_bits
+        asl     a
+        tax
+        lda     case_bits+1
+        rol     a
+
+        ldy     #FileEntry::aux_type
+        sta     (block_ptr),y
+        txa
+        iny
+        sta     (block_ptr),y
+
+        jsr     get_option
+       IF ZERO
+        ;; Option not set, so zero case bits; memory string preserved
+        ldax    #0
+       ELSE
+        ;; Option set, so write case bits as is.
+        ldax    case_bits
+       END_IF
+      END_IF
+
+        ldy     #FileEntry::case_bits
+        sta     (block_ptr),y
+        iny
+        txa
+        sta     (block_ptr),y
+        FALL_THROUGH_TO write_block
+
+write_block:
+        MLI_CALL WRITE_BLOCK, block_params
+ret:    rts
+
+    END_IF
+
+        ;; --------------------------------------------------
+        ;; Volume
+
+        copy16  #kVolumeDirKeyBlock, block_params::block_num
+        MLI_CALL READ_BLOCK, block_params
+        bcs     ret
+
+        jsr     get_case_bits_per_option_and_adjust_string
+        stax    block_buffer + VolumeDirectoryHeader::case_bits
+        jmp     write_block
+
+        ;; --------------------------------------------------
+        ;; Helpers
+
+        ;; Returns Z=0 if option set, Z=1 otherwise
+get_option:
+        CALL    ReadSetting, X=#DeskTopSettings::options
+        and     #DeskTopSettings::kOptionsSetCaseBits
+        rts
+
+        ;; Returns A,X=case bits if option set, A,X=0 otherwise
+get_case_bits_per_option_and_adjust_string:
+        jsr     get_option
+    IF ZERO
+        ;; Option not set, so zero case bits, adjust memory string
+        CALL    UpcaseString, AX=#stashed_name
+        CALL    AdjustFileNameCase, AX=#stashed_name
+        ldax    #0
+    ELSE
+        ;; Option set, so write case bits as is, leave string alone
+        ldax    case_bits
+    END_IF
+        rts
+
+        ;; --------------------------------------------------
+        block_buffer := $800
+        DEFINE_READWRITE_BLOCK_PARAMS block_params, block_buffer, SELF_MODIFIED
+.endproc ; ApplyCaseBits
+
+;;; ============================================================
 ;;; Dynamically load parts of Desktop
 
 ;;; Call `LoadDynamicRoutine` or `RestoreDynamicRoutine`
@@ -13040,145 +13106,6 @@ RestoreDynamicRoutine   := LoadDynamicRoutineImpl::restore
         ldx     #SELF_MODIFIED_BYTE
         rts
 .endproc ; AShiftX
-
-;;; ============================================================
-;;; Given a path and a prospective name, update the filesystem with
-;;; the desired case bits, considering type and the option
-;;; `DeskTopSettings::kOptionsSetCaseBits`.
-;;;
-;;; Volume - If option set, write case bits to volume header;
-;;; otherwise clear case bits in volume header and recase the string
-;;; in memory.
-;;;
-;;; Regular File - If option set, write case bits to directory entry;
-;;; otherwise clear case bits in directory entry and recase the string
-;;; in memory.
-;;;
-;;; AppleWorks File - Write case bits to auxtype. If option set, also
-;;; write case bits to directory entry. Otherwise, clear case bits in
-;;; directory entry. (The string in memory is never recased, which
-;;; makes this not a superset of the regular file case.)
-;;;
-;;; Inputs: `src_path_buf` is file, `stashed_name` is new name
-;;; Outputs: `stashed_name` had "resulting" file case
-
-.proc ApplyCaseBits
-        CALL    CalculateCaseBits, AX=#stashed_name
-        stax    case_bits
-
-        jsr     GetSrcFileInfo
-        bcs     ret
-        copy8   DEVNUM, block_params::unit_num
-
-        lda     src_file_info_params::storage_type
-    IF A <> #ST_VOLUME_DIRECTORY
-        ;; --------------------------------------------------
-        ;; File
-
-        CALL    GetFileEntryBlock, AX=#src_path_buf
-        bcs     ret
-        stax    block_params::block_num
-
-        block_ptr := $08
-        CALL    GetFileEntryBlockOffset, AX=#block_buffer ; Y is already the entry number
-        stax    block_ptr
-
-        MLI_CALL READ_BLOCK, block_params
-        bcs     ret
-
-        ;; Is AppleWorks?
-        ldy     #FileEntry::file_type
-        lda     (block_ptr),y
-      IF A NOT_IN #FT_ADB, #FT_AWP, #FT_ASP
-
-        ;; --------------------------------------------------
-        ;; Non-AppleWorks file
-
-        jsr     get_case_bits_per_option_and_adjust_string
-      ELSE
-
-        ;; --------------------------------------------------
-        ;; AppleWorks file
-
-        ;; Per Per File Type Notes: File Type $19 (25) All Auxiliary Types (etc)
-        ;; https://web.archive.org/web/2007/http://web.pdx.edu/~heiss/technotes/ftyp/ftn.19.xxxx.html
-        ;;
-        ;; Like as GS/OS case bits, except:
-        ;; * Shifted left by one bit; low bit is clear
-        ;; * Word is stored byte-swapped
-        lda     case_bits
-        asl     a
-        tax
-        lda     case_bits+1
-        rol     a
-
-        ldy     #FileEntry::aux_type
-        sta     (block_ptr),y
-        txa
-        iny
-        sta     (block_ptr),y
-
-        jsr     get_option
-       IF ZERO
-        ;; Option not set, so zero case bits; memory string preserved
-        ldax    #0
-       ELSE
-        ;; Option set, so write case bits as is.
-        ldax    case_bits
-       END_IF
-      END_IF
-
-        ldy     #FileEntry::case_bits
-        sta     (block_ptr),y
-        iny
-        txa
-        sta     (block_ptr),y
-        FALL_THROUGH_TO write_block
-
-write_block:
-        MLI_CALL WRITE_BLOCK, block_params
-ret:    rts
-
-    END_IF
-
-        ;; --------------------------------------------------
-        ;; Volume
-
-        copy16  #kVolumeDirKeyBlock, block_params::block_num
-        MLI_CALL READ_BLOCK, block_params
-        bcs     ret
-
-        jsr     get_case_bits_per_option_and_adjust_string
-        stax    block_buffer + VolumeDirectoryHeader::case_bits
-        jmp     write_block
-
-        ;; --------------------------------------------------
-        ;; Helpers
-
-        ;; Returns Z=0 if option set, Z=1 otherwise
-get_option:
-        CALL    ReadSetting, X=#DeskTopSettings::options
-        and     #DeskTopSettings::kOptionsSetCaseBits
-        rts
-
-        ;; Returns A,X=case bits if option set, A,X=0 otherwise
-get_case_bits_per_option_and_adjust_string:
-        jsr     get_option
-    IF ZERO
-        ;; Option not set, so zero case bits, adjust memory string
-        CALL    UpcaseString, AX=#stashed_name
-        CALL    AdjustFileNameCase, AX=#stashed_name
-        ldax    #0
-    ELSE
-        ;; Option set, so write case bits as is, leave string alone
-        ldax    case_bits
-    END_IF
-        rts
-
-        ;; --------------------------------------------------
-        block_buffer := $800
-        DEFINE_READWRITE_BLOCK_PARAMS block_params, block_buffer, SELF_MODIFIED
-.endproc ; ApplyCaseBits
 
 ;;; ============================================================
 

src/desktop/desktop.s
@@ -33,7 +33,7 @@
         DEFSEG SegmentLoader,      $2000, $0200
         DEFSEG SegmentDeskTopAux,  $4000, $8000
         DEFSEG SegmentDeskTopLC,   $D000, $2200
-        DEFSEG SegmentDeskTopMain, $4000, $6E00
+        DEFSEG SegmentDeskTopMain, $4000, $6D00
         DEFSEG SegmentInitializer, $0800, $0800
         DEFSEG SegmentInvoker,     $0290, $0160
 

commit 7141f82b249479a1bf76622fab8459cb041f1ab1

Desktop: Fix bad selection/paint after drag selecting around window
Caught by accident when automating a test case. Yay! Same root cause
as the fix in 7668b93 - clearing selection can change what icons are
cached. Similar fix.

This regressed in 22975d4, so new in 1.6

src/desktop/main.s
@@ -5351,6 +5351,7 @@ END_PARAM_BLOCK
         jsr     ExtendSelectionModifierDown
         bmi     :+
 clear:  jsr     ClearSelection
+        CALL    CacheWindowIconList, A=window_id
 :
 
         ;; --------------------------------------------------

tests/desktop/repaints.lua
@@ -174,7 +174,7 @@ end)
   window.
 ]]
 test.Step(
-  "drag select on desktop doesn't select file icons - after icon click",
+  "drag select on desktop doesn't select file icons - with file icon selection",
   function()
     a2d.SelectPath("/A2.DESKTOP/READ.ME")
     a2d.MoveWindowBy(40, 30)
@@ -183,8 +183,6 @@ test.Step(
     a2d.Drag(rect[1] - 20, rect[2] - 10,
              rect[3] + 20, rect[4] + 10)
     a2d.InMouseKeysMode(function(m) m.Home() end)
-
-    -- BUG: Bad paint inside window!!!
 
     test.Snap("verify no mispaint")
     test.ExpectEquals(#a2d.GetSelectedIcons(), 0, "nothing should be selected")