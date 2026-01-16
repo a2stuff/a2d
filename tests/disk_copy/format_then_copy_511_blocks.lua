45537bf4ab03581b8009990b7244ca3e05b13107
Joshua Bell <inexorabletash@gmail.com>
2026-01-16 09:46:03 -0800

    Tests: Add test for edge case with 511 blocks noted with MAME/CFFA2
    With an unformatted image 511*512 bytes (511 blocks), MAME/CFFA2
    is kinda weird:
    
    * Use Special > Format Disk. A2D makes a driver call to ask how big
      the disk is. The driver reports 512 blocks (!). A2D issues the
      format command which succeeds, and then writes the volume header
      indicating the disk has 512 blocks.
    
    * Now use Special > Copy Disk to copy it to another disk image created
      the same way. 511 blocks copy as expected. A2D tries to read the
      512th block and the read fails - but blink and you'll miss it. In
      what is ostensibly a bug, A2D still tries to write a 512th block
      using whatever happens to be in the buffer. The write *succeeds*,
      and the disk image file size is increased by 512 bytes.

---
+++ tests/disk_copy/format_then_copy_511_blocks.lua
@@ -0,0 +1,85 @@
+--[[ BEGINCONFIG ========================================
+
+MODELARGS="-sl2 mouse -sl6 cffa2 -sl7 cffa2"
+DISKARGS="-hard3 $HARDIMG -hard1 sizes/image_511_blocks_random.hdv -hard2 sizes/image_511_blocks_random.hdv"
+
+======================================== ENDCONFIG ]]
+
+a2d.ConfigureRepaintTime(0.25)
+
+--[[
+  Format then Disk Copy Disk Copy with 511 blocks.
+]]
+test.Variants(
+  {
+    {"Format then Quick Copy 511 blocks", "quick"},
+    {"Format then Disk Copy 511 blocks", "disk"},
+  },
+  function(idx, name, what)
+    a2d.CloseAllWindows()
+    a2d.ClearSelection()
+    a2d.InvokeMenuItem(a2d.SPECIAL_MENU, a2d.SPECIAL_FORMAT_DISK-2)
+
+    a2d.FormatEraseSelectSlotDrive(6, 1)
+    a2d.ClearTextField()
+    apple2.Type("NEW.NAME" .. idx)
+    a2d.DialogOK()
+    a2dtest.WaitForAlert()
+    a2d.DialogOK()
+
+    --[[
+      BUG: MAME/CFFA2 reports device as 512 blocks so it gets
+      formatted as 512 blocks
+    ]]
+
+    emu.wait(10)
+
+    a2d.ClearSelection()
+    a2d.CopyDisk()
+
+    a2d.InvokeMenuItem(3, idx) -- Options > Quick Copy or Disk Copy
+
+    -- select source
+    apple2.UpArrowKey() -- S6D2
+    apple2.UpArrowKey() -- S6D1
+    a2d.WaitForRepaint()
+    a2d.DialogOK()
+
+    -- select destination
+    apple2.UpArrowKey() -- S6D2
+    a2d.WaitForRepaint()
+    a2d.DialogOK()
+
+    -- insert source
+    a2dtest.WaitForAlert()
+    a2d.DialogOK()
+
+    -- insert destination
+    a2dtest.WaitForAlert()
+    a2d.DialogOK()
+
+    -- confirmation
+    a2dtest.WaitForAlert()
+    a2d.DialogOK()
+
+    -- complete
+    a2dtest.WaitForAlert({timeout=7200})
+    if what == "quick" then
+      test.Snap("verify block counts are equal")
+    else
+      --[[
+        BUG: MAME/CFFA2 will fail the read of the 512th block but
+        successfully write it, expanding the size of the image and
+        writing an arbitrary previously read block to it.
+      ]]
+      test.Snap("verify total block counts are 511")
+    end
+    a2d.DialogOK()
+
+    -- cleanup
+    a2d.OAShortcut("Q") -- File > Quit
+    a2d.WaitForDesktopReady()
+
+    a2dtest.WaitForAlert()
+    a2d.DialogOK()
+end)
