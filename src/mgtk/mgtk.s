a2stuff/a2d

commit 071b9dc0ad11719e28e6395367866798d0ab15db

MGTK: Reduce cursor flicker when drawing/updating scrollbars

src/mgtk/mgtk.s
@@ -6088,13 +6088,16 @@ filler: ldx     menu_item_index
 .proc FillAndFrameRect
         stax    fill_params
         stax    draw_params
+        stax    shield_params
         lda     #MGTK::pencopy
         jsr     SetFillMode
+
+        MGTK_CALL MGTK::ShieldCursor, 0, shield_params
         MGTK_CALL MGTK::PaintRect, 0, fill_params
         lda     #MGTK::notpencopy
         jsr     SetFillMode
         MGTK_CALL MGTK::FrameRect, 0, draw_params
-        rts
+        jmp     UnshieldCursorImpl
 .endproc ; FillAndFrameRect
 
 
@@ -7481,10 +7484,16 @@ end:    rts
         EXIT_CALL MGTK::Error::window_not_found
 .endproc ; WindowByIdOrExit
 
+.proc PaintWinRect
+        MGTK_CALL MGTK::ShieldCursor, winrect
+        MGTK_CALL MGTK::PaintRect, winrect
+        jmp     UnshieldCursorImpl
+.endproc ; PaintWinRect
 
 .proc FrameWinRect
+        MGTK_CALL MGTK::ShieldCursor, winrect
         MGTK_CALL MGTK::FrameRect, winrect
-        rts
+        jmp     UnshieldCursorImpl
 .endproc ; FrameWinRect
 
 .proc InWinRect
@@ -8457,7 +8466,7 @@ in_close_box:  .byte   0
 toggle: sta     in_close_box
 
         jsr     SetFillModeXOR
-        MGTK_CALL MGTK::PaintRect, winrect
+        jsr     PaintWinRect
 
 loop:   jsr     GetAndReturnEvent
 
@@ -9027,22 +9036,23 @@ toggle: eor     params::activate
         RTS_IF A = (window),y   ; no-op if no change
         sta     (window),y
 
-        jsr     HideCursorAndSaveParams ; restored by call below
-        lda     params::activate
-        jsr     DrawOrEraseScrollbar
-        jmp     ShowCursorAndRestoreParams
+        jsr     SaveParamsAndStack
+        CALL    DrawOrEraseScrollbar, A=params::activate
+        jmp     RestoreParamsAndStack
 .endproc ; ActivateCtlImpl
 
-
+;;; Z=1 to erase, Z=0 to draw
 .proc DrawOrEraseScrollbar
-        bne     do_draw
-
+    IF ZERO
+        ;; erase
         jsr     GetScrollbarScrollArea
         jsr     SetStandardPort
-        MGTK_CALL MGTK::PaintRect, winrect
-        rts
+        jmp     PaintWinRect
+    END_IF
 
-do_draw:
+        ;; ----------------------------------------
+        ;; track
+
         bit     which_control
         bmi     vert_scrollbar
         bit     current_winfo::hscroll
@@ -9057,8 +9067,11 @@ has_scroll:
         jsr     GetScrollbarScrollArea
 
         MGTK_CALL MGTK::SetPattern, light_speckles_pattern
-        MGTK_CALL MGTK::PaintRect, winrect
+        jsr     PaintWinRect
         MGTK_CALL MGTK::SetPattern, standard_port::pattern
+
+        ;; ----------------------------------------
+        ;; thumb
 
         bit     which_control
         bmi     vert_thumb
@@ -9703,11 +9716,11 @@ check_win:
         RTS_IF A = (window),y   ; no-op if no change
         sta     (window),y
 
-        jsr     HideCursorAndSaveParams ; restored by call below
+        jsr     SaveParamsAndStack ; restored by call below
         jsr     SetStandardPort
         ;; TODO: Clarify dependency on `SetStandardPort`'s return value
         jsr     DrawOrEraseScrollbar
-        jmp     ShowCursorAndRestoreParams
+        jmp     RestoreParamsAndStack
 .endproc ; UpdateThumbImpl
 
 ;;; ============================================================

commit 87d9c83755e286320229fa4f0a77047062cc90cf

MGTK: Fix regression in detecting no-op thumb drags
Regressed in 071b9dc as a zero page location that was previously
believed safe to store the old thumb location is now trashed. Use SMC
instead; costs a couple of bytes but much less fragile.

Caught by: tests/desk_acc/text_preview.lua

src/mgtk/mgtk.s
@@ -9430,17 +9430,15 @@ got_ctl:lda     params::which_ctl
         jsr     GetThumbRect
 
         ;; Stash initial position, to detect no-op
-        initial_pos := $8A
-
         ldx     #0
         bit     which_control
     IF NS
         ldx     #2
     END_IF
         lda     winrect,x
-        sta     initial_pos
+        sta     initial_pos_lo
         lda     winrect+1,x
-        sta     initial_pos+1
+        sta     initial_pos_hi
 
         jsr     SaveParamsAndStack ; restored by call below (in `drag_done`)
         jsr     SetDesktopPort
@@ -9519,18 +9517,23 @@ drag_done:
         ;; Did position change?
         ldx     #0
         bit     which_control
-        bpl     :+
+    IF NS
         ldx     #2
-:       lda     winrect,x
-        cmp     initial_pos
-        bne     :+
+    END_IF
+        lda     winrect,x
+        initial_pos_lo := *+1
+        cmp     #SELF_MODIFIED_BYTE
+    IF EQ
         lda     winrect+1,x
-        cmp     initial_pos+1
-        bne     :+
+        initial_pos_hi := *+1
+        cmp     #SELF_MODIFIED_BYTE
+      IF EQ
         lda     thumb_pos       ; (out) thumbpos = original value
         ldx     #0              ; (out) thumbmoved = 0
         beq     store           ; always
-:
+      END_IF
+    END_IF
+
         jsr     SetUpThumbDivision
 
         jsr     FixedDiv
@@ -10253,7 +10256,7 @@ pos:    jmp     PositionKbdMouse
         lda     kbd_mouse_x+1
         sbc     #>(kScreenWidth-1)
 
-    IF_POS
+    IF POS
         lda     #>(kScreenWidth-1)
         sta     kbd_mouse_x+1
         lda     #<(kScreenWidth-1)