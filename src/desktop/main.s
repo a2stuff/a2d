a2stuff/a2d

commit 93db31bfc436fa57530df4c1e570aaa9f7c1e225

DeskTop: Fix crash after canceling prompt when dragging files
When dragging files on the same volume, a check is made for "move or
copy?"; while mostly this is by looking at the source/destination
paths and modifiers, there is also a check to see if relinking can be
done, by doing a test block read. If this fails, an alert is shown
with OK/Cancel for a retry.

But c/o some refactors, if Cancel is selected then DeskTop would
crash because the recovery state is not valid. Oops! Patch in
the state.

src/desktop/main.s
@@ -9626,6 +9626,12 @@ FinishOperation:
 .proc DoCopyOrMoveSelection
         lda     selected_window_id
     IF NOT_ZERO                 ; dragging volume always copies
+        ;; In case `CheckMoveOrCopy` fails, recovery path needs to be functional
+        copy16  #operations::DoNothing, operation_complete_callback
+        tsx
+        stx     saved_stack
+        CLEAR_BIT7_FLAG do_op_flag
+
         jsr     GetWindowPath
         jsr     CheckMoveOrCopy
     END_IF

tests/desktop/move_and_copy_eject.lua
@@ -142,3 +142,39 @@ test.Step(
     drive:load(src)
 end)
 
+--[[
+  Open a window for a floppy disk. Drag a file to a folder on the same
+  disk. When alert shows, click Cancel. Verify that DeskTop doesn't
+  crash.
+]]
+test.Step(
+  "no crash on cancel after failed copy-or-move check",
+  function()
+    local drive = s6d1
+    local src = drive.filename
+
+    a2d.CreateFolder("/WITH.FILES/FOLDER")
+    emu.wait(1)
+
+    a2d.OpenPath("/WITH.FILES")
+    emu.wait(1)
+
+    a2d.Select("FOLDER")
+    local dst_x, dst_y = a2dtest.GetSelectedIconCoords()
+
+    a2d.Select("LOREM.IPSUM")
+    local src_x, src_y = a2dtest.GetSelectedIconCoords()
+
+    drive:unload()
+    a2d.Drag(src_x, src_y, dst_x, dst_y)
+
+    a2dtest.WaitForAlert()
+    a2d.DialogCancel()
+    emu.wait(1)
+
+    a2dtest.ExpectNotHanging()
+
+    -- cleanup
+    drive:load(src)
+    a2d.DeletePath("/WITH.FILES/FOLDER")
+end)

commit 604e2e1aff78dd58450597fa8966cbd5f49119c8

DeskTop: Fix moving files
Regression introduced in 93db31bfc4.

Also some related test fixes.

src/desktop/main.s
@@ -9691,11 +9691,12 @@ FinishOperation:
         lda     selected_window_id
     IF NOT_ZERO                 ; dragging volume always copies
         ;; In case `CheckMoveOrCopy` fails, recovery path needs to be functional
-        copy16  #operations::DoNothing, operation_complete_callback
         tsx
         stx     saved_stack
+        copy16  #operations::DoNothing, operation_complete_callback
         CLEAR_BIT7_FLAG do_op_flag
 
+        lda     selected_window_id
         jsr     GetWindowPath
         jsr     CheckMoveOrCopy
     END_IF

tests/desktop/forked_files.lua
@@ -36,6 +36,9 @@ test.Step(
 
     -- cleanup
     a2d.EraseVolume("RAM1")
+    a2d.InMouseKeysMode(function(m)
+        m.MoveToApproximately(0, 0)
+    end)
 end)
 
 --[[

tests/desktop/move_and_copy_space.lua
@@ -26,7 +26,8 @@ test.Step(
 
     -- copy volume
     a2d.CopyPath("/A2.DESKTOP", "/RAM1")
-    a2dtest.WaitForAlert({match="Extras/BASIC%.system"})
+    a2dtest.WaitForAlert({match="file is too large"})
+    test.Expect(a2dtest.OCRScreen():upper():find("EXTRAS/BASIC%.SYSTEM"), "file name should be visible")
     a2d.DialogCancel()
 
     -- cleanup

tests/desktop/open.lua
@@ -1,6 +1,6 @@
 --[[ BEGINCONFIG ========================================
 
-MODELARGS="-sl2 mouse -sl7 scsi"
+MODELARGS="-sl1 ramfactor -sl2 mouse -sl7 scsi"
 DISKARGS="-hard1 $HARDIMG"
 
 ======================================== ENDCONFIG ]]