4ed685e09b49f3b40a0cd49521ab0a756caadb99
Joshua Bell <inexorabletash@gmail.com>
2026-02-20 16:32:07 -0800

    Tests: Add test pass helper / review tool
    Running `bin/testpass` will run the passed tests (or the "important"
    ones) and build a manifest.txt file of the generated ZIP files. It
    will copy in an HTML/JS single page app into the results and will then
    launch a Python HTTP server and a browser pointing at the pp.
    
    The app lets you scroll up and down through the tests and see the
    output, and review any snaps generated by the tests.
    
    Very very rough, but enough to allow kicking off a multi-hour test
    pass, then review the results later.

---
+++ res/testutil/index.js
@@ -0,0 +1,125 @@
+const $ = s => document.querySelector(s);
+const $$ = s => [...document.querySelectorAll(s)];
+
+function order(a, b) {
+  return (a > b) ? 1 : (a < b) ? -1 : 0;
+}
+
+window.addEventListener('DOMContentLoaded', async e => {
+  const list = $('#list');
+
+  // Iterate manifest
+  const manifest = await (await fetch('manifest.txt')).text();
+  for (const path of manifest.split(/\n/).filter(s => s)) {
+    const option = document.createElement('option');
+    option.className = 'test';
+    option.innerText = path;
+    list.append(option);
+
+    // Load and iterate ZIP
+    const blob = await (await fetch(path)).arrayBuffer();
+    const {entries} = await unzipit.unzip(blob);
+
+    // Stash properties on list item
+    option.output = await entries['output.txt'].text();
+    option.entries = entries;
+
+    Object.keys(entries)
+      .filter(k => k.endsWith('.png'))
+      .sort((a, b) => order(a.name,b.name))
+      .forEach(key => {
+        const entry = entries[key];
+
+        const subopt = document.createElement('option');
+        subopt.className = 'snap';
+        subopt.innerText = entry.name;
+        subopt.parent = option;
+        subopt.instructions = entry.name
+          .replace(/^\d\d\d\d - /, '')
+          .replace(/\.png$/, '');
+        list.append(subopt);
+
+        subopt.entry = entry;
+      });
+  }
+
+  list.addEventListener('change', async e => {
+    if (list.selectedOptions.length === 0)
+      return;
+    const option = list.selectedOptions[0];
+    review(option);
+  });
+
+  list.focus();
+
+});
+
+function blobToDataURL(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onload = e => resolve(reader.result);
+    reader.onerror = e => reject(reader.error);
+  });
+}
+
+async function review(option) {
+  if (option.output) {
+    $('#output').innerHTML = ansiToHTML(option.output);
+    $('#snap').src = '';
+    $('#instructions').innerText = '';
+  } else if (option.entry) {
+    const blob = await option.entry.blob('image/png');
+    const url = await blobToDataURL(blob);
+    $('#snap').src = url;
+    $('#output').value = option.parent.output;
+    $('#instructions').innerText = option.instructions;
+  }
+}
+
+function ansiToHTML(text) {
+  // HTML escaping
+  text = text.replace(/[&<>"']/g, c => {
+    switch (c) {
+    case '&': return '&amp;';
+    case '<': return '&lt;';
+    case '>': return '&gt;';
+    default: return c;
+    }
+  });
+
+  // ANSI sequences
+  // NOTE: Assumes it's always a pair
+
+
+  const styles = {
+    30: 'color: black',
+    31: 'color: red',
+    32: 'color: green',
+    33: 'color: yellow',
+    34: 'color: blue',
+    35: 'color: magenta',
+    36: 'color: cyan',
+    37: 'color: white',
+
+    40: 'background-color: black',
+    41: 'background-color: red',
+    42: 'background-color: green',
+    43: 'background-color: yellow',
+    44: 'background-color: blue',
+    45: 'background-color: magenta',
+    46: 'background-color: cyan',
+    47: 'background-color: white',
+  };
+
+  text = text
+    .replace(/\x1B\(B/g, '')
+    .replace(/\x1B\[(\d+)m/g, function(_, n) {
+      return '<span style="' + styles[n] + '">';
+    })
+    .replace(/\x1B\[m/g, '</span>');
+
+
+  return text;
+
+}
