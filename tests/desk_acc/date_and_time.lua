035b68c27e5d85e0f83557dde3a39d0d8920bd3c
Joshua Bell <inexorabletash@gmail.com>
2026-02-25 17:59:11 -0800

    Tests: Use more scoped OCR for Date & Time DA tests

--- tests/desk_acc/date_and_time.lua
+++ tests/desk_acc/date_and_time.lua
@@ -132,11 +132,11 @@ test.Step(
   function()
     a2d.InvokeMenuItem(a2d.APPLE_MENU, a2d.CONTROL_PANELS)
     a2d.InvokeMenuItem(a2d.VIEW_MENU, a2d.VIEW_BY_NAME)
-    test.ExpectNotMatch(a2dtest.OCRScreen(), "Today", "dates should not say Today")
+    test.ExpectNotMatch(a2dtest.OCRFrontWindowContent(), "Today", "dates should not say Today")
     a2d.SelectAndOpen("DATE.AND.TIME")
     test.Snap("verify dialog date matches packaged file dates")
     a2dtest.ExpectFullRepaint(a2d.DialogOK)
-    test.ExpectMatch(a2dtest.OCRScreen(), "Today", "dates should now say Today")
+    test.ExpectMatch(a2dtest.OCRFrontWindowContent(), "Today", "dates should now say Today")
     a2d.SelectAndOpen("DATE.AND.TIME")
     a2dtest.ExpectMinimalRepaint(a2d.DialogOK)
     a2d.Reboot()
@@ -201,12 +201,12 @@ test.Step(
     apple2.TabKey() -- to hour
     apple2.TabKey() -- to min
     apple2.TabKey() -- to period
-    local ocr = a2dtest.OCRScreen({invert=true})
+    local ocr = a2dtest.OCRFrontWindowContent({invert=true})
     test.Expect(ocr:find("AM") or ocr:find("PM"), "period field should be enabled")
 
     apple2.UpArrowKey()
     emu.wait(1)
-    local ocr2 = a2dtest.OCRScreen({invert=true})
+    local ocr2 = a2dtest.OCRFrontWindowContent({invert=true})
     test.Expect(
       (ocr:find("AM") and ocr2:find("PM")) or
       (ocr:find("PM") and ocr2:find("PM")),
@@ -217,7 +217,7 @@ test.Step(
     for i = 1, 24 do
       apple2.UpArrowKey()
       local expect = ((i - 1) % 12) + 1
-      test.ExpectMatch(a2dtest.OCRScreen({invert=true}), tostring(expect),
+      test.ExpectMatch(a2dtest.OCRFrontWindowContent({invert=true}), tostring(expect),
                   "should be 12 hour cycle")
     end
     a2d.DialogOK()
@@ -244,7 +244,7 @@ test.Step(
     apple2.TabKey() -- to min
     apple2.TabKey() -- to back to day
 
-    local ocr = a2dtest.OCRScreen({invert=true})
+    local ocr = a2dtest.OCRFrontWindowContent({invert=true})
     test.Expect(not ocr:find("AM") and not ocr:find("PM"), "period field should be disabled")
 
     apple2.TabKey() -- to month
@@ -252,7 +252,7 @@ test.Step(
     apple2.TabKey() -- to hour
     for i = 1, 24 do
       apple2.UpArrowKey()
-      test.ExpectMatch(a2dtest.OCRScreen({invert=true}), string.format("%02d", i % 24),
+      test.ExpectMatch(a2dtest.OCRFrontWindowContent({invert=true}), string.format("%02d", i % 24),
                   "should be 24 hour cycle")
     end
     a2d.DialogOK()
