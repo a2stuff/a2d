a2stuff/a2d

commit a8702f02781d6af2aedb2c2e819ed1709c0c947e

DeskTop: Add Startup.Items support
If a "Startup.Items" folder is present in the system directory, (i.e.
adjacent to DESKTOP.SYSTEM), then on startup, after windows are
restored, each item in the directory will be opened one by one.

Anything that can be opened with File > Open works. Putting
accessories, folders, and previewable files (text, graphics, etc) into
the folder makes sense. It makes even more sense to put aliases in the
folder, rather than moving or copying the originals in.

For example, after creating the "Startup.Items" folder, hold down Open
Apple and Solid Apple and drag in the "Round.Corners" accessory from
Extras into "Startup.Items" to create an alias. Then when you restart
DeskTop it will automatically run the accessory.

Putting something like a system or binary program in there doesn't
make a lot of sense since DeskTop needs to exit to run it.

tests/lib/a2d.lua
@@ -298,6 +298,20 @@ local function CheckSelectionName(name, options)
   end
 end
 
+local function WaitForSelectionName(name)
+  util.WaitFor(
+    string.format("%q to be selected", name), function()
+      local selected = a2d.GetSelectedIcons()
+      if #selected ~= 1 then
+        return false
+      end
+      if name:lower() ~= selected[1].name:lower() then
+        return false
+      end
+      return true
+  end)
+end
+
 function a2d.Select(name, options)
   options = default_options(options)
 
@@ -305,8 +319,8 @@ function a2d.Select(name, options)
     a2d.ClearSelection()
   end
   apple2.Type(name)
-  a2d.WaitForRepaint() -- TODO: spin here?
-  CheckSelectionName(name, options)
+  a2d.WaitForRepaint()
+  WaitForSelectionName(name)
 end
 
 -- additional option: {close_current=true}
@@ -433,8 +447,8 @@ function a2d.RenameSelection(newname, options)
   a2d.ClearTextField()
   apple2.Type(newname)
   apple2.ReturnKey()
-  emu.wait(5) -- I/O
-  CheckSelectionName(newname, options)
+  a2d.WaitForRepaint()
+  WaitForSelectionName(newname)
 end
 
 function a2d.RenamePath(path, newname, options)
@@ -458,7 +472,7 @@ function a2d.DuplicateSelection(newname, options)
   apple2.Type(newname)
   apple2.ReturnKey()
   a2d.WaitForRepaint()
-  CheckSelectionName(newname, options)
+  WaitForSelectionName(newname)
 end
 
 function a2d.DuplicatePath(path, newname)
@@ -499,8 +513,8 @@ function a2d.CreateFolder(path, options)
   a2d.ClearTextField()
   apple2.Type(name)
   apple2.ReturnKey()
-  emu.wait(5) -- I/O
-  CheckSelectionName(name, options)
+  a2d.WaitForRepaint()
+  WaitForSelectionName(name)
 end
 
 function a2d.FormatVolume(name, opt_new_name)

src/common.inc
@@ -22,6 +22,7 @@
 .define kPathnameBellProc      .concat(kFilenameLocalDir, "/Bell.proc")
 .define kPathnameQuitSave      .concat(kFilenameLocalDir, "/Quit.tmp")
 .define kFilenameDADir         "Apple.Menu"
+.define kFilenameStartupItemsDir "Startup.Items"
 .define kFilenameExtrasDir     "Extras"
 .define kFilenameRAMCardDir    "DeskTop"
 

src/desktop/desktop.s
@@ -33,7 +33,7 @@
         DEFSEG SegmentLoader,      $2000, $0200
         DEFSEG SegmentDeskTopAux,  $4000, $8000
         DEFSEG SegmentDeskTopLC,   $D000, $2200
-        DEFSEG SegmentDeskTopMain, $4000, $6E00
+        DEFSEG SegmentDeskTopMain, $4000, $6F00
         DEFSEG SegmentInitializer, $0800, $0800
         DEFSEG SegmentInvoker,     $0290, $0160
 

src/desktop/init.s
@@ -998,7 +998,9 @@ slot_string_table:
     END_IF
 
         ;; And start pumping events
-        jmp     main::MainLoop
+        PUSH_RETURN_ADDRESS main::MainLoop
+        TAIL_CALL main::ExecuteStartupItems
+
 .endproc ; FinalSetup
 
 ;;; ============================================================

src/desktop/main.s
@@ -13018,6 +13018,120 @@ ok:     RETURN  A=#0
 .endproc ; AboutDialogProc
 
 ;;; ============================================================
+;;; Execute items in "Startup.Items" folder
+
+;;; Note that while this routine is only executed once on startup, it
+;;; can't be in the Initializer segment as that occupies the same
+;;; memory area as DAs that may be executed.
+
+;;; Uses `DIR_READ_DATA_BUFFER` and `IO_BUFFER`
+.proc ExecuteStartupItems
+
+        ;; Does the directory exist?
+        CALL GetFileInfo, AX=#str_startup_items
+        RTS_IF CS
+        RTS_IF lda file_info_params::file_type : A <> #FT_DIRECTORY
+
+        ;; Loop over all entries
+        ldx     #1              ; 1-based, for convenience
+    REPEAT
+        txa
+        pha
+
+        ;; Get Nth entry if it exists
+        jsr     _GetNthEntry
+      IF CS
+        pla
+        rts
+      END_IF
+
+        ;; Construct path and execute it
+        CALL    CopyToSrcPath, AX=#str_startup_items
+        CALL    AppendFilenameToSrcPath, AX=#filename_buf
+        CALL    LaunchFileWithPathOnSystemDisk
+
+        pla
+        tax
+    WHILE inx : NOT ZERO
+        rts
+
+;;; We seek to the Nth entry each time because keeping the directory
+;;; open would consume buffers that items like DAs are likely to use.
+
+;;; Inputs: A = index (1-based, for convenience)
+;;; Output: C=0 and `filename_buf` populated on success, C=1 on failure (e.g. EOF)
+.proc _GetNthEntry
+        read_dir_buffer := DIR_READ_DATA_BUFFER
+
+;;; Local variables on ZP
+PARAM_BLOCK, $06
+dir_ptr         .addr
+index           .byte
+entry_length    .byte
+entries_per_block .byte
+entry_in_block  .byte
+END_PARAM_BLOCK
+
+        sta     index
+
+        MLI_CALL OPEN, open_params
+        lda     open_params::ref_num
+        sta     read_params::ref_num
+        sta     close_params::ref_num
+        MLI_CALL READ, read_params
+
+        copy8   #1, entry_in_block ; First block has header instead of entry
+        copy8   read_dir_buffer + SubdirectoryHeader::entries_per_block, entries_per_block
+        copy8   read_dir_buffer + SubdirectoryHeader::entry_length, entry_length
+
+        copy16  #read_dir_buffer + .sizeof(SubdirectoryHeader), dir_ptr
+
+    REPEAT
+        ldy     #FileEntry::storage_type_name_length
+        lda     (dir_ptr),y
+        and     #NAME_LENGTH_MASK
+      IF NOT ZERO                 ; non-entry (deleted, etc)
+        tay
+        ;; Found Nth item?
+       IF dec index : ZERO
+        sty     filename_buf
+        DO
+        copy8   (dir_ptr),y, filename_buf,y
+        WHILE dey : NOT ZERO
+        clc                     ; C=0 for success
+        BREAK_IF CC             ; always
+       END_IF
+      END_IF
+
+        ;; Any more entries in block?
+        inc     entry_in_block
+      IF lda entry_in_block : A = entries_per_block
+        MLI_CALL READ, read_params
+        BREAK_IF CS             ; C=1 for EOF
+        copy16  #read_dir_buffer + 4, dir_ptr
+        copy8   #0, entry_in_block
+      ELSE
+        add16_8 dir_ptr, entry_length
+      END_IF
+    FOREVER
+
+        php
+        MLI_CALL CLOSE, close_params
+        plp
+        rts
+
+        DEFINE_OPEN_PARAMS open_params, str_startup_items, IO_BUFFER
+        DEFINE_READWRITE_PARAMS read_params, read_dir_buffer, BLOCK_SIZE
+        DEFINE_CLOSE_PARAMS close_params
+
+.endproc ; _GetNthEntry
+
+str_startup_items:
+        PASCAL_STRING kFilenameStartupItemsDir
+
+.endproc ; ExecuteStartupItems
+
+;;; ============================================================
 ;;; Given a path and a prospective name, update the filesystem with
 ;;; the desired case bits, considering type and the option
 ;;; `DeskTopSettings::kOptionsSetCaseBits`.

tests/desktop/startup_items.lua
@@ -1,0 +1,52 @@
+a2d.ConfigureRepaintTime(0.25)
+
+--[[
+  Create a Startup.Items folder, verify items execute sequentially.
+]]
+test.Step(
+  "Startup.Items execute sequentially",
+  function()
+    a2d.CreateFolder("/A2.DESKTOP/STARTUP.ITEMS")
+
+    a2d.CopyPath("/A2.DESKTOP/SAMPLE.MEDIA/LOREM.IPSUM", "/A2.DESKTOP/STARTUP.ITEMS")
+    a2d.CopyPath("/A2.DESKTOP/SAMPLE.MEDIA/SHAKESPEARE", "/A2.DESKTOP/STARTUP.ITEMS")
+
+    a2d.CloseAllWindows()
+    a2d.Reboot()
+    a2d.WaitForDesktopReady()
+
+    util.WaitFor(
+      "expected Lorem Ipsum text showing", function()
+        return a2dtest.OCRScreen():match("Lorem ipsum dolor sit amet")
+    end)
+
+
+    a2d.CloseWindow()
+
+    util.WaitFor(
+      "expected Hamlet text showing", function()
+        return a2dtest.OCRScreen():match("To be, or not to be, that is the question")
+    end)
+
+    a2d.CloseWindow()
+
+    -- cleanup
+    a2d.DeletePath("/A2.DESKTOP/STARTUP.ITEMS")
+end)
+
+--[[
+  Non-folder Startup.Items is ignored.
+]]
+test.Step(
+  "Startup.Items must be a folder",
+  function()
+    a2d.DuplicatePath("/A2.DESKTOP/SAMPLE.MEDIA/LOREM.IPSUM", "STARTUP.ITEMS")
+    a2d.CopyPath("/A2.DESKTOP/SAMPLE.MEDIA/STARTUP.ITEMS", "/A2.DESKTOP")
+
+    a2d.CloseAllWindows()
+    a2d.Reboot()
+    a2d.WaitForDesktopReady()
+
+    -- cleanup
+    a2d.DeletePath("/A2.DESKTOP/STARTUP.ITEMS")
+end)

commit 246f3ae9aa3e15f54185499c75aa0c350f1fb68b

Test Fixes
Adding "WaitForSelectionName" in a8702f0 broke some tests that
expected specific error messagess. Restore the expected messages.

Also some timing tweaks to fix flakes.

tests/desktop/apple_menu_ejected.lua
@@ -45,6 +45,7 @@ test.Step(
     a2dtest.WaitForAlert({match="insert the system disk"})
     drive:load(current)
     a2d.DialogOK()
+    emu.wait(2)
     test.ExpectEqualsIgnoreCase(a2dtest.GetFrontWindowTitle(), "CONTROL.PANELS", "Control Panels window should be open")
     a2d.CloseWindow()
 end)

tests/lib/a2d.lua
@@ -298,9 +298,11 @@ local function CheckSelectionName(name, options)
   end
 end
 
-local function WaitForSelectionName(name)
-  util.WaitFor(
-    string.format("%q to be selected", name), function()
+local function WaitForSelectionName(name, options)
+  options = default_options(options)
+
+  if not util.WaitForNoError(
+    function()
       local selected = a2d.GetSelectedIcons()
       if #selected ~= 1 then
         return false
@@ -309,7 +311,17 @@ local function WaitForSelectionName(name)
         return false
       end
       return true
-  end)
+    end, {timeout=5}) then
+    local selected = a2d.GetSelectedIcons()
+    if #selected ~= 1 then
+      error(string.format("Failed to select %q - have %d selected",
+                          name, #selected), options.level)
+    end
+    if name:lower() ~= selected[1].name:lower() then
+      error(string.format("Failed to select %q - have %q selected",
+                          name, selected[1].name), options.level)
+    end
+  end
 end
 
 function a2d.Select(name, options)
@@ -320,7 +332,7 @@ function a2d.Select(name, options)
   end
   apple2.Type(name)
   a2d.WaitForRepaint()
-  WaitForSelectionName(name)
+  WaitForSelectionName(name, options)
 end
 
 -- additional option: {close_current=true}
@@ -448,7 +460,7 @@ function a2d.RenameSelection(newname, options)
   apple2.Type(newname)
   apple2.ReturnKey()
   a2d.WaitForRepaint()
-  WaitForSelectionName(newname)
+  WaitForSelectionName(newname, options)
 end
 
 function a2d.RenamePath(path, newname, options)
@@ -472,7 +484,7 @@ function a2d.DuplicateSelection(newname, options)
   apple2.Type(newname)
   apple2.ReturnKey()
   a2d.WaitForRepaint()
-  WaitForSelectionName(newname)
+  WaitForSelectionName(newname, options)
 end
 
 function a2d.DuplicatePath(path, newname)
@@ -514,7 +526,7 @@ function a2d.CreateFolder(path, options)
   apple2.Type(name)
   apple2.ReturnKey()
   a2d.WaitForRepaint()
-  WaitForSelectionName(name)
+  WaitForSelectionName(name, options)
 end
 
 function a2d.FormatVolume(name, opt_new_name)

tests/lib/a2dtest.lua
@@ -318,14 +318,11 @@ end
 --------------------------------------------------
 
 function a2dtest.ExpectNotHanging()
-  local dhr = a2dtest.SnapshotDHRWithoutClock()
   a2d.OpenMenu(a2d.APPLE_MENU)
-  local new = a2dtest.SnapshotDHRWithoutClock()
-  test.Expect(not a2dtest.CompareDHR(dhr, new), "about dialog should have shown", {snap=true}, 1)
+  test.ExpectMatch(a2dtest.OCRScreen(), "About Apple II DeskTop", "Apple menu should have shown")
   apple2.EscapeKey()
   a2d.WaitForRepaint()
-  local new2 = a2dtest.SnapshotDHRWithoutClock()
-  test.Expect(a2dtest.CompareDHR(dhr, new2), "about dialog should have closed", {snap=true}, 1)
+  test.ExpectNotMatch(a2dtest.OCRScreen(), "About Apple II DeskTop", "Apple menu should have closed")
 end
 
 --------------------------------------------------

tests/lib/util.lua
@@ -23,6 +23,23 @@ function util.WaitFor(message, func, options, level)
   error(string.format("Timeout (%ds) waiting for %s", timeout, message), level + 2)
 end
 
+function util.WaitForNoError(func, options)
+  local timeout = 60
+  if options and options.timeout then
+    timeout = options.timeout
+  end
+  if level == nil then
+    level = 1
+  end
+  for i = 1, timeout do
+    if func() then
+      return true
+    end
+    emu.wait(1)
+  end
+  return false
+end
+
 function util.SlurpFile(pathname)
   local f = assert(io.open(pathname, "rb"))
   local bytes = f:read("*all")