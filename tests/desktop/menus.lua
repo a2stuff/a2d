a2stuff/a2d

commit 5d917faeebacd565e779e8deffdf3b9dfbc153a6

MGTK: Respond to new cursor position after menu is drawn
Follow up to c5f17c6 - since drawing a menu can be slow, if the cursor
was moved while drawing, check for the new position and update the
item and/or menu that should now be shown.

src/mgtk/mgtk.s
@@ -6423,6 +6423,9 @@ menu_item  .byte
 event_loop:
         COPY_BYTES kLastCursorPosLen, cursor_pos::xcoord, last_cursor_pos
 
+        ;; After `ShowMenu` (which can be slow), check for updated cursor
+        ;; position.
+event_loop_recheck:
         jsr     GetAndReturnEvent
 
         ;; --------------------
@@ -6436,7 +6439,7 @@ event_loop:
         bit     was_in_menu_flag
         jmi     handle_click
         lda     cur_open_menu_id
-        bne     event_loop
+        bne     event_loop_recheck
         jeq     handle_click    ; always
     END_IF
 
@@ -6563,7 +6566,7 @@ imb_change:
         sta     cur_open_menu_id
 
         jsr     ShowMenu
-        jmp     event_loop
+        jmp     event_loop_recheck
 
         ;; --------------------------------------------------
         ;; Over an item the current menu

commit 333cd768b2f7066e9728b7521d8729400f0da2f1

MGTK: Fix keyboard control of menus if cursor over menu bar
Regression in 5d917fa - if the cursor was over the menu, then when the
Escape key is pressed the menu would show but then immediately close,
as a "move over the menu bar" was detected. Sync the cursor position
when initiating the menu event loop to prevent this.

src/mgtk/mgtk.s
@@ -6414,6 +6414,7 @@ menu_item  .byte
         jpl     in_menu_bar     ; use mouse coords for menu
 
         copy8   #kKeyboardMouseStateInactive, kbd_mouse_state
+        COPY_BYTES kLastCursorPosLen, cursor_pos::xcoord, last_cursor_pos
         ldx     #0
         jsr     GetMenu         ; X = index
         lda     curmenu::menu_id

tests/desktop/menus.lua
@@ -635,3 +635,15 @@ test.Step(
     test.ExpectNotMatch(a2dtest.OCRScreen(), "Rename *^M", "rename shortcut should not be ^M")
     apple2.EscapeKey()
 end)
+
+test.Step(
+  "Esc shows menu properly even if cursor over menu bar",
+  function()
+    a2d.InMouseKeysMode(function(m)
+        m.MoveToApproximately(400, 0)
+    end)
+    apple2.EscapeKey()
+    emu.wait(5)
+    test.ExpectMatch(a2dtest.OCRScreen(), "About This Apple II", "menu should still be showing")
+    apple2.EscapeKey()
+end)