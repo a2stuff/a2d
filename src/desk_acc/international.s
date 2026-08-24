5298bea330027b47f1ae34a74f62964974e10654
Joshua Bell <inexorabletash@gmail.com>
2026-08-24 14:11:10 -0700

    International: Fix cursor repaint when samples update
    A change in 0b3e8759 introduced cursor shielding but used a rect that
    only covered the character fields, not the format samples. Introduce a
    new rect for each sample and use that for correct cursor shielding.

--- src/desk_acc/international.s
+++ src/desk_acc/international.s
@@ -69,6 +69,7 @@
         kFieldWidth = kTextBoxTextHOffset * 2 + 7
         kFieldHeight = kSystemFontHeight + 4
         kSampleLeft = kFieldLeft + kFieldWidth + kMarginX
+        kSampleWidth = 50
 
 .macro DEFINE_FIELD name, string, sample, row
         DEFINE_RECT_SZ .ident(.sprintf("%s_rect", .string(name))), kFieldLeft, row, kFieldWidth, kFieldHeight
@@ -76,6 +77,7 @@
         DEFINE_POINT .ident(.sprintf("%s_char_pos", .string(name))), kFieldLeft+kTextBoxTextHOffset+1, row+2+kSystemFontHeight
         DEFINE_LABEL name, string,      kLabelLeft, row+2+kSystemFontHeight
         DEFINE_LABEL .ident(.sprintf("%s_sample", .string(name))), .sprintf("%s  ", sample), kSampleLeft, row+2+kSystemFontHeight
+        DEFINE_RECT_SZ .ident(.sprintf("%s_sample_rect", .string(name))), kSampleLeft, row+2, kSampleWidth, kSystemFontHeight
 .endmacro
         DEFINE_FIELD date, res_string_label_date_separator, "10/11/12", kRow1
         kDateSampleOffset1 = 3
@@ -595,12 +597,15 @@ char:   .byte   SELF_MODIFIED_BYTE
         sta     drawchar_params::char
         sta     date_sample_label_str+kDateSampleOffset1
         sta     date_sample_label_str+kDateSampleOffset2
+        MGTK_CALL MGTK::MoveTo, date_char_pos
         MGTK_CALL MGTK::ShieldCursor, date_hilite
         MGTK_CALL MGTK::PaintRect, date_hilite
-        MGTK_CALL MGTK::MoveTo, date_char_pos
         MGTK_CALL MGTK::DrawText, drawchar_params
+        MGTK_CALL MGTK::UnshieldCursor
+
         MGTK_CALL MGTK::SetTextBG, settextbg_white_params
         MGTK_CALL MGTK::MoveTo, date_sample_label_pos
+        MGTK_CALL MGTK::ShieldCursor, date_sample_rect
         MGTK_CALL MGTK::DrawString, date_sample_label_str
         MGTK_CALL MGTK::UnshieldCursor
         rts
@@ -610,12 +615,15 @@ char:   .byte   SELF_MODIFIED_BYTE
         CALL    ReadSetting, X=#DeskTopSettings::intl_time_sep
         sta     drawchar_params::char
         sta     time_sample_label_str+kTimeSampleOffset
+        MGTK_CALL MGTK::MoveTo, time_char_pos
         MGTK_CALL MGTK::ShieldCursor, time_hilite
         MGTK_CALL MGTK::PaintRect, time_hilite
-        MGTK_CALL MGTK::MoveTo, time_char_pos
         MGTK_CALL MGTK::DrawText, drawchar_params
+        MGTK_CALL MGTK::UnshieldCursor
+
         MGTK_CALL MGTK::SetTextBG, settextbg_white_params
         MGTK_CALL MGTK::MoveTo, time_sample_label_pos
+        MGTK_CALL MGTK::ShieldCursor, time_sample_rect
         MGTK_CALL MGTK::DrawString, time_sample_label_str
         MGTK_CALL MGTK::UnshieldCursor
         rts
@@ -625,12 +633,15 @@ char:   .byte   SELF_MODIFIED_BYTE
         CALL    ReadSetting, X=#DeskTopSettings::intl_deci_sep
         sta     drawchar_params::char
         sta     deci_sample_label_str+kDeciSampleOffset
+        MGTK_CALL MGTK::MoveTo, deci_char_pos
         MGTK_CALL MGTK::ShieldCursor, deci_hilite
         MGTK_CALL MGTK::PaintRect, deci_hilite
-        MGTK_CALL MGTK::MoveTo, deci_char_pos
         MGTK_CALL MGTK::DrawText, drawchar_params
+        MGTK_CALL MGTK::UnshieldCursor
+
         MGTK_CALL MGTK::SetTextBG, settextbg_white_params
         MGTK_CALL MGTK::MoveTo, deci_sample_label_pos
+        MGTK_CALL MGTK::ShieldCursor, deci_sample_rect
         MGTK_CALL MGTK::DrawString, deci_sample_label_str
         MGTK_CALL MGTK::UnshieldCursor
         rts
@@ -640,12 +651,15 @@ char:   .byte   SELF_MODIFIED_BYTE
         CALL    ReadSetting, X=#DeskTopSettings::intl_thou_sep
         sta     drawchar_params::char
         sta     thou_sample_label_str+kThouSampleOffset
+        MGTK_CALL MGTK::MoveTo, thou_char_pos
         MGTK_CALL MGTK::ShieldCursor, thou_hilite
         MGTK_CALL MGTK::PaintRect, thou_hilite
-        MGTK_CALL MGTK::MoveTo, thou_char_pos
         MGTK_CALL MGTK::DrawText, drawchar_params
+        MGTK_CALL MGTK::UnshieldCursor
+
         MGTK_CALL MGTK::SetTextBG, settextbg_white_params
         MGTK_CALL MGTK::MoveTo, thou_sample_label_pos
+        MGTK_CALL MGTK::ShieldCursor, thou_sample_rect
         MGTK_CALL MGTK::DrawString, thou_sample_label_str
         MGTK_CALL MGTK::UnshieldCursor
         rts
