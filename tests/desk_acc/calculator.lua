a2stuff/a2d

commit 6d097e8d02ab9446a9afc2575ddabeb9bad16b62

MGTK: Add Draw{Text,String}{Right,Centered} variants
Rather than forcing clients to implement centering and right-alignment
themselves, add it to MGTK itself. Since computing the text width is
the first step of DrawText, and offsetting the drawing position is the
last step, it ends up being fairly minimal code.

No functional changes.

src/desk_acc/calculator.s
@@ -320,25 +320,14 @@ spaces_string:
 error_string:
         PASCAL_STRING res_string_error_string
 
-.params textwidth_params
-textptr:        .addr   text_buffer1
-textlen:        .byte   15
-result:         .word   0
-.endparams
-
         kDAWindowId = $80
 
 .params closewindow_params
 window_id:     .byte   kDAWindowId
 .endparams
 
-.params text_pos_params3
-left:   .word   0
-base:   .word   16
-.endparams
-
-.params text_pos_params2
-left:   .word   15
+.params text_pos_params
+left:   .word   kDisplayWidth-15
 base:   .word   16
 .endparams
 
@@ -1231,8 +1220,8 @@ invert_rect:
         MGTK_CALL MGTK::GetWinPort, getwinport_params
     IF A <> #MGTK::Error::window_obscured
         MGTK_CALL MGTK::SetPort, grafport
-        CALL    PreDisplayBuffer, XY=#text_buffer1
-        MGTK_CALL MGTK::DrawText, drawtext_params1
+        CALL    PreDisplayBuffer
+        MGTK_CALL MGTK::DrawTextRight, drawtext_params1
         MGTK_CALL MGTK::UnshieldCursor
     END_IF
         rts
@@ -1242,27 +1231,19 @@ invert_rect:
         MGTK_CALL MGTK::GetWinPort, getwinport_params
     IF A <> #MGTK::Error::window_obscured
         MGTK_CALL MGTK::SetPort, grafport
-        CALL    PreDisplayBuffer, XY=#text_buffer2
-        MGTK_CALL MGTK::DrawText, drawtext_params2
+        CALL    PreDisplayBuffer
+        MGTK_CALL MGTK::DrawTextRight, drawtext_params2
         MGTK_CALL MGTK::UnshieldCursor
     END_IF
         rts
 .endproc ; DisplayBuffer2
 
 .proc PreDisplayBuffer
-        stx     textwidth_params::textptr ; text buffer address in x,y
-        sty     textwidth_params::textptr+1
-        MGTK_CALL MGTK::TextWidth, textwidth_params
-        lda     #kDisplayWidth-15 ; ???
-        sec
-        sbc     textwidth_params::result
-        sta     text_pos_params3::left
-
         MGTK_CALL MGTK::ShieldCursor, clear_display_params
 
-        MGTK_CALL MGTK::MoveTo, text_pos_params2 ; clear with spaces
-        MGTK_CALL MGTK::DrawString, spaces_string
-        MGTK_CALL MGTK::MoveTo, text_pos_params3 ; set up for display
+        MGTK_CALL MGTK::MoveTo, text_pos_params ; clear with spaces
+        MGTK_CALL MGTK::DrawStringRight, spaces_string
+        MGTK_CALL MGTK::MoveTo, text_pos_params ; set up for display
         rts
 .endproc ; PreDisplayBuffer
 

src/desk_acc/sci.calc.s
@@ -448,25 +448,14 @@ spaces_string:
 error_string:
         PASCAL_STRING res_string_error_string
 
-.params textwidth_params
-textptr:        .addr   text_buffer1
-textlen:        .byte   15
-result:         .word   0
-.endparams
-
         kDAWindowId = $80
 
 .params closewindow_params
 window_id:     .byte   kDAWindowId
 .endparams
 
-.params text_pos_params3
-left:   .word   0
-base:   .word   16
-.endparams
-
-.params text_pos_params2
-left:   .word   15 + kBasicOffset
+.params text_pos_params
+left:   .word   kDisplayRight-15
 base:   .word   16
 .endparams
 
@@ -1420,8 +1409,8 @@ invert_rect:
         MGTK_CALL MGTK::GetWinPort, getwinport_params
     IF A <> #MGTK::Error::window_obscured
         MGTK_CALL MGTK::SetPort, grafport
-        CALL    PreDisplayBuffer, XY=#text_buffer1
-        MGTK_CALL MGTK::DrawText, drawtext_params1
+        CALL    PreDisplayBuffer
+        MGTK_CALL MGTK::DrawTextRight, drawtext_params1
         MGTK_CALL MGTK::UnshieldCursor
     END_IF
         rts
@@ -1431,27 +1420,19 @@ invert_rect:
         MGTK_CALL MGTK::GetWinPort, getwinport_params
     IF A <> #MGTK::Error::window_obscured
         MGTK_CALL MGTK::SetPort, grafport
-        CALL    PreDisplayBuffer, XY=#text_buffer2
-        MGTK_CALL MGTK::DrawText, drawtext_params2
+        CALL    PreDisplayBuffer
+        MGTK_CALL MGTK::DrawTextRight, drawtext_params2
         MGTK_CALL MGTK::UnshieldCursor
     END_IF
         rts
 .endproc ; DisplayBuffer2
 
 .proc PreDisplayBuffer
-        stx     textwidth_params::textptr ; text buffer address in x,y
-        sty     textwidth_params::textptr+1
-        MGTK_CALL MGTK::TextWidth, textwidth_params
-        lda     #kDisplayRight-15 ; ???
-        sec
-        sbc     textwidth_params::result
-        sta     text_pos_params3::left
-
         MGTK_CALL MGTK::ShieldCursor, clear_display_params
 
-        MGTK_CALL MGTK::MoveTo, text_pos_params2 ; clear with spaces
-        MGTK_CALL MGTK::DrawString, spaces_string
-        MGTK_CALL MGTK::MoveTo, text_pos_params3 ; set up for display
+        MGTK_CALL MGTK::MoveTo, text_pos_params ; clear with spaces
+        MGTK_CALL MGTK::DrawStringRight, spaces_string
+        MGTK_CALL MGTK::MoveTo, text_pos_params ; set up for display
         rts
 .endproc ; PreDisplayBuffer
 
@@ -1497,13 +1478,7 @@ loop:
         MGTK_CALL MGTK::PaintRect, SELF_MODIFIED, rect_addr ; draw button face
 
         MGTK_CALL MGTK::MoveTo, 0, pos
-        copy16  label, params::string
-        MGTK_CALL MGTK::StringWidth, string_width_params
-        lsr16   params::width
-        sub16   #0, params::width, params::dx
-        MGTK_CALL MGTK::Move, move_by_params
-
-        MGTK_CALL MGTK::DrawString, 0, label
+        MGTK_CALL MGTK::DrawStringCentered, SELF_MODIFIED, label
 
         add16_8 ptr, #.sizeof(btn_c)
         jmp     loop
@@ -1514,16 +1489,6 @@ break:
         MGTK_CALL MGTK::ShowCursor
 
         rts
-
-.params params
-string: .addr   0               ; used as string for `StringWidth`
-width:  .addr   0               ; used as width for `StringWidth`, X for `Move`
-dx := width
-dy:     .word   0               ; used as Y for `Move`
-.endparams
-string_width_params := params + 0
-move_by_params := params + 2
-
 .endproc ; DrawContent
 
 ;;; ============================================================

src/desk_acc/benchmark.s
@@ -183,7 +183,7 @@ pattern_plaid:
         MGTK_CALL MGTK::SetPenSize, pensize_normal
 
         MGTK_CALL MGTK::MoveTo, title_label_pos
-        CALL    DrawStringCentered, AX=#title_label_str
+        MGTK_CALL MGTK::DrawStringCentered, title_label_str
 
         MGTK_CALL MGTK::FrameRect, meter_frame
 
@@ -200,7 +200,7 @@ pattern_plaid:
         ldx     #0
         jsr     IntToString
         MGTK_CALL MGTK::Move, pt_labeldelta
-        CALL    DrawStringCentered, AX=#str_from_int
+        MGTK_CALL MGTK::DrawStringCentered, str_from_int
 
         pla
         clc
@@ -474,26 +474,6 @@ hit:    lda     winfo::window_id
         rts
 
 .endproc ; ProbeSpeed
-
-;;; ============================================================
-
-.proc DrawStringCentered
-        params := $06
-        str := params
-        width := params+2
-        dx := params
-        dy := params+2
-
-        stax    str
-        stax    @addr
-        MGTK_CALL MGTK::StringWidth, params
-        lsr16   width           ; /= 2
-        sub16   #0, width, dx
-        copy16  #0, dy
-        MGTK_CALL MGTK::Move, params
-        MGTK_CALL MGTK::DrawString, SELF_MODIFIED, @addr
-        rts
-.endproc ; DrawStringCentered
 
 ;;; ============================================================
 

src/desk_acc/change.type.s
@@ -500,10 +500,10 @@ yes:    RETURN  C=0
         MGTK_CALL MGTK::SetPenSize, pensize_normal
 
         MGTK_CALL MGTK::MoveTo, type_label_pos
-        CALL    DrawStringRight, AX=#type_label_str
+        MGTK_CALL MGTK::DrawStringRight, type_label_str
 
         MGTK_CALL MGTK::MoveTo, auxtype_label_pos
-        CALL    DrawStringRight, AX=#auxtype_label_str
+        MGTK_CALL MGTK::DrawStringRight, auxtype_label_str
 
         MGTK_CALL MGTK::FrameRect, type_rect
         MGTK_CALL MGTK::FrameRect, auxtype_rect
@@ -541,23 +541,6 @@ yes:    RETURN  C=0
 
 digits: .byte   "0123456789ABCDEF"
 .endproc ; GetDigits
-
-;;; ============================================================
-
-.proc DrawStringRight
-        params := $06
-        str := params
-        width := params+2
-
-        stax    str
-        stax    @addr
-        MGTK_CALL MGTK::StringWidth, params
-        sub16   #0, width, params+MGTK::Point::xcoord
-        copy16  #0, params+MGTK::Point::ycoord
-        MGTK_CALL MGTK::Move, params
-        MGTK_CALL MGTK::DrawString, SELF_MODIFIED, @addr
-        rts
-.endproc ; DrawStringRight
 
 ;;; ============================================================
 

src/desk_acc/control.panel.s
@@ -1066,7 +1066,7 @@ notpencopy:     .byte   MGTK::notpencopy
         MGTK_CALL MGTK::DrawString, caret_blink_slow_label_str
 
         MGTK_CALL MGTK::MoveTo, caret_blink_fast_label_pos
-        CALL    DrawStringRight, AX=#caret_blink_fast_label_str
+        MGTK_CALL MGTK::DrawStringRight, caret_blink_fast_label_str
 
         BTK_CALL BTK::RadioDraw, caret_blink_button1
         BTK_CALL BTK::RadioDraw, caret_blink_button2
@@ -1079,9 +1079,9 @@ notpencopy:     .byte   MGTK::notpencopy
         MGTK_CALL MGTK::MoveTo, caret_blink_button1_shortcut_label_pos
         MGTK_CALL MGTK::DrawString, caret_blink_button1_shortcut_label_str
         MGTK_CALL MGTK::MoveTo, caret_blink_button2_shortcut_label_pos
-        CALL    DrawStringCentered, AX=#caret_blink_button2_shortcut_label_str
+        MGTK_CALL MGTK::DrawStringCentered, caret_blink_button2_shortcut_label_str
         MGTK_CALL MGTK::MoveTo, caret_blink_button3_shortcut_label_pos
-        CALL    DrawStringRight, AX=#caret_blink_button3_shortcut_label_str
+        MGTK_CALL MGTK::DrawStringRight, caret_blink_button3_shortcut_label_str
     END_IF
 
 
@@ -1619,41 +1619,6 @@ done:   rts
 
         jmp     InputLoop
 .endproc ; HandleRGBClick
-
-;;; ============================================================
-
-.proc DrawStringRight
-        params := $06
-        str := params
-        width := params+2
-
-        stax    str
-        stax    @addr
-        MGTK_CALL MGTK::StringWidth, params
-        sub16   #0, width, params+MGTK::Point::xcoord
-        copy16  #0, params+MGTK::Point::ycoord
-        MGTK_CALL MGTK::Move, params
-        MGTK_CALL MGTK::DrawString, SELF_MODIFIED, @addr
-        rts
-.endproc ; DrawStringRight
-
-.proc DrawStringCentered
-        params := $06
-        str := params
-        width := params+2
-        dx := params
-        dy := params+2
-
-        stax    str
-        stax    @addr
-        MGTK_CALL MGTK::StringWidth, params
-        lsr16   width           ; /= 2
-        sub16   #0, width, dx
-        copy16  #0, dy
-        MGTK_CALL MGTK::Move, params
-        MGTK_CALL MGTK::DrawString, SELF_MODIFIED, @addr
-        rts
-.endproc ; DrawStringCentered
 
 ;;; ============================================================
 

src/desk_acc/show.duet.file.s
@@ -143,13 +143,13 @@ str_instruct:   PASCAL_STRING res_string_instructions
         MGTK_CALL MGTK::FrameRect, frame_rect
 
         MGTK_CALL MGTK::MoveTo, pos_playing
-        CALL    DrawStringCentered, AX=#str_playing
+        MGTK_CALL MGTK::DrawStringCentered, str_playing
         MGTK_CALL MGTK::MoveTo, pos_credit1
-        CALL    DrawStringCentered, AX=#str_credit1
+        MGTK_CALL MGTK::DrawStringCentered, str_credit1
         MGTK_CALL MGTK::MoveTo, pos_credit2
-        CALL    DrawStringCentered, AX=#str_credit2
+        MGTK_CALL MGTK::DrawStringCentered, str_credit2
         MGTK_CALL MGTK::MoveTo, pos_instruct
-        CALL    DrawStringCentered, AX=#str_instruct
+        MGTK_CALL MGTK::DrawStringCentered, str_instruct
 
         MGTK_CALL MGTK::FlushEvents
 
@@ -165,28 +165,6 @@ str_instruct:   PASCAL_STRING res_string_instructions
         MGTK_CALL MGTK::ShowCursor
         rts
 .endproc ; Init
-
-;;; ============================================================
-;;; Draw centered string
-;;; Input: A,X = string address, `pos` used, has ycoord
-;;; Trashes $6...$9
-.proc DrawStringCentered
-        params := $06
-        str := params
-        width := params+2
-        dx := params
-        dy := params+2
-
-        stax    str
-        stax    @addr
-        MGTK_CALL MGTK::StringWidth, params
-        lsr16   width           ; /= 2
-        sub16   #0, width, dx
-        copy16  #0, dy
-        MGTK_CALL MGTK::Move, params
-        MGTK_CALL MGTK::DrawString, SELF_MODIFIED, @addr
-        rts
-.endproc ; DrawStringCentered
 
 ;;; ============================================================
 

src/desk_acc/show.font.file.s
@@ -247,11 +247,6 @@ line_addrs:
 
 
 .proc DrawWindow
-PARAM_BLOCK params, $06
-data    .addr
-width   .word
-END_PARAM_BLOCK
-
         MGTK_CALL MGTK::GetWinPort, getwinport_params
         RTS_IF A = #MGTK::Error::window_obscured
 
@@ -259,24 +254,20 @@ END_PARAM_BLOCK
         MGTK_CALL MGTK::HideCursor
 
         copy16  #kInitialY, pos::ycoord
-
+        copy16  #kDAWidth/2, pos::xcoord ; center it
 
         copy8   #0, index
     DO
         lda     index
         asl
         tax
-        copy16  line_addrs,x, params::data
+        copy16  line_addrs,x, @addr
 
         ;; Position the string
-        MGTK_CALL MGTK::StringWidth, params
-        sub16   #kDAWidth, params::width, pos::xcoord ; center it
-        lsr16   pos::xcoord
         add16   pos::ycoord, #kLineHeight, pos::ycoord ; next row
 
         MGTK_CALL MGTK::MoveTo, pos
-        copy16  params::data, @addr
-        MGTK_CALL MGTK::DrawString, SELF_MODIFIED, @addr
+        MGTK_CALL MGTK::DrawStringCentered, SELF_MODIFIED, @addr
 
         inc     index
     WHILE lda index : A <> #kLineCount

src/desk_acc/show.text.file.s
@@ -1005,7 +1005,7 @@ reserved:       .byte   0
 .endparams
 mode_mapinfo_viewloc_xcoord := mode_mapinfo::viewloc::xcoord
 
-        DEFINE_POINT mode_pos, 0, 10
+        DEFINE_POINT mode_pos, kLabelWidth/2, 10
 
 .params winframerect_params
 window_id:      .byte   kDAWindowId
@@ -1031,12 +1031,6 @@ window_id:      .byte   kDAWindowId
         ;; Clear background
         MGTK_CALL MGTK::PaintRect, mode_mapinfo::maprect
 
-        ;; Center string
-        text_params     := $6
-        text_addr       := text_params + 0
-        text_length     := text_params + 2
-        text_width      := text_params + 3
-
         lda     fixed_mode_flag
     IF NOT_ZERO
         ldax    #fixed_str
@@ -1045,15 +1039,8 @@ window_id:      .byte   kDAWindowId
     END_IF
         stax    text_addr
 
-        ldy     #0
-        copy8   (text_addr),y, text_length
-        inc16   text_addr       ; point past length
-        MGTK_CALL MGTK::TextWidth, text_params
-
-        sub16   #kLabelWidth, text_width, mode_pos::xcoord
-        lsr16   mode_pos::xcoord ; /= 2
         MGTK_CALL MGTK::MoveTo, mode_pos
-        MGTK_CALL MGTK::DrawText, text_params
+        MGTK_CALL MGTK::DrawStringCentered, SELF_MODIFIED, text_addr
 
         ;; Reset port
         COPY_STRUCT default_port, winfo::port

commit f00dfb214681114d26c8dcc1b676f771f4c6b9e8

Calculator & Sci.Calc: Fix clearing of display
Regressed in 6d097e8 - something in the use of spaces to clear the
display got knocked off kilter by the switch to DrawStringRight. It's
faster, simpler, and more maintainable to paint a rect anyway, so do
that instead.

Fixes #916

src/desk_acc/calculator.s
@@ -315,8 +315,6 @@ textlen:        .byte   15
 text_buffer2:
         .res    kTextBufferSize+2, 0
 
-spaces_string:
-        PASCAL_STRING "          "
 error_string:
         PASCAL_STRING res_string_error_string
 
@@ -1235,9 +1233,7 @@ invert_rect:
 
 .proc PreDisplayBuffer
         MGTK_CALL MGTK::ShieldCursor, clear_display_params
-
-        MGTK_CALL MGTK::MoveTo, text_pos_params ; clear with spaces
-        MGTK_CALL MGTK::DrawStringRight, spaces_string
+        MGTK_CALL MGTK::PaintRect, clear_display_params
         MGTK_CALL MGTK::MoveTo, text_pos_params ; set up for display
         rts
 .endproc ; PreDisplayBuffer

src/desk_acc/sci.calc.s
@@ -443,8 +443,6 @@ textlen:        .byte   15
 text_buffer2:
         .res    kTextBufferSize+2, 0
 
-spaces_string:
-        PASCAL_STRING "          "
 error_string:
         PASCAL_STRING res_string_error_string
 
@@ -1424,9 +1422,7 @@ invert_rect:
 
 .proc PreDisplayBuffer
         MGTK_CALL MGTK::ShieldCursor, clear_display_params
-
-        MGTK_CALL MGTK::MoveTo, text_pos_params ; clear with spaces
-        MGTK_CALL MGTK::DrawStringRight, spaces_string
+        MGTK_CALL MGTK::PaintRect, clear_display_params
         MGTK_CALL MGTK::MoveTo, text_pos_params ; set up for display
         rts
 .endproc ; PreDisplayBuffer

tests/desk_acc/calculator.lua
@@ -160,6 +160,12 @@ test.Variants(
     ExpectExpression("6/2", "2")
     ExpectExpression("6/2=", "3")
 
+    ExpectExpression("1/1234=", "8.10372772E-04")
+    a2d.WaitForRepaint()
+    local ocr = OCRDisplay()
+    test.ExpectEquals(OCRDisplay(), "0", "display should be cleared")
+    apple2.EscapeKey()
+
     a2d.CloseWindow()
 end)
 