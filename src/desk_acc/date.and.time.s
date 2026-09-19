a2stuff/a2d

commit 10c1f58df4ef2a31e42973374e505f01189e26f5

Date & Time DA: Support setting some system clocks
Add clock detection and setting routines for some common system
clocks:

* Apple IIgs
* No-Slot Clock
* ThunderClock
* ROMX RTC
* The Cricket!

Previously, the Date & Time DA was read/write if there was no system
clock, and simply updated the ProDOS date/time stamp used for file
operations. If there was a system clock it was read-only. The
assumption was that a user would use the utility provided with the
clock; ProDOS clock drivers only support reading the clock, not
setting it, so this wasn't trivial.

If a clock driver is present but not one of the currently supported
three then the DA remains read-only.

A few caveats:

* The ThunderClock only supports day/month/day-of-week and infers the
  year using a 7 year lookup table. So setting the ThunderClock to an
  arbitrary year is not supported.

* Virtual ][ does not support setting the No-Slot Clock. It supports
  setting the ThunderClock if appropriate configured, but seems to be
  buggy.

* The Cricket! clock and ROMX RTC are not covered by tests.

Other clocks may be supported in the future.

Resolved #38

src/inc/apple2.inc
@@ -154,12 +154,15 @@ RAMRDOFF        := $C002        ; (W) Read from main 48K RAM ($200-$BFFF)
 RAMRDON         := $C003        ; (W) Read from auxiliary 48K RAM ($200-$BFFF)
 RAMWRTOFF       := $C004        ; (W) Write to main 48K RAM ($200-$BFFF)
 RAMWRTON        := $C005        ; (W) Write to auxiliary 48K RAM ($200-$BFFF)
+SETSLOTCXROM    := $C006        ; (W) Slot ROM at $Cx00
+SETINTCXROM     := $C007        ; (W) Internal ROM at $Cx00
 ALTZPOFF        := $C008        ; (W) Use main zero page/stack/LC
 ALTZPON         := $C009        ; (W) Use auxiliary zero page/stack/LC
 
 CLR80VID        := $C00C        ; (W) Disable 80-column hardware
 SET80VID        := $C00D        ; (W) Enable 80-column hardware
 RDBNK2          := $C011        ; (R7) bit 7=1 if LCBANK2 enabled
+RDCXROM         := $C015        ; (R7) bit 7=1 if SLOTCXROM active
 RDALTZP         := $C016        ; (R7) bit 7=1 if auxiliary ZP/stack/LC enabled
 RD80STORE       := $C018        ; (R7) bit 7=1 if 80STORE enabled
 RDPAGE2         := $C01C        ; (R7) bit 7=1 if PAGE2 on
@@ -231,6 +234,7 @@ HR3_ON          := $C0B7
 TEXT16_OFF      := $C0B8
 TEXT16_ON       := $C0B9
 
+C8OFF           := $CFFF
 
 ;;; ============================================================
 ;;; Aux Memory / Extended 80 Column Card

tests/desk_acc/date_and_time_no_clock.lua
@@ -1,0 +1,398 @@
+--[[ BEGINCONFIG ========================================
+
+MODELARGS="-sl1 ramfactor -sl2 mouse -sl7 cffa2"
+DISKARGS="-hard1 $HARDIMG -hard2 tests.hdv"
+
+======================================== ENDCONFIG ]]
+
+a2d.ConfigureRepaintTime(2)
+
+--[[
+  Run these tests on a system without a real-time clock.
+
+  The UI should be enabled, and if the date/time is changed
+  then ProDOS's date/time is set so that subsequent file
+  operations are time-stamped.
+]]
+
+a2d.RemoveClockDriverAndReboot()
+
+--[[
+  Start with a fresh disk image. Run DeskTop. Apple Menu > Control
+  Panels. View > by Name. Open Date and Time. Verify that the date
+  shown in the dialog matches the file dates. Click OK without
+  changing anything. Verify that the entire desktop repaints, and that
+  dates in the window are shown with "Today". Open Date and Time.
+  Click OK without changing anything. Verify that the entire desktop
+  does not repaint
+
+  NOTE: Requires that the last git commit date matches the date the
+  files were actually built.
+]]
+test.Step(
+  "Fresh disk image",
+  function()
+    a2d.InvokeMenuItem(a2d.APPLE_MENU, a2d.CONTROL_PANELS)
+    a2d.InvokeMenuItem(a2d.VIEW_MENU, a2d.VIEW_BY_NAME)
+    test.ExpectNotMatch(a2dtest.OCRFrontWindowContent(), "Today", "dates should not say Today")
+    a2d.SelectAndOpen("DATE.AND.TIME")
+    test.Snap("verify dialog date matches packaged file dates")
+    a2dtest.ExpectFullRepaint(a2d.DialogOK)
+    test.ExpectMatch(a2dtest.OCRFrontWindowContent(), "Today", "dates should now say Today")
+    a2d.SelectAndOpen("DATE.AND.TIME")
+    a2dtest.ExpectMinimalRepaint(a2d.DialogOK)
+    a2d.Reboot()
+    a2d.WaitForDesktopReady()
+end)
+
+--[[
+  Run Apple Menu > Control Panels > Date and Time. Set date. Reboot
+  system, and re-run DeskTop. Create a new folder. Use File > Get
+  Info. Verify that the date was saved/restored.
+]]
+test.Step(
+  "Change date - persisted",
+  function()
+    a2d.InvokeMenuItem(a2d.APPLE_MENU, a2d.CONTROL_PANELS)
+    a2d.SelectAndOpen("DATE.AND.TIME")
+    apple2.UpArrowKey() -- change day
+    apple2.TabKey()
+    apple2.UpArrowKey() -- change month
+    apple2.TabKey()
+    apple2.UpArrowKey() -- change year
+    test.Snap("verify date is modified")
+    a2d.DialogOK()
+    -- Should write timestamp to DESKTOP.SYSTEM. Restart to verify.
+    a2d.Reboot()
+    a2d.WaitForDesktopReady()
+
+    -- Create new folder
+    a2d.CreateFolder("/RAM1/NOT.TODAY")
+
+    -- Change date again to avoid "Today"
+    apple2.SetProDOSDate(1999, 9, 13)
+
+    -- Inspect file
+    a2d.SelectPath("/RAM1/NOT.TODAY")
+    a2d.OAShortcut("I") -- File > Get Info
+    test.Snap("verify date matches set previously set date")
+    a2d.DialogOK()
+    a2d.DeletePath("/RAM1/NOT.TODAY")
+    a2d.CloseAllWindows()
+end)
+
+--[[
+  Launch DeskTop. Run the Date and Time DA, and change the setting to
+  12 hour. Verify that the time is shown as 12-hour, and if less than
+  10 is displayed without a leading 0. Use the Right Arrow and Left
+  Arrow keys and the mouse, and verify that the AM/PM field is
+  selectable. Select the AM/PM field. Use the up and down arrow keys
+  and the arrow buttons, and verify that the field toggles. Select the
+  hours field. Use the up and down arrow keys and the arrow buttons,
+  and verify that the field cycles from 1 through 12.
+]]
+test.Step(
+  "12-hour field behavior",
+  function()
+    apple2.SetProDOSTime(0, 0)
+    a2d.InvokeMenuItem(a2d.APPLE_MENU, a2d.CONTROL_PANELS)
+    a2d.SelectAndOpen("DATE.AND.TIME")
+    a2d.OAShortcut("1") -- 12-hour
+    apple2.TabKey() -- to month
+    apple2.TabKey() -- to year
+    apple2.TabKey() -- to hour
+    apple2.TabKey() -- to min
+    apple2.TabKey() -- to period
+    local ocr = a2dtest.OCRFrontWindowContent({invert=true})
+    test.Expect(ocr:find("AM") or ocr:find("PM"), "period field should be enabled")
+
+    apple2.UpArrowKey()
+    emu.wait(1)
+    local ocr2 = a2dtest.OCRFrontWindowContent({invert=true})
+    test.Expect(
+      (ocr:find("AM") and ocr2:find("PM")) or
+      (ocr:find("PM") and ocr2:find("PM")),
+      "period field should be modifiable")
+
+    apple2.LeftArrowKey() -- to min
+    apple2.LeftArrowKey() -- to hour
+    for i = 1, 24 do
+      apple2.UpArrowKey()
+      a2d.WaitForRepaint()
+      local expect = ((i - 1) % 12) + 1
+      test.ExpectMatch(a2dtest.OCRFrontWindowContent({invert=true}), tostring(expect),
+                  "should be 12 hour cycle")
+    end
+    a2d.DialogOK()
+end)
+
+--[[
+  Launch DeskTop. Run the Date and Time DA, and change the setting to
+  24 hour. Verify that the time is shown as 24-hour, and if less than
+  10 is displayed with a leading 0. Use the Right Arrow and Left Arrow
+  keys and the mouse, and verify that the AM/PM field is not
+  selectable. Use the up and down arrow keys and the arrow buttons,
+  and verify that the field cycles from 0 through 23.
+]]
+test.Step(
+  "24-hour field behavior",
+  function()
+    apple2.SetProDOSTime(0, 0)
+    a2d.InvokeMenuItem(a2d.APPLE_MENU, a2d.CONTROL_PANELS)
+    a2d.SelectAndOpen("DATE.AND.TIME")
+    a2d.OAShortcut("2") -- 12-hour
+    apple2.TabKey() -- to month
+    apple2.TabKey() -- to year
+    apple2.TabKey() -- to hour
+    apple2.TabKey() -- to min
+    apple2.TabKey() -- to back to day
+
+    local ocr = a2dtest.OCRFrontWindowContent({invert=true})
+    test.Expect(not ocr:find("AM") and not ocr:find("PM"), "period field should be disabled")
+
+    apple2.TabKey() -- to month
+    apple2.TabKey() -- to year
+    apple2.TabKey() -- to hour
+    for i = 1, 24 do
+      apple2.UpArrowKey()
+      a2d.WaitForRepaint()
+      test.ExpectMatch(a2dtest.OCRFrontWindowContent({invert=true}), string.format("%02d", i % 24),
+                  "should be 24 hour cycle")
+    end
+    a2d.DialogOK()
+end)
+
+--[[
+  Launch DeskTop. Run the Date and Time DA. Change the month and year,
+  and verify that the day range is clamped to 28, 29, 30 or 31 as
+  appropriate, including for leap years.
+]]
+test.Step(
+  "Day ranges",
+  function()
+    a2d.InvokeMenuItem(a2d.APPLE_MENU, a2d.CONTROL_PANELS)
+
+    function IncDate(y1, m1, d1, y2, m2, d2)
+      apple2.SetProDOSDate(y1, m1, d1)
+      local yy,mm,dd = apple2.GetProDOSDate()
+      a2d.SelectAndOpen("DATE.AND.TIME")
+      apple2.UpArrowKey()
+      a2d.DialogOK()
+      local y,m,d = apple2.GetProDOSDate()
+      test.ExpectEquals(
+        y, y2,
+        string.format("year: %d/%d/%d should increment to %d/%d/%d", y1, m1, d1, y2, m2, d2))
+      test.ExpectEquals(
+        m, m2,
+        string.format("month: %d/%d/%d should increment to %d/%d/%d", y1, m1, d1, y2, m2, d2))
+      test.ExpectEquals(
+        d, d2,
+        string.format("day: %d/%d/%d should increment to %d/%d/%d", y1, m1, d1, y2, m2, d2))
+    end
+
+    function DecDate(y1, m1, d1, y2, m2, d2)
+      apple2.SetProDOSDate(y1, m1, d1)
+      local yy,mm,dd = apple2.GetProDOSDate()
+      a2d.SelectAndOpen("DATE.AND.TIME")
+      apple2.DownArrowKey()
+      a2d.DialogOK()
+      local y,m,d = apple2.GetProDOSDate()
+      test.ExpectEquals(
+        y, y2,
+        string.format("year: %d/%d/%d should decrement to %d/%d/%d", y1, m1, d1, y2, m2, d2))
+      test.ExpectEquals(
+        m, m2,
+        string.format("month: %d/%d/%d should decrement to %d/%d/%d", y1, m1, d1, y2, m2, d2))
+      test.ExpectEquals(
+        d, d2,
+        string.format("day: %d/%d/%d should decrement to %d/%d/%d", y1, m1, d1, y2, m2, d2))
+    end
+
+
+    -- 31 day month
+    IncDate(2023, 1, 28, 2023, 1, 29)
+    IncDate(2023, 1, 29, 2023, 1, 30)
+    IncDate(2023, 1, 30, 2023, 1, 31)
+    IncDate(2023, 1, 31, 2023, 1, 1)
+    DecDate(2023, 1, 1, 2023, 1, 31)
+
+    -- 30 day month
+    IncDate(2023, 4, 28, 2023, 4, 29)
+    IncDate(2023, 4, 29, 2023, 4, 30)
+    IncDate(2023, 4, 30, 2023, 4, 1)
+    DecDate(2023, 4, 1, 2023, 4, 30)
+
+    -- 28 day month
+    IncDate(2023, 2, 28, 2023, 2, 1)
+    DecDate(2023, 2, 1, 2023, 2, 28)
+
+    -- 29 day month
+    IncDate(2024, 2, 28, 2024, 2, 29)
+    IncDate(2024, 2, 29, 2024, 2, 1)
+    DecDate(2024, 2, 1, 2024, 2, 29)
+end)
+
+-- Dialog control metrics
+local incr_x, incr_y = 264, 12
+local decr_x, decr_y = 264, 25
+local field_y = 21
+local day_x = 29
+local month_x = day_x + 45
+local year_x = month_x + 45
+local hour_x = year_x + 45
+local minute_x = hour_x + 40
+local period_x = minute_x + 20
+
+--[[
+  Launch DeskTop. Run the Date and Time DA. Click on the up/down
+  arrows. Verify that they invert correctly when the button is down.
+
+  Launch DeskTop. Run the Date and Time DA. Click in the various
+  fields (day/month/year/hour/minutes/period). Verify that the
+  appropriate field highlights.
+
+  Launch DeskTop. Run the Date and Time DA. Change the setting to 12
+  hour. Click on the AM/PM field. Verify that the field highlights.
+]]
+test.Step(
+  "Clicking fields",
+  function()
+    a2d.InvokeMenuItem(a2d.APPLE_MENU, a2d.CONTROL_PANELS)
+    a2d.SelectAndOpen("DATE.AND.TIME")
+    a2d.OAShortcut("1") -- 12-hour
+
+    local dialog_x, dialog_y = a2dtest.GetFrontWindowContentRect()
+
+    a2d.InMouseKeysMode(function(m)
+        m.MoveToApproximately(dialog_x + incr_x, dialog_y + incr_y)
+        m.ButtonDown()
+        emu.wait(10/60)
+        test.Snap("verify up button inverted")
+        m.ButtonUp()
+
+        m.MoveToApproximately(dialog_x + decr_x, dialog_y + decr_y)
+        m.ButtonDown()
+        emu.wait(10/60)
+        test.Snap("verify down button inverted")
+        m.ButtonUp()
+
+        m.MoveToApproximately(dialog_x + day_x, dialog_y + field_y)
+        m.Click()
+        test.Snap("verify day focused")
+        m.MoveToApproximately(dialog_x + month_x, dialog_y + field_y)
+        m.Click()
+        test.Snap("verify month focused")
+        m.MoveToApproximately(dialog_x + year_x, dialog_y + field_y)
+        m.Click()
+        test.Snap("verify year focused")
+        m.MoveToApproximately(dialog_x + hour_x, dialog_y + field_y)
+        m.Click()
+        test.Snap("verify hour focused")
+        m.MoveToApproximately(dialog_x + minute_x, dialog_y + field_y)
+        m.Click()
+        test.Snap("verify minute focused")
+        m.MoveToApproximately(dialog_x + period_x, dialog_y + field_y)
+        m.Click()
+        test.Snap("verify period focused")
+    end)
+    a2d.DialogOK()
+end)
+
+--[[
+  Launch DeskTop. Run the Date and Time DA. Change the setting to 24
+  hour. Click where the AM/PM field would be, to the right of the
+  minutes field. Verify that nothing happens.
+]]
+test.Step(
+  "Clicking 24-hour",
+  function()
+    a2d.InvokeMenuItem(a2d.APPLE_MENU, a2d.CONTROL_PANELS)
+    a2d.SelectAndOpen("DATE.AND.TIME")
+    a2d.OAShortcut("2") -- 24-hour
+
+    local dialog_x, dialog_y = a2dtest.GetFrontWindowContentRect()
+
+    a2d.InMouseKeysMode(function(m)
+        m.MoveToApproximately(dialog_x + period_x, dialog_y + field_y)
+        m.Click()
+        test.Snap("verify period not focused")
+    end)
+    a2d.DialogOK()
+end)
+
+--[[
+  Launch DeskTop. Run the Date and Time DA. Click the year field.
+  Click the up arrow. Verify that the year increments. Click the down
+  arrow. Verify that the year decrements. Verify that only the clicked
+  buttons highlight, and that they un-highlight correctly when the
+  button is released.
+]]
+test.Step(
+  "Arrows",
+  function()
+    a2d.InvokeMenuItem(a2d.APPLE_MENU, a2d.CONTROL_PANELS)
+    a2d.SelectAndOpen("DATE.AND.TIME")
+    a2d.OAShortcut("1") -- 12-hour
+
+    local dialog_x, dialog_y = a2dtest.GetFrontWindowContentRect()
+
+    a2d.InMouseKeysMode(function(m)
+        m.MoveToApproximately(dialog_x + year_x, dialog_y + field_y) -- year
+        m.Click()
+        test.Snap("verify year focused")
+
+        m.MoveToApproximately(dialog_x+incr_x, dialog_y+incr_y) -- up arrow
+        m.ButtonDown()
+        emu.wait(2/60)
+        test.Snap("verify up button inverted")
+        m.ButtonUp()
+        emu.wait(2/60)
+        test.Snap("verify year increments")
+
+        m.MoveToApproximately(dialog_x+decr_x, dialog_y+decr_y) -- down arrow
+        m.ButtonDown()
+        emu.wait(2/60)
+        test.Snap("verify down button inverted")
+        m.ButtonUp()
+        emu.wait(2/60)
+        test.Snap("verify year decrements")
+    end)
+    a2d.DialogOK()
+end)
+
+--[[
+  Apple Menu > Control Panels. View > by Name. Run Date and Time.
+  Change the date to the same date as one of the files in the folder.
+  Click OK. Verify that the entire desktop repaints, and that dates in
+  the window are shown with "Today".
+]]
+test.Step(
+  "Today",
+  function()
+    -- Create file with known date
+    local y, m, d = apple2.GetProDOSDate()
+    apple2.SetProDOSDate(1999, 9, 13)
+    a2d.CreateFolder("/RAM1/WILL.BE.TODAY")
+    -- Change the date so it's not current
+    apple2.SetProDOSDate(y, m, d)
+
+    -- Show window and resize/move it
+    a2d.OpenPath("/RAM1")
+    a2d.InvokeMenuItem(a2d.VIEW_MENU, a2d.VIEW_BY_NAME)
+    a2d.GrowWindowBy(250, 0)
+    a2d.MoveWindowBy(0, 100)
+    test.ExpectMatch(a2dtest.OCRScreen(), "September 13, 1999", "date should be shown in full")
+
+    -- Use Date & Time to set date
+    a2d.InvokeMenuItem(a2d.APPLE_MENU, a2d.CONTROL_PANELS)
+    apple2.SetProDOSDate(1998, 9, 13) -- so we know the delta
+    a2d.SelectAndOpen("DATE.AND.TIME")
+    local dialog_x, dialog_y = a2dtest.GetFrontWindowContentRect()
+    a2d.InMouseKeysMode(function(m)
+        m.MoveToApproximately(dialog_x + year_x, dialog_y+field_y) -- year
+        m.Click()
+    end)
+    apple2.UpArrowKey()
+    a2dtest.ExpectFullRepaint(a2d.DialogOK)
+    test.ExpectMatch(a2dtest.OCRScreen(), "Today", "bottom date should show Today")
+end)

src/desk_acc/this.apple.s
@@ -1763,75 +1763,9 @@ sigtable_ieee488:       .byte   4, $05, $50, $07, $38, $0B, $FF, $0C, $CF
 ;;; ============================================================
 
         .include "../lib/with_interrupts_disabled.s"
+        .include "../lib/detect_z80.s"
 
 ;;; ============================================================
-;;; Detect Z80
-;;; Assumes $06 points at $Cn00, returns carry set if found
-
-;;; This routine gets swapped into $0FFD for execution
-.assert * > $0FFD + sizeof_Z80Routine, error, "Z80 collision"
-
-.proc Z80Routine
-        target := $0FFD
-        ;; .org $FFFD
-        patch := *+2
-        .byte   $32, $00, $e0   ; ld ($Es00),a   ; s=slot being probed turn off Z80, next PC is $0000
-        .byte   $3e, $01        ; ld a,$01
-        .byte   $32, $08, $00   ; ld (flag),a
-        .byte   $c3, $fd, $ff   ; jp $FFFD
-        flag := *
-        .byte   $00             ; flag: .db $00
-.endproc ; Z80Routine
-        sizeof_Z80Routine = .sizeof(Z80Routine)
-
-.proc DetectZ80
-        ;; Convert $Cn to $En, update Z80 code
-        lda     $07             ; $Cn
-        ora     #$E0
-        sta     Z80Routine::patch
-
-        ;; Clear detection flag
-        copy8   #0, Z80Routine::flag
-
-        ;; Put routine in place
-        jsr     SwapRoutine
-
-        ;; On IIgs, slow mode
-        jsr     IsIIgs
-    IF CC
-        lda     CYAREG          ; bit=7 = fast mode
-        pha                     ; save
-        and     #$7F            ; mask it off
-        sta     CYAREG          ; update it
-    END_IF
-
-        ;; Try to invoke Z80
-        ldy     #0
-        sta     ($06),y
-
-        ;; On IIgs, restore mode
-        jsr     IsIIgs
-    IF CC
-        pla
-        sta     CYAREG
-    END_IF
-
-        ;; Restore memory
-        jsr     SwapRoutine
-
-        ;; Flag will be set to 1 by routine if Z80 was present.
-        lda     Z80Routine::flag
-        ror                     ; move flag into carry
-        rts
-
-.proc SwapRoutine
-        ldx     #.sizeof(Z80Routine)-1
-    DO
-        swap8   Z80Routine::target,x, Z80Routine,x
-    WHILE dex : POS
-        rts
-.endproc ; SwapRoutine
-.endproc ; DetectZ80
 
 ;;; Detect Uthernet II
 ;;; Assumes $06 points at $Cn00, returns carry set if found

src/lib/datetime.s
@@ -116,38 +116,7 @@ clock_24hours:
 
 ;;; ============================================================
 
-;;; Day of the Week calculation (valid 1900-03-01 to 2155-12-31)
-;;; c/o http://6502.org/source/misc/dow.htm
-;;; Inputs: Y = year (0=1900), X = month (1=Jan), A = day (1...31)
-;;; Output: A = weekday (0=Sunday)
-.proc DayOfWeek
-        tmp := $06
-
-        cpx     #3              ; Year starts in March to bypass
-        bcs     :+              ; leap year problem
-        dey                     ; If Jan or Feb, decrement year
-:       eor     #$7F            ; Invert A so carry works right
-        cpy     #200            ; Carry will be 1 if 22nd century
-        adc     month_offset_table-1,X ; A is now day+month offset
-        sta     tmp
-        tya                     ; Get the year
-        jsr     mod7            ; Do a modulo to prevent overflow
-        sbc     tmp             ; Combine with day+month
-        sta     tmp
-        tya                     ; Get the year again
-        lsr                     ; Divide it by 4
-        lsr
-        clc                     ; Add it to y+m+d and fall through
-        adc     tmp
-
-mod7:   adc     #7              ; Returns (A+3) modulo 7
-        bcc     mod7            ; for A in 0..255
-        rts
-
-month_offset_table:
-        .byte   1,5,6,3,1,5,3,0,4,2,6,4
-        ASSERT_TABLE_SIZE month_offset_table, 12
-.endproc ; DayOfWeek
+        .include "day_of_week.s"
 
 ;;; ============================================================
 ;;; Parse date/time

src/lib/day_of_week.s
@@ -1,0 +1,34 @@
+;;; ============================================================
+
+;;; Day of the Week calculation (valid 1900-03-01 to 2155-12-31)
+;;; c/o http://6502.org/source/misc/dow.htm
+;;; Inputs: Y = year (0=1900), X = month (1=Jan), A = day (1...31)
+;;; Output: A = weekday (0=Sunday)
+.proc DayOfWeek
+        tmp := $06
+
+        cpx     #3              ; Year starts in March to bypass
+        bcs     :+              ; leap year problem
+        dey                     ; If Jan or Feb, decrement year
+:       eor     #$7F            ; Invert A so carry works right
+        cpy     #200            ; Carry will be 1 if 22nd century
+        adc     month_offset_table-1,X ; A is now day+month offset
+        sta     tmp
+        tya                     ; Get the year
+        jsr     mod7            ; Do a modulo to prevent overflow
+        sbc     tmp             ; Combine with day+month
+        sta     tmp
+        tya                     ; Get the year again
+        lsr                     ; Divide it by 4
+        lsr
+        clc                     ; Add it to y+m+d and fall through
+        adc     tmp
+
+mod7:   adc     #7              ; Returns (A+3) modulo 7
+        bcc     mod7            ; for A in 0..255
+        rts
+
+month_offset_table:
+        .byte   1,5,6,3,1,5,3,0,4,2,6,4
+        ASSERT_TABLE_SIZE month_offset_table, 12
+.endproc ; DayOfWeek

src/lib/detect_z80.s
@@ -1,0 +1,72 @@
+;;; ============================================================
+;;; Detect Z80
+;;; Assumes $06/$07 points at $Cn00, returns carry set if found
+
+;;; Requires a `IsIIgs` routine defined that returns C=0 if IIgs
+
+;;; This routine gets swapped into $0FFD for execution
+.assert * < $0FFD || * > $0FFD + sizeof_Z80Routine, error, "Z80 collision"
+
+.proc Z80Routine
+        target := $0FFD
+        ;; .org $FFFD
+        patch := *+2
+        .byte   $32, $00, $e0   ; ld ($Es00),a   ; s=slot being probed turn off Z80, next PC is $0000
+        .byte   $3e, $01        ; ld a,$01
+        .byte   $32, $08, $00   ; ld (flag),a
+        .byte   $c3, $fd, $ff   ; jp $FFFD
+        flag := *
+        .byte   $00             ; flag: .db $00
+.endproc ; Z80Routine
+        sizeof_Z80Routine = .sizeof(Z80Routine)
+
+.proc DetectZ80
+        ;; Convert $Cn to $En, update Z80 code
+        lda     $07             ; $Cn
+        ora     #$E0
+        sta     Z80Routine::patch
+
+        ;; Clear detection flag
+        copy8   #0, Z80Routine::flag
+
+        ;; Put routine in place
+        jsr     SwapRoutine
+
+        ;; On IIgs, slow mode
+        jsr     IsIIgs
+    IF CC
+        lda     CYAREG          ; bit=7 = fast mode
+        pha                     ; save
+        and     #$7F            ; mask it off
+        sta     CYAREG          ; update it
+    END_IF
+
+        ;; Try to invoke Z80
+        ldy     #0
+        sta     ($06),y
+
+        ;; On IIgs, restore mode
+        jsr     IsIIgs
+    IF CC
+        pla
+        sta     CYAREG
+    END_IF
+
+        ;; Restore memory
+        jsr     SwapRoutine
+
+        ;; Flag will be set to 1 by routine if Z80 was present.
+        lda     Z80Routine::flag
+        ror                     ; move flag into carry
+        rts
+
+.proc SwapRoutine
+        ldx     #.sizeof(Z80Routine)-1
+    DO
+        swap8   Z80Routine::target,x, Z80Routine,x
+    WHILE dex : POS
+        rts
+.endproc ; SwapRoutine
+.endproc ; DetectZ80
+
+.assert * < $0FFD || * > $0FFD + sizeof_Z80Routine, error, "Z80 collision"

commit 35ee4711ded548334d2d38e55f608f19398011bd

Date & Time: Always set ProDOS date if no clock present
This regressed in 10c1f58:

* Before: if no clock, ProDOS date/time set to build date or whatever
  user set

* After: if no clock, ProDOS date/time only set if dirty

While the behavior before was subtle, it was more consistent in the
no-clock case and this change was unintentional. So back to the
previous behavior.

src/desk_acc/date.and.time.s
@@ -51,7 +51,15 @@
 
 ;;; Input: Y = read only flag (zero or non-zero)
 .proc RunDA
-        sty     readonly_flag
+        sty     flags
+
+        .assert kFlagsSetTime = V_FLAG_MASK, error, "bad constant"
+    IF bit flags : VS
+        ;; If the caller wants us to set the time, start off with the
+        ;; dirty flag set.
+        copy8   #kResultDateChanged, dialog_result
+    END_IF
+
         jsr     init_window
         RETURN  A=dialog_result
 .endproc ; RunDA
@@ -142,7 +150,10 @@ selected_field:
 
 ;;; DA is read-only if there is a system clock but we don't know
 ;;; how to update it.
-readonly_flag:                  ; zero (read/write) or non-zero (read-only)
+
+kFlagsReadOnly = N_FLAG_MASK    ; bit7 = read-only
+kFlagsSetTime  = V_FLAG_MASK    ; bit6 = always set ProDOS time
+flags:
         .byte   0
 
 ;;; Originally Feb 26, 1985 (the author date?); now updated by build.
@@ -413,8 +424,9 @@ init_window:
         cmp     #CHAR_ESCAPE
         jeq     OnKeyOK
 
-        ldx     readonly_flag
-        bne     InputLoop
+        .assert kFlagsReadOnly = N_FLAG_MASK, error, "bad constant"
+        bit     flags
+        bmi     InputLoop
 
         ;; All controls are active
         cmp     #CHAR_LEFT
@@ -517,8 +529,9 @@ hit:
 
         ;; ----------------------------------------
 
-        ldx     readonly_flag
-        bne     miss
+        .assert kFlagsReadOnly = N_FLAG_MASK, error, "bad constant"
+        bit     flags
+        bmi     miss
 
         jsr     FindHitTarget
         cpx     #0
@@ -555,11 +568,9 @@ hit_target_jump_table:
 .endproc ; OnKeyOK
 
 .proc OnOK
-        lda     readonly_flag
-    IF ZERO
-      IF bit dialog_result : NS
+        .assert kResultDateChanged = N_FLAG_MASK, error, "bad constant"
+    IF bit dialog_result : NS
         jsr     UpdateProDOS
-      END_IF
     END_IF
         jmp     Destroy
 .endproc ; OnOK
@@ -693,7 +704,7 @@ finish:
 
         ;; Set dirty bit
         lda     dialog_result
-        ora     #$80            ; date changed
+        ora     #kResultDateChanged ; date changed
         sta     dialog_result
 
         rts
@@ -844,8 +855,8 @@ str_pm: PASCAL_STRING "PM"
 ;;; Tear down the window and exit
 
 ;;; Used in Aux to store result during tear-down
-;;; bit7 = time changed
-;;; bit6 = options changed
+kResultDateChanged    = N_FLAG_MASK ; bit7 = date/time changed
+kResultOptionsChanged = V_FLAG_MASK ; bit6 = options changed
 dialog_result:  .byte   0
 
 .proc Destroy
@@ -926,8 +937,9 @@ label_downarrow:
 
         MGTK_CALL MGTK::SetPenMode, penXOR
 
-        ldx     readonly_flag
-    IF ZERO
+
+        .assert kFlagsReadOnly = N_FLAG_MASK, error, "bad constant"
+    IF bit flags : NC
         MGTK_CALL MGTK::MoveTo, label_uparrow_pos
         MGTK_CALL MGTK::DrawString, label_uparrow
         MGTK_CALL MGTK::FrameRect, up_arrow_rect
@@ -950,8 +962,8 @@ label_downarrow:
         CALL    DrawField, A=#Field::minute
         CALL    DrawField, A=#Field::period
 
-        ldx     readonly_flag
-    IF ZERO
+        .assert kFlagsReadOnly = N_FLAG_MASK, error, "bad constant"
+    IF bit flags : NC
         CALL    SelectField, A=#Field::day
     END_IF
 
@@ -1210,7 +1222,7 @@ loop:
 
         ;; Set dirty bit
         lda     dialog_result
-        ora     #$40            ; settings changed
+        ora     #kResultOptionsChanged ; settings changed
         sta     dialog_result
 
     IF lda selected_field : A = #Field::period
@@ -1285,12 +1297,13 @@ loop:
         lda     MACHID
         and     #kMachIDHasClock
     IF ZERO
-        ;; no system clock - DA is read/write
-        ldy     #0
+        ;; no system clock - DA is read/write, and we want to
+        ;; set the ProDOS time when the DA is done
+        ldy     #aux::kFlagsSetTime
     ELSE
         jsr     CanSetClock     ; returns C=0 if clock can be set
         lda     #0
-        ror
+        ror                     ; sets `aux::kFlagsReadOnly`
         tay
     END_IF
 

src/inc/apple2.inc
@@ -4,6 +4,10 @@
 ;;;
 ;;; ============================================================
 
+;;; Mostly for asserting that flags match BIT results
+N_FLAG_MASK = %10000000
+V_FLAG_MASK = %01000000
+
 ;;; Zero Page
 
 COUT_HOOK       := $36

tests/desk_acc/date_and_time_no_clock.lua
@@ -43,7 +43,7 @@ test.Step(
     end)
     test.ExpectMatch(a2dtest.OCRFrontWindowContent(), "Today", "dates should now say Today")
     desktop.SelectAndOpen("DATE.AND.TIME")
-    a2dtest.ExpectMinimalRepaint(function()
+    a2dtest.ExpectFullRepaint(function()
         a2d.DialogOK()
         a2dtest.WaitForSystemTask()
     end)